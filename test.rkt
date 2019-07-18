051b0f9f6c9d7d3dba1578ad9c2f610a2d5f1d17
Samuel Thomas <sgpthomas@gmail.com>
2019-07-18 18:10:02 -0400

    test.rkt is actually runnable now

--- test.rkt
+++ test.rkt
@@ -1,82 +1,21 @@
 #lang racket
-(require "component.rkt"
+(require "ast.rkt"
          "futil.rkt"
          "futil-prims.rkt"
          "dis-graphs.rkt")
 
-(require "futil.rkt" "futil-prims.rkt")
-(define/module triv ((a : 32) (b : 32)) ((out : 32))
-  ([add = new comp/add]
-   [id = new comp/id]
-   [a -> add @ left]
-   [b -> add @ right]
-   [add @ out -> id @ in]
-   [id @ out -> out])
-  [(add)]
-  [(id)])
-
-;; [(add)]
-;; [(id)]
-
-;; (component-control (triv))
-;; (convert-graph (triv) (list-ref (car (compute (triv) '((a . 1) (b . 2)))) 1))
-(plot (triv) (list-ref (car (compute (triv) '((a . 1) (b . 2)))) 2) '(a))
-
-
-(animate (triv) '((a . 1) (b . 2)))
-
-
-(define/module add4 ((a : 32) (b : 32) (c : 32) (d : 32)) ((out : 32))
-  ([add1 = new comp/add]
-   [add2 = new comp/add]
-   [add3 = new comp/add]
-   [a -> add1 @ left]
-   [b -> add1 @ right]
-   [c -> add2 @ left]
-   [d -> add2 @ right]
-   [add1 @ out -> add3 @ left]
-   [add2 @ out -> add3 @ right]
-   [id = new comp/id]
-   [add3 @ out -> id @ in]
-   [id @ out -> out])
-  [(a) (b) (c) (d)]
-  [(add3) (add2) (add1)]
-  [(id)])
-
-(plot (add4) )
-(animate (add4) '((a . 1) (b . 2) (c . 3) (d . 4)))
-
-(define/module times4 ((a : 32)) ((out : 32))
-  ([add = new comp/add]
-   [a -> add @ left]
-   [const 0 : 32 -> add @ right]
-   [id = new id]
-   [add @ out -> id @ in]
-   [id @ out -> add @ right]
-   [id @ out -> out])
-  []
-  [(if (add out) 0 id)]
-  [(if (add out) 0 id)]
-  [(if (add out) 0 id)]
-  [(if (add out) 0 id)])
-
-(while (wire port)
-  [()])
-
 (define/module decr ((in : 32)) ((out : 32))
-  ([sub = new comp/sub]
+  ([sub = new comp/trunc-sub]
    [const one 1 : 32 -> sub @ right]
    [in -> sub @ left]
    [sub @ out -> out])
   [])
 
-(ast-tuple-state (compute (decr) '((in . 1))))
+;; (ast-tuple-state (compute (decr) '((in . 1))))
 
-(animate (test) '((in . 10) (other . 20)))
-
-(require "futil.rkt" "futil-prims.rkt" "dis-graphs.rkt")
+;; (require "futil.rkt" "futil-prims.rkt" "dis-graphs.rkt")
 (define/module counter2.0 ((in : 32)) ((out : 32))
-  ([sub = new comp/sub]
+  ([sub = new comp/trunc-sub]
    [reg = new comp/reg]
    [in -> sub @ left]
    [const decr 1 : 32 -> sub @ right]
@@ -86,8 +25,8 @@
   [(ifen (in inf#)
          ([])
          ([(in)]))])
-(component-control (counter2.0))
-(plot-compute (counter2.0) '((in . 10)))
+;; (component-control (counter2.0))
+;; (plot-compute (counter2.0) '((in . 10)))
 
 (define/module consumer ((n : 32)) ((out : 32))
   ([counter = new counter2.0]
@@ -119,7 +58,7 @@
   [(while (counter out) ([(b zero)]))])
 (plot-compute (mult) '((a . 3) (b . 4)))
 
-(require "futil.rkt" "futil-prims.rkt" "dis-graphs.rkt")
+;; (require "futil.rkt" "futil-prims.rkt" "dis-graphs.rkt")
 (define/module simp ((a : 32) (b : 32)) ((out : 32))
   ([add = new comp/add]
    [a -> add @ left]
