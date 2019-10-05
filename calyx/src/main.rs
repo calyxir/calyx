74d84c6b53f588ff78c322a26586f41785229043
Samuel Thomas <sgpthomas@gmail.com>
2019-10-04 21:53:03 -0400

    added install option to Makefile

--- calyx/src/main.rs
+++ calyx/src/main.rs
@@ -10,10 +10,10 @@ extern crate clap;
 fn main() {
     let matches = clap_app!(calyx =>
                             (version: "0.1.0")
-                            (author: "Samuel Thomas <sgt43@cornell.edu>")
+                            (author: "Samuel Thomas <sgt43@cornell.edu>, Kenneth Fang <kwf37@cornell.edu>")
                             (about: "Optimization passes for futil")
                             (@arg FILE: +required ... "Input file"))
-    .get_matches();
+        .get_matches();
 
     let filename = matches.value_of("FILE").unwrap();
     let mut syntax: ast::Namespace = parse::parse_file(filename);
