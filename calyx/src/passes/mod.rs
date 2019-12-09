dd7b6b0a3983fab0b224759fbc18b6d6ef20fbbf
Samuel Thomas <sgpthomas@gmail.com>
2019-12-09 17:31:45 -0500

    completed fsm gen for enable fsm components

--- calyx/src/passes/mod.rs
+++ calyx/src/passes/mod.rs
@@ -1,4 +1,4 @@
-pub mod collapse_seqs;
+// pub mod collapse_seqs;
 pub mod fsm;
 pub mod fsm_enable;
 pub mod fsm_if;
@@ -6,5 +6,5 @@ pub mod fsm_ifen;
 pub mod fsm_par;
 pub mod fsm_seq;
 pub mod fsm_while;
-pub mod if_to_mux;
+// pub mod if_to_mux;
 pub mod visitor;
