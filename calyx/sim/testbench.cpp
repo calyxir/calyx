d9248fd1ec7017c8ae92406f30e6ccbc940c1a57
Kenneth Fang <kwf37@cornell.edu>
2019-12-15 13:27:01 -0500

    unignored cpp

---
+++ calyx/sim/testbench.cpp
@@ -0,0 +1,35 @@
+#include "Vsimple.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+int main(int argc, char **argv, char **env)
+{
+    int i;
+    int clk;
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vsimple *top = new Vsimple;
+    // init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("simple.vcd");
+    // initialize simulation inputs
+    top->clk = 1;
+    top->reset = 1;
+    top->valid = 1;
+    // run simulation for 100 clock periods
+    for (i = 0; i < 20; i++)
+    {
+        // dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++)
+        {
+            tfp->dump(2 * i + clk);
+            top->clk = !top->clk;
+            top->eval();
+        }
+        if (Verilated::gotFinish())
+            exit(0);
+    }
+    tfp->close();
+    exit(0);
+}
