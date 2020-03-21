b31a31780c005301c0d0ebed157e510081c76351
Rachit Nigam <rachit.nigam12@gmail.com>
2020-03-20 22:06:43 -0400

    remove unused files

--- verilator/obj_dir/counter_tb.d
+++
@@ -1,4 +0,0 @@
-counter_tb.o: ../counter_tb.cpp Vcounter.h \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilated_vcd_c.h
