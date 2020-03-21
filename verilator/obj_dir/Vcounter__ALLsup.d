939d74d228897ad2eb59d3f5fc28a3cb3b33fb00
Rachit Nigam <rachit.nigam12@gmail.com>
2020-03-20 22:06:43 -0400

    remove unused files

--- verilator/obj_dir/Vcounter__ALLsup.d
+++
@@ -1,6 +0,0 @@
-Vcounter__ALLsup.o: Vcounter__ALLsup.cpp Vcounter__Trace.cpp \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilated_vcd_c.h \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/4.022/share/verilator/include/verilated.h \
-  Vcounter__Syms.h Vcounter.h Vcounter__Syms.cpp \
-  Vcounter__Trace__Slow.cpp
