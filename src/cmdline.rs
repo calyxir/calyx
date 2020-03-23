31ba26f619a2a57702aeb6ad4e45cd1105e9b642
Samuel Thomas <sgpthomas@gmail.com>
2020-03-23 08:47:41 -0700

    added a backend option to the cli

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -1,7 +1,11 @@
+use crate::backend::{traits::Backend, verilog::gen};
+use crate::errors;
+use crate::lang::context;
 use std::path::PathBuf;
+use std::str::FromStr;
 use structopt::StructOpt;
 
-/// Definition of the command line interface. Uses the `structopt` derive macro
+// Definition of the command line interface. Uses the `structopt` derive macro
 #[derive(StructOpt, Debug)]
 #[structopt(
     name = env!("CARGO_PKG_NAME"),
@@ -21,4 +25,47 @@ pub struct Opts {
     /// Enable debug mode output.
     #[structopt(short = "d", long = "debug")]
     pub enable_debug: bool,
+
+    /// Select a backend.
+    #[structopt(short = "b", long = "backend", default_value = "verilog")]
+    pub backend: BackendOpt,
+}
+
+// ================== Backend Variant and Parsing ===================== //
+
+#[derive(Debug, StructOpt)]
+pub enum BackendOpt {
+    Verilog,
+    None,
+}
+
+/// Command line parsing for the Backend enum
+impl FromStr for BackendOpt {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "verilog" => Ok(Self::Verilog),
+            "none" => Ok(Self::None),
+            x => Err(format!("{} is not a valid backend.", x)),
+        }
+    }
+}
+
+impl ToString for BackendOpt {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Verilog => "verilog",
+            Self::None => "none",
+        }
+        .to_string()
+    }
+}
+
+impl BackendOpt {
+    pub fn run(&self, context: &context::Context) -> Result<(), errors::Error> {
+        match self {
+            BackendOpt::Verilog => gen::VerilogBackend::run(&context),
+            BackendOpt::None => Ok(()),
+        }
+    }
 }
