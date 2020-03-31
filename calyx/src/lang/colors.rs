b57a23ff482b06db16570e98a51f37b7cffc14c0
Samuel Thomas <sgpthomas@gmail.com>
2020-03-31 14:04:15 -0700

    rename color methods

--- calyx/src/lang/colors.rs
+++ calyx/src/lang/colors.rs
@@ -2,46 +2,46 @@ use pretty::termcolor::{Color, ColorSpec};
 use pretty::RcDoc;
 
 pub trait ColorHelper {
-    fn define(self) -> Self;
-    fn port(self) -> Self;
-    fn keyword(self) -> Self;
-    fn ident(self) -> Self;
-    fn control(self) -> Self;
-    fn enable(self) -> Self;
+    fn define_color(self) -> Self;
+    fn port_color(self) -> Self;
+    fn keyword_color(self) -> Self;
+    fn ident_color(self) -> Self;
+    fn control_color(self) -> Self;
+    fn enable_color(self) -> Self;
 }
 
 impl<'a> ColorHelper for RcDoc<'a, ColorSpec> {
-    fn define(self) -> Self {
+    fn define_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Blue)).set_bold(true);
         self.annotate(c)
     }
 
-    fn port(self) -> Self {
+    fn port_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Green));
         self.annotate(c)
     }
 
-    fn keyword(self) -> Self {
+    fn keyword_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Yellow));
         self.annotate(c)
     }
 
-    fn ident(self) -> Self {
+    fn ident_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Blue));
         self.annotate(c)
     }
 
-    fn control(self) -> Self {
+    fn control_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Green));
         self.annotate(c)
     }
 
-    fn enable(self) -> Self {
+    fn enable_color(self) -> Self {
         let mut c = ColorSpec::new();
         c.set_fg(Some(Color::Yellow));
         self.annotate(c)
