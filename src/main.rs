a264e624e3a8ba4fc1dd9f9de1d9dc1bd0c69a6b
Samuel Thomas <sgpthomas@gmail.com>
2020-04-17 12:37:13 -0700

    Fixed command line pass registry not working with NameGenerator

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,10 @@ use calyx::{
 use std::collections::HashMap;
 use structopt::StructOpt;
 
-type PassResult = Result<Box<dyn Visitor>, errors::Error>;
+type PassClosure =
+    Box<dyn Fn(&Context, &mut NameGenerator) -> Result<(), errors::Error>>;
 
-fn pass_map() -> HashMap<String, Box<dyn Fn(&Context) -> PassResult>> {
+fn pass_map() -> HashMap<String, PassClosure> {
     use passes::{
         automatic_par::AutomaticPar, collapse_seq::CollapseSeq,
         connect_clock::ConnectClock, fsm_seq::FsmSeq,
@@ -19,73 +20,70 @@ fn pass_map() -> HashMap<String, Box<dyn Fn(&Context) -> PassResult>> {
         remove_if::RemoveIf,
     };
 
-    let mut names: HashMap<String, Box<dyn Fn(&Context) -> PassResult>> =
-        HashMap::new();
+    let mut names: HashMap<String, PassClosure> = HashMap::new();
     names.insert(
         LatencyInsensitive::name().to_string(),
-        Box::new(|ctx| {
-            let r = LatencyInsensitive::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            LatencyInsensitive::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
     names.insert(
         CollapseSeq::name().to_string(),
-        Box::new(|ctx| {
-            let r = CollapseSeq::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            CollapseSeq::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
     names.insert(
         AutomaticPar::name().to_string(),
-        Box::new(|ctx| {
-            let r = AutomaticPar::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            AutomaticPar::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
     names.insert(
         RemoveIf::name().to_string(),
-        Box::new(|ctx| {
-            let r = RemoveIf::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            RemoveIf::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
     names.insert(
         RedundantPar::name().to_string(),
-        Box::new(|ctx| {
-            let r = RedundantPar::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            RedundantPar::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
     names.insert(
         ConnectClock::name().to_string(),
-        Box::new(|ctx| {
-            let r = ConnectClock::do_pass_default(ctx)?;
-            Ok(Box::new(r))
+        Box::new(|ctx, _| {
+            ConnectClock::do_pass_default(ctx)?;
+            Ok(())
         }),
     );
-    // names.insert(
-    //     FsmSeq::name().to_string(),
-    //     Box::new(move |ctx| {
-    //         let r = FsmSeq::new(&mut name_gen);
-    //         r.do_pass(ctx)?;
-    //         Ok(Box::new(r))
-    //     }),
-    // );
+    names.insert(
+        FsmSeq::name().to_string(),
+        Box::new(|ctx, mut name_gen| {
+            FsmSeq::new(&mut name_gen).do_pass(&ctx)?;
+            Ok(())
+        }),
+    );
     names.insert(
         "all".to_string(),
-        Box::new(move |ctx| {
+        Box::new(|ctx, mut name_gen| {
             RedundantPar::do_pass_default(ctx)?;
             RemoveIf::do_pass_default(ctx)?;
             CollapseSeq::do_pass_default(ctx)?;
             AutomaticPar::do_pass_default(ctx)?;
-            // fsm generation
+            // // fsm generation
             LatencyInsensitive::do_pass_default(&ctx)?;
-            let mut name_gen = NameGenerator::default();
             FsmSeq::new(&mut name_gen).do_pass(&ctx)?;
 
             // interfacing generation
-            let r = ConnectClock::do_pass_default(&ctx)?;
-            Ok(Box::new(r))
+            ConnectClock::do_pass_default(&ctx)?;
+            Ok(())
         }),
     );
     names
@@ -94,6 +92,9 @@ fn pass_map() -> HashMap<String, Box<dyn Fn(&Context) -> PassResult>> {
 fn main() -> Result<(), errors::Error> {
     // parse the command line arguments into Opts struct
     let opts: Opts = Opts::from_args();
+
+    // Construct the context.
+    let context = Context::from_opts(&opts)?;
 
     // Construct pass manager.
     let names = pass_map();
@@ -108,12 +109,13 @@ fn main() -> Result<(), errors::Error> {
         return Ok(());
     }
 
-    // Construct the context.
-    let context = Context::from_opts(&opts)?;
+    // Construct the name generator
+    let mut name_gen = NameGenerator::default();
+
     // run all passes specified by the command line
     for name in opts.pass {
         if let Some(pass) = names.get(&name) {
-            pass(&context)?;
+            pass(&context, &mut name_gen)?;
         } else {
             let known_passes: String =
                 names.keys().cloned().collect::<Vec<_>>().join(", ");
