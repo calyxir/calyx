6adc1ec90a68eeaaed073423f818f6fd7fca8c14
Adrian Sampson <adrian@radbox.org>
2020-07-17 17:21:48 -0400

    Try emitting strings directly

--- relay-futil/aot/to_futil.py
+++ relay-futil/aot/to_futil.py
@@ -1,40 +1,55 @@
 from tvm import relay
 from tvm.relay.expr_functor import ExprFunctor
+import textwrap
+
+PREAMBLE = """
+import "primitives/std.lib";
+"""
+
+# For now, components can only be emitted with the name `main`. Someday
+# we should actually allow multiple components!
+COMPONENT_FMT = """
+component main() -> () {}
+"""
+
+
+def mk_block(decl, contents, indent=2):
+    """Formats a block like this:
+
+        decl {
+          contents
+        }
+
+    where `decl` is one line but contents can be multiple lines.
+    """
+    return decl + ' {\n' + textwrap.indent(contents, indent * ' ') + '\n}'
 
 
 class ToSource(ExprFunctor):
     def __init__(self):
         super(ToSource, self).__init__()
-        self.declare = ""
-        self.input_const = []
 
     def visit_var(self, var):
         return "a variable called {}".format(var.name_hint)
 
     def visit_function(self, func):
-        expr = self.visit(func.body)
+        body = self.visit(func.body)
 
-        source = self.declare
+        # Construct a FuTIL component. For now, the component is
+        # *always* called `main`. Someday, we should actually support
+        # multiple functions as multiple components.
+        cells = mk_block('cells', '')
+        wires = mk_block('wires', '')
+        control = mk_block('control', body)
+        component = mk_block(
+            'component main() -> ()',
+            '\n'.join([cells, wires, control])
+        )
 
-        args = ""
-        if isinstance(func, relay.GlobalVar):
-            func = self.gv_map[func]
-        end = len(func.params) - 1
-        init = ""
-        for i in range(len(func.params)):
-            args += f"args[{i+len(self.input_const)}]"
-            if i != end:
-                args += ", "
-
-        source += f"""
-        args: {args}
-        init: {init}
-        function: {expr}
-        }});
-        """
-        return source
+        return component
 
 
 def to_source(mod, program, gv_map, ctx, name) -> str:
     convert = ToSource()
-    return convert.visit(program)
+    src = convert.visit(program)
+    return "{}\n{}".format(PREAMBLE.strip(), src)
