055d22353a5f3cd46b9fe54e466a6ddc30688c5c
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2020-09-25 15:33:13 -0400

    Use constant reference for vectors.

--- benchmarks/print_headers/printer.cpp
+++ benchmarks/print_headers/printer.cpp
@@ -1,25 +1,23 @@
 #include <vector>
 #include <cstdio>
 
-using std::vector;
-
 template <typename T>
-void print_vec(std::vector<T> &input) {
-	for (uint i = 0; i < input.size(); i++) {
-    printf("%.4f\n", input.at(i));
-	}
-}
-
-template<typename T>
-void print_vec_2d(std::vector<std::vector<T>> &input) {
-  for (uint i = 0; i < input.size(); i++) {
-    print_vec(input.at(i));
+void print_vec(const std::vector<T> &input) {
+  for (const auto &i : input) {
+    printf("%.4f\n", i);
   }
 }
 
 template<typename T>
-void print_vec_3d(std::vector<std::vector<std::vector<T>>> &input) {
-  for (uint i = 0; i < input.size(); i++) {
-    print_vec_2d(input.at(i));
+void print_vec_2d(const std::vector<std::vector<T>> &input) {
+  for (const auto &1d_vector : input) {
+    print_vec(1d_vector);
+  }
+}
+
+template<typename T>
+void print_vec_3d(const std::vector<std::vector<std::vector<T>>> &input) {
+  for (const auto &2d_vector : input) {
+    print_vec_2d(2d_vector);
   }
 }
