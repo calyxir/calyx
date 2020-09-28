24e5e1196b7cc3ea3322a774a80d38a963fc8a10
cgyurgyik <gyurgyikcp@gmail.com>
2020-09-27 20:21:42 -0400

    [mlir-dialect] Remove license.

--- mlir/futil-opt/futil-opt.cpp
+++ mlir/futil-opt/futil-opt.cpp
@@ -1,10 +1,4 @@
-//===- standalone-opt.cpp ---------------------------------------*- C++ -*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
+//===- futil-opt.cpp --------------------------------------------*- C++ -*-===//
 
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
--- mlir/futil-translate/futil-translate.cpp
+++ mlir/futil-translate/futil-translate.cpp
@@ -1,15 +1,4 @@
-//===- standalone-translate.cpp ---------------------------------*- C++ -*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This is a command line utility that translates a file from/to MLIR using one
-// of the registered translations.
-//
-//===----------------------------------------------------------------------===//
+//===- futil-translate.cpp --------------------------------------*- C++ -*-===//
 
 #include "mlir/InitAllTranslations.h"
 #include "mlir/Support/LogicalResult.h"
