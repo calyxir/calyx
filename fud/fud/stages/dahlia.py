7cad504a67d5cc8373361d57f3c56be571cea2ad
Samuel Thomas <sgpthomas@gmail.com>
2020-09-30 11:07:32 -0400

    added documentation and fixed no tracing with fud

--- fud/fud/stages/dahlia.py
+++ fud/fud/stages/dahlia.py
@@ -1,10 +1,12 @@
-from fud.stages import Stage, Step, SourceType, Source
+from fud.stages import Stage, Step, SourceType
+
 
 class DahliaStage(Stage):
     def __init__(self, config):
-        super().__init__('dahlia', 'futil', config, 'Compiles a Dahlia program to FuTIL')
+        super().__init__('dahlia', 'futil', config,
+                         'Compiles a Dahlia program to FuTIL')
 
-    def define(self):
+    def _define(self):
         main = Step(SourceType.Path)
         main.set_cmd(f'{self.cmd} {{ctx[input_path]}} -b futil --lower')
         return [main]
