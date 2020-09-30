32e31f1362115bc9638eebb1f222f15e07cce7c2
Samuel Thomas <sgpthomas@gmail.com>
2020-09-30 11:07:32 -0400

    added documentation and fixed no tracing with fud

--- fud/fud/stages/vcdump.py
+++ fud/fud/stages/vcdump.py
@@ -5,7 +5,7 @@ class VcdumpStage(Stage):
     def __init__(self, config):
         super().__init__('vcd', 'vcd_json', config, 'Transform VCD file to JSON')
 
-    def define(self):
+    def _define(self):
         main = Step(SourceType.File)
         main.set_cmd(f'{self.cmd} --pretty')
         return [main]
