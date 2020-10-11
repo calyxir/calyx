a912e7c3b4727b1ea4cae8c589f0ed9a208adbe7
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-11 10:36:19 -0400

    comments for backend traits

--- calyx/src/backend/traits.rs
+++ calyx/src/backend/traits.rs
@@ -6,22 +6,24 @@ use crate::{
 use pretty::termcolor::ColorSpec;
 use pretty::RcDoc;
 
-/// All backends must implement this trait.
-/// `Backend::name` returns the name of this backend.
-/// `Backend::validate` should return `Ok(())` if the
-/// program is in the expected form and `Err(...)` otherwise.
-/// `Backend::emit` should convert the program to a formted string
-/// `Backend::run` is the composition of these two functions.
+/// A backend for FuTIL.
 pub trait Backend {
+    /// The name of this backend.
     fn name() -> &'static str;
+    /// Validate this program for emitting using this backend. Returns an
+    /// Err(..) if the program has unexpected constructs.
     fn validate(prog: &context::Context) -> Result<()>;
+    /// Transforms the program into a formatted string representing a valid
+    /// and write it to `write`.
     fn emit(prog: &context::Context, write: OutputFile) -> Result<()>;
+    /// Convience function to validate and emit the program.
     fn run(prog: &context::Context, file: OutputFile) -> Result<()> {
         Self::validate(&prog)?;
         Self::emit(prog, file)
     }
 }
 
+/// Represents something that can be transformed in to a RcDoc.
 pub trait Emitable {
     fn doc<'a>(
         &self,
