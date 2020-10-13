af1bb1290dd16968b8053c64ed2aa7361b0502b9
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-12 20:09:45 -0400

    library ast comments

--- calyx/src/frontend/library/ast.rs
+++ calyx/src/frontend/library/ast.rs
@@ -4,20 +4,29 @@ use crate::errors::{Error, FutilResult};
 use crate::frontend::ast::{Id, Portdef};
 use std::collections::HashMap;
 
+/// A FuTIL library.
 #[derive(Clone, Debug)]
 pub struct Library {
+    /// The primitives defined by this library.
     pub primitives: Vec<Primitive>,
 }
 
+/// Representation of a Primitive.
 #[derive(Clone, Debug)]
 pub struct Primitive {
+    /// Name of this primitive.
     pub name: Id,
+    /// Paramters for this primitive.
     pub params: Vec<Id>,
+    /// The input/output signature for this primitive.
     pub signature: Vec<ParamPortdef>,
+    /// Key-value attributes for this primitive.
     pub attributes: HashMap<String, u64>,
+    /// Available implementations for this primitive.
     pub implementation: Vec<Implementation>,
 }
 
+/// A parameter port definition.
 #[derive(Clone, Debug)]
 pub struct ParamPortdef {
     pub name: Id,
@@ -25,16 +34,18 @@ pub struct ParamPortdef {
     pub direction: ir::Direction,
 }
 
+/// Represents an abstract width of a primitive signature.
 #[derive(Clone, Debug)]
 pub enum Width {
+    /// The width is a constant.
     Const { value: u64 },
+    /// The width is a parameter.
     Param { value: Id },
 }
 
 impl ParamPortdef {
     pub fn resolve(
         &self,
-        prim: &Id,
         val_map: &HashMap<&Id, u64>,
     ) -> FutilResult<Portdef> {
         match &self.width {
@@ -48,7 +59,7 @@ impl ParamPortdef {
                     width: *width,
                 }),
                 None => Err(Error::SignatureResolutionFailed(
-                    prim.clone(),
+                    self.name.clone(),
                     value.clone(),
                 )),
             },
