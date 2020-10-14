6021c8b2af1fd3b89125ff1b2d573085268695cc
tedbauer <tjb272@cornell.edu>
2020-10-14 14:33:33 -0400

    Tidy code up a bit

--- frontends/mrxl/mrxl/gen_futil.py
+++ frontends/mrxl/mrxl/gen_futil.py
@@ -1,7 +1,5 @@
 from . import ast
 
-COUNTER = 0
-
 
 def emit_mem_decl(name, size, par):
     banked_mems = []
@@ -30,7 +28,7 @@ group cond{2}{0} {{
     '''.format(suffix, arr_size, bank_suffix)
 
 
-def emit_idx_group(suffix, b=None):
+def emit_idx_group(suffix, s_idx, b=None):
     bank_suffix = "_b" + str(b) + "_" if b is not None else ""
     return '''
 group incr_idx{1}{0} {{
@@ -42,7 +40,17 @@ group incr_idx{1}{0} {{
 
   incr_idx{1}{0}[done] = idx{1}{0}.done;
 }}
-    '''.format(COUNTER, bank_suffix)
+    '''.format(s_idx, bank_suffix)
+
+
+def emit_compute_op(exp, op, dest, name2arr, bank_suffix):
+    if isinstance(exp, ast.VarExpr):
+        if isinstance(op, ast.Map):
+            return "{}{}.read_data".format(name2arr[exp.name], bank_suffix)
+        else:
+            return "{}.out".format(dest, suffix)
+    else:
+        return "{}'d{}".format(32, exp.value)
 
 
 def emit_eval_body_group(suffix, stmt, b=None):
@@ -54,33 +62,30 @@ def emit_eval_body_group(suffix, stmt, b=None):
         idx = 0 if isinstance(stmt.op, ast.Map) else 1
         name2arr[bi.dest[idx]] = bi.src
         mem_offsets.append(
-            "{0}{1}.addr0 = idx{1}_{2}.out;".format(bi.src, bank_suffix, suffix)
+            "{0}{1}.addr0 = idx{1}_{2}.out;".format(
+                bi.src, bank_suffix, suffix
+            )
         )
 
     if isinstance(stmt.op, ast.Map):
         mem_offsets.append(
-            "{0}{1}.addr0 = idx{1}_{2}.out;".format(stmt.dest, bank_suffix, suffix)
+            "{0}{1}.addr0 = idx{1}_{2}.out;".format(
+                stmt.dest, bank_suffix, suffix
+            )
         )
 
-    lhs = stmt.op.body.lhs
-    rhs = stmt.op.body.rhs
+    compute_left_op = emit_compute_op(
+        stmt.op.body.lhs, stmt.op, stmt.dest, name2arr, bank_suffix
+    )
 
-    if isinstance(lhs, ast.VarExpr):
-        if isinstance(stmt.op, ast.Map):
-            compute_left_op = "{}{}.read_data".format(name2arr[lhs.name], bank_suffix)
-        else:
-            compute_left_op = "{}.out".format(stmt.dest, suffix)
-    else:
-        compute_left_op = "{}'d{}".format(32, lhs.value)
-
-
-    if isinstance(rhs, ast.VarExpr):
-        compute_right_op = "{}{}.read_data".format(name2arr[rhs.name], bank_suffix)
-    else:
-        compute_right_op = "{}'d{}".format(32, rhs.value)
+    compute_right_op = emit_compute_op(
+        stmt.op.body.rhs, stmt.op, stmt.dest, name2arr, bank_suffix
+    )
 
     if isinstance(stmt.op, ast.Map):
-        write = "{0}{1}.write_data = adder_op{1}_{2}.out;".format(stmt.dest, bank_suffix, suffix)
+        write = "{0}{1}.write_data = adder_op{1}_{2}.out;".format(
+                stmt.dest, bank_suffix, suffix
+        )
     else:
         write = "{}.in = adder_op{}.out;".format(stmt.dest, suffix)
 
@@ -108,76 +113,87 @@ group eval_body{6}_{0} {{
         )
 
 
-def gen_stmt_impl(stmt, arr_size, name2par):
+def gen_reduce_impl(stmt, arr_size, s_idx):
     result = dict()
 
     cells = []
-    if isinstance(stmt.op, ast.Map):
-        for b in range(name2par[stmt.dest]):
-            cells.append("le_b{}_{} = prim std_lt(32);".format(b, COUNTER))
-            cells.append("idx_b{}_{} = prim std_reg(32);".format(b, COUNTER))
-            cells.append("adder_idx_b{}_{} = prim std_add(32);".format(b, COUNTER))
-    else:
-        cells.append("le{} = prim std_lt(32);".format(COUNTER))
-        cells.append("idx{} = prim std_reg(32);".format(COUNTER))
-        cells.append("adder_idx{} = prim std_add(32);".format(COUNTER))
-
     op_name = "mult" if stmt.op.body.op == "mul" else "add"
-    if isinstance(stmt.op, ast.Map):
-        for b in range(name2par[stmt.dest]):
-            cells.append("adder_op_b{}_{} = prim std_{}(32);".format(b, COUNTER, op_name))
-    else:
-        cells.append("adder_op{} = prim std_{}(32);".format(COUNTER, op_name))
+    cells.append("le{} = prim std_lt(32);".format(s_idx))
+    cells.append("idx{} = prim std_reg(32);".format(s_idx))
+    cells.append("adder_idx{} = prim std_add(32);".format(s_idx))
+    cells.append("adder_op{} = prim std_{}(32);".format(s_idx, op_name))
 
     wires = []
-    if isinstance(stmt.op, ast.Map):
-        for b in range(name2par[stmt.dest]):
-            wires.append(emit_cond_group(COUNTER, arr_size // name2par[stmt.dest], b))
-    else:
-        wires.append(emit_cond_group(COUNTER, arr_size))
-    if isinstance(stmt.op, ast.Map):
-        for b in range(name2par[stmt.dest]):
-            wires.append(emit_idx_group(COUNTER, b))
-    else:
-        wires.append(emit_idx_group(COUNTER))
-
-    if isinstance(stmt.op, ast.Map):
-        for b in range(name2par[stmt.dest]):
-            wires.append(emit_eval_body_group(COUNTER, stmt, b))
-    else:
-            wires.append(emit_eval_body_group(COUNTER, stmt))
+    wires.append(emit_cond_group(s_idx, arr_size))
+    wires.append(emit_idx_group(s_idx))
+    wires.append(emit_eval_body_group(s_idx, stmt))
 
     control = []
-    map_loops = []
-    for b in range(name2par[stmt.dest]):
-        map_loops.append('''
-        while le{0}{1}.out with cond{0}{1} {{
-          seq {{ eval_body{0}{1}; incr_idx{0}{1}; }}
-        }}
-        '''.format("_b" + str(b) + "_", COUNTER
-        ))
-
     control.append('''
-    par {{
-      {} 
-    }}
-    '''.format("\n".join(map_loops)))
+while le{}.out with cond{} {{
+  seq {{ eval_body{}; incr_idx{}; }}
+}}
+    '''.format(s_idx))
 
     return {"cells": cells, "wires": wires, "control": control}
 
 
-def emit(prog):
-    global COUNTER
-    mems = []
-    regs = []
+def gen_map_impl(stmt, arr_size, bank_factor, s_idx):
+    result = dict()
 
+    cells = []
+    for b in range(bank_factor):
+        cells.append("le_b{}_{} = prim std_lt(32);".format(b, s_idx))
+        cells.append("idx_b{}_{} = prim std_reg(32);".format(b, s_idx))
+        cells.append("adder_idx_b{}_{} = prim std_add(32);".format(b, s_idx))
+
+    op_name = "mult" if stmt.op.body.op == "mul" else "add"
+    for b in range(bank_factor):
+        cells.append("adder_op_b{}_{} = prim std_{}(32);".format(
+            b, s_idx, op_name
+        ))
+
+    wires = []
+    for b in range(bank_factor):
+        wires.append(emit_cond_group(s_idx, arr_size // bank_factor, b))
+        wires.append(emit_idx_group(s_idx, b))
+        wires.append(emit_eval_body_group(s_idx, stmt, b))
+
+    control = []
+    map_loops = []
+    for b in range(bank_factor):
+        map_loops.append('''
+{2}while le{0}{1}.out with cond{0}{1} {{
+{2}  seq {{ eval_body{0}{1}; incr_idx{0}{1}; }}
+{2}}}
+        '''.format("_b" + str(b) + "_", s_idx, 8 * " "))
+
+    control.append('''
+{1}par {{
+{1}  {0}
+{1}}}
+    '''.format("".join(map_loops), 6 * " "))
+
+    return {"cells": cells, "wires": wires, "control": control}
+
+
+def gen_stmt_impl(stmt, arr_size, name2par, s_idx):
+    if isinstance(stmt.op, ast.Map):
+        return gen_map_impl(stmt, arr_size, name2par[stmt.dest], s_idx)
+    else:
+        return gen_reduce_impl(stmt, arr_size, s_idx)
+
+
+def emit(prog):
     cells = []
     wires = []
     control = []
 
+    # All arrays must be the same size. The first array we see determines the
+    # size that we'll assume for the rest of the program's arrays.
     arr_size = None
-    used_names = []
-    
+
+    # Collect banking factors.
     name2par = dict()
     for stmt in prog.stmts:
         if isinstance(stmt.op, ast.Map):
@@ -185,6 +201,8 @@ def emit(prog):
             for b in stmt.op.bind:
                 name2par[b.src] = stmt.op.par
 
+    # Collect memory and register declarations.
+    used_names = []
     for decl in prog.decls:
         used_names.append(decl.name)
         if decl.type.size:  # A memory
@@ -193,6 +211,7 @@ def emit(prog):
         else:  # A register
             cells.append(emit_reg_decl(decl.name, 32))
 
+    # Collect implicit memory and register declarations.
     for stmt in prog.stmts:
         if stmt.dest not in used_names:
             if isinstance(stmt.op, ast.Map):
@@ -201,14 +220,12 @@ def emit(prog):
                 cells.append(emit_reg_decl(stmt.dest, 32))
             used_names.append(stmt.dest)
 
-    assert arr_size is not None
-
-    for stmt in prog.stmts:
-        stmt_impl = gen_stmt_impl(stmt, arr_size, name2par)
+    # Generate FuTIL.
+    for i, stmt in enumerate(prog.stmts):
+        stmt_impl = gen_stmt_impl(stmt, arr_size, name2par, i)
         cells += stmt_impl["cells"]
         wires += stmt_impl["wires"]
         control += stmt_impl["control"]
-        COUNTER += 1
 
     emitted = '''
 import "primitives/std.lib";
@@ -228,9 +245,8 @@ component main() -> () {{
   }}
 }}
 '''.format(
-        "\n ".join(cells),
-        "\n ".join(wires),
-        "\n ".join(control)
+        "\n".join(cells),
+        "\n".join(wires),
+        "".join(control)
     )
     print(emitted)
-
