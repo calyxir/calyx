83e95e7334848c1032b3c79cc06e24a47f3cd2c0
tedbauer <tjb272@cornell.edu>
2020-10-14 17:21:36 -0400

    Clean up __init__

--- frontends/mrxl/mrxl/__init__.py
+++ frontends/mrxl/mrxl/__init__.py
@@ -8,18 +8,8 @@ from .interp import interp, InterpError
 def main():
     with open(sys.argv[1]) as f:
         txt = f.read()
-    with open(sys.argv[2]) as f:
-        data = json.load(f)
     ast = parse(txt)
-
-    try:
-        #out = interp(ast, data)
-        out = emit(ast)
-    except InterpError as exc:
-        print(str(exc), file=sys.stderr)
-        sys.exit(1)
-
-    #print(json.dumps(out, sort_keys=True, indent=2))
+    out = emit(ast)
 
 
 if __name__ == '__main__':
