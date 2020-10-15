4b308b016609f607e4b3adaaf35331e3006f20b4
Samuel Thomas <sgpthomas@gmail.com>
2020-10-15 12:00:51 -0400

    added module comment

--- calyx/src/ir/analysis/mod.rs
+++ calyx/src/ir/analysis/mod.rs
@@ -1,3 +1,8 @@
+//! Module for performing analysis on FuTIL components.
+//! This module contains analyses that construct external data-structures
+//! to make certain kinds of analysis more efficient.
+//! This doesn't include queries that can be answered by directly looking at the IR.
+
 mod graph;
 
 pub use graph::GraphAnalysis;
