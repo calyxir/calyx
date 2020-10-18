afe0e33b658b366b929dff155d5aedee89e01320
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-18 15:03:21 -0400

    remove calyx::lang

--- calyx/src/lang/mod.rs
+++
@@ -1,7 +0,0 @@
-//pub mod component;
-//pub mod context;
-//pub mod macros;
-//pub mod pretty_print;
-//pub mod structure;
-//pub mod structure_builder;
-//pub mod structure_iter;
--- calyx/src/lib.rs
+++ calyx/src/lib.rs
@@ -2,6 +2,5 @@ pub mod backend;
 pub mod errors;
 pub mod frontend;
 pub mod ir;
-pub mod lang;
 pub mod passes;
 pub mod utils;
