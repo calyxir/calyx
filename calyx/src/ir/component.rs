49303d7a499dd15fdbcaabc11249a6209a972a27
Rachit Nigam <rachit.nigam12@gmail.com>
2020-10-21 19:51:34 -0400

    implement constructor for ir::Component

--- calyx/src/ir/component.rs
+++ calyx/src/ir/component.rs
@@ -1,5 +1,6 @@
-use super::{Assignment, Cell, Control, Group, Id, RRC};
+use super::{Assignment, Cell, Group, Id, RRC, Control, Builder, CellType};
 use std::rc::Rc;
+use std::cell::RefCell;
 
 /// In memory representation of a Component.
 #[derive(Debug)]
@@ -24,6 +25,39 @@ pub struct Component {
 /// - find_<construct>: Returns a reference to the construct with the given
 ///   name.
 impl Component {
+    /// Construct a new Component with the given `name` and signature fields.
+    pub fn new<S, I, O>(
+        name: S,
+        inputs: Vec<(I, u64)>,
+        outputs: Vec<(O, u64)>,
+    ) -> Self
+    where
+        S: AsRef<str>,
+        I: AsRef<str>,
+        O: AsRef<str>,
+    {
+        let this_sig = Builder::cell_from_signature(
+            "this".into(),
+            CellType::ThisComponent,
+            inputs
+                .into_iter()
+                .map(|(name, width)| (name.as_ref().into(), width))
+                .collect(),
+            outputs
+                .into_iter()
+                .map(|(name, width)| (name.as_ref().into(), width))
+                .collect(),
+        );
+        Component {
+            name: name.as_ref().into(),
+            signature: this_sig,
+            cells: vec![],
+            groups: vec![],
+            continuous_assignments: vec![],
+            control: Rc::new(RefCell::new(Control::empty())),
+        }
+    }
+
     /// Return a reference to the group with `name` if present.
     pub fn find_group<S>(&self, name: &S) -> Option<RRC<Group>>
     where
