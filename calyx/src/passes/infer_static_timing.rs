21ae02123edbb7f8b57c02b7833c1b61c78e0d30
tedbauer <tjb272@cornell.edu>
2020-10-28 12:55:30 -0400

    Some progress on infer static timing

--- calyx/src/passes/infer_static_timing.rs
+++ calyx/src/passes/infer_static_timing.rs
@@ -1,14 +1,19 @@
 use std::collections::HashMap;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 use crate::frontend::library::ast as lib;
 use crate::guard;
 use crate::ir;
+use crate::ir::analysis::GraphAnalysis;
 use crate::ir::traversal::{Action, Named, VisResult, Visitor};
 
-#[derive(Default)]
-pub struct InferStaticTiming {}
+pub struct InferStaticTiming<'a> {
+    /// primitive name -> (go signal, done signal, latency)
+    prim_latency_data: HashMap<&'a str, (&'a str, &'a str, u64)>
+}
 
-impl Named for InferStaticTiming {
+impl Named for InferStaticTiming<'_> {
     fn name() -> &'static str {
         "infer-static-timing"
     }
@@ -18,54 +23,82 @@ impl Named for InferStaticTiming {
     }
 }
 
-fn infer_cycles(dst_name: String, assignments: &Vec<ir::Assignment>) -> u64 {
-    for assign in assignments {
-        if assign.dst.borrow().get_parent_name() == dst_name {
-            match &assign.guard {
-                ir::Guard::Port(port) => {
-                    if port.borrow().name == "done" {
-                        return 1 + infer_cycles(port.borrow().get_parent_name().to_string(), assignments);
-                    }
-                }
-                _ => continue
-            }
-        }
+impl Default for InferStaticTiming<'_> {
+    fn default() -> Self {
+        let prim_latency_data = [
+            ("std_reg", ("write_en", "done", 1))
+        ]
+        .iter()
+        .cloned()
+        .collect();
+        InferStaticTiming { prim_latency_data }
     }
-    return 1;
 }
 
-impl Visitor for InferStaticTiming {
+/// Attempts to infer the number of cycles starting when
+/// group[go] is high, and port is high.
+fn infer_latency<'a>(port: &ir::Port,
+                 group: &ir::Group,
+                 analysis: &GraphAnalysis,
+                 cells: &Vec<Rc<RefCell<ir::Cell>>>,
+                 latency_data: &HashMap<&'a str, (&'a str, &'a str, u64)>) -> Option<u64> {
+    for write in analysis.writes_to(port) {
+        let cell_type: ir::CellType = &cells
+            .iter()
+            .find(|c| c.borrow().name == write.borrow().get_parent_name())
+            .unwrap()
+            .borrow()
+            .prototype
+        println!("write: {:?}", write);
+        if let ir::CellType::Primitive{name, param_binding} = &cells.iter().find(|c| c.borrow().name == write.borrow().get_parent_name()).unwrap().borrow().prototype {
+            println!("reg type: {:?}", name);
+            let (go, done, latency) = latency_data.get(name.to_string().as_str()).unwrap();
+            println!("{}, {}, {}", go, done, latency);
+
+            // if write is to a "done" port, then return latency + infer_latency("go" port)
+            if write.borrow().name == done {
+                // Find port with name == parent, signal == g
+                let p: &ir::Port = &group.assignments.iter().find(|a| a.dst.borrow().name.to_string() == *go && a.dst.borrow().get_parent_name() == write.borrow().get_parent_name()).unwrap().dst.borrow();
+                return Some(latency + infer_latency(p, group, analysis, cells, latency_data).unwrap())
+            }
+        }
+
+        println!("found done")
+    }
+    return Some(0);
+    /*
+    for write in analysis.writes_to(port) {
+        // 1? return 1
+        // "done" port? prim_latency_data[port] + infer_latency("go" port)
+        // else: return None
+    }
+    */
+}
+
+
+impl Visitor for InferStaticTiming<'_> {
     fn start(
         &mut self,
         comp: &mut ir::Component,
         _c: &lib::LibrarySignatures,
     ) -> VisResult {
 
-        let mut attrs: HashMap<String, u64> = HashMap::new();
+        let analysis = GraphAnalysis::from(&comp);
+
         for group in &comp.groups {
-            let assignments = &group.borrow().assignments;
-            for assign in assignments {
-                if assign.dst.borrow().is_hole() && assign.dst.borrow().name == "done" {
-                    if assign.src.borrow().name == "done" {
-                        attrs.insert(
-                            group.borrow().name.to_string(),
-                            infer_cycles(assign.src.borrow().get_parent_name().to_string(), assignments)
-                        );
+            for asgn in &group.borrow().assignments {
+                if asgn.dst.borrow().name == "done" && asgn.dst.borrow().get_parent_name() == group.borrow().name {
+                    println!("name: {}", asgn.dst.borrow().get_parent_name());
+                    println!("latency: {}", infer_latency(&asgn.dst.borrow(), &group.borrow(), &analysis, &comp.cells, &self.prim_latency_data).unwrap());
+
+                    /*
+                    for read in analysis.writes_to(&asgn.dst.borrow()) {
+                        println!("read: {:?}", read);
                     }
+                    */
                 }
             }
         }
-        
-        for group in &comp.groups {
-            let group_name = group.borrow().name.to_string().clone();
-            if attrs.contains_key(&group_name) {
-                    group.borrow_mut().attributes.insert(
-                        "static".to_string(),
-                        *attrs.get(&group_name).unwrap()
-                    );
-            }
-        }
-
         Ok(Action::Stop)
     }
 }
