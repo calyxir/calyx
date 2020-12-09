e2fdc9f1788ad647818ee126674d0aee8e26324d
Rachit Nigam <rachit.nigam12@gmail.com>
2020-12-09 17:07:09 -0500

    start working on compile_invoke

---
+++ calyx/src/passes/compile_invoke.rs
@@ -0,0 +1,20 @@
+use crate::frontend::library::ast::LibrarySignatures;
+use crate::ir::traversal::{Action, Named, VisResult, Visitor};
+use crate::ir::{self, Component, Control};
+use crate::{build_assignments, structure};
+
+#[derive(Default)]
+pub struct CompileInvoke;
+
+impl Named for CompileInvoke {
+    fn name() -> &'static str {
+        "compile-empty"
+    }
+
+    fn description() -> &'static str {
+        "Rewrites empty control to invocation to empty group"
+    }
+}
+
+impl Visitor<()> for CompileInvoke {
+}
