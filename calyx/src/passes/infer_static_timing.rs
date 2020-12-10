9fb1df4a801e13cfc3052f80bb41d7b018380497
tedbauer <tjb272@cornell.edu>
2020-12-10 16:21:02 -0500

    Move to non-recursive approach

--- calyx/src/passes/infer_static_timing.rs
+++ calyx/src/passes/infer_static_timing.rs
@@ -6,7 +6,6 @@
 //! that are not only `done` signals, this pass will ignore that group.
 use std::collections::HashMap;
 
-use crate::analysis::GraphAnalysis;
 use crate::errors::Error;
 use crate::frontend::library::ast as lib;
 use crate::ir;
@@ -41,71 +40,105 @@ impl Default for InferStaticTiming<'_> {
 /// group[go] is high, and port is high. If inference is
 /// not possible, returns None.
 fn infer_latency<'a>(
-    port: &ir::Port,
     group: &ir::Group,
-    analysis: &GraphAnalysis,
     latency_data: &HashMap<&'a str, (&'a str, &'a str, u64)>,
+    comp: &ir::Component,
 ) -> Option<u64> {
-    // Get this `port`s parent
-    if let ir::PortParent::Cell(cell) = &port.parent {
-        match &cell.upgrade().unwrap().borrow().prototype {
-            ir::CellType::Primitive { name, .. } => {
-                // From the parent, get the timing information.
-                let data = latency_data.get(name.as_ref());
-                // If the timing is defined
-                if let Some((go, done, latency)) = data {
-                    // If this is the "done" port for this primitive.
-                    if port.name == *done {
-                        // Get the go port for this cell.
-                        /*if let ir::PortParent::Cell(cell_wref) = &port.parent {
-                            let go_port = cell.upgrade().borrow().get(go);
-                        }*/
-                        // Find all assignments to the "go" port for this primitive.
-                        let go_port: &ir::Port = &group
-                            .assignments
-                            .iter()
-                            .find(|a| {
-                                let a_dst = a.dst.borrow();
-                                let a_dst_name = a_dst.name.to_string();
-                                let a_prnt_name = a_dst.get_parent_name();
-                                let b_prnt_name = port.get_parent_name();
-                                // This is a write to this cell's go signal.
-                                a_dst_name == *go && a_prnt_name == b_prnt_name
-                            })
-                            .unwrap()
-                            .dst
-                            .borrow();
 
-                        if let Some(write) = analysis.writes_to(go_port).next()
-                        {
-                            return infer_latency(
-                                &write.borrow(),
-                                group,
-                                analysis,
-                                latency_data,
-                            )
-                            .map(|write_latency| write_latency + latency);
+    let mut writes: HashMap<String, String> = HashMap::new();
+    let mut cell2latency: HashMap<String, u64> = HashMap::new();
+
+    // Build a write dependency graph. An edge (`a`, `b`) exists in this graph
+    // if port `a` writes to port `b`, `a` is a "done" port or constant, and `b`
+    // is a "go" port. For example, if `a` and `b` are registers, these assignments
+    // would correspond to such an edge:
+    //
+    // ```
+    // b.write_en = a.done;
+    // ```
+    for asgn in &group.assignments {
+        
+        let src_parent_name: String = asgn
+            .src
+            .borrow()
+            .get_parent_name()
+            .to_string();
+        
+        let dst_parent_name: String = asgn
+            .dst
+            .borrow()
+            .get_parent_name()
+            .to_string();
+
+        match (&asgn.dst.borrow().parent, &asgn.src.borrow().parent) {
+
+            (ir::PortParent::Cell(dst_cell), ir::PortParent::Cell(src_cell)) =>  {
+                // A cell writes to a cell: to be added to the graph, the source needs to be a "done" port and the dest needs to be a "go" port.
+                // If any other type of port else writes to a "go" port, we can't infer static timing.
+                if let (ir::CellType::Primitive { name: dst_cell_prim_type, .. }, ir::CellType::Primitive {name: src_cell_prim_type, .. }) 
+                    = (&dst_cell.upgrade().unwrap().borrow().prototype, &src_cell.upgrade().unwrap().borrow().prototype) {
+                    let data_dst = latency_data.get(dst_cell_prim_type.as_ref());
+                    let data_src = latency_data.get(src_cell_prim_type.as_ref());
+                    if let (Some((go_dst, _, _)), Some((_, done_src, _))) = (data_dst, data_src) {
+
+                        if asgn.dst.borrow().name == *go_dst && asgn.src.borrow().name == *done_src {
+                            writes.insert(src_parent_name.to_string(), dst_parent_name.to_string());
                         }
-                    } else if port.name == *go {
-                        // Right now, we're just assuming there's 1 write.
-                        if let Some(write) = analysis.writes_to(port).next() {
-                            return infer_latency(
-                                &write.borrow(),
-                                group,
-                                analysis,
-                                latency_data,
-                            );
+
+                        if asgn.dst.borrow().name == *go_dst && asgn.src.borrow().name != *done_src {
+                            return None
+                        }
+                    }
+                }
+
+                // A constant writes to a cell: to be added to the graph, the cell needs to be a "done" port.
+                if let (ir::CellType::Primitive { name: dst_cell_prim_type, .. }, ir::CellType::Constant { val: _, width: _})
+                    = (&dst_cell.upgrade().unwrap().borrow().prototype, &src_cell.upgrade().unwrap().borrow().prototype) {
+                    let data = latency_data.get(dst_cell_prim_type.as_ref());
+                    if let Some((go, _, _)) = data {
+                        if asgn.dst.borrow().name == *go {
+                            writes.insert(src_parent_name.to_string(), dst_parent_name.to_string());
                         }
                     }
                 }
             }
 
-            ir::CellType::Constant { .. } => return Some(0),
-            ir::CellType::Component { .. } => return None,
-            ir::CellType::ThisComponent => return None,
+            // Something is written to a group: to be added to the graph, this needs to be a "done" port.
+            (ir::PortParent::Group(_), _) => {
+                if asgn.dst.borrow().name == "done" {
+                    writes.insert(src_parent_name, dst_parent_name);
+                }
+
+            }
+            
+            // If we encounter anything else, no need to add it to the graph.
+            _ => ()
         }
     }
-    None
+
+    // Starting at the constant written to a "go" port, walk the write dependency graph.
+    // Populate cell2latency with the latency of a node of the graph, plus the latency
+    // of the port that's written to the "go" signal of the node.
+    let start: String = "_1_1".to_string();
+    cell2latency.insert("_1_1".to_string(), 0);
+    let mut curr_node = start;
+    while curr_node != group.name.to_string() {
+        let w = writes.get(&curr_node).unwrap().to_string();
+        if w != group.name.to_string() {
+            if let ir::CellType::Primitive { name, .. } = &comp.find_cell(&w).unwrap().borrow().prototype {
+                let data = latency_data.get(name.as_ref());
+                if let Some((_go, _done, latency)) = data {
+                    cell2latency.insert(writes.get(&curr_node).unwrap().to_string(),*latency + cell2latency.get(&curr_node).unwrap());
+                }
+            }
+        } else {
+            // If the cell is the containing group, no need to add any more latency.
+            cell2latency.insert(w,*cell2latency.get(&curr_node.to_string()).unwrap());
+        }
+        curr_node = writes.get(&curr_node).unwrap().to_string();
+    }
+
+    Some(*cell2latency.get(group.name.as_ref()).unwrap())
 }
 
 impl Visitor<()> for InferStaticTiming<'_> {
@@ -114,21 +147,18 @@ impl Visitor<()> for InferStaticTiming<'_> {
         comp: &mut ir::Component,
         _c: &lib::LibrarySignatures,
     ) -> VisResult<()> {
-        let analysis = GraphAnalysis::from(&*comp);
 
         let mut latency_result: Option<u64> = None;
         for group in &comp.groups {
             for asgn in &group.borrow().assignments {
                 let asgn_dst = asgn.dst.borrow();
-                let asgn_src = asgn.src.borrow();
                 if asgn_dst.name == "done"
                     && asgn_dst.get_parent_name() == group.borrow().name
                 {
                     if let Some(latency) = infer_latency(
-                        &asgn_src,
                         &group.borrow(),
-                        &analysis,
                         &self.prim_latency_data,
+                        comp
                     ) {
                         let grp = group.borrow();
                         if let Some(curr_lat) = grp.attributes.get("static") {
