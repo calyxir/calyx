bb3b70ca58fcba2423dea68b2cece31b07683bf2
Karen Z <karenzh28@gmail.com>
2020-12-13 17:24:06 -0500

    main, interpretgroup sans cell map

--- interp/src/main.rs
+++ interp/src/main.rs
@@ -1,7 +1,82 @@
-use calyx::ir;
+use calyx::{
+    errors::{Error, FutilResult},
+    frontend::{library, parser},
+    ir,
+    utils::OutputFile,
+};
+use std::path::PathBuf;
+pub use structopt::StructOpt;
 
-pub mod interpreter;
+mod interpreter;
+pub mod interpretgroup;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, StructOpt)]
+#[structopt(name = "interpreter", about = "group interpreter CLI")]
+pub struct Opts {
+    // Input file
+    #[structopt(parse(from_os_str))]
+    pub file: Option<PathBuf>,
+
+    // Output file, default is stdout
+    #[structopt(short = "o", long = "output", default_value)]
+    pub output: OutputFile,
+
+    /// Path to the primitives library
+    #[structopt(long, short, default_value = "..")]
+    pub lib_path: PathBuf,
+
+    // Component to interpret
+    #[structopt(short = "c", long = "component", default_value = "main")]
+    pub component: String,
+
+    // Group to interpret
+    #[structopt(short = "g", long = "group")]
+    pub group: String,
+}
+
+fn get_ast() {}
+
+fn main() -> FutilResult<()> {
+    let opts = Opts::from_args();
+    println!("{:?}", opts);
+
+    // construct interpreter
+    let interpreter: interpretgroup::GroupInterpreter =
+        interpretgroup::GroupInterpreter {
+            component: opts.component.clone(),
+            group: opts.group.clone(),
+        };
+
+    // get input file
+    let namespace = match &opts.file {
+        Some(file) => parser::FutilParser::parse_file(&file),
+        None => Err(Error::InvalidFile(
+            "Must provide a FuTIL file as input (for now)!".to_string(),
+        )),
+    }?;
+
+    // only library for now is primitives std
+    let library: Vec<_> = namespace
+        .libraries
+        .iter()
+        .map(|path| {
+            library::parser::LibraryParser::parse_file(
+                &opts.lib_path.join(path),
+            )
+        })
+        .collect::<FutilResult<Vec<_>>>()?;
+
+    // IR
+    let ir = ir::from_ast::ast_to_ir(
+        namespace.components,
+        &library,
+        namespace.libraries,
+        false,
+        false,
+    )?;
+
+    // run the interpreter (in this case, group interpreter)
+    interpreter.interpret(&ir)?;
+
+    return Ok(());
 }
