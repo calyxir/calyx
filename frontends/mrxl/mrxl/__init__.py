5b7b38a4e183f9f9b08b3399fdd6f7d59610495e
tedbauer <tjb272@cornell.edu>
2020-12-15 17:21:18 -0500

    Allow user to run interpreter

--- frontends/mrxl/mrxl/__init__.py
+++ frontends/mrxl/mrxl/__init__.py
@@ -1,16 +1,37 @@
 import sys
 import json
+import argparse
 from .parse import parse
 from .gen_futil import emit
 from .interp import interp, InterpError
 
 
 def main():
-    with open(sys.argv[1]) as f:
+    parser = argparse.ArgumentParser('Interpret a MrXL program, or compile it to FuTIL.')
+    parser.add_argument('--i', '--interpret', action='store_true', help='Interpret the input MrXL program (leave this off to compile)')
+    parser.add_argument('--data', metavar='<datafile>', type=str, help="Input data, required to interpret")
+    parser.add_argument('filename', metavar='<file>', type=str, help="MrXL program to compile or interpet")
+
+    args = parser.parse_args()
+    with open(args.filename) as f:
         txt = f.read()
+    
+    if args.data:
+        with open(args.data) as f:
+            data = json.load(f)
+    
     ast = parse(txt)
-    out = emit(ast)
 
+    if args.i:
+        try:
+            out = interp(ast, data)
+        except InterpError as exc:
+            print(str(exc), file=sys.stderr)
+            sys.exit(1)
 
+    else:
+        out = emit(ast)
+
+    print(out)
 if __name__ == '__main__':
     main()
