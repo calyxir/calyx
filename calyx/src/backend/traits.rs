3f324863f94c748b95d3306fee7187bc29f3e40b
Rachit Nigam <rachit.nigam12@gmail.com>
2020-12-25 16:19:01 -0500

    use fewer libraries in test

--- calyx/src/backend/traits.rs
+++ calyx/src/backend/traits.rs
@@ -11,7 +11,10 @@ pub trait Backend {
     /// and write it to `write`.
     fn emit(prog: &ir::Context, write: &mut OutputFile) -> FutilResult<()>;
     /// Link the extern collected while parsing the program.
-    fn link_externs(prog: &ir::Context, write: &mut OutputFile) -> FutilResult<()>;
+    fn link_externs(
+        prog: &ir::Context,
+        write: &mut OutputFile,
+    ) -> FutilResult<()>;
     /// Convience function to validate and emit the program.
     fn run(&self, prog: &ir::Context, mut file: OutputFile) -> FutilResult<()> {
         Self::validate(&prog)?;
