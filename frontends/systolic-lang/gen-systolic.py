d85f8aa92b045a7ece5ee266b67d5c65e7d99057
Rachit Nigam <rachit.nigam12@gmail.com>
2021-01-11 18:27:23 -0500

    remove dobule buffering from systolic arrays

--- frontends/systolic-lang/gen-systolic.py
+++ frontends/systolic-lang/gen-systolic.py
@@ -24,7 +24,7 @@ component mac_pe(top: 32, left: 32) -> (out: 32) {
 
   wires {
 
-    group do_mul {
+    group do_mul<"static"=4> {
       mul.left = top;
       mul.right = left;
       mul.go = !mul.done ? 1'd1;
@@ -64,9 +64,6 @@ NAME_SCHEME = {
     # Move data between internal registers
     'register move down': '{pe}_down_move',
     'register move right': '{pe}_right_move',
-
-    # Move data between PEs
-    'pe move': 'pe_{row}_{col}_move',
 }
 
 
@@ -149,10 +146,10 @@ def instantiate_memory(top_or_left, idx, size):
     """
     if top_or_left == 'top':
         name = f't{idx}'
-        target_reg = f'top_0_{idx}_read'
+        target_reg = f'top_0_{idx}'
     elif top_or_left == 'left':
         name = f'l{idx}'
-        target_reg = f'left_{idx}_0_read'
+        target_reg = f'left_{idx}_0'
     else:
         raise f'Invalid top_or_left: {top_or_left}'
 
@@ -180,62 +177,16 @@ def instantiate_memory(top_or_left, idx, size):
 def instantiate_pe(row, col, right_edge=False, down_edge=False):
     """
     Instantiate the PE and all the registers connected to it.
-    Generates a group NAME_SCHEME['pe move'] that enables the PE and
-    moves the data from top and left registers to bottom and right registers
-    respectively.
     """
     # Add all the required cells.
     pe = f'pe_{row}_{col}'
-    group = NAME_SCHEME['pe move'].format(row=row, col=col)
     cells = [
         f'{pe} = {PE_NAME};',
-        create_register(f'top_{row}_{col}_read', BITWIDTH),
-        create_register(f'left_{row}_{col}_read', BITWIDTH),
+        create_register(f'top_{row}_{col}', BITWIDTH),
+        create_register(f'left_{row}_{col}', BITWIDTH),
     ]
 
-    # If this is the last PE in the array, skip generating the group
-    if right_edge and down_edge:
-        return ('\n'.join(cells), '')
-
-    # Instantiate registers forward registers if this is not on the edge of the
-    # compute fabric.
-    if not right_edge:
-        cells.append(create_register(f'right_{row}_{col}_write', BITWIDTH))
-    if not down_edge:
-        cells.append(create_register(f'down_{row}_{col}_write', BITWIDTH))
-
-    structure_stmts = ""
-
-    # Ports guarding the done condition for this group.
-    done_guards = ["1'd1"]
-
-    if not right_edge:
-        src_reg = f'left_{row}_{col}_read'
-        dst_reg = f'right_{row}_{col}_write'
-        done_guards.append(f"{dst_reg}.done")
-        structure_stmts += f"""
-            {dst_reg}.in = {src_reg}.out;
-            {dst_reg}.write_en = 1'd1;"""
-
-    if not down_edge:
-        src_reg = f'top_{row}_{col}_read'
-        dst_reg = f'down_{row}_{col}_write'
-        done_guards.append(f"{dst_reg}.done")
-        structure_stmts += f"""
-            {dst_reg}.in = {src_reg}.out;
-            {dst_reg}.write_en = 1'd1;"""
-
-    # Add the done condition for this group.
-    guard = ' & '.join(done_guards)
-    structure_stmts += f"""
-            {group}[done] = {guard} ? 1'd1;"""
-
-    structure = f"""
-    group {group} {{
-        {textwrap.indent(textwrap.dedent(structure_stmts), 6*" ")}
-    }}"""
-
-    return ('\n'.join(cells), structure)
+    return '\n'.join(cells)
 
 
 def instantiate_data_move(row, col, right_edge, down_edge):
@@ -249,8 +200,8 @@ def instantiate_data_move(row, col, right_edge, down_edge):
 
     if not right_edge:
         group_name = NAME_SCHEME['register move right'].format(pe=name)
-        src_reg = f'right_{row}_{col}_write'
-        dst_reg = f'left_{row}_{col+1}_read'
+        src_reg = f'left_{row}_{col}'
+        dst_reg = f'left_{row}_{col+1}'
         mover = textwrap.indent(textwrap.dedent(f'''
         group {group_name} {{
             {dst_reg}.in = {src_reg}.out;
@@ -261,8 +212,8 @@ def instantiate_data_move(row, col, right_edge, down_edge):
 
     if not down_edge:
         group_name = NAME_SCHEME['register move down'].format(pe=name)
-        src_reg = f'down_{row}_{col}_write'
-        dst_reg = f'top_{row+1}_{col}_read'
+        src_reg = f'top_{row}_{col}'
+        dst_reg = f'top_{row+1}_{col}'
         mover = textwrap.indent(textwrap.dedent(f'''
         group {group_name} {{
             {dst_reg}.in = {src_reg}.out;
@@ -447,12 +398,8 @@ def generate_control(top_length, top_depth, left_length, left_depth):
         # Invoke the PEs and move the data to the next layer.
         for (r, c) in elements:
             more_control += [
-                f'invoke pe_{r}_{c}(top = top_{r}_{c}_read.out, left = left_{r}_{c}_read.out)()',
+                f'invoke pe_{r}_{c}(top = top_{r}_{c}.out, left = left_{r}_{c}.out)()',
             ]
-            if (c != top_length - 1) or (r != left_length - 1):
-                more_control += [
-                    NAME_SCHEME['pe move'].format(row=r, col=c)
-                ]
 
         nodes = ";\n            ".join(more_control)
         more_control_str = textwrap.indent(textwrap.dedent(f'''
@@ -462,7 +409,7 @@ def generate_control(top_length, top_depth, left_length, left_depth):
 
         control.append(more_control_str)
 
-    ## Move all the results into output memory
+    # Move all the results into output memory
     mover_groups = []
     for row in range(left_length):
         for col in range(top_length):
@@ -517,10 +464,9 @@ def create_systolic_array(top_length, top_depth, left_length, left_depth):
     for row in range(left_length):
         for col in range(top_length):
             # Instantiate the PEs
-            (c, s) = instantiate_pe(
+            c = instantiate_pe(
                 row, col, col == top_length - 1, row == left_length - 1)
             cells.append(c)
-            wires.append(s)
 
             # Instantiate the mover fabric
             s = instantiate_data_move(
