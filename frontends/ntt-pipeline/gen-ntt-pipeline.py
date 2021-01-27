1ffd03f6c1f30b805b96d44e95a8c620d06ea307
Chris Gyurgyik <37983775+cgyurgyik@users.noreply.github.com>
2021-01-27 12:46:06 -0500

    Reduce `mult_pipe`s in NTT from N to N/2. (#370)
    * Reduce mul pipes.
    
    * Fix comment.

--- frontends/ntt-pipeline/gen-ntt-pipeline.py
+++ frontends/ntt-pipeline/gen-ntt-pipeline.py
@@ -13,44 +13,74 @@ def pp_block(decl, contents, indent=2):
     return ''.join((decl, ' {\n', textwrap.indent(contents, indent * ' '), '\n}'))
 
 
-def generate_pipeline_operations(n, num_stages):
+def get_pipeline_data(n, num_stages):
     """Returns the operations for each row in each stage in tuple format.
     Recall that each stage will consist of N operations, following the
     pattern:
       `a[i] +- (a[j] * phis[p])`
 
     From this, we can have a log2(`n`) x `n` array that stores the tuple value:
-      `(i, op, j, p)`
+      `(i, op, m)`
       where `i` is the input index of the lhs,
-            `j` is the input index of the rhs,
-            `op` is the operation (either + or -),
-            and `p` is the index within phis.
+            `op` is the operation conducted, in {+, -},
+            and `m` is the index of the register holding the product.
     """
-    structure = [[() for _ in range(n)] for _ in range(num_stages)]
+    operations = [[() for _ in range(n)] for _ in range(num_stages)]
+    t = n
+    for i in range(0, num_stages):
+        t >>= 1
+        j = t
+        mult_register = 0
+        while j < n:
+            for k in range(j, j + t):
+                operations[i][k - t] = ((k - t, '+', mult_register))
+                operations[i][k] = ((k - t, '-', mult_register))
+                mult_register += 1
+            j += t << 1
+    return operations
+
+
+def get_multiply_data(n, num_stages):
+    """Returns a tuple value for each unique product that contains:
+       (register index, input index, phi index)
+
+    Since each stage in the pipeline has at most N / 2 unique products calculated,
+    we want to only use N / 2 `mult_pipeline`s in each stage. This requires us to
+    map which products go with which calculation.
+
+    This calculation is usually referred to as `V`, and would look something like:
+      `V = a[x] * phis[y]`
+    and used to calculate:
+      `a[k]   = U + V mod q`
+      `a[k+t] = U - V mod q`
+    """
+    mults = [[] for _ in range(num_stages)]
     phi_index = 1
     t = n
     for i in range(0, num_stages):
         t >>= 1
         j = t
+        register_index = 0
         while j < n:
-            for k in range(j, j + t):
-                structure[i][k - t] = ((k - t, '+', k, phi_index))
-                structure[i][k] = ((k - t, '-', k, phi_index))
+            for input_index in range(j, j + t):
+                mults[i].append((register_index, input_index, phi_index))
+                register_index += 1
             j += t << 1
             phi_index += 1
-    return structure
+    return mults
 
 
-def pp_table(structure, n, num_stages):
+def pp_table(operations, multiplies, n, num_stages):
     """Pretty prints a table describing the calculations made during the pipeline."""
     stage_titles = ['a'] + ['Stage {}'.format(i) for i in range(num_stages)]
     table = PrettyTable(stage_titles)
     for row in range(n):
         table_row = ['{}'.format(row)]
         for stage in range(num_stages):
-            lhs, op, rhs, phi_idx = structure[stage][row]
+            lhs, op, mult_register = operations[stage][row]
+            _, rhs, phi_index = multiplies[stage][mult_register]
             table_row.append('a[{}] {} a[{}] * phis[{}]'
-                             .format(lhs, op, rhs, phi_idx))
+                             .format(lhs, op, rhs, phi_index))
         table.add_row(table_row)
     table = table.get_string().split("\n")
     table = [' '.join(('//', line)) for line in table]
@@ -76,7 +106,8 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
     num_stages = int(math.log2(n))
     bitwidth = num_stages + 1
 
-    structure = generate_pipeline_operations(n, num_stages)
+    operations = get_pipeline_data(n, num_stages)
+    multiplies = get_multiply_data(n, num_stages)
 
     # Used to determine the index of the component
     # for the `sadd` and `ssub` primitives.
@@ -95,26 +126,28 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
 
         return saved_count
 
-    def pp_stage_groups(stage, row, structure_tuple):
-        lhs, op, rhs, phi_index = structure_tuple
+    def pp_mul_group(stage, mul_tuple):
+        mul_index, k, phi_index = mul_tuple
+        mult_group_name = 's{}_mul{}'.format(stage, mul_index)
+        mult_wires = [
+            'mult_pipe{}.left = phi{}.out;'.format(mul_index, phi_index),
+            'mult_pipe{}.right = r{}.out;'.format(mul_index, k),
+            'mult_pipe{}.go = !mult_pipe{}.done ? 1\'d1;'.format(mul_index, mul_index),
+            'mul{}.write_en = mult_pipe{}.done;'.format(mul_index, mul_index),
+            'mul{}.in = mult_pipe{}.out;'.format(mul_index, mul_index),
+            '{}[done] = mul{}.done ? 1\'d1;'.format(mult_group_name, mul_index)
+        ]
+        return pp_block('group {}'.format(mult_group_name), '\n'.join(mult_wires))
+
+    def pp_op_mod_group(stage, row, operations_tuple):
+        lhs, op, mul_index = operations_tuple
         comp = 'add' if op == '+' else 'sub'
 
-        mult_group_name = 's{}_r{}_mul'.format(stage, row)
-        mult_wires = [
-            'mult_pipe{}.left = phi{}.out;'.format(row, phi_index),
-            'mult_pipe{}.right = r{}.out;'.format(row, rhs),
-            'mult_pipe{}.go = !mult_pipe{}.done ? 1\'d1;'.format(row, row),
-            'mul{}.write_en = mult_pipe{}.done;'.format(row, row),
-            'mul{}.in = mult_pipe{}.out;'.format(row, row),
-            '{}[done] = mul{}.done ? 1\'d1;'.format(mult_group_name, row)
-        ]
-        mult_group = pp_block('group {}'.format(mult_group_name), '\n'.join(mult_wires))
-
         comp_index = fresh_comp_index(comp)
-        mod_group_name = 's{}_r{}_mod'.format(stage, row)
+        mod_group_name = 's{}_r{}_op_mod'.format(stage, row)
         mod_wires = [
             '{}{}.left = r{}.out;'.format(comp, comp_index, lhs),
-            '{}{}.right = mul{}.out;'.format(comp, comp_index, row, row),
+            '{}{}.right = mul{}.out;'.format(comp, comp_index, mul_index),
             'mod_pipe{}.left = {}{}.out;'.format(row, comp, comp_index, comp, row),
             'mod_pipe{}.right = {}\'d{};'.format(row, input_bitwidth, q),
             'mod_pipe{}.go = !mod_pipe{}.done ? 1\'d1;'.format(row, row),
@@ -122,9 +155,7 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
             'A{}.in = mod_pipe{}.out;'.format(row, row),
             '{}[done] = A{}.done ? 1\'d1;'.format(mod_group_name, row)
         ]
-        mod_group = pp_block('group {}'.format(mod_group_name), '\n'.join(mod_wires))
-
-        return '\n'.join((mult_group, mod_group))
+        return pp_block('group {}'.format(mod_group_name), '\n'.join(mod_wires))
 
     def pp_precursor_group(row):
         group_name = 'precursor_{}'.format(row)
@@ -169,8 +200,10 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
                 precursors = ['precursor_{};'.format(r) for r in range(n)]
                 ntt_stages.append(pp_block('par', '\n'.join(precursors)))
 
-            rows = ['seq {{ s{}_r{}_mul;'.format(s, r) + ' s{}_r{}_mod; }}'.format(s, r) for r in range(n)]
-            ntt_stages.append(pp_block('par', '\n'.join(rows)))
+            multiplies = ['s{}_mul{};'.format(s, i) for i in range(n // 2)]
+            op_mods = ['s{}_r{}_op_mod;'.format(s, r) for r in range(n)]
+            ntt_stages.append(pp_block('par', '\n'.join(multiplies)))
+            ntt_stages.append(pp_block('par', '\n'.join(op_mods)))
 
         controls = pp_block('seq', '\n'.join((preambles, '\n'.join(ntt_stages), epilogues)))
         return pp_block('control', controls)
@@ -182,10 +215,10 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
         ]
         r_registers = ['r{} = prim std_reg({});'.format(row, input_bitwidth) for row in range(n)]
         A_registers = ['A{} = prim std_reg({});'.format(row, input_bitwidth) for row in range(n)]
-        mul_registers = ['mul{} = prim std_reg({});'.format(row, input_bitwidth) for row in range(n)]
+        mul_registers = ['mul{} = prim std_reg({});'.format(i, input_bitwidth) for i in range(n // 2)]
         phi_registers = ['phi{} = prim std_reg({});'.format(row, input_bitwidth) for row in range(n)]
         mod_pipes = ['mod_pipe{} = prim std_smod_pipe({});'.format(row, input_bitwidth) for row in range(n)]
-        mult_pipes = ['mult_pipe{} = prim std_smult_pipe({});'.format(row, input_bitwidth) for row in range(n)]
+        mult_pipes = ['mult_pipe{} = prim std_smult_pipe({});'.format(i, input_bitwidth) for i in range(n // 2)]
         adds = ['add{} = prim std_sadd({});'.format(row, input_bitwidth) for row in range(n // 2)]
         subs = ['sub{} = prim std_ssub({});'.format(row, input_bitwidth) for row in range(n // 2)]
 
@@ -196,12 +229,13 @@ def generate_ntt_pipeline(input_bitwidth, n, q):
     def wires():
         preamble_groups = [pp_preamble_group(r) for r in range(n)]
         precursor_groups = [pp_precursor_group(r) for r in range(n)]
-        stage_groups = [pp_stage_groups(s, r, structure[s][r]) for s in range(num_stages) for r in range(n)]
+        mul_groups = [pp_mul_group(s, multiplies[s][i]) for s in range(num_stages) for i in range(n // 2)]
+        op_mod_groups = [pp_op_mod_group(s, r, operations[s][r]) for s in range(num_stages) for r in range(n)]
         epilogue_groups = [pp_epilogue_group(r) for r in range(n)]
-        groups = preamble_groups + precursor_groups + stage_groups + epilogue_groups
+        groups = preamble_groups + precursor_groups + mul_groups + op_mod_groups + epilogue_groups
         return pp_block('wires', '\n'.join(groups))
 
-    pp_table(structure, n, num_stages)
+    pp_table(operations, multiplies, n, num_stages)
     print('import "primitives/std.lib";')
     print(
         pp_block('component main() -> ()', '\n'.join(
