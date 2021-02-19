6ac24d5cb8e5e474fd227521f81a3d2601d16cba
cgyurgyik <gyurgyikcp@gmail.com>
2021-02-18 21:33:19 -0500

    Use table method suggested by adrian.

--- fixedpoint/fixedpoint.py
+++ fixedpoint/fixedpoint.py
@@ -1,4 +1,5 @@
 import numpy as np
+from itertools import product
 
 
 def decimal_to_fixed_p(num, width, int_bit, frac_bit):
@@ -27,11 +28,11 @@ def decimal_to_fixed_p(num, width, int_bit, frac_bit):
 
 
 def fixed_p_to_decimal(fp, width, int_bit, frac_bit):
-    '''
-    given fixedpoint representation, width, integer bit and fractinal bit,
+    """
+    Given fixedpoint representation, width, integer bit and fractinal bit,
     returns the number.
     example: fixed_p_to_decimal ('01011001',8,5,3) returns 11.125
-    '''
+    """
     int_b = fp[:int_bit]
     frac_b = fp[int_bit:width]
     int_num = int(int_b, 2)
@@ -41,13 +42,22 @@ def fixed_p_to_decimal(fp, width, int_bit, frac_bit):
     return num
 
 
-def compute_chebyshev_polynomials(x, width, i_bit, f_bit):
+def binary_to_base10(bit_list):
+    """Takes a binary number in list form
+    e.g. [1, 0, 1, 0], and returns
+    the corresponding base 10 number.
     """
-    Computes the Chebyshev polynomials based on the recurrence
-    relation described here:
-      en.wikipedia.org/wiki/Chebyshev_polynomials#Definition
-    """
+    out = 0
+    for b in bit_list:
+        out = (out << 1) | b
+    return out
 
+
+def compute_exp_frac_table(frac_bit):
+    """Computes a table of size 2^frac_bit
+    for every value of e^x that can be
+    represented by fixed point in the range [0, 1].
+    """
     # Chebyshev approximation coefficients for e^x in [0, 1].
     coeffs = [
         1.7534,
@@ -58,22 +68,48 @@ def compute_chebyshev_polynomials(x, width, i_bit, f_bit):
         0.000027075
     ]
 
-    # Change from [0, 1] to [-1, 1] for
-    # better approximation with chebyshev.
-    u = (2 * x - 1)
+    def chebyshev_polynomial_approx(x):
+        """Computes the Chebyshev polynomials
+        based on the recurrence relation
+        described here:
+        en.wikipedia.org/wiki/Chebyshev_polynomials#Definition
+        """
+        # Change from [0, 1] to [-1, 1] for
+        # better approximation with chebyshev.
+        u = (2 * x - 1)
 
-    Ti = 1
-    Tn = None
-    T = u
-    num_coeffs = len(coeffs)
-    c = coeffs[0]
-    for i in range(1, num_coeffs):
-        c = c + T * coeffs[i]
-        Tn = 2 * u * T - Ti
-        Ti = T
-        T = Tn
+        Ti = 1
+        Tn = None
+        T = u
+        num_coeffs = len(coeffs)
+        c = coeffs[0]
+        for i in range(1, num_coeffs):
+            c = c + T * coeffs[i]
+            Tn = 2 * u * T - Ti
+            Ti = T
+            T = Tn
 
-    return c
+        return c
+
+    # Gets the permutations of 2^f_bit,
+    # in increasing order.
+    binary_permutations = map(
+        list,
+        product(
+            [0, 1],
+            repeat=frac_bit
+        )
+    )
+
+    e_table = [0] * (2 ** frac_bit)
+    for p in binary_permutations:
+        i = binary_to_base10(p)
+        fraction = float(
+            i / 2 ** (frac_bit)
+        )
+        e_table[i] = chebyshev_polynomial_approx(fraction)
+
+    return e_table
 
 
 def exp(x, width, int_bit, frac_bit, print_results=False):
@@ -93,32 +129,20 @@ def exp(x, width, int_bit, frac_bit, print_results=False):
     frac_b = fp_x[int_bit:width]
     frac_bin = int(frac_b, 2)
 
-    # Compute the decimal values.
-
-    # TODO(cgyurgyik): Instead of first converting this to a
-    # floating point number, we need to instead do all the
-    # math using fixed point. Uh-oh overflow...
-    frac_base = float(frac_bin / 2 ** (frac_bit))
-
     # Split e^x into e^i * e^f.
     e_i = 2.71828 ** int_bin
-    e_f = compute_chebyshev_polynomials(
-        frac_base,
-        width,
-        frac_bit,
-        int_bit
-    )
+
+    e_table = compute_exp_frac_table(frac_bit)
+    e_f = e_table[frac_bin]
+
     # Compute e^i * e^f.
     expected = e_i * e_f
 
     if print_results:
         actual = 2.71828 ** x
-        print('e ^', x)
-        print('approx: ', expected)
-        print('actual: ', actual)
-        print('reldif: ', (expected - actual) / expected * 100, '%')
+        print(f'e^{x}')
+        print(f'approx: {expected}')
+        print(f'actual: {actual}')
+        print(f'reldef: {(expected - actual) / expected * 100}%')
 
     return expected
-
-
-exp(25.25, 32, 16, 16, print_results=True)
