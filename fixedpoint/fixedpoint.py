153722a2dc8d716ea2918a06d47468023e0c9598
cgyurgyik <gyurgyikcp@gmail.com>
2021-02-18 21:45:21 -0500

    Cleanup

--- fixedpoint/fixedpoint.py
+++ fixedpoint/fixedpoint.py
@@ -3,24 +3,24 @@ from itertools import product
 
 
 def decimal_to_fixed_p(num, width, int_bit, frac_bit):
-    '''
-    Given the number, width, integer bit and fractional bit,
+    """Given the number, width, integer bit and fractional bit,
     returns the fixed point representation.
     Example: decimal_to_fixed_p(11.125,8,5,3) returns 01011001 = 2^3+2^1+2^0+2^(-3)
     Precondition: There is no overflow
-    (integer part of the number should be representable with int_bit number of bits )
-    '''
-    # seperate to int and fractional parts
+    (integer part of the number should be representable with int_bit number of bits).
+    """
+    # separate into integer and fractional parts
     intg, frac = str(num).split(".")
     int_b = np.binary_repr(int(intg), width=int_bit)
     frac = "0." + frac
+
     # multiply fractional part with 2**frac_bit to turn into integer
-    # frac= int(float(frac) * float(2**frac_bit))
     frac = float(frac) * float(2 ** frac_bit)
     _, f = str(frac).split(".")
-    # raises Exception when the number can't be represented as fixed numbers
+    # raises Exception when the number can't
+    # be represented in fixed point format.
     if f != "0":
-        raise Exception("number can't be represented as fixedpoint numbers")
+        raise Exception("Can't be represented as fixedpoint numbers.")
     frac = int(frac)
     frac_b = np.binary_repr(frac, width=frac_bit)
     r = int_b + frac_b
@@ -28,9 +28,8 @@ def decimal_to_fixed_p(num, width, int_bit, frac_bit):
 
 
 def fixed_p_to_decimal(fp, width, int_bit, frac_bit):
-    """
-    Given fixedpoint representation, width, integer bit and fractinal bit,
-    returns the number.
+    """Given fixedpoint representation, width,
+    integer bit and fractinal bit, returns the number.
     example: fixed_p_to_decimal ('01011001',8,5,3) returns 11.125
     """
     int_b = fp[:int_bit]
