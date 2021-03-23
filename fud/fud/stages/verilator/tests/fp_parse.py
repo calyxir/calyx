c26acb3f720748157b1f56542a12f3b4224807b3
cgyurgyik <gyurgyikcp@gmail.com>
2021-03-22 21:09:31 -0400

    Add pytest.

--- fud/fud/stages/verilator/tests/fp_parse.py
+++ fud/fud/stages/verilator/tests/fp_parse.py
@@ -8,12 +8,8 @@ from hypothesis import given, strategies as st
 import pytest
 
 
-def x():
-    assert 0
-
-
 @given(bits=st.lists(st.booleans(), min_size=2, max_size=256), is_signed=st.booleans())
-def verify_fixed_point_round_trip(bits, is_signed):
+def test_fixed_point_round_trip(bits, is_signed):
     """Given a bit string representation of an integer,
     selects a pseudorandom `int_width` in the
     interval [1, N - 1], and round trips the value
@@ -36,14 +32,14 @@ def verify_fixed_point_round_trip(bits, is_signed):
     expected = int(bitstring, 2)
     actual = fp_round_trip(bitstring)
     assert (
-        expected == actual
+            expected == actual
     ), f"""width: {width}, int_width:{int_width}
         is_signed: {is_signed}, bits: {bitstring}
         expected: {expected}, actual: {actual}"""
 
 
 @given(bits=st.lists(st.booleans(), min_size=2, max_size=256))
-def verify_twos_complement_negation_round_trip(bits):
+def test_twos_complement_negation_round_trip(bits):
     """Verifies that the negation of the negation
     in twos complement is the original bitstring."""
     bitstring = "".join(["1" if x else "0" for x in bits])
@@ -60,8 +56,3 @@ def verify_twos_complement_negation_round_trip(bits):
     ), f"""original: {bitstring},
         round-tripped: {round_trip(bitstring)},
         width: {width}"""
-
-
-if __name__ == "__main__":
-    verify_fixed_point_round_trip()
-    verify_twos_complement_negation_round_trip()
