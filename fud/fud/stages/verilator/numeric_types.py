5c677b400fa5c0d56a913349858d8cfd94e1c3f8
cgyurgyik <gyurgyikcp@gmail.com>
2021-03-30 22:16:07 -0400

    sp

--- fud/fud/stages/verilator/numeric_types.py
+++ fud/fud/stages/verilator/numeric_types.py
@@ -180,8 +180,8 @@ class FixedPoint(NumericType):
         if int_overflow or frac_overflow:
             raise InvalidNumericType(
                 f"""Trying to represent {value} with:
-integer width: {self.int_width}
-fractional width: {self.frac_width}
+    Integer width: {self.int_width}
+    Fractional width: {self.frac_width}
 has led to overflow.
 {'Required int width: {}'.format(required_int_width) if int_overflow else ''}
 {'Required fractional width: {}'.format(required_frac_width) if frac_overflow else ''}
