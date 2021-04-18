bfb63cb32d778ace8736a535a5194e7c2f612c19
cgyurgyik <gyurgyikcp@gmail.com>
2021-04-18 16:06:22 -0400

    Revert div_pipe.

--- primitives/binary_operators.sv
+++ primitives/binary_operators.sv
@@ -218,8 +218,8 @@ module std_fp_smult_pipe #(
     output logic signed [WIDTH-1:0] out,
     output logic                    done
 );
-  logic signed [WIDTH-1:0] rtmp;
   logic signed [WIDTH-1:0] ltmp;
+  logic signed [WIDTH-1:0] rtmp;
   logic signed [(WIDTH << 1) - 1:0] out_tmp;
   reg done_buf[1:0];
   always_ff @(posedge clk) begin
@@ -271,7 +271,7 @@ module std_fp_sdiv_pipe #(
   assign right_abs = right[WIDTH-1] ? -right : right;
   assign left_abs = left[WIDTH-1] ? -left : left;
   assign out_quotient = left[WIDTH-1] ^ right[WIDTH-1] ? -comp_out_q : comp_out_q;
-  assign out_remainder = left[WIDTH-1] ? comp_out_r : -comp_out_r;
+  assign out_remainder = left[WIDTH-1] ? $signed(right - comp_out_r) : comp_out_r;
 
   std_fp_div_pipe #(
     .WIDTH(WIDTH),
@@ -402,19 +402,70 @@ module std_div_pipe #(
     output logic [WIDTH-1:0] out_quotient,
     output logic             done
 );
-  std_fp_div_pipe #(
-    .WIDTH(WIDTH),
-    .INT_WIDTH(WIDTH),
-    .FRAC_WIDTH(0)
-  ) comp (
-    .clk(clk),
-    .done(done),
-    .go(go),
-    .left(left),
-    .right(right),
-    .out_quotient(out_quotient),
-    .out_remainder(out_remainder)
-  );
+
+  logic [WIDTH-1:0] dividend;
+  logic [(WIDTH-1)*2:0] divisor;
+  logic [WIDTH-1:0] quotient;
+  logic [WIDTH-1:0] quotient_msk;
+  logic start, running, finished;
+
+  assign start = go && !running;
+  assign finished = !quotient_msk && running;
+
+  always_ff @(posedge clk) begin
+    if (!go) begin
+      running <= 0;
+      done <= 0;
+      out_remainder <= 0;
+      out_quotient <= 0;
+    end else if (start && left == 0) begin
+      out_remainder <= 0;
+      out_quotient <= 0;
+      done <= 1;
+    end
+
+    if (start) begin
+      running <= 1;
+      dividend <= left;
+      divisor <= right << WIDTH - 1;
+      quotient <= 0;
+      quotient_msk <= 1 << WIDTH - 1;
+    end else if (finished) begin
+      running <= 0;
+      done <= 1;
+      out_remainder <= dividend;
+      out_quotient <= quotient;
+    end else begin
+      if (divisor <= dividend) begin
+        dividend <= dividend - divisor;
+        quotient <= quotient | quotient_msk;
+      end
+      divisor <= divisor >> 1;
+      quotient_msk <= quotient_msk >> 1;
+    end
+  end
+
+  `ifdef VERILATOR
+    // Simulation self test against unsynthesizable implementation.
+    always @(posedge clk) begin
+      if (finished && dividend != $unsigned(left % right))
+        $error(
+          "\nstd_div_pipe (Remainder): Computed and golden outputs do not match!\n",
+          "left: %0d", $unsigned(left),
+          "  right: %0d\n", $unsigned(right),
+          "expected: %0d", $unsigned(left % right),
+          "  computed: %0d", $unsigned(dividend)
+        );
+      if (finished && quotient != $unsigned(left / right))
+        $error(
+          "\nstd_div_pipe (Quotient): Computed and golden outputs do not match!\n",
+          "left: %0d", $unsigned(left),
+          "  right: %0d\n", $unsigned(right),
+          "expected: %0d", $unsigned(left / right),
+          "  computed: %0d", $unsigned(quotient)
+        );
+    end
+  `endif
 endmodule
 
 
@@ -543,6 +594,7 @@ module std_smult_pipe #(
   );
 endmodule
 
+/* verilator lint_off WIDTH */
 module std_sdiv_pipe #(
     parameter WIDTH = 32
 ) (
@@ -554,19 +606,50 @@ module std_sdiv_pipe #(
     output signed [WIDTH-1:0] out_remainder,
     output logic              done
 );
-  std_fp_sdiv_pipe #(
-    .WIDTH(WIDTH),
-    .INT_WIDTH(WIDTH),
-    .FRAC_WIDTH(0)
+
+  logic signed [WIDTH-1:0] left_abs;
+  logic signed [WIDTH-1:0] right_abs;
+  logic signed [WIDTH-1:0] comp_out_q;
+  logic signed [WIDTH-1:0] comp_out_r;
+
+  assign right_abs = right[WIDTH-1] ? -right : right;
+  assign left_abs = left[WIDTH-1] ? -left : left;
+  assign out_quotient = left[WIDTH-1] ^ right[WIDTH-1] ? -comp_out_q : comp_out_q;
+  assign out_remainder = left[WIDTH-1] == 1 ? $signed(right - comp_out_r) : comp_out_r;
+
+  std_div_pipe #(
+    .WIDTH(WIDTH)
   ) comp (
     .clk(clk),
     .done(done),
     .go(go),
-    .left(left),
-    .right(right),
-    .out_quotient(out_quotient),
-    .out_remainder(out_remainder)
+    .left(left_abs),
+    .right(right_abs),
+    .out_quotient(comp_out_q),
+    .out_remainder(comp_out_r)
   );
+
+  `ifdef VERILATOR
+    // Simulation self test against unsynthesizable implementation.
+    always @(posedge clk) begin
+      if (done && out_quotient != $signed(left / right))
+        $error(
+          "\nstd_sdiv_pipe (Quotient): Computed and golden outputs do not match!\n",
+          "left: %0d", left,
+          "  right: %0d\n", right,
+          "expected: %0d", $signed(left / right),
+          "  computed: %0d", $signed(out_quotient)
+        );
+      if (done && out_remainder != $signed(((left % right) + right) % right))
+        $error(
+          "\nstd_sdiv_pipe (Remainder): Computed and golden outputs do not match!\n",
+          "left: %0d", left,
+          "  right: %0d\n", right,
+          "expected: %0d", $signed(((left % right) + right) % right),
+          "  computed: %0d", $signed(out_remainder)
+        );
+    end
+  `endif
 endmodule
 
 module std_sgt #(
