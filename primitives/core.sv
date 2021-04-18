668e6f8f4ab987c564affc1c2309240e80ce4bef
cgyurgyik <gyurgyikcp@gmail.com>
2021-04-18 14:24:51 -0400

    Refactor primitives library.

--- primitives/core.sv
+++ primitives/core.sv
@@ -1,5 +1,5 @@
 /**
- * Core primitives for FuTIL.
+ * Core primitives for Calyx.
  * Implements core primitives used by the compiler.
  *
  * Conventions:
@@ -7,6 +7,116 @@
  * - Port names must be snake_case, no caps.
  */
 `default_nettype none
+
+module std_const #(
+    parameter WIDTH = 32,
+    parameter value = 0
+) (
+   output logic [WIDTH - 1:0] out
+);
+  assign out = value;
+endmodule
+
+module std_slice #(
+    parameter IN_WIDTH  = 32,
+    parameter OUT_WIDTH = 32
+) (
+   input wire                   logic [ IN_WIDTH-1:0] in,
+   output logic [OUT_WIDTH-1:0] out
+);
+  assign out = in[OUT_WIDTH-1:0];
+
+  `ifdef VERILATOR
+    always_comb begin
+      if (IN_WIDTH < OUT_WIDTH)
+        $error(
+          "std_slice: Input width less than output width\n",
+          "IN_WIDTH: %0d", IN_WIDTH,
+          "OUT_WIDTH: %0d", OUT_WIDTH
+        );
+    end
+  `endif
+endmodule
+
+module std_pad #(
+    parameter IN_WIDTH  = 32,
+    parameter OUT_WIDTH = 32
+) (
+   input wire logic [IN_WIDTH-1:0]  in,
+   output logic     [OUT_WIDTH-1:0] out
+);
+  localparam EXTEND = OUT_WIDTH - IN_WIDTH;
+  assign out = { {EXTEND {1'b0}}, in};
+
+  `ifdef VERILATOR
+    always_comb begin
+      if (IN_WIDTH > OUT_WIDTH)
+        $error(
+          "std_pad: Output width less than input width\n",
+          "IN_WIDTH: %0d", IN_WIDTH,
+          "OUT_WIDTH: %0d", OUT_WIDTH
+        );
+    end
+  `endif
+endmodule
+
+module std_not #(
+    parameter WIDTH = 32
+) (
+   input wire               logic [WIDTH-1:0] in,
+   output logic [WIDTH-1:0] out
+);
+  assign out = ~in;
+endmodule
+
+module std_and #(
+    parameter WIDTH = 32
+) (
+   input wire               logic [WIDTH-1:0] left,
+   input wire               logic [WIDTH-1:0] right,
+   output logic [WIDTH-1:0] out
+);
+  assign out = left & right;
+endmodule
+
+module std_or #(
+    parameter WIDTH = 32
+) (
+   input wire               logic [WIDTH-1:0] left,
+   input wire               logic [WIDTH-1:0] right,
+   output logic [WIDTH-1:0] out
+);
+  assign out = left | right;
+endmodule
+
+module std_xor #(
+    parameter WIDTH = 32
+) (
+   input wire               logic [WIDTH-1:0] left,
+   input wire               logic [WIDTH-1:0] right,
+   output logic [WIDTH-1:0] out
+);
+  assign out = left ^ right;
+endmodule
+
+module std_reg #(
+    parameter WIDTH = 32
+) (
+   input wire [ WIDTH-1:0]    in,
+   input wire                 write_en,
+   input wire                 clk,
+    // output
+   output logic [WIDTH - 1:0] out,
+   output logic               done
+);
+
+  always_ff @(posedge clk) begin
+    if (write_en) begin
+      out <= in;
+      done <= 1'd1;
+    end else done <= 1'd0;
+  end
+endmodule
 
 module std_mem_d1 #(
     parameter WIDTH = 32,
@@ -124,216 +234,6 @@ module std_mem_d4 #(
       done <= 1'd1;
     end else done <= 1'd0;
   end
-endmodule
-
-module std_reg #(
-    parameter WIDTH = 32
-) (
-   input wire [ WIDTH-1:0]    in,
-   input wire                 write_en,
-   input wire                 clk,
-    // output
-   output logic [WIDTH - 1:0] out,
-   output logic               done
-);
-
-  always_ff @(posedge clk) begin
-    if (write_en) begin
-      out <= in;
-      done <= 1'd1;
-    end else done <= 1'd0;
-  end
-endmodule
-
-module std_const #(
-    parameter WIDTH = 32,
-    parameter value = 0
-) (
-   output logic [WIDTH - 1:0] out
-);
-  assign out = value;
-endmodule
-
-module std_slice #(
-    parameter IN_WIDTH  = 32,
-    parameter OUT_WIDTH = 32
-) (
-   input wire                   logic [ IN_WIDTH-1:0] in,
-   output logic [OUT_WIDTH-1:0] out
-);
-  assign out = in[OUT_WIDTH-1:0];
-
-  `ifdef VERILATOR
-    always_comb begin
-      if (IN_WIDTH < OUT_WIDTH)
-        $error(
-          "std_slice: Input width less than output width\n",
-          "IN_WIDTH: %0d", IN_WIDTH,
-          "OUT_WIDTH: %0d", OUT_WIDTH
-        );
-    end
-  `endif
-endmodule
-
-module std_pad #(
-    parameter IN_WIDTH  = 32,
-    parameter OUT_WIDTH = 32
-) (
-   input wire logic [IN_WIDTH-1:0]  in,
-   output logic     [OUT_WIDTH-1:0] out
-);
-  localparam EXTEND = OUT_WIDTH - IN_WIDTH;
-  assign out = { {EXTEND {1'b0}}, in};
-
-  `ifdef VERILATOR
-    always_comb begin
-      if (IN_WIDTH > OUT_WIDTH)
-        $error(
-          "std_pad: Output width less than input width\n",
-          "IN_WIDTH: %0d", IN_WIDTH,
-          "OUT_WIDTH: %0d", OUT_WIDTH
-        );
-    end
-  `endif
-endmodule
-
-module std_lsh #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left << right;
-endmodule
-
-module std_rsh #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left >> right;
-endmodule
-
-module std_add #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left + right;
-endmodule
-
-module std_sub #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left - right;
-endmodule
-
-module std_not #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] in,
-   output logic [WIDTH-1:0] out
-);
-  assign out = ~in;
-endmodule
-
-module std_and #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left & right;
-endmodule
-
-module std_or #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left | right;
-endmodule
-
-module std_xor #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left ^ right;
-endmodule
-
-module std_gt #(
-    parameter WIDTH = 32
-) (
-   input wire   logic [WIDTH-1:0] left,
-   input wire   logic [WIDTH-1:0] right,
-   output logic out
-);
-  assign out = left > right;
-endmodule
-
-module std_lt #(
-    parameter WIDTH = 32
-) (
-   input wire   logic [WIDTH-1:0] left,
-   input wire   logic [WIDTH-1:0] right,
-   output logic out
-);
-  assign out = left < right;
-endmodule
-
-module std_eq #(
-    parameter WIDTH = 32
-) (
-   input wire   logic [WIDTH-1:0] left,
-   input wire   logic [WIDTH-1:0] right,
-   output logic out
-);
-  assign out = left == right;
-endmodule
-
-module std_neq #(
-    parameter WIDTH = 32
-) (
-   input wire   logic [WIDTH-1:0] left,
-   input wire   logic [WIDTH-1:0] right,
-   output logic out
-);
-  assign out = left != right;
-endmodule
-
-module std_ge #(
-    parameter WIDTH = 32
-) (
-    input wire   logic [WIDTH-1:0] left,
-    input wire   logic [WIDTH-1:0] right,
-    output logic out
-);
-  assign out = left >= right;
-endmodule
-
-module std_le #(
-    parameter WIDTH = 32
-) (
-   input wire   logic [WIDTH-1:0] left,
-   input wire   logic [WIDTH-1:0] right,
-   output logic out
-);
-  assign out = left <= right;
 endmodule
 
 `default_nettype wire
