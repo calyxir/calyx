9ff3b46f3e620401d10cf2ab16499ab3cdb4a171
cgyurgyik <gyurgyikcp@gmail.com>
2021-04-26 21:36:16 -0400

    black

--- primitives/interpret_p4_config.py
+++ primitives/interpret_p4_config.py
@@ -67,12 +67,11 @@ table_add <table_name> <action> <match>/<length> => <action data(s)>"""
 
     actions = []
     for line in file.readlines():
-        actions.append(
-            parse_action(line, unique_address)
-        )
+        actions.append(parse_action(line, unique_address))
         unique_address = unique_address + 1
 
     return actions
+
 
 def write_to_action_memory(action_memory_id: CompVar, actions: List[Action]):
     """Writes each action ID to an address in the Action Memory."""
@@ -84,25 +83,27 @@ def write_to_action_memory(action_memory_id: CompVar, actions: List[Action]):
         # Write `action_id` to `action_addr`.
         group_name = CompVar(f"wr_action_memory_{action_id}")
         connections = [
-            Connect(ConstantPort(32, action_id), CompPort(action_memory_id, "write_data")),
+            Connect(
+                ConstantPort(32, action_id), CompPort(action_memory_id, "write_data")
+            ),
             Connect(ConstantPort(5, action_addr), CompPort(action_memory_id, "addr0")),
             Connect(ConstantPort(1, 1), CompPort(action_memory_id, "write_en")),
             Connect(CompPort(action_memory_id, "done"), HolePort(group_name, "done")),
         ]
-        groups.append(
-            Group(group_name, connections, static_delay=1)
-        )
+        groups.append(Group(group_name, connections, static_delay=1))
 
     return groups, SeqComp([Enable(g.id.name) for g in groups])
 
+
 def ip_to_decimal(ip: str) -> int:
-    octet1 = 16777216 # 256 ** 3
-    octet2 = 65536 # 256 ** 2
+    octet1 = 16777216  # 256 ** 3
+    octet2 = 65536  # 256 ** 2
     octet3 = 256
     ips = ip.split(".", maxsplit=3)
     ips = [int(ip) for ip in ips]
 
     return ips[0] * octet1 + ips[1] * octet2 + ips[2] * octet3 + ips[3]
+
 
 def write_to_match_engine(match_engine_id: CompVar, actions: List[Action]):
     """Writes each IP (with prefix length) to the Match Engine."""
@@ -119,10 +120,13 @@ def write_to_match_engine(match_engine_id: CompVar, actions: List[Action]):
                 in_connects=[
                     ("write_en", ConstantPort(1, 1)),
                     ("write_index", ConstantPort(5, address)),
-                    ("in", ConstantPort(32, ip_to_decimal(ip), representation="binary")),
-                    ("prefix_len", ConstantPort(6, prefix_length))
+                    (
+                        "in",
+                        ConstantPort(32, ip_to_decimal(ip), representation="binary"),
+                    ),
+                    ("prefix_len", ConstantPort(6, prefix_length)),
                 ],
-                out_connects=[]
+                out_connects=[],
             )
         )
 
@@ -156,15 +160,13 @@ if __name__ == "__main__":
         outputs=[],
         structs=[
             Cell(action_memory_id, Stdlib().mem_d1(32, 32, 5)),
-            Cell(match_engine_id, CompInst("TCAM_IPv4",[]))
-        ] + groups,
-        controls=ParComp([control0, control1])
+            Cell(match_engine_id, CompInst("TCAM_IPv4", [])),
+        ]
+        + groups,
+        controls=ParComp([control0, control1]),
     )
-
 
     Program(
         imports=[Import("primitives/core.futil"), Import("primitives/tcam.futil")],
-        components=[
-            main
-        ]
+        components=[main],
     ).emit()
