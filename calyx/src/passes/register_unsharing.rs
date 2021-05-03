733f824ff506d6fd0e33760d19b60161fd98fd8f
Griffin Berlstein <griffin@berlste.in>
2021-05-03 13:11:58 -0400

    checkpoint for unsharing

--- calyx/src/passes/register_unsharing.rs
+++ calyx/src/passes/register_unsharing.rs
@@ -1,7 +1,8 @@
-use crate::ir::traversal::{Action, Named, VisResult, Visitor};
-use crate::ir::{self, LibrarySignatures};
-
 use crate::analysis::ReachingDefinitionAnalysis;
+use crate::ir::traversal::{Action, Named, VisResult, Visitor};
+use crate::ir::{self, Builder, LibrarySignatures};
+use crate::utils::NameGenerator;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Default)]
 pub struct RegisterUnsharing {}
@@ -16,21 +17,90 @@ impl Named for RegisterUnsharing {
     }
 }
 
+struct BookKeeper {
+    name_gen: NameGenerator,
+    analysis: ReachingDefinitionAnalysis,
+    widths: HashMap<ir::Id, u64>,
+}
+
+impl BookKeeper {
+    fn gen_name(&mut self, id: &ir::Id) -> ir::Id {
+        ir::Id::new(self.name_gen.gen_name(format!("unshr_{}", id)), None)
+    }
+
+    fn new(comp: &ir::Component) -> Self {
+        let widths = comp
+            .cells
+            .iter()
+            .filter_map(|c| {
+                if let ir::CellType::Primitive { name, .. } =
+                    &c.borrow().prototype
+                {
+                    if name == "std_reg" {
+                        if let Some(in_port) = c.borrow().find("in") {
+                            Some((
+                                c.borrow().name.clone(),
+                                in_port.borrow().width,
+                            ))
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect();
+        // (griffin) I'm sorry for the above.
+        // There's probably a cleaner way to write this
+        // TODO(griffin): fix?
+
+        let analysis =
+            ReachingDefinitionAnalysis::new(&comp, &comp.control.borrow());
+        let name_gen = NameGenerator::default();
+
+        Self {
+            name_gen,
+            analysis,
+            widths,
+        }
+    }
+
+    fn create_new_regs(&mut self, builder: &mut Builder) {
+        let overlap = self.analysis.calculate_overlap();
+
+        for (name, sets) in &overlap {
+            if sets.len() > 1 {
+                for defs in &sets[1..] {
+                    builder.add_primitive(
+                        self.gen_name(name),
+                        "std_reg",
+                        &[*self.widths.get(name).unwrap()],
+                    );
+                }
+            }
+        }
+    }
+}
+
 impl Visitor for RegisterUnsharing {
     fn start(
         &mut self,
         comp: &mut ir::Component,
         _c: &LibrarySignatures,
     ) -> VisResult {
-        let analysis =
-            ReachingDefinitionAnalysis::new(&comp, &comp.control.borrow());
+        let mut bookkeeper = BookKeeper::new(comp);
+        let mut builder = Builder::from(comp, _c, false);
 
-        for (group, z) in &analysis.reach {
+        bookkeeper.create_new_regs(&mut builder);
+        for (group, z) in &bookkeeper.analysis.reach {
             println!("Group {}", group);
             println!("  {:?}", z);
         }
 
-        for (x, y) in analysis.calculate_overlap() {
+        for (x, y) in &bookkeeper.analysis.calculate_overlap() {
             println!("Overlapping defns for {}", x);
             for def in y {
                 println!("   {:?}\n", def);
