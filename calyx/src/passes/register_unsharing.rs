01dac37b1095f480ec73265f8436e2551fea1e10
Griffin Berlstein <griffin@berlste.in>
2021-05-07 15:19:09 -0400

    rewriting logic

--- calyx/src/passes/register_unsharing.rs
+++ calyx/src/passes/register_unsharing.rs
@@ -1,6 +1,6 @@
 use crate::analysis::ReachingDefinitionAnalysis;
 use crate::ir::traversal::{Action, Named, VisResult, Visitor};
-use crate::ir::{self, Builder, LibrarySignatures};
+use crate::ir::{self, Builder, Cell, Group, LibrarySignatures, RRC};
 use crate::utils::NameGenerator;
 use std::collections::{HashMap, HashSet};
 
@@ -18,16 +18,13 @@ impl Named for RegisterUnsharing {
 }
 
 struct BookKeeper {
-    name_gen: NameGenerator,
     analysis: ReachingDefinitionAnalysis,
     widths: HashMap<ir::Id, u64>,
+    group_map: HashMap<ir::Id, RRC<Group>>,
+    cell_map: HashMap<ir::Id, RRC<Cell>>,
 }
 
 impl BookKeeper {
-    fn gen_name(&mut self, id: &ir::Id) -> ir::Id {
-        ir::Id::new(self.name_gen.gen_name(format!("unshr_{}", id)), None)
-    }
-
     fn new(comp: &ir::Component) -> Self {
         let widths = comp
             .cells
@@ -59,28 +56,95 @@ impl BookKeeper {
 
         let analysis =
             ReachingDefinitionAnalysis::new(&comp, &comp.control.borrow());
-        let name_gen = NameGenerator::default();
+        let group_map = HashMap::new();
+        let cell_map = HashMap::new();
 
         Self {
-            name_gen,
             analysis,
             widths,
+            group_map,
+            cell_map,
         }
     }
 
-    fn create_new_regs(&mut self, builder: &mut Builder) {
+    fn create_new_regs(
+        &mut self,
+        builder: &mut Builder,
+    ) -> Vec<(ir::Id, ir::Id, Vec<ir::Id>)> {
         let overlap = self.analysis.calculate_overlap();
+
+        let mut rename_list = vec![];
 
         for (name, sets) in &overlap {
             if sets.len() > 1 {
                 for defs in &sets[1..] {
-                    builder.add_primitive(
-                        self.gen_name(name),
-                        "std_reg",
-                        &[*self.widths.get(name).unwrap()],
-                    );
+                    let new_name = builder
+                        .add_primitive(
+                            format!("unshr_{}", name),
+                            "std_reg",
+                            &[*self.widths.get(name).unwrap()],
+                        )
+                        .borrow()
+                        .name
+                        .clone();
+                    rename_list.push((
+                        new_name.clone(),
+                        name.clone(),
+                        defs.iter()
+                            .map(|(_, groupname)| groupname.clone())
+                            .collect(),
+                    ));
                 }
             }
+        }
+        rename_list
+    }
+
+    fn get_cell(&mut self, builder: &Builder, cell_name: &ir::Id) -> RRC<Cell> {
+        if self.cell_map.contains_key(cell_name) {
+            self.cell_map.get(cell_name).unwrap().clone()
+        } else {
+            let cell = builder.component.find_cell(&cell_name.clone()).unwrap();
+            self.cell_map.insert(cell_name.clone(), cell.clone());
+            cell
+        }
+    }
+
+    fn get_group(
+        &mut self,
+        builder: &Builder,
+        group_name: &ir::Id,
+    ) -> RRC<Group> {
+        if self.group_map.contains_key(group_name) {
+            self.group_map.get(group_name).unwrap().clone()
+        } else {
+            let group =
+                builder.component.find_group(group_name).unwrap().clone();
+            self.group_map.insert(group_name.clone(), group.clone());
+            group
+        }
+    }
+
+    fn rename(
+        &mut self,
+        builder: &mut Builder,
+        rename_list: &[(ir::Id, ir::Id, Vec<ir::Id>)],
+    ) {
+        let mut grp_map: HashMap<&ir::Id, Vec<(RRC<Cell>, RRC<Cell>)>> =
+            HashMap::new();
+        for (new_name, old_name, grouplist) in rename_list {
+            for group in grouplist {
+                grp_map.entry(group).or_default().push((
+                    self.get_cell(builder, old_name),
+                    self.get_cell(builder, new_name),
+                ))
+            }
+        }
+
+        for (grp, rename_cells) in grp_map {
+            let group = self.get_group(builder, grp);
+            let mut group_ref = group.borrow_mut();
+            builder.rename_port_uses(&rename_cells, &mut group_ref.assignments)
         }
     }
 }
@@ -94,18 +158,20 @@ impl Visitor for RegisterUnsharing {
         let mut bookkeeper = BookKeeper::new(comp);
         let mut builder = Builder::from(comp, _c, false);
 
-        bookkeeper.create_new_regs(&mut builder);
-        for (group, z) in &bookkeeper.analysis.reach {
-            println!("Group {}", group);
-            println!("  {:?}", z);
-        }
+        let rename_list = bookkeeper.create_new_regs(&mut builder);
 
-        for (x, y) in &bookkeeper.analysis.calculate_overlap() {
-            println!("Overlapping defns for {}", x);
-            for def in y {
-                println!("   {:?}\n", def);
-            }
-        }
+        bookkeeper.rename(&mut builder, &rename_list);
+        // for (group, z) in &bookkeeper.analysis.reach {
+        //     println!("Group {}", group);
+        //     println!("  {:?}", z);
+        // }
+
+        // for (x, y) in &bookkeeper.analysis.calculate_overlap() {
+        //     println!("Overlapping defns for {}", x);
+        //     for def in y {
+        //         println!("   {:?}\n", def);
+        //     }
+        // }
         Ok(Action::Stop)
     }
 }
