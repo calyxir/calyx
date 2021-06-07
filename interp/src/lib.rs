a700b997f7fe66c7de80568b592bd2ea1feeb75a
Griffin Berlstein <griffin@berlste.in>
2021-06-07 11:18:53 -0400

    add test suite

--- interp/src/lib.rs
+++ interp/src/lib.rs
@@ -2,3 +2,4 @@ pub mod environment;
 pub mod interpreter;
 pub mod primitives;
 pub mod update;
+mod test;
---
+++ interp/src/test.rs
@@ -0,0 +1,10 @@
+#[cfg(test)]
+
+#[cfg(test)]
+mod sample_test {
+
+    #[test]
+    fn test1() {
+        assert!(true);
+    }
+}
