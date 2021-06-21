5055c8bb30164e42328eaf0112e518c64da911be
Alma Thaler <at564@cornell.edu>
2021-06-21 15:32:31 -0400

    getters

--- interp/src/stk_env.rs
+++ interp/src/stk_env.rs
@@ -2,10 +2,14 @@
 
 use super::{primitives, primitives::Primitive, values::Value};
 use calyx::ir;
+use std::cell::RefCell;
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::rc::Rc;
 
-struct Smoosher<K, V> {
-    pub ds: VecDeque<HashMap<K, V>>,
+//use push front and pop front and iterator is in right order then
+
+struct Smoosher<K: Eq + std::hash::Hash, V> {
+    pub ds: VecDeque<Rc<RefCell<HashMap<K, V>>>>,
     //note: piece of aux data? to keep track of fork index?
     //maybe a length to keep track of indecies in general
     //if two forks are related have to agree on same fork index
@@ -17,30 +21,41 @@ struct Smoosher<K, V> {
 
 //methods we will implement
 // new, get, set, clone, top, bottom, smoosh, diff
-impl<K, V> Smoosher<K, V> {
+impl<K: Eq + std::hash::Hash, V> Smoosher<K, V> {
     fn new(k: K, v: V) -> Smoosher<K, V> {
         let hm: HashMap<K, V> = HashMap::new();
-        let mut ds: VecDeque<HashMap<K, V>> = VecDeque::new();
-        ds.push_back(hm);
+        let rc_rc_hm: Rc<RefCell<HashMap<K, V>>> = Rc::new(RefCell::new(hm));
+        let mut ds: VecDeque<Rc<RefCell<HashMap<K, V>>>> = VecDeque::new();
+        ds.push_back(rc_rc_hm);
         Smoosher { ds }
     }
 
     ///get(k) returns an Option containing the most recent binding of k. As in, returns the value associated
     ///with k from the topmost HashMap that contains some key-value pair (k, v). If no HashMap exists with
     ///a key-value pair (k, v), returns None.
-    fn get(k: K) -> Option<&V> {
-        todo!()
+    fn get(&self, k: &K) -> Option<&V> {
+        for hm in self.ds.iter() {
+            if let Some(val) = &&hm.borrow().get(k) {
+                return Some(val);
+            }
+        }
+        None
     }
 
     ///forgot why we put this down
-    fn get_mut(k: K) -> Option<&mut V> {
-        todo!()
+    fn get_mut(&self, k: &K) -> Option<&mut V> {
+        for hm in self.ds.iter() {
+            if let Some(mut val) = &&hm.borrow().get(k) {
+                return Some(&mut *val);
+            }
+        }
+        None
     }
 
     ///set(k, v) mutates the current Smoosher, inserting the key-value pair (k, v) to the topmost HashMap of
     ///the Smoosher. Overwrites the existing (k, v') pair if one exists in the topmost HashMap at the time
     ///of the set(k, v) call.
-    fn set(k: K, v: V) {
+    fn set(&mut self, k: K, v: V) {
         todo!()
     }
 
@@ -81,7 +96,8 @@ impl<K, V> Smoosher<K, V> {
         todo!()
     }
 
-    ///didn't write this down :/
+    ///in order to set unmodified values to zero
+    ///
     fn diff(&self, top_i: u64, bottom_i: u64) -> Vec<(K, V)> {
         todo!()
     }
