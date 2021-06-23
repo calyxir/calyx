1614a1668b27d573eddfc216c97ba3ee4e3f7166
Alma Thaler <at564@cornell.edu>
2021-06-23 17:03:06 -0400

    planning smoosh, need to add RC iterator

--- interp/src/stk_env.rs
+++ interp/src/stk_env.rs
@@ -125,12 +125,11 @@ impl<K: Eq + std::hash::Hash, V> Smoosher<K, V> {
     //it. Invariant this method enforces: you cannot mutate a scope that has children
     //forks.
     fn fork(&mut self) -> Self {
-        //first save self's head
+        //first save self's head, and replace it with a clean HM
         let old_head = mem::replace(&mut self.head, HashMap::new());
         //create tail to both Self and the new Smoosher
         let new_tail = Rc::new(self.tail.push(old_head));
         //update Self and create new Smoosher
-        self.head = HashMap::new();
         self.tail = Rc::clone(&new_tail); //will this die after the end of [fork]?
         Smoosher {
             head: HashMap::new(),
@@ -138,16 +137,47 @@ impl<K: Eq + std::hash::Hash, V> Smoosher<K, V> {
         }
     }
 
+    //Pushes a new, empty scope onto [self]
     fn new_scope(&mut self) {
-        todo!()
+        let old_head = mem::replace(&mut self.head, HashMap::new());
+        let new_tail = Rc::new(self.tail.push(old_head));
+        self.tail = Rc::clone(&new_tail);
     }
 
-    fn top(&self) -> &Rc<RefCell<HashMap<K, V>>> {
-        todo!()
+    //Returns a pointer to the newest scope of this Smoosher
+    fn top(&self) -> &HashMap<K, V> {
+        &self.head
     }
 
     fn smoosh(&mut self, top_i: u64, bottom_i: u64) -> () {
         todo!()
+    }
+
+    /// Transposes the bindings from the topmost [levels] HMs onto the HM
+    /// [levels] deep down from the top. Calling self.Smoosh(0) has no effect,
+    /// calling self.Smoosh(1) will transpose the bindings from the topmost scope
+    /// onto the scope directly below it. [Levels] is how many scopes to smoosh
+    /// down. Calling [smoosh_lvl] with a [levels] greater than the number of
+    /// scopes has undefined behavior.
+    fn smoosh_lvl(&mut self, levels: u64) {
+        let upd_stk = List::<HashMap<K, V>>::new();
+        //if levels >= 1, add head + (levels-1) from the top
+        //other HMs to update_stack
+        if levels >= 1 {
+            let top = mem::replace(&mut self.head, HashMap::new());
+            upd_stk.push(top);
+            //now mutably borrow the other nodes and put them on upd_stk
+            let iter = self.tail.iter();
+            let mut count = levels;
+            for hm in iter {
+                if count > 0 {}
+                count -= 1;
+            }
+        }
+        //give a var name to target level
+
+        //then iterate thru the update_stack, borrow the HMs mutable (they're in RCs)
+        //do a lot of mem::replaces to move all bindings into target level
     }
 
     fn merge(&mut self, other: &mut Self) -> Self {
