c3354831e408eeed7d4c40c14971649a9fe2ca48
Alma Thaler <at564@cornell.edu>
2021-06-24 13:31:30 -0400

    Refactored Smoosher
    - The Tail is no longer an Rc of a list, but just a List, as the List is
    composed of Option<Rc<...>>s anyways
    
    - Smoosh is a consuming function
    
    - Added split method to List

--- interp/src/stk_env.rs
+++ interp/src/stk_env.rs
@@ -1,5 +1,4 @@
 //a stack of environments, to be used like a version tree
-
 //This version: linked list! from the rust book detailing how to do linked lists
 
 use super::{primitives, primitives::Primitive, values::Value};
@@ -8,14 +7,31 @@ use std::cell::RefCell;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::convert::TryInto;
 use std::mem;
-use std::rc::Rc; //got this from the "Learning Rust..." LL book
+use std::rc::Rc;
 
-//Invariant for use that makes implementing easier
-//(this version can be used unsafely):
-//After fork, a new_scope MUST be pushed. It's unsafe to continue modifying
-//the root of the fork.
+//Invariants:
+//=> To Smoosh X levels down, no fork may still exist from any node amongst those
+// X levels. So the stack must look like:
+//  _
+// |_|
+//  |
+//  _
+// |_|
+//  |
+//  _
+// |_|
+//  |
+//=> To merge, both branches must be X levels from their shared root
+//  _     _
+// |_|   |_|
+//  |     |
+//   \ _ /
+//    |*|
+//     |
+//
 
 // From "Learning Rust with Entirely Too Many Linked Lists" (2018), Chapter 4.5:
+#[derive(Default, Debug)]
 pub struct List<T> {
     head: Link<T>,
 }
@@ -24,16 +40,28 @@ type Link<T> = Option<Rc<Node<T>>>;
 
 //a way to unwrap and grab mutably?
 //
-
+#[derive(Debug)]
 struct Node<T> {
     //problem: node owns its element (? is this a problem)
     elem: T,
     next: Link<T>,
 }
 
+impl<T> Clone for List<T> {
+    fn clone(&self) -> Self {
+        List {
+            head: self.head.clone(),
+        }
+    }
+}
+
 impl<T> List<T> {
-    pub fn new() -> Self {
-        List { head: None }
+    pub fn is_empty(&self) -> bool {
+        if let Some(node) = &self.head {
+            false
+        } else {
+            true
+        }
     }
 
     //renamed from "append" to "push"
@@ -46,21 +74,25 @@ impl<T> List<T> {
         }
     }
 
-    //List w/o its head. Don't need it for this DS, but including it for practice
+    ///List w/o its head.
     pub fn tail(&self) -> List<T> {
         List {
             head: self.head.as_ref().and_then(|node| node.next.clone()),
         }
     }
 
-    //Hacky method that returns a mutable reference to what was the head of the list
-    //replaces head w/ that node's next
-    pub fn mut_head(&mut self) -> Option<&mut T> {
-        if let Some(ref mut node) = Rc::get_mut(&mut self.head.as_ref()) {
-            //problem area
-            return Some(&mut node.elem);
+    ///Hacky method that consumes the current list, returning
+    ///its head (if it exists), and a list with that head removed (or empty)
+    pub fn split(self) -> (Option<T>, Self) {
+        if self.is_empty() {
+            return (None, self);
         } else {
-            return None;
+            if let Ok(head) = Rc::try_unwrap(self.head.unwrap()) {
+                let tail_list = List { head: head.next }; //better: not cloning the tail
+                return (Some(head.elem), tail_list);
+            } else {
+                panic!("Cannot unwrap the head of this list. You probably tried Smooshing while a fork exists!")
+            }
         }
     }
 
@@ -92,21 +124,15 @@ impl<'a, T> Iterator for Iter<'a, T> {
 //
 
 //When a new scope is added, head is added to the tail and becomes immutable
+#[derive(Default, Debug)]
 struct Smoosher<K: Eq + std::hash::Hash, V> {
-    head: HashMap<K, V>,           //mutable
-    tail: Rc<List<HashMap<K, V>>>, //read-only
+    head: HashMap<K, V>,       //mutable
+    tail: List<HashMap<K, V>>, //read-only
 }
 
 impl<K: Eq + std::hash::Hash, V> Smoosher<K, V> {
-    fn new() -> Smoosher<K, V> {
-        Smoosher {
-            head: HashMap::new(),
-            tail: Rc::new(List::new()),
-        }
-    }
-
-    //Gets the highest-scoped binding containing [k], if it exists.
-    //else returns None.
+    ///Gets the highest-scoped binding containing [k], if it exists.
+    ///else returns None.
     fn get(&self, k: &K) -> Option<&V> {
         //first check if it's in the highest one
         if let Some(val) = self.head.get(k) {
@@ -123,79 +149,90 @@ impl<K: Eq + std::hash::Hash, V> Smoosher<K, V> {
         }
     }
 
-    //doesn't seem possible rn. if you want to edit the binding of a key in a previous
-    //scope... not sure how you could
+    ///doesn't seem possible rn. if you want to edit the binding of a key in a previous
+    ///scope... not sure how you could
     fn get_mut(&mut self, k: &K) -> Option<&mut V> {
         todo!()
     }
 
-    //Sets a new binding of [k] to [v] in the highest scope
+    ///Sets a new binding of [k] to [v] in the highest scope
     fn set(&mut self, k: K, v: V) {
         self.head.insert(k, v);
     }
 
-    //Returns a new Smoosher and mutates [self]. The new Smoosher has a new scope
-    //as [head] and all of [self] as [tail]. [Self] has a fresh scope pushed onto
-    //it. Invariant this method enforces: you cannot mutate a scope that has children
-    //forks.
+    ///Returns a new Smoosher and mutates [self]. The new Smoosher has a new scope
+    ///as [head] and all of [self] as [tail]. [Self] has a fresh scope pushed onto
+    ///it. Invariant this method enforces: you cannot mutate a scope that has children
+    ///forks.
     fn fork(&mut self) -> Self {
         //first save self's head, and replace it with a clean HM
-        let old_head = mem::replace(&mut self.head, HashMap::new());
-        //create tail to both Self and the new Smoosher
-        let new_tail = Rc::new(self.tail.push(old_head));
+        let old_head = mem::replace(&mut self.head, HashMap::new()); //can replace with mem::take()
+                                                                     //create tail to both Self and the new Smoosher
+        let new_tail = self.tail.push(old_head);
         //update Self and create new Smoosher
-        self.tail = Rc::clone(&new_tail); //will this die after the end of [fork]?
+        self.tail = new_tail.clone(); //will this die after the end of [fork]? no
         Smoosher {
             head: HashMap::new(),
-            tail: Rc::clone(&new_tail),
+            tail: new_tail,
         }
     }
 
-    //Pushes a new, empty scope onto [self]
+    ///Pushes a new, empty scope onto [self]
     fn new_scope(&mut self) {
         let old_head = mem::replace(&mut self.head, HashMap::new());
-        let new_tail = Rc::new(self.tail.push(old_head));
-        self.tail = Rc::clone(&new_tail);
+        self.tail = self.tail.push(old_head);
     }
 
-    //Returns a pointer to the newest scope of this Smoosher
+    //maybe not necessary VV
+
+    ///Returns a pointer to the newest scope of this Smoosher. A Smoosher upon
+    ///instantiation will always have 1 empty HM as its first scope, so this
+    ///method will always return.
     fn top(&self) -> &HashMap<K, V> {
         &self.head
     }
 
-    fn smoosh(&mut self, top_i: u64, bottom_i: u64) -> () {
-        todo!()
+    /// Returns a Smoosher
+    fn smoosh_once(self) -> Self {
+        //move head to a sep variable
+        let wr_head = self.head;
+        //now move the head of the tail into the head of the smoosher
+        let interm_tail = self.tail;
+        let (new_head, new_tail) = interm_tail.split();
+        if let Some(mut new_head) = new_head {
+            for (k, v) in wr_head {
+                new_head.insert(k, v);
+            }
+            return Smoosher {
+                head: new_head,
+                tail: new_tail,
+            };
+        } else {
+            panic!()
+        }
     }
 
     /// Transposes the bindings from the topmost [levels] HMs onto the HM
-    /// [levels] deep down from the top. Calling self.Smoosh(0) has no effect,
+    /// [levels] deep down from the top (topmost HM considered to have index 0).
+    /// Calling self.Smoosh(0) has no effect,
     /// calling self.Smoosh(1) will transpose the bindings from the topmost scope
     /// onto the scope directly below it. [Levels] is how many scopes to smoosh
     /// down. Calling [smoosh_lvl] with a [levels] greater than the number of
     /// scopes has undefined behavior.
-    fn smoosh_lvl(&mut self, levels: u64) {
-        let upd_stk = List::<HashMap<K, V>>::new();
-        //if levels >= 1, add head + (levels-1) from the top
-        //other HMs to update_stack
-        if levels >= 1 {
-            let top = mem::replace(&mut self.head, HashMap::new());
-            upd_stk.push(top);
-            //now mutably borrow the other nodes and put them on upd_stk
-            //how to mutably borrow the other nodes?
-            // let iter = self.tail.iter();
-            // let mut count = levels;
-            // for hm in iter {
-            //     if count > 0 {}
-            //     count -= 1;
-            // }
-        }
-        //give a var name to target level
-
-        //then iterate thru the update_stack, borrow the HMs mutable (they're in RCs)
-        //do a lot of mem::replaces to move all bindings into target level
+    /// Required: There exist no forks from any of the topmost [levels] scopes.
+    fn smoosh(self, levels: u64) -> Self {
+        let upd_stk = List::<HashMap<K, V>>::default();
+        todo!()
     }
 
-    fn merge(&mut self, other: &mut Self) -> Self {
+    ///works on a two branches w/ exactly one layer.
+    ///so both Smooshers have the same head of their [tail]
+    ///
+    fn merge_once(self, other: Self) -> Self {
+        todo!()
+    }
+
+    fn merge(self, other: Self) -> Self {
         todo!()
     }
 
