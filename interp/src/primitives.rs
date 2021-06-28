558e85c70d8fa47ce5acd00536dd9131ed58962c
Griffin Berlstein <griffin@berlste.in>
2021-06-28 13:50:28 -0400

    doc comments for the stateful changes

--- interp/src/primitives.rs
+++ interp/src/primitives.rs
@@ -442,8 +442,16 @@ pub trait ExecuteStateful: ValidateInput {
         self.reset(inputs)
     }
 
+    /// This function transfers the update held in a primitive's buffer into the
+    /// state contained within the primitive itself. Until this method is
+    /// invoked, the primitive's internal state will remain unchanged by
+    /// execution. This is to prevent ephemeral changes due to repeated
+    /// invocations
     fn commit_updates(&mut self);
 
+    /// Resets the primitive's update buffer without commiting the held changes,
+    /// effectively reverting the write and ensuring it does not occur. Use to
+    /// reset stateful primitives after a group execution.
     fn clear_update_buffer(&mut self);
 }
 
