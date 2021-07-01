dfed32ca2f03db74759644cd6ddaa61bfa188f3b
Griffin Berlstein <griffin@berlste.in>
2021-07-01 17:03:17 -0400

    fud name tweak

--- fud/fud/stages/interpreter.py
+++ fud/fud/stages/interpreter.py
@@ -140,7 +140,7 @@ def convert_to_json(output_dir, data, round_float_to_fixed):
                     raise error
 
         output_json[k] = list([convert(x) for x in arr.flatten()])
-    out_path = output_dir / "data.json"
+    out_path = output_dir / _FILE_NAME
 
     with out_path.open("w") as f:
         sjson.dump(output_json, f, indent=2, use_decimal=True)
