4051bff42b822b242dabe394860de74e0ddcc108
Griffin Berlstein <griffin@berlste.in>
2021-07-08 17:44:27 -0400

    fix obvious scaffolding errors

--- interp/src/interpreter/steppers/group_interpreter.rs
+++ interp/src/interpreter/steppers/group_interpreter.rs
@@ -1,25 +1,32 @@
-use crate::Environment;
+use crate::environment::InterpreterState;
+use calyx::ir::{Assignment, Port};
 
 pub struct GroupInterpeter {}
 
 impl GroupInterpeter {
+    fn new<'a, I>(env: InterpreterState, done_signal: &Port, assigns: I) -> Self
+    where
+        I: Iterator<Item = &'a Assignment>,
+    {
+        todo!()
+    }
     fn step_cycle(&mut self) {
-        todo();
+        todo!();
     }
     fn step_convergence(&mut self) {
-        todo();
+        todo!();
     }
     fn step_cycle_convergence(&mut self) {
-        todo();
+        todo!();
     }
 
-    fn run_group(&mut self);
+    fn run_group(&mut self) {}
 
     pub fn is_done(&self) -> bool {
         todo!()
     }
 
-    fn deconstruct(self) -> Environment {
+    fn deconstruct(self) -> InterpreterState {
         todo!()
     }
 }
---
+++ interp/src/interpreter/steppers/mod.rs
@@ -0,0 +1,3 @@
+mod component_interpreter;
+mod control_interpreter;
+mod group_interpreter;
