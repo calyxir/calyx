5efa34c5810435f934068e98451293a0f13e2bdc
Rachit Nigam <rachit.nigam12@gmail.com>
2021-07-10 14:59:21 +0530

    fix StdSub

--- interp/src/primitives/combinational.rs
+++ interp/src/primitives/combinational.rs
@@ -75,8 +75,13 @@ comb_primitive!(StdAdd[WIDTH](left: WIDTH, right: WIDTH) -> (out: WIDTH) {
 });
 comb_primitive!(StdSub[WIDTH](left: WIDTH, right: WIDTH) -> (out: WIDTH) {
   let left_64 = left.as_u64();
-  let right_64 = right.as_u64();
-  let init_val = left_64 - right_64;
+  // Bitwise subtraction: left - right = left + (!right + 1)
+  let right = Value {
+      vec: !(right.vec.clone()),
+  };
+  let right_64 = right.as_u64() + 1;
+  //need to allow overflow -- we are dealing only with bits
+  let init_val = left_64 + right_64;
   let bitwidth: usize = left.vec.len();
   Value::from_init(init_val, bitwidth).into()
 });
