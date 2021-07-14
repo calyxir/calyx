258f0d9058b17d03e89ff51fb8c7a33389950d46
Griffin Berlstein <griffin@berlste.in>
2021-07-14 10:24:03 -0400

    Continued sketching of EnableInterpreter

--- interp/src/interpreter/steppers/control_interpreter.rs
+++ interp/src/interpreter/steppers/control_interpreter.rs
@@ -1,17 +1,92 @@
+use super::super::simulation_utils::get_done_port;
 use super::AssignmentInterpreter;
+use crate::environment::InterpreterState;
 use calyx::ir::{self, Assignment, Control, Group};
 
-pub struct EmptyInterpreter {}
-pub struct EnableInterpreter {}
+pub trait Interpreter {
+    fn step(&mut self);
+
+    fn run(&mut self);
+
+    fn run_and_deconstruct(self) -> InterpreterState;
+}
+
+pub struct EmptyInterpreter<'a> {
+    env: InterpreterState,
+    _continuous: &'a [Assignment],
+    _empty: &'a ir::Empty,
+}
+
+impl<'a> Interpreter for EmptyInterpreter<'a> {
+    fn step(&mut self) {}
+
+    fn run(&mut self) {}
+
+    fn run_and_deconstruct(self) -> InterpreterState {
+        self.env
+    }
+}
+
+pub struct EnableInterpreter<'a> {
+    continuous: &'a [Assignment],
+    enable: &'a ir::Enable,
+    interp: AssignmentInterpreter<'a>,
+}
+
+impl<'a> EnableInterpreter<'a> {
+    pub fn new(
+        env: InterpreterState,
+        continuous: &'a [Assignment],
+        enable: &'a ir::Enable,
+    ) -> Self {
+        let grp_ref = enable.group.borrow();
+        let assigns =
+            grp_ref.assignments.iter().chain(continuous.iter()).cloned();
+        let done = get_done_port(&grp_ref);
+        let interp = AssignmentInterpreter::new_owned(
+            env,
+            &done.borrow() as &ir::Port as *const ir::Port,
+            assigns.collect(),
+        );
+        Self {
+            continuous,
+            enable,
+            interp,
+        }
+    }
+}
+
+impl<'a> EnableInterpreter<'a> {
+    fn reset(self) -> InterpreterState {
+        self.interp
+            .reset(self.enable.group.borrow().assignments.iter())
+    }
+}
+
+impl<'a> Interpreter for EnableInterpreter<'a> {
+    fn step(&mut self) {
+        self.interp.step();
+    }
+
+    fn run(&mut self) {
+        self.interp.run();
+    }
+
+    fn run_and_deconstruct(mut self) -> InterpreterState {
+        self.interp.run();
+        self.reset()
+    }
+}
+
 pub struct SeqInterpreter {}
 pub struct ParInterpreter {}
 pub struct IfInterpreter {}
 pub struct WhileInterpreter {}
 pub struct InvokeInterpreter {}
 
-pub enum ControlInterpreter {
-    Empty(EnableInterpreter),
-    Enable(EnableInterpreter),
+pub enum ControlInterpreter<'a> {
+    Empty(EnableInterpreter<'a>),
+    Enable(EnableInterpreter<'a>),
     Seq(SeqInterpreter),
     Par(ParInterpreter),
     If(IfInterpreter),
