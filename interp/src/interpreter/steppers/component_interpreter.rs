ecc554221adbb73ffe753f807655fbbf88abd6c0
Griffin Berlstein <griffin@berlste.in>
2021-07-20 11:38:56 -0400

    remove unused fields from the struct

--- interp/src/interpreter/steppers/component_interpreter.rs
+++ interp/src/interpreter/steppers/component_interpreter.rs
@@ -3,12 +3,7 @@ use super::control_interpreter::{
     ControlInterpreter, Interpreter, StructuralInterpreter,
 };
 use crate::environment::InterpreterState;
-use crate::values::Value;
-use std::collections::HashMap;
-use std::ops::{Deref, DerefMut};
-
 use calyx::ir::{self, Component};
-use std::cell::Ref;
 
 enum StructuralOrControl<'a> {
     Structural(StructuralInterpreter<'a>),
@@ -28,60 +23,29 @@ impl<'a> From<ControlInterpreter<'a>> for StructuralOrControl<'a> {
 }
 
 pub struct ComponentInterpreter<'a> {
-    // in_ports: HashMap<ir::Id, Value>,
-    comp: &'a Component,
-    control: Ref<'a, ir::Control>, // keep a ref to the control struct alive
     interp: StructuralOrControl<'a>,
 }
 
 impl<'a> ComponentInterpreter<'a> {
-    pub fn from_component(comp: &'a Component, env: InterpreterState) -> Self {
+    pub fn from_component(
+        comp: &'a Component,
+        control: &'a ir::Control,
+        env: InterpreterState,
+    ) -> Self {
         let interp;
-        let control = comp.control.borrow();
-
-        // SAFETY REQUIREMENTS
-        // (https://doc.rust-lang.org/std/primitive.pointer.html#method.as_ref)
-        // 1. The pointer is properly aligned.
-        //
-        // This is covered (I hope) because we are getting this pointer from the
-        // refcell.
-        //
-        // 2. The pointer must be "dereferenceable"
-        //
-        //    Same as the previous reason
-        //
-        // 3. The pointer must point to an initialized instance of T
-        //
-        //    Covered as before
-        //
-        //
-        // 4. You must enforce aliasing rules. In particular, for the duration
-        //    of this lifetime, the memory pointed to must not get mutated.
-        //
-        //    The Ref object which is used to get this reference is kept by the
-        //    component interpreter and as a result it is not possible to obtain
-        //    a mutable reference to the data, nor is it possible for the data
-        //    to be deallocated while the component interpreter is alive. When
-        //    the component interpreter drops, then this reference will
-        //    disappear as well
-        let ctrl_ptr: &ir::Control = unsafe { &*comp.control.as_ptr() };
 
         if control_is_empty(&control) {
             interp = StructuralInterpreter::from_component(comp, env).into();
         } else {
             interp = ControlInterpreter::new(
-                &ctrl_ptr,
+                control,
                 env,
                 &comp.continuous_assignments,
             )
             .into()
         };
 
-        Self {
-            comp,
-            control,
-            interp,
-        }
+        Self { interp }
     }
 }
 
