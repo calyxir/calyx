4ee1a79ce22d34894615723729ba42aaaf68afc1
yn224 <yn224@cornell.edu>
2021-07-30 05:19:25 -0400

    [Interpreter] Panic message improvement attempt

--- interp/src/utils.rs
+++ interp/src/utils.rs
@@ -47,6 +47,36 @@ impl From<&RRC<Port>> for PortRef {
     }
 }
 
+pub(super) struct PortAssignment<'a>(*const Port, &'a Assignment);
+
+impl<'a> Hash for PortAssignment<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
+impl<'a> PartialEq for PortAssignment<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self.0, other.0)
+    }
+}
+
+impl<'a> Eq for PortAssignment<'a> {}
+
+impl<'a, 'b> PortAssignment<'a> {
+    pub fn new(p_ref: &'b Port, a_ref: &'a Assignment) -> Self {
+        Self(p_ref as *const Port, a_ref)
+    }
+
+    pub fn get_port(&self) -> &Port {
+        unsafe { self.0.as_ref().unwrap() }
+    }
+
+    pub fn get_assignment(&self) -> &Assignment {
+        &self.1
+    }
+}
+
 #[derive(Debug, Clone)]
 pub(super) struct AssignmentRef<'a>(&'a Assignment);
 
