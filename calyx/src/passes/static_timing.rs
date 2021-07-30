024997b6d16db681a2767b8ccdb33a3e8abee221
Griffin Berlstein <griffin@berlste.in>
2021-07-30 07:00:01 -0400

    Patch for the static timing bug (#611)
    * patch for the static timing bug
    
    * Small error in wellformed check
    
    * fsm_incr
    
    * add simple test case
    
    * update expect files

--- calyx/src/passes/static_timing.rs
+++ calyx/src/passes/static_timing.rs
@@ -104,7 +104,8 @@ impl Visitor for StaticTiming {
                 let body_done =
                     guard!(fsm["out"]).eq(guard!(body_end_const["out"]));
                 // Should we increment the FSM this cycle.
-                let fsm_incr = !body_done.clone();
+                let fsm_incr = !body_done.clone()
+                    & (guard!(cond_stored["out"]) | cond_computed.clone());
 
                 // Compute the cond group
                 let cond_go =
