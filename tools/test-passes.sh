64a115490a82fd3ab22e7293cadbde1942e55b7d
yn224 <yn224@cornell.edu>
2021-07-31 20:42:03 -0400

    [Tools] Error fix and documentation update

--- tools/test-passes.sh
+++ tools/test-passes.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Takes in two arguments
-# - First argument: relative path to the test file
-# - Second argument: pattern of interest
+# Script to run lowered Calyx program and detect conflicting assignment with
+# known pattern after applying different passes.
+# Usage:
+#   ./flag-compare.sh <calyx program> <pattern>
+
+path=$1
+pattern=$2
 
 passes=(
   "well-formed" "papercut" "guard-canonical" "infer-static-timing" "collapse-control" "resource-sharing"
@@ -9,26 +13,25 @@ passes=(
   "go-insertion" "component-interface-inserter" "hole-inliner" "clk-insertion" "reset-insertion"
   "merge-assign"
 )
+# Other passes:
 # "compile-control" "externalize" "simplify-guards" "synthesis-papercut" "register-unsharing" "par-to-seq"
 len=${#passes[@]}
 
 rm -rf pass_seq
 mkdir pass_seq
 flag=""
-for (( i = 0; i < $len; i++ )); do
+for (( i = 0; i < len; i++ )); do
   pass="-p ${passes[i]}"
   flag+=" $pass"
-  fud e $1 -s futil.flags "$flag" --to futil-lowered > "pass_seq/$i-${passes[i]}.futil"
+  fud e "$path" -s futil.flags "$flag" --to futil-lowered > "pass_seq/$i-${passes[i]}.futil"
 done
-
-pattern=$2
 
 echo "======================================Result======================================"
 for file in pass_seq/*; do
   if [ -f "$file" ]; then
-    name=$(echo $file | cut -d "/" -f 2)
-    echo $name
-    grep -n "$pattern" $file
+    name=$(echo "$file" | cut -d "/" -f 2)
+    echo "$name"
+    grep -n "$pattern" "$file"
     echo ""
   fi
 done
