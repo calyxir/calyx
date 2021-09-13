e51c43812cc2c4da3238fc6ae7df35a1282f8a93
Adrian Sampson <adrian@radbox.org>
2021-09-12 22:06:09 -0400

    Fetch git hash straight from this repo
    No need to parse Cargo.lock anymore.

--- web/rust/build.rs
+++ web/rust/build.rs
@@ -1,15 +1,13 @@
-use cargo_lock::Lockfile;
 use std::fs::File;
 use std::io::Write;
+use std::process::Command;
 
+// Based on: https://stackoverflow.com/a/44407625/39182
 fn main() {
-    let lockfile = Lockfile::load("Cargo.lock").unwrap();
-    let package: cargo_lock::Package = lockfile
-        .packages
-        .into_iter()
-        .find(|pkg| pkg.name.as_str() == "calyx")
-        .unwrap();
-    let git_hash = format!("{}", package.source.unwrap().precise().unwrap());
+    let out_data = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
+    let out_str = String::from_utf8(out_data.stdout).unwrap();
+    let git_hash = out_str.trim();
+
     let json = format!("{{ \"version\": \"{}\" }}", git_hash);
 
     let mut file = File::create("calyx_hash.json").unwrap();
