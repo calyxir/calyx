140935afc7e0474d435b198bdc41639ffe7f1801
Adrian Sampson <asampson@cs.washington.edu>
2021-10-02 12:46:43 -0400

    Revive the web demo, in-tree (#683)
    * Add snapshot of futil-site
    
    From 1509326e54f58267fef32cde25a3e03405d2655d in cucapra/futil-site.
    
    * Remove unused make stuff
    
    * Hook up web crate correctly
    
    * Fetch git hash straight from this repo
    
    No need to parse Cargo.lock anymore.
    
    * Update pass manager invocation
    
    * Fresher crate name & wasm-bindgen
    
    * Remove unnecessary (?) build step
    
    I'm not sure why this was here in the first place, so this may break
    something...
    
    * Ignore more stuff
    
    * Pass list update
    
    * Spruce up web demo's README
    
    * Used distributed Prism theme
    
    Instead of checking in the code here, we can get it directly from the
    prismjs distribution in node_modules.
    
    * Fill out more package.json stuff
    
    * Restore all-important tdcc pass
    
    * cargo fmt
    
    * use frontend::Workspace instead of NamespaceDef
    
    Co-authored-by: Rachit Nigam <rachit.nigam12@gmail.com>

---
+++ web/rust/src/lib.rs
@@ -0,0 +1,57 @@
+use calyx::{
+    errors::{self, CalyxResult},
+    frontend, ir,
+    pass_manager::PassManager,
+};
+use wasm_bindgen::prelude::*;
+
+// Contruct a workspace from a namspace
+fn ws_from_ns(ns: frontend::NamespaceDef) -> CalyxResult<frontend::Workspace> {
+    if !ns.imports.is_empty() {
+        return Err(errors::Error::Misc(
+            "import not supported in the web demo".to_string(),
+        ));
+    }
+    let mut ws = frontend::Workspace::default();
+    ws.externs
+        .extend(ns.externs.into_iter().map(|(p, es)| (p.into(), es)));
+
+    // Add components defined by this namespace to either components or
+    // declarations
+    ws.components.extend(&mut ns.components.into_iter());
+
+    Ok(ws)
+}
+
+fn compile(
+    passes: &[String],
+    library: &str,
+    namespace: &str,
+) -> Result<String, errors::Error> {
+    let pm = PassManager::default_passes()?;
+
+    let ns = frontend::parser::CalyxParser::parse(
+        (library.to_string() + "\n" + namespace).as_bytes(),
+    )?;
+    let ws = ws_from_ns(ns)?;
+
+    // Build the IR representation
+    let mut rep = ir::from_ast::ast_to_ir(ws, false, false)?;
+
+    pm.execute_plan(&mut rep, passes, &[])?;
+
+    let mut buffer: Vec<u8> = vec![];
+    for comp in &rep.components {
+        ir::IRPrinter::write_component(comp, &mut buffer)?;
+    }
+    Ok(String::from_utf8(buffer).unwrap())
+}
+
+#[wasm_bindgen]
+pub fn run(passes: &JsValue, library: &str, namespace: &str) -> String {
+    let test: Vec<String> = passes.into_serde().unwrap();
+    match compile(&test, library, namespace) {
+        Ok(s) => s,
+        Err(e) => format!("Error:\n{:?}", e),
+    }
+}
