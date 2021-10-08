4ca09f59a338bfa17804bfafc8b09f83e8ec8649
Griffin Berlstein <griffin@berlste.in>
2021-10-08 13:12:59 -0400

    add some documentation for the iir module

--- interp/src/interpreter_ir/mod.rs
+++ interp/src/interpreter_ir/mod.rs
@@ -1,3 +1,13 @@
+//! A read-only variant of the IR used for more ergonomic interpretation.
+//!
+//! This module exposes alternate definitions for the core Calyx IR structures. The
+//! primary reason for this duplications is that the interpreter needs to be able to
+//! take apart control structures in a modular way without destroying the actual
+//! structure. These variants allow subtrees of the control structure to be held without
+//! causing lifetime issues with other components of the interpreter. As an auxillary
+//! effect this enables (relatively) cheap cloning for control structures as they are
+//! only needed in a read-only capacity.
+
 mod component;
 mod control;
 
