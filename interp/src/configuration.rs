ffce2fa8466c87163a42efe90a01b6c07afbdcb5
Griffin Berlstein <griffin@berlste.in>
2021-10-22 13:55:45 -0400

    minor tweak

--- interp/src/configuration.rs
+++ interp/src/configuration.rs
@@ -5,10 +5,10 @@ lazy_static! {
     pub static ref SETTINGS: RwLock<Config> = RwLock::new(Config::default());
 }
 pub struct Config {
-    // enables/disables "sloppy" interpretation which returns 0 for invalid indicies
-    // rather than erroring
+    /// enables/disables "sloppy" interpretation which returns 0 for invalid indicies
+    /// rather than erroring
     pub allow_invalid_memory_access: bool,
-    // upgrades overflow/underflow warnings into errors
+    /// upgrades overflow/underflow warnings into errors
     pub error_on_overflow: bool,
 }
 impl Default for Config {
