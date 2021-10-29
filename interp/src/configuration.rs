f9db71f3f9ff948a6cc83b21713f3af7b5f0bf32
Griffin Berlstein <griffin@berlste.in>
2021-10-29 11:25:41 -0400

    [Interpreter] Logging, Arbitrary Precision, and Configuration (#758)
    * sketch new print
    
    * sketch of hierarch printing
    
    * fix hierarchical printing bug
    
    * relay fix
    
    * fix printing bug for 4d mems
    
    * fix convergence bug
    
    * add missing print messages
    
    * basic warning message scaffolding
    
    * remove the unused import
    
    * change the definitions
    
    * fix all the stateful prims
    
    * fix comb defs
    
    * push errors into the core interpretation loop
    
    Also added a configuration var
    
    * sloppy memory mode for all the components
    
    * fix printing bug
    
    * div bug wrangling
    
    * minor tweak
    
    * messing with some precision stuff
    
    * checkpoint
    
    * remove old value trait
    
    * stub out overflow message
    
    * updating computation still
    
    * add to div
    
    * fp mult
    
    * ensure interpreter uses the proper entrypoint
    
    * fix doc test
    
    * fix calls
    
    * use new constructor
    
    * documentation

---
+++ interp/src/configuration.rs
@@ -0,0 +1,24 @@
+use lazy_static::*;
+use std::sync::RwLock;
+
+lazy_static! {
+    /// Global configuration object which stores the current configuration options for
+    /// simulation and debugging. It is behind a RW lock but is largely meant to be
+    /// read-only after simulation has begun.
+    pub static ref SETTINGS: RwLock<Config> = RwLock::new(Config::default());
+}
+pub struct Config {
+    /// enables/disables "sloppy" interpretation which returns 0 for invalid indicies
+    /// rather than erroring
+    pub allow_invalid_memory_access: bool,
+    /// upgrades overflow/underflow warnings into errors
+    pub error_on_overflow: bool,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            allow_invalid_memory_access: false,
+            error_on_overflow: false,
+        }
+    }
+}
