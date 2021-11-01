65252e6e5a3e0b20f36bd4ccd2d7c60081740111
Griffin Berlstein <griffin@berlste.in>
2021-11-01 15:49:39 -0400

    more new name structures as methods

--- interp/src/structures/names.rs
+++ interp/src/structures/names.rs
@@ -1,5 +1,6 @@
 use crate::interpreter_ir as iir;
 use calyx::ir::Id;
+use std::ops::Deref;
 use std::rc::Rc;
 
 #[derive(Debug, Clone)]
@@ -20,10 +21,42 @@ impl InstanceName {
     }
 }
 
+pub struct ComponentQIN(Vec<InstanceName>);
+
+impl Deref for ComponentQIN {
+    type Target = Vec<InstanceName>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl ComponentQIN {
+    pub fn new_extend(&self, inst: InstanceName) -> Self {
+        let mut inner = self.0.clone();
+        inner.push(inst);
+        Self(inner)
+    }
+
+    pub fn new_single(
+        component_id: &Rc<iir::Component>,
+        instance: &Id,
+    ) -> Self {
+        let inst = InstanceName::new(component_id, instance);
+        Self::from(inst)
+    }
+}
+
+impl<T: Into<InstanceName>> From<T> for ComponentQIN {
+    fn from(input: T) -> Self {
+        let inst: InstanceName = input.into();
+        Self(vec![inst])
+    }
+}
 /// The fully-qualified instance name of some calyx entity
 pub struct QualifiedInstanceName {
     /// The instance names of the ancestors in the state tree
-    prefix: Vec<InstanceName>,
+    prefix: ComponentQIN,
     /// Cell name
     name: Id,
 }
@@ -45,7 +78,7 @@ impl From<QualifiedInstanceName> for QualifiedName {
         Self {
             prefix: qin
                 .prefix
-                .into_iter()
+                .iter()
                 .map(|x| x.component_id.name.clone())
                 .collect(),
             name: qin.name,
