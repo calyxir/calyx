c92ae752e3efd62d7f3e0795619f0538d195f5e3
cgyurgyik <gyurgyikcp@gmail.com>
2021-11-01 19:30:47 -0700

    Use reset everywhere.

--- primitives/binary_operators.sv
+++ primitives/binary_operators.sv
@@ -34,31 +34,32 @@ module std_fp_mult_pipe #(
     input  logic [WIDTH-1:0] right,
     input  logic             go,
     input  logic             clk,
+    input  logic             reset,
     output logic [WIDTH-1:0] out,
     output logic             done
 );
   logic [WIDTH-1:0]          rtmp;
   logic [WIDTH-1:0]          ltmp;
   logic [(WIDTH << 1) - 1:0] out_tmp;
+  // Buffer used to walk through the 3 cycles of the pipeline.
   logic done_buf[2:0];
 
-  // Done signal connected to the final position of the done_buf
   assign done = done_buf[2];
 
   // If the done buffer is completely empty and go is high then execution
   // just started.
-  logic is_started;
-  assign is_started = go == 1 & done_buf[0] == 0 & done_buf[1] == 0 & done_buf[0] == 0;
+  logic start;
+  assign start = go & done_buf[0] == 0 & done_buf[1] == 0;
 
   // Start sending the done signal.
   always_ff @(posedge clk) begin
-    if (is_started)
+    if (start)
       done_buf[0] <= 1;
     else
       done_buf[0] <= 0;
   end
 
-  // Push the done signal through the pipeline
+  // Push the done signal through the pipeline.
   always_ff @(posedge clk) begin
     if (go) begin
       done_buf[2] <= done_buf[1];
@@ -69,8 +70,7 @@ module std_fp_mult_pipe #(
     end
   end
 
-  // Output is latched when go is low and otherwise either gets value from
-  // out_tmp;
+  // Output is latched when go is low and otherwise from out_tmp.
   always_ff @(posedge clk) begin
     if (go)
       out <= out_tmp[(WIDTH << 1) - INT_WIDTH - 1 : WIDTH - INT_WIDTH];
@@ -109,6 +109,7 @@ module std_fp_div_pipe #(
 ) (
     input  logic             go,
     input  logic             clk,
+    input  logic             reset,
     input  logic [WIDTH-1:0] left,
     input  logic [WIDTH-1:0] right,
     output logic [WIDTH-1:0] out_remainder,
@@ -123,8 +124,17 @@ module std_fp_div_pipe #(
     logic start, running, finished, invalid_divisor;
 
     assign start = go && !running;
-    assign finished = running && (idx == ITERATIONS - 1);
     assign invalid_divisor = start && left == 0;
+    assign finished = idx == ITERATIONS - 1 && running;
+
+    always_ff @(posedge clk) begin
+      if (reset || finished || invalid_divisor)
+        running <= 0;
+      else if (start)
+        running <= 1;
+      else
+        running <= running;
+    end
 
     always_comb begin
       if (acc >= {1'b0, right}) begin
@@ -144,24 +154,12 @@ module std_fp_div_pipe #(
         done <= 0;
     end
 
-    // `running` signal
-    always @(posedge clk) begin
-      if (!go | finished | invalid_divisor)
-        running <= 0;
-      else if (start)
-        running <= 1;
-      else
-        running <= running;
-    end
-
     // Index increment
     always_ff @(posedge clk) begin
       if (start)
         idx <= 0;
-      else if (running)
-        idx <= idx + 1;
       else
-        idx <= 0;
+        idx <= idx + 1;
     end
 
     always_ff @(posedge clk) begin
@@ -169,10 +167,6 @@ module std_fp_div_pipe #(
         // Return zero if the divisor is zero.
         out_remainder <= 0;
         out_quotient <= 0;
-      end else if (!go) begin
-        // Latch outputs when not executing.
-        out_remainder <= out_remainder;
-        out_quotient <= out_quotient;
       end else if (start) begin
         out_quotient <= 0;
         out_remainder <= left;
@@ -241,11 +235,12 @@ module std_fp_smult_pipe #(
     parameter INT_WIDTH = 16,
     parameter FRAC_WIDTH = 16
 ) (
-    input  [WIDTH-1:0] left,
-    input  [WIDTH-1:0] right,
+    input  [WIDTH-1:0]              left,
+    input  [WIDTH-1:0]              right,
+    input  logic                    reset,
     input  logic                    go,
     input  logic                    clk,
-    output logic [WIDTH-1:0] out,
+    output logic [WIDTH-1:0]        out,
     output logic                    done
 );
   std_fp_mult_pipe #(
@@ -256,6 +251,7 @@ module std_fp_smult_pipe #(
   ) comp (
     .clk(clk),
     .done(done),
+    .reset(reset),
     .go(go),
     .left(left),
     .right(right),
@@ -270,6 +266,7 @@ module std_fp_sdiv_pipe #(
 ) (
     input                     clk,
     input                     go,
+    input                     reset,
     input  signed [WIDTH-1:0] left,
     input  signed [WIDTH-1:0] right,
     output signed [WIDTH-1:0] out_quotient,
@@ -314,6 +311,7 @@ module std_fp_sdiv_pipe #(
     .INT_WIDTH(INT_WIDTH),
     .FRAC_WIDTH(FRAC_WIDTH)
   ) comp (
+    .reset(reset),
     .clk(clk),
     .done(done),
     .go(go),
@@ -354,6 +352,7 @@ module std_mult_pipe #(
 ) (
     input  logic [WIDTH-1:0] left,
     input  logic [WIDTH-1:0] right,
+    input  logic             reset,
     input  logic             go,
     input  logic             clk,
     output logic [WIDTH-1:0] out,
@@ -365,6 +364,7 @@ module std_mult_pipe #(
     .FRAC_WIDTH(0),
     .SIGNED(0)
   ) comp (
+    .reset(reset),
     .clk(clk),
     .done(done),
     .go(go),
@@ -377,6 +377,7 @@ endmodule
 module std_div_pipe #(
     parameter WIDTH = 32
 ) (
+    input                    reset,
     input                    clk,
     input                    go,
     input        [WIDTH-1:0] left,
@@ -395,14 +396,7 @@ module std_div_pipe #(
   assign start = go && !running;
   assign finished = !quotient_msk && running;
   assign div_by_zero = start && left == 0;
-
-  always_ff @(posedge clk) begin
-    // Early return if the divisor is zero
-    if (finished | div_by_zero)
-      done <= 1;
-    else
-      done <= 0;
-  end
+  assign done = finished || div_by_zero;
 
   always_ff @(posedge clk) begin
     if (finished | div_by_zero | !go)
@@ -444,9 +438,7 @@ module std_div_pipe #(
 
   // Calculating quotient
   always_ff @(posedge clk) begin
-    if (start)
-      quotient <= 0;
-    else if (finished)
+    if (start || finished)
       quotient <= 0;
     else if (divisor <= dividend)
       quotient  <= quotient | quotient_msk;
@@ -523,6 +515,7 @@ endmodule
 module std_smult_pipe #(
     parameter WIDTH = 32
 ) (
+    input  logic                    reset,
     input  logic                    go,
     input  logic                    clk,
     input  signed       [WIDTH-1:0] left,
@@ -536,6 +529,7 @@ module std_smult_pipe #(
     .FRAC_WIDTH(0),
     .SIGNED(1)
   ) comp (
+    .reset(reset),
     .clk(clk),
     .done(done),
     .go(go),
@@ -549,6 +543,7 @@ endmodule
 module std_sdiv_pipe #(
     parameter WIDTH = 32
 ) (
+    input                           reset,
     input                           clk,
     input                           go,
     input  logic signed [WIDTH-1:0] left,
@@ -593,6 +588,7 @@ module std_sdiv_pipe #(
   std_div_pipe #(
     .WIDTH(WIDTH)
   ) comp (
+    .reset(reset),
     .clk(clk),
     .done(done),
     .go(go),
