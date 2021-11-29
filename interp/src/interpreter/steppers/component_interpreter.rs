756361e4f8f9d3fba29d9960569bad1a809cc4c4
Griffin Berlstein <griffin@berlste.in>
2021-11-29 14:13:14 -0500

    doc comment

--- interp/src/interpreter/steppers/component_interpreter.rs
+++ interp/src/interpreter/steppers/component_interpreter.rs
@@ -59,7 +59,8 @@ pub struct ComponentInterpreter {
     go_port: RRC<Port>,
     input_hash_set: Rc<HashSet<*const ir::Port>>,
     qual_name: ComponentQIN,
-    full_name_clone: ir::Id, // used to satisfy the Named requirement for primitives
+    /// used to satisfy the Named requirement for primitives, primarially for error messages
+    full_name_clone: ir::Id,
 }
 
 impl ComponentInterpreter {
