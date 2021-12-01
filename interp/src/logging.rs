7d819508077fddb14ce0f1252cb160231d2d1b79
Griffin Berlstein <griffin@berlste.in>
2021-12-01 17:10:52 -0500

    documentation

--- interp/src/logging.rs
+++ interp/src/logging.rs
@@ -7,7 +7,9 @@ pub(crate) use slog::{debug, error, info, o, trace, warn, Logger};
 use slog::{Drain, Level};
 
 lazy_static! {
-    /// Global root logger. Note should be initialized after SETTINGS
+    /// Global root logger. Note should be initialized after SETTINGS to ensure
+    /// warning suppression, if desired. Directly using the root logger is not
+    /// recommended unless a sublogger is unavailable (see [new_sublogger]).
     pub static ref ROOT_LOGGER: Logger = {
         let decorator = slog_term::TermDecorator::new().stderr().build();
         let drain = slog_term::FullFormat::new(decorator).build();
@@ -24,6 +26,9 @@ lazy_static! {
     };
 }
 
+/// Utility method for creating subloggers for components/primitives/etc. This
+/// is the prefered method for getting a logger. Initializes the source key with
+/// the supplied name.
 pub fn new_sublogger<S: AsRef<str>>(source_name: S) -> Logger {
     ROOT_LOGGER.new(o!("source" => String::from(source_name.as_ref())))
 }
