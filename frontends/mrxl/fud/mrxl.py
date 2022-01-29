0ddaa4d980056a56b43909318a498674b17e59c4
Rachit Nigam <rachit.nigam12@gmail.com>
2022-01-28 23:41:02 -0500

    more comments in the example mrxl fud stage

--- frontends/mrxl/fud/mrxl.py
+++ frontends/mrxl/fud/mrxl.py
@@ -10,6 +10,10 @@ class MrXLStage(Stage):
     name = "mrxl"
 
     def __init__(self):
+        """
+        Initialize this stage. Initializing a stage *does not* construct its
+        computation graph.
+        """
         super().__init__(
             src_state="mrxl",
             target_state="futil",
@@ -20,15 +24,31 @@ class MrXLStage(Stage):
 
     @staticmethod
     def defaults():
+        """
+        Specify defaults that should be added to fud's configuration file when
+        this stage is registered.
+        """
         return {"exec": "mrxl"}
 
     def _define_steps(self, builder, config):
+        """
+        Define the steps that will execute in this stage. Each step represents
+        a delayed computation that will occur when the stage is executed.
+        """
+
+        # Commands at the top-level are evaluated when the computation is being
+        # staged
         cmd = config["stages", self.name, "exec"]
 
+        # Computations within a step are delayed from being executed until
+        # the full execution pipeline is generated.
         @builder.step(description=cmd)
         def run_mrxl(mrxl_prog: SourceType.Path) -> SourceType.Stream:
             return shell(f"{cmd} {str(mrxl_prog)}")
 
+        # Define a schedule using the steps.
+        # A schedule *looks* like an imperative program but actually represents
+        # a computation graph that is executed later on.
         return run_mrxl(builder.input())
 
 
