cb069dce06338d89cedbfde5a3bf47eb1da3c5a0
Rachit Nigam <rachit.nigam12@gmail.com>
2022-02-14 13:37:15 -0500

    and_then for chained comparison

--- calyx/src/passes/top_down_static_timing.rs
+++ calyx/src/passes/top_down_static_timing.rs
@@ -50,12 +50,8 @@ impl Schedule {
         println!("transitions:");
         self.transitions
             .iter()
-            .sorted_by(|(k1, k2, g1), (k3, k4, g2)| match k1.cmp(k3) {
-                std::cmp::Ordering::Equal => match k2.cmp(k4) {
-                    std::cmp::Ordering::Equal => g1.cmp(g2),
-                    other => other,
-                },
-                other => other,
+            .sorted_by(|(k1, k2, g1), (k3, k4, g2)| {
+                k1.cmp(k3).then_with(|| k2.cmp(k4)).then_with(|| g1.cmp(g2))
             })
             .for_each(|(i, f, g)| {
                 println!("({})->({})\n  {}", i, f, Printer::guard_str(g));
