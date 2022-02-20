1db5506aded9c29eb18807d3858ce31bef271265
Rachit Nigam <rachit.nigam12@gmail.com>
2022-02-20 15:50:09 -0500

    use icarus in flag compare

--- tools/flag-compare.sh
+++ tools/flag-compare.sh
@@ -5,8 +5,8 @@
 
 set -uf -o pipefail
 
-flag1='-p dead-group-removal -p all'
-flag2='-p dead-group-removal -p all -x tdcc:no-early-transitions'
+flag1=' -p dead-group-removal -p all'
+flag2=' -p dead-group-removal -p all -d top-down-st'
 file="$1"
 data="$2"
 out_stage=${3:-dat}
@@ -19,7 +19,8 @@ echo "$flag2" > out2.json
 
 echo "Running with flags: $flag1"
 fud e "$file" --to "$out_stage" \
-  -s interpreter.data "$data" \
+  --through icarus-verilog \
+  -s verilog.data "$data" \
   -s verilog.data "$data" \
   -s verilog.cycle_limit 1000 \
   -s futil.flags "$flag1" >> out1.json &
@@ -27,7 +28,8 @@ EXEC1=$!
 
 echo "Running with flags: $flag2"
 fud e "$file" --to "$out_stage" \
-  -s interpreter.data "$data" \
+  --through icarus-verilog \
+  -s verilog.data "$data" \
   -s verilog.data "$data" \
   -s verilog.cycle_limit 1000 \
   -s futil.flags "$flag2" >> out2.json &
