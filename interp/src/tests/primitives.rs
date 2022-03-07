cdddc438cedc6319fa259035d7875c705eecab90
Griffin Berlstein <griffin@berlste.in>
2022-03-07 13:34:44 -0500

    remove remainder from sdiv prop-test

--- interp/src/tests/primitives.rs
+++ interp/src/tests/primitives.rs
@@ -1613,9 +1613,9 @@ mod property_tests {
             mult.do_tick().unwrap();
             let output = mult.do_tick().unwrap();
             let out = extract_output!(output, "out_quotient");
-            let remainder = extract_output!(output, "out_remainder");
+            // let remainder = extract_output!(output, "out_remainder");
             assert_eq!(out.as_i64(),i64::wrapping_div(in_left, in_right));
-            assert_eq!(remainder.as_i64(), in_left.rem_euclid(in_right));
+            // assert_eq!(remainder.as_i64(), in_left.rem_euclid(in_right));
         }
     }
 }
