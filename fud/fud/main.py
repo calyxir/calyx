db23a54d0bf43dfb727dd06d48273352adfffb02
Rachit Nigam <rachit.nigam12@gmail.com>
2022-03-09 07:56:07 -0500

    fud state inference for targets (#946)
    * fud state inference for targets
    
    * fmt
    
    * add command to remove configuration file

--- fud/fud/main.py
+++ fud/fud/main.py
@@ -292,6 +292,9 @@ def main():
         elif args.command == "config":
             if args.edit:
                 os.system(f"{os.getenv('EDITOR')} '{cfg.config_file}'")
+            elif args.remove:
+                log.info(f"Removing {cfg.config_file}")
+                os.remove(cfg.config_file)
             else:
                 display_config(args, cfg)
         elif args.command == "check":
@@ -364,6 +367,14 @@ def config_config(parser):
         "-e",
         "--edit",
         help="Edit the configuration file using $EDITOR",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--remove",
+        help=(
+            "Delete the stored configuration."
+            " Next invocation of `fud` will create a fresh config."
+        ),
         action="store_true",
     )
     parser.add_argument("key", help="The key to perform an action on.", nargs="?")
