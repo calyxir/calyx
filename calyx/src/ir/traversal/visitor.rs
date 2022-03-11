5c9d342cd238d64d90fd576c552a5654555aa1ae
Rachit Nigam <rachit.nigam12@gmail.com>
2022-03-11 15:50:34 -0500

    remove Loggable trait

--- calyx/src/ir/traversal/visitor.rs
+++ calyx/src/ir/traversal/visitor.rs
@@ -23,34 +23,6 @@ pub trait Named {
     fn description() -> &'static str;
 }
 
-/// Implementator of trait provide various logging methods.
-pub trait Loggable {
-    /// Log output to STDERR.
-    /// `context` is the location from which the logger is being called.
-    /// Usage:
-    /// ```
-    /// self.elog("number-of-groups", groups.len());
-    /// ```
-    fn elog<S, T>(&self, context: S, msg: T)
-    where
-        S: std::fmt::Display,
-        T: std::fmt::Display;
-}
-
-/// Blanket implementation for Loggable for traits implementing Named
-impl<T> Loggable for T
-where
-    T: Named,
-{
-    fn elog<S, M>(&self, context: S, msg: M)
-    where
-        S: std::fmt::Display,
-        M: std::fmt::Display,
-    {
-        eprintln!("{}.{}: {}", T::name(), context, msg)
-    }
-}
-
 /// Trait defining method that can be used to construct a Visitor from an
 /// [ir::Context].
 /// This is useful when a pass needs to construct information using the context
