806fa39d6bed940a7058a166abf70123f6ba90e2
Rachit Nigam <rachit.nigam12@gmail.com>
2022-04-18 16:52:13 -0400

    Pass to unroll `while` loops with `@bound` (#975)
    * Add pass to unroll loops with @bound
    
    * add tests

--- calyx/src/passes/mod.rs
+++ calyx/src/passes/mod.rs
@@ -29,6 +29,7 @@ mod simplify_guards;
 mod synthesis_papercut;
 mod top_down_compile_control;
 mod top_down_static_timing;
+mod unroll_bound;
 mod well_formed;
 mod wire_inliner;
 
@@ -61,5 +62,6 @@ pub use simplify_guards::SimplifyGuards;
 pub use synthesis_papercut::SynthesisPapercut;
 pub use top_down_compile_control::TopDownCompileControl;
 pub use top_down_static_timing::TopDownStaticTiming;
+pub use unroll_bound::UnrollBounded;
 pub use well_formed::WellFormed;
 pub use wire_inliner::WireInliner;
