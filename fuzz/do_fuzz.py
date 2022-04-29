8059a186d64863c3cf3943b840031384a2749760
crystalhu26 <jh2346@cornell.edu>
2022-04-28 22:13:12 -0400

    do_fuzz add documentation and remove some redundant parts

--- fuzz/do_fuzz.py
+++ fuzz/do_fuzz.py
@@ -3,9 +3,13 @@ import logging
 import os
 import sys
 import random
+from deepdiff import DeepDiff
 
 
 def generate_data(template):
+    """
+    Randomly generate data based on the template
+    """
     with open(template) as f:
         template_file = json.load(f)
     for obj in template_file:
@@ -22,6 +26,9 @@ def generate_data(template):
 
 
 def revised_generate_list(input_data, data_format):
+    """
+    Generate a list of data based on datatype contained in the list, data width, and sign.
+    """
     try:
         # base case
         if isinstance(input_data, int):
@@ -36,6 +43,9 @@ def revised_generate_list(input_data, data_format):
 
 
 def generate_int(data_format):
+    """
+    Generate an integer based on the given data width and sign.
+    """
     is_signed = data_format['is_signed']
     width = data_format['width']
 
@@ -47,6 +57,9 @@ def generate_int(data_format):
 
 
 def construct_json(data_list, obj, template):
+    """
+    Insert the generated data to data template
+    """
     with open(template) as json_file:
         data = json.load(json_file)
     data[obj]["data"] = data_list
@@ -56,6 +69,9 @@ def construct_json(data_list, obj, template):
 
 
 def compare_object(a, b):
+    """
+    Compare input objects a and b. In this case, we only expect a and b to be either dictionary or list.
+    """
     if type(a) != type(b):
         return False
     elif type(a) is dict:
@@ -67,6 +83,9 @@ def compare_object(a, b):
 
 
 def compare_dict(a, b):
+    """
+    Compare input dictionaries a and b.
+    """
     if len(a) != len(b):
         return False
     else:
@@ -80,6 +99,9 @@ def compare_dict(a, b):
 
 
 def compare_list(a, b):
+    """
+    Compare input lists a and b.
+    """
     if len(a) != len(b):
         return False
     else:
@@ -90,32 +112,40 @@ def compare_list(a, b):
 
 
 def do_fuzz(prog, spec, template, iteration):
+    """
+    Run data generated from input template on both input file prog and spec.
+    Raise exception if two files produce unequal results.
+    Note: prog and spec do not need to be Calyx file, but should be convertable to Calyx using fud
+    """
     try:
-        prog_name = os.path.splitext(prog)[0]
-        prog_file_type = os.path.splitext(prog)[1]
-        spec_name = os.path.splitext(spec)[0]
-        spec_file_type = os.path.splitext(spec)[1]
+        original_prog = prog
+        original_spec = spec
+        prog_name, prog_file_type = os.path.splitext(prog)
+        spec_name, spec_file_type = os.path.splitext(spec)
         if prog_file_type != ".futil":
             prog = prog_name + ".futil"
-            os.system(f"fud e {prog} --to futil > {prog}")
+            os.system(f"fud e {original_prog} --to futil > {prog}")
 
         elif spec_file_type != ".futil":
             spec = spec_name + ".futil"
-            os.system(f"fud e {spec} --to futil > {spec}")
+            os.system(f"fud e {original_spec} --to futil > {spec}")
 
         for i in range(iteration):
             generate_data(template)
-            if os.stat(template).st_size != 0:
-                os.system(
-                    f"fud e {prog} -s verilog.data {template} --to dat -q --through icarus-verilog > result1.json")
-                os.system(
-                    f"fud e {spec} -s verilog.data {template} --to dat -q --through icarus-verilog > result2.json")
-                with open('result1.json', 'r') as f1, open('result2.json', 'r') as f2:
-                    f1_result = json.load(f1)
-                    f2_result = json.load(f2)
-                    obj1 = f1_result['memories']
-                    obj2 = f2_result['memories']
-                    assert compare_object(obj1, obj2), "Failed with unequal output!"
+            if os.stat(template).st_size == 0:
+                continue
+            os.system(
+                f"fud e {prog} -s verilog.data {template} --to dat -q --through icarus-verilog > result1.json")
+            os.system(
+                f"fud e {spec} -s verilog.data {template} --to dat -q --through icarus-verilog > result2.json")
+            with open('result1.json', 'r') as f1, open('result2.json', 'r') as f2:
+                f1_result = json.load(f1)
+                f2_result = json.load(f2)
+                obj1 = f1_result['memories']
+                obj2 = f2_result['memories']
+                diff = DeepDiff(obj1, obj2)
+                assert compare_object(obj1, obj2), f"Failed with unequal output! {diff}"
+
     except Exception as e:
         logging.error(e)
         logging.error(f'fail unequal output for prog: {prog}, spec: {spec}')
