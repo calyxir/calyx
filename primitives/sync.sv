9f282895b42f65921bd00bfdc6e442e2cf976650
Pai Li <pl582@cornell.edu>
2022-06-30 15:15:23 -0400

    extend std_sync_reg to two writers

--- primitives/sync.sv
+++ primitives/sync.sv
@@ -6,26 +6,33 @@
 module std_sync_reg #(
     parameter WIDTH = 32
 ) (
-   input wire [ WIDTH-1:0]    in,
+   input wire [ WIDTH-1:0]    in_0,
+   input wire [ WIDTH-1:0]    in_1,
    input wire                 read_en,
-   input wire                 write_en,
+   input wire                 write_en_0,
+   input wire                 write_en_1,
    input wire                 clk,
    input wire                 reset,
     // output
    output logic [WIDTH - 1:0] out,
-   output logic               write_done,
-   output logic               read_done,
-   output logic               blocked
+   output logic               write_done_0,
+   output logic               write_done_1,
+   output logic               read_done
 );
 
   logic is_full;
   logic [WIDTH - 1:0] state;
+  logic one_hot;
+  logic arbiter;
+  logic write_en;
 
   // States
   logic READ_ST, WRITE_ST;
 
+  assign write_en = write_en_0 || write_en_1;
   assign READ_ST = is_full && read_en;
   assign WRITE_ST = !is_full && write_en;
+  assign one_hot = !(write_en_0 && write_en_1);
 
   // State transitions
   always_ff @(posedge clk) begin
@@ -38,6 +45,24 @@ module std_sync_reg #(
     else
       is_full <= is_full;
   end
+
+  // Value of arbiter.
+  always_ff @(posedge clk) begin
+    if (reset) 
+      arbiter <= 0;
+    else if (WRITE_ST) begin
+      if (one_hot)
+        arbiter <= arbiter;
+      else if (arbiter == 0)
+        arbiter <= 1;
+      else 
+        arbiter <= 0;
+      end
+    else 
+      arbiter <= arbiter;
+    end
+    
+      
 
   // Value of output port.
   // Note that output is only available for one cycle.
@@ -54,23 +79,55 @@ module std_sync_reg #(
   always_ff @(posedge clk) begin
     if (reset)
       state <= 0;
-    else if (WRITE_ST)
-      state <= in;
+    else if (WRITE_ST) begin
+      if (one_hot) begin
+        if (write_en_0) 
+          state <= in_0;
+        else 
+          state <= in_1;
+      end
+      else if (arbiter == 0)
+        state <= in_0;
+      else 
+        state <= in_1;
+    end
     else if (READ_ST)
       state <= 'x;  // This could've been a latch but explicitly make it undefined.
     else
       state <= state;
   end
 
-  // Done signal for write commital
+  // Done signal for write_0 commital
   always_ff @(posedge clk) begin
     if (reset)
-      write_done <= 0;
-    else if (WRITE_ST)
-      write_done <= 1;
+      write_done_0 <= 0;
+    else if (WRITE_ST) begin
+      if (one_hot && write_en_0 == 1) 
+        write_done_0 <= 1;
+      else if (arbiter == 0)
+        write_done_0 <= 1;
+      else 
+        write_done_0 <= 0;
+    end
     else
-      write_done <= 0;
+      write_done_0 <= 0;
   end
+
+  //Done signal for write_1 commital
+  always_ff @(posedge clk) begin
+    if (reset)
+      write_done_1 <= 1;
+    else if (WRITE_ST) begin
+      if (one_hot && write_en_1 == 1)
+        write_done_1 <= 1;
+      else if (arbiter == 1)
+        write_done_1 <= 1;
+      else 
+        write_done_1 <= 0;
+    end
+    else
+      write_done_1 <= 0;
+    end
 
   // Done signal for read commital
   always_ff @(posedge clk) begin
@@ -80,16 +137,6 @@ module std_sync_reg #(
       read_done <= 1;
     else
       read_done <= 0;
-  end
-
-  // Blocked signal
-  always_ff @(posedge clk) begin
-    if (reset)
-      blocked <= 0;
-    else if ((!is_full && read_en) || (is_full && write_en))
-      blocked <= 1;
-    else
-      blocked <= 0;
   end
 
 endmodule
