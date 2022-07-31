0e8c1a2be31aafe75b75e70f7d00eeb81b17ee63
Nathaniel Navarro <nrn25@cornell.edu>
2022-07-31 15:15:54 +0300

    Remove vestigial emulation stage

--- fud/fud/stages/xilinx/__init__.py
+++ fud/fud/stages/xilinx/__init__.py
@@ -1,5 +1,4 @@
 from .xclbin import XilinxStage
-from .emulation import HwEmulationStage
 from .execution import HwExecutionStage
 
-__all__ = ["XilinxStage", "HwEmulationStage", "HwExecutionStage"]
+__all__ = ["XilinxStage", "HwExecutionStage"]
