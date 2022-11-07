63d7e935c0fb634ab94646e16a5d97fdf6e6b34f
Jan-Paul Vincent Ramos <JanPaul.Ramos@protonmail.com>
2022-11-07 17:49:30 -0500

    Update reserved_names.rs

--- calyx/src/ir/reserved_names.rs
+++ calyx/src/ir/reserved_names.rs
@@ -1,4 +1,5 @@
 pub const RESERVED_NAMES: &[&str] = &[
     "reg", "wire", "always", "posedge", "negedge", "logic", "tri", "input",
     "output", "if", "generate", "var", "go", "done", "clk", "and", "process",
+    "disable",
 ];
