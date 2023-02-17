b5e1ed2aefd4acd1b4cef13ae8a3a72b4ed36fcd
Ryan Doenges <me@ryandoeng.es>
2023-02-16 20:42:53 -0500

    fix weird extraction failure

--- vcalyx/ocaml/extr/Extraction.v
+++ vcalyx/ocaml/extr/Extraction.v
@@ -1,17 +1,15 @@
 (*! Extraction to OCaml !*)
+From VCalyx Require
+     Syntax
+     Parse.
 Require Export Coq.extraction.Extraction.
 From Coq.extraction Require Import
      ExtrOcamlBasic
      ExtrOcamlString
-     ExtrOcamlNatInt
-     ExtrOcamlZInt.
-From VCalyx Require
-     Syntax
-     Parse.
+     ExtrOcamlNatInt.
 
 (* This will extract all the listed identifiers and all their
 transitive dependencies. *)
 Extraction "extr.ml"
            VCalyx.Syntax.context
            VCalyx.Parse.parse_context.
-
