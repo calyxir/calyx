af5f92177bbd64891a2759342e00f7fdec17187e
Ryan Doenges <me@ryandoeng.es>
2023-03-03 14:45:13 -0500

    Some combinators on Mealy machines.

--- vcalyx/coq/Mealy.v
+++ vcalyx/coq/Mealy.v
@@ -17,9 +17,30 @@ Definition step {S I O: Type} (m: mealy S I O) : S -> I -> S :=
     fun s i => snd (m s i).
 
 (** A [tuple_mealy] is a Mealy machine where inputs and outputs are
-    tuples [V^n] and [V^m] respectively. *)
+    tuples [V^n] and [V^m], represented as untyped lists. *)
 Definition tuple_mealy (V S: Type) (n m : nat) :=
-  mealy S (Vect.vect V n) (Vect.vect V m).
+  mealy S (list V) (list V).
+
+(* Sequential composition of two mealy machines, where the second
+   machine consumes all the outputs of the first machine as inputs. *)
+Definition seq (S1 S2 X Y Z: Type) (m1: mealy S1 X Y) (m2: mealy S2 Y Z) : mealy (S1 * S2) X Z :=
+  (* destructuring bindings '(pat : typ) are indicated with a tick
+     mark ' and allow you to put anything you'd put in a let
+     expression or the left hand side of a pattern match into a
+     function parameter. The type annotations here are optional
+     because Coq can infer them from the return type of this function
+     [mealy (S1 * S2) X Z]. *)
+  fun '((s1, s2): S1 * S2) (x: X) =>
+    let (y, s1') := m1 s1 x in
+    let (z, s2') := m2 s2 y in
+    (z, (s1', s2')).
+
+(* Parallel composition of two mealy machines. *)
+Definition par (S1 S2 U V X Y: Type) (m1: mealy S1 U V) (m2: mealy S2 X Y) : mealy (S1 * S2) (U * X) (V * Y) :=
+  fun '((s1, s2): S1 * S2) '((u, x): U * X) =>
+    let (v, s1') := m1 s1 u in
+    let (y, s2') := m2 s2 x in
+    ((v, y), (s1', s2')).
 
 (** * Mealy Machine Semantics: Stream Functions *)
 Section StreamFnInterp.
