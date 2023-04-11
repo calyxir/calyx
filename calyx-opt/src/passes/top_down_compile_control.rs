b2908c9d84930b6c45ef8c43300274e6ae8def48
Caleb <cmk265@cornell.edu>
2023-04-11 17:09:16 -0400

    tdcc

--- calyx-opt/src/passes/top_down_compile_control.rs
+++ calyx-opt/src/passes/top_down_compile_control.rs
@@ -72,6 +72,7 @@ fn control_exits(con: &ir::Control, exits: &mut Vec<PredEdge>) {
         ir::Control::Invoke(_) => unreachable!("`invoke` statements should have been compiled away. Run `{}` before this pass.", passes::CompileInvoke::name()),
         ir::Control::StaticEnable(_) => unreachable!("`static enable` statements should have been compiled away. Run `{}` before this pass.", passes::TopDownStaticTiming::name()),
         ir::Control::Par(_) => unreachable!(),
+        ir::Control::Static(_) => unreachable!(" static control should have been compiled away. Run the static compilation passes before this pass")
     }
 }
 
@@ -200,6 +201,7 @@ fn compute_unique_ids(con: &mut ir::Control, cur_state: u64) -> u64 {
         ir::Control::Empty(_) => cur_state,
         ir::Control::Invoke(_) => unreachable!("`invoke` statements should have been compiled away. Run `{}` before this pass.", passes::CompileInvoke::name()),
         ir::Control::StaticEnable(_) => unreachable!("`static enable` statements should have been compiled away. Run `{}` before this pass.", passes::TopDownStaticTiming::name()),
+        ir::Control::Static(_) => unreachable!("static control should have been compiled away. Run the static compilation passes before this pass")
     }
 }
 
@@ -451,6 +453,7 @@ impl Schedule<'_, '_> {
         ir::Control::Invoke(_) => unreachable!("`invoke` statements should have been compiled away. Run `{}` before this pass.", passes::CompileInvoke::name()),
         ir::Control::Empty(_) => unreachable!("`empty` statements should have been compiled away. Run `{}` before this pass.", passes::CompileEmpty::name()),
         ir::Control::StaticEnable(_) => unreachable!("`static enable` statements should have been compiled away. Run `{}` before this pass.", passes::TopDownStaticTiming::name()),
+        ir::Control::Static(_) => unreachable!("static control should have been compiled away. Run the static compilation passes before this pass")
     }
     }
 
