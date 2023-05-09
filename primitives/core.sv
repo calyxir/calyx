5b63368cdd317dd33f8af802b7d468550c484310
Rachit Nigam <rachit.nigam12@gmail.com>
2023-05-09 16:35:11 +0530

    fix tests

--- primitives/core.sv
+++ primitives/core.sv
@@ -114,16 +114,6 @@ module std_xor #(
   assign out = left ^ right;
 endmodule
 
-module std_add #(
-    parameter WIDTH = 32
-) (
-   input wire               logic [WIDTH-1:0] left,
-   input wire               logic [WIDTH-1:0] right,
-   output logic [WIDTH-1:0] out
-);
-  assign out = left + right;
-endmodule
-
 module std_sub #(
     parameter WIDTH = 32
 ) (
