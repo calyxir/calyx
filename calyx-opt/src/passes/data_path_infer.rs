bb279fbc6444644e8b2b6b332d4b667ddffef735
Rachit Nigam <rachit.nigam12@gmail.com>
2023-05-09 17:08:49 +0530

    more constraining

--- calyx-opt/src/passes/data_path_infer.rs
+++ calyx-opt/src/passes/data_path_infer.rs
@@ -1,6 +1,7 @@
 use crate::traversal::{Action, Named, VisResult, Visitor};
 use calyx_ir as ir;
-use std::collections::HashSet;
+use ir::RRC;
+use std::{collections::HashSet, rc::Rc};
 
 #[derive(Default)]
 /// Infers `@control` and `@data` annotations for cells.
@@ -16,7 +17,10 @@ use std::collections::HashSet;
 ///
 /// Because the last constraint is recursive, we use an iterative algorithm to
 /// infer the annotations.
-pub struct DataPathInfer;
+pub struct DataPathInfer {
+    /// Cells that cannot be marked as a @data cell
+    control_cells: HashSet<ir::Id>,
+}
 
 impl Named for DataPathInfer {
     fn name() -> &'static str {
@@ -28,8 +32,53 @@ impl Named for DataPathInfer {
     }
 }
 
+impl DataPathInfer {
+    fn port_and_cg(
+        &mut self,
+        port: RRC<ir::Port>,
+        mb_cg: &Option<RRC<ir::CombGroup>>,
+    ) {
+        let cond_port = port.borrow();
+        assert!(!cond_port.is_hole());
+        self.control_cells.insert(cond_port.get_parent_name());
+
+        // All ports used in the combinational group cannot be data ports
+        // since they are used to compute the condition.
+        if let Some(cgr) = mb_cg {
+            let cg = cgr.borrow();
+            for assign in &cg.assignments {
+                let src = assign.src.borrow();
+                assert!(!src.is_hole());
+                self.control_cells.insert(src.get_parent_name());
+            }
+        }
+    }
+}
+
 impl Visitor for DataPathInfer {
-    fn start(
+    fn finish_if(
+        &mut self,
+        s: &mut ir::If,
+        _comp: &mut ir::Component,
+        _sigs: &ir::LibrarySignatures,
+        _comps: &[ir::Component],
+    ) -> VisResult {
+        self.port_and_cg(Rc::clone(&s.port), &s.cond);
+        Ok(Action::Continue)
+    }
+
+    fn finish_while(
+        &mut self,
+        s: &mut ir::While,
+        _comp: &mut ir::Component,
+        _sigs: &ir::LibrarySignatures,
+        _comps: &[ir::Component],
+    ) -> VisResult {
+        self.port_and_cg(Rc::clone(&s.port), &s.cond);
+        Ok(Action::Continue)
+    }
+
+    fn finish(
         &mut self,
         comp: &mut ir::Component,
         _sigs: &ir::LibrarySignatures,
@@ -37,43 +86,47 @@ impl Visitor for DataPathInfer {
     ) -> VisResult {
         // Seed using all cells that have been marked as @control and those that
         // appear in the guard of an assignment
-        let mut con_cells: HashSet<ir::Id> = comp
-            .cells
-            .iter()
-            .filter_map(|c| {
-                let cell = c.borrow();
-                if cell.attributes.has(ir::BoolAttr::Control) {
-                    Some(cell.name())
-                } else {
-                    None
-                }
-            })
-            .collect();
+        self.control_cells.extend(comp.cells.iter().filter_map(|c| {
+            let cell = c.borrow();
+            if cell.attributes.has(ir::BoolAttr::Control) {
+                Some(cell.name())
+            } else {
+                None
+            }
+        }));
 
         // If this is used in guards, then it cannot be marked as @data
         comp.for_each_assignment(|asgn| {
             asgn.guard.all_ports().into_iter().for_each(|p| {
                 let port = p.borrow();
                 if !port.is_hole() {
-                    con_cells.insert(port.get_parent_name());
+                    self.control_cells.insert(port.get_parent_name());
                 }
             });
             // If this assignment writes to the done condition of a group, then the source
             // cannot be marked as @data
             if asgn.dst.borrow().is_hole() {
-                con_cells.insert(asgn.src.borrow().get_parent_name());
+                self.control_cells
+                    .insert(asgn.src.borrow().get_parent_name());
             }
         });
         comp.for_each_static_assignment(|asgn| {
             asgn.guard.all_ports().into_iter().for_each(|p| {
                 let port = p.borrow();
                 if !port.is_hole() {
-                    con_cells.insert(port.get_parent_name());
+                    self.control_cells.insert(port.get_parent_name());
                 }
             })
         });
 
-        // This is a purely structural pass
+        // Mark all control cells
+        for c in comp.cells.iter() {
+            let mut cell = c.borrow_mut();
+            if self.control_cells.contains(&cell.name()) {
+                cell.attributes.insert(ir::BoolAttr::Control, 1);
+            }
+        }
+
         Ok(Action::Stop)
     }
 }
