5f6a09584c13683e38c5d7842207096450134d02
Anshuman Mohan <anshumanmohan@live.com>
2023-05-15 18:49:28 -0400

    Tidy main

--- frontends/mrxl/mrxl/main.py
+++ frontends/mrxl/mrxl/main.py
@@ -7,41 +7,45 @@ from .interp import interp, InterpError
 
 
 def main():
+    """Main entry point for the MrXL interpreter and compiler."""
+
     parser = argparse.ArgumentParser(
-        "Interpret a MrXL program, or compile it to Calyx."
+        "Either interpret a MrXL program, or compile it to Calyx"
     )
     parser.add_argument(
         "--i",
         "--interpret",
         action="store_true",
-        help="Interpret the input MrXL program (leave this off to compile)",
+        help="Interpret the input MrXL program (drop this flag in order to compile)",
     )
     parser.add_argument(
         "--data",
         metavar="<datafile>",
         type=str,
-        help="Input data, required to interpret",
+        help="Input data, required if interpreting",
     )
     parser.add_argument(
         "filename",
         metavar="<file>",
         type=str,
-        help="MrXL program to compile or interpet",
+        help="The MrXL program to interpret/compile",
     )
 
     args = parser.parse_args()
-    with open(args.filename) as f:
-        txt = f.read()
+    with open(args.filename, encoding="UTF-8") as file:
+        txt = file.read()
 
     if args.data:
-        with open(args.data) as f:
-            data = json.load(f)
+        with open(args.data, encoding="UTF-8") as file:
+            data = json.load(file)
 
     ast = parse(txt)
 
     if args.i:
+        if not args.data:
+            raise ValueError("Must provide data if interpreting")
         try:
-            print(interp(ast, data))
+            print(interp(ast, data))  # type: ignore
         except InterpError as exc:
             print(str(exc), file=sys.stderr)
             sys.exit(1)
