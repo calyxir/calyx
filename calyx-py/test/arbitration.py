7a4bcec6b3dc562ecab38a1d707cbbbb571be8aa
Anshuman Mohan <anshumanmohan@live.com>
2023-05-17 16:40:45 -0400

    Lift to own method

--- calyx-py/test/arbitration.py
+++ calyx-py/test/arbitration.py
@@ -2,15 +2,24 @@
 from calyx.builder import Builder
 
 
-def build():
-    """Just two memories."""
-    prog = Builder()
-    main = prog.component("main")
-    main.input("in", 32)
+def add_wrap(prog):
+    """Inserts the wrap component into the program.
+    It just has two memories and nothing else.
+    """
+    main = prog.component("wrap3")
+    main.input("i", 32)
+    main.input("j", 32)
+
     main.output("out", 32)
 
     _ = main.mem_d1("mem_1", 32, 4, 32, is_ref=True)
     _ = main.mem_d1("mem_2", 32, 4, 32, is_ref=True)
+
+
+def build():
+    """Just two memories."""
+    prog = Builder()
+    add_wrap(prog)
 
     return prog.program
 
