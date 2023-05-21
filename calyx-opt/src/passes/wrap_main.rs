6b9b7ccd6d57c8e75ed2df289e12e7673e24bb3f
Rachit Nigam <rachit.nigam12@gmail.com>
2023-05-21 17:43:08 +0530

    wrap-main pass works

---
+++ calyx-opt/src/passes/wrap_main.rs
@@ -0,0 +1,75 @@
+use std::borrow::BorrowMut;
+
+use crate::traversal::{Action, Named, VisResult, Visitor};
+use calyx_ir as ir;
+
+#[derive(Default)]
+/// If the top-level component is not named `main`, adds a new `main` component
+/// and makes it the top-level component.
+/// This is useful because a lot of our tools rely on the name `main` being the design under test (DUT).
+///
+/// For more information, see https://github.com/cucapra/calyx/issues/1376
+pub struct WrapMain;
+
+impl Named for WrapMain {
+    fn name() -> &'static str {
+        "wrap-main"
+    }
+
+    fn description() -> &'static str {
+        "If the top-level component is not named `main`, adds a new `main` component and makes it the top-level component."
+    }
+}
+
+impl Visitor for WrapMain {
+    fn precondition(ctx: &calyx_ir::Context) -> Option<String>
+    where
+        Self: Sized,
+    {
+        if ctx.entrypoint().name == "main" {
+            Some("Top-level component is already named `main'".to_string())
+        } else {
+            None
+        }
+    }
+
+    fn start_context(&mut self, ctx: &mut ir::Context) -> VisResult {
+        let entry = ctx.entrypoint();
+        let sig = entry.signature.borrow();
+
+        // If any of the ports in the entrypoint component are non-interface ports, refuse to run this pass.
+        if let Some(p) = sig.ports.iter().find(|p| {
+            let port = p.borrow();
+            let attr = &port.attributes;
+            !(attr.has(ir::BoolAttr::Clk)
+                || attr.has(ir::BoolAttr::Reset)
+                || attr.has(ir::NumAttr::Go)
+                || attr.has(ir::NumAttr::Done))
+        }) {
+            let pn = p.borrow().name;
+            log::warn!(
+                "Entrypoint component `{}' has non-interface port `{}'. Cannot wrap it in `main' component. The component might not simulate with the Calyx test bench or generate results with the synthesis scripts without modification.",
+                entry.name,
+                pn
+            );
+            return Ok(Action::Stop);
+        }
+        drop(sig);
+
+        // Remove top-level attribute from previous component
+        ctx.entrypoint_mut()
+            .attributes
+            .remove(ir::BoolAttr::TopLevel);
+
+        // Create a new `main' component
+        let mut main = ir::Component::new("main", vec![], true, false, None);
+        main.borrow_mut()
+            .attributes
+            .insert(ir::BoolAttr::TopLevel, 1);
+        ctx.entrypoint = main.name;
+        ctx.components.push(main);
+
+        // Purely context directed pass
+        Ok(Action::Stop)
+    }
+}
