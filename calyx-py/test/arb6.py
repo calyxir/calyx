f465cd01ee75b8e4d2e5f60613f8997b01b77f20
Anshuman Mohan <anshumanmohan@live.com>
2023-05-22 17:32:50 -0400

    Progress in wrap2

--- calyx-py/test/arb6.py
+++ calyx-py/test/arb6.py
@@ -16,6 +16,38 @@ def add_eq(comp: cb.ComponentBuilder, port, const, cell, group):
     return eq_cell, eq_group
 
 
+def add_lt(comp: cb.ComponentBuilder, port, const, cell, group):
+    """Adds wiring into component `comp` to check `port < const`.
+    1. Within component `comp`, creates a group called `group`.
+    2. Within `group`, creates a cell called `cell` that checks for lt.
+    3. Puts the values of `port` and `const` into `cell`.
+    4. Returns the lt-checking cell and the lt-checking group.
+    """
+    lt_cell = comp.lt(cell, 32)
+    with comp.comb_group(group) as lt_group:
+        lt_cell.left = comp.this()[port]
+        lt_cell.right = const
+    return lt_cell, lt_group
+
+
+def add_sub(comp: cb.ComponentBuilder, port, const, cell, ans_reg, group):
+    """Adds wiring into component `comp` to compute `port - const`.
+    1. Within component `comp`, creates a group called `group`.
+    2. Within `group`, creates a cell called `cell` that computes the difference.
+    3. Puts the values of `port` and `const` into `cell`.
+    4. Then puts the answer of the computation into `ans_reg`
+    4. Returns the sub-checking cell and the sub-checking group.
+    """
+    sub_cell = comp.sub(cell, 32)
+    with comp.group(group) as sub_group:
+        sub_cell.left = comp.this()[port]
+        sub_cell.right = const
+        ans_reg.write_en = 1
+        ans_reg.in_ = sub_cell.out
+        sub_group.done = ans_reg.done
+    return sub_cell, sub_group
+
+
 def add_mem_load(comp: cb.ComponentBuilder, mem, j, ans, group):
     """Loads a value from one memory into another.
     1. Within component `comp`, creates a group called `group`.
@@ -31,38 +63,48 @@ def add_mem_load(comp: cb.ComponentBuilder, mem, j, ans, group):
     return load_grp
 
 
-def add_wrap(prog):
-    """Inserts the component `wrap` into the program.
+def add_wrap2(prog):
+    """Inserts the component `wrap2` into the program.
 
     It has:
-    - two inputs, `i` and `j`
-    - two ref memories, `mem1` and `mem2`
-    - one output, `out`
+    - two inputs, `i` and `j`. 0 <= i < 2 and 0 <= j < 12.
+    - six ref memories, `mem1` through `mem6`, of size 4 each.
+    - one ref memory, `ans`.
 
-    It also returns the component `wrap`.
+    The invoker wants to pretend that there are actually _two_ memories
+    of size _12_ each. The invoker wants to index the memories while
+    living in this fiction.
 
-    Assume 0 <= i < 2 and 0 <= j < 4.
-    if i == 0, then out = mem1[j]
-    if i == 1, then out = mem2[j]
+    This component will return mem[i][j], but indexed according to the fiction.
     """
 
-    wrap: cb.ComponentBuilder = prog.component("wrap")
+    wrap: cb.ComponentBuilder = prog.component("wrap2")
     wrap.input("i", 32)
     wrap.input("j", 32)
 
-    mem1 = wrap.mem_d1("mem1", 32, 4, 32, is_ref=True)
-    mem2 = wrap.mem_d1("mem2", 32, 4, 32, is_ref=True)
+    # Six memory cells, plus an answer cell.
+    mems = [wrap.mem_d1(f"mem{i}", 32, 4, 32, is_ref=True) for i in range(6)]
     ans = wrap.mem_d1("ans", 32, 1, 32, is_ref=True)
 
-    eq0_cell, eq0_grp = add_eq(wrap, "i", 0, "eq0", "i_eq_0")
-    eq1_cell, eq1_grp = add_eq(wrap, "i", 1, "eq1", "i_eq_1")
-    emit_from_mem1 = add_mem_load(wrap, mem1, "j", ans, "load_from_mem1")
-    emit_from_mem2 = add_mem_load(wrap, mem2, "j", ans, "load_from_mem2")
+    # We will need j%4, so we'll store it in a cell.
+    j_mod_4 = wrap.reg("j_mod_4", 32)
 
-    wrap.control += cb.par(
-        cb.if_(eq0_cell.out, eq0_grp, emit_from_mem1),
-        cb.if_(eq1_cell.out, eq1_grp, emit_from_mem2),
-    )
+    # Additional cells to compute equality, lt, and difference
+    eq0 = add_eq(wrap, "i", 0, "eq0", "i_eq_0")
+    eq1 = add_eq(wrap, "i", 1, "eq1", "i_eq_1")
+    lt1 = add_lt(wrap, "j", 4, "lt1", "j_lt_4")
+    lt2 = add_lt(wrap, "j", 8, "lt2", "j_lt_8")
+    sub1 = add_sub(wrap, "j", cb.const(32, 4), "sub", j_mod_4, "j_less_4")
+    sub2 = add_sub(wrap, "j", cb.const(32, 8), "sub", j_mod_4, "j_less_8")
+
+    emit_from_mems = [
+        add_mem_load(wrap, mems[k], "j", ans, f"load_from_mem{k}") for k in range(6)
+    ]
+
+    # wrap.control += cb.par(
+    #     cb.if_(eq0_cell.out, eq0_grp, emit_from_mem1),
+    #     cb.if_(eq1_cell.out, eq1_grp, emit_from_mem2),
+    # )]
     return wrap
 
 
@@ -99,7 +141,7 @@ def add_main(prog, wrap):
 def build():
     """Top-level function to build the program."""
     prog = cb.Builder()
-    wrap = add_wrap(prog)
+    wrap = add_wrap2(prog)
     add_main(prog, wrap)
     return prog.program
 
