7212f52b78d9026a160153402588b5408380126c
Anshuman Mohan <anshumanmohan@live.com>
2023-05-23 12:24:29 -0400

    Comments and refactoring

--- calyx-py/test/arbiter_6.py
+++ calyx-py/test/arbiter_6.py
@@ -3,10 +3,10 @@ import calyx.builder as cb
 
 
 def add_eq(comp: cb.ComponentBuilder, port, const, cell, group):
-    """Adds wiring into component `comp` to check `port == const`.
-    1. Within component `comp`, creates a group called `group`.
-    2. Within `group`, creates a cell called `cell` that checks equality.
-    3. Puts the values of `port` and `const` into `cell`.
+    """Adds wiring into component {comp} to check if {port} == {const}.
+    1. Within {comp}, creates a group called {group}.
+    2. Within {group}, creates a cell called {cell} that checks equality.
+    3. Puts the values of {port} and {const} into {cell}.
     4. Returns the equality-checking cell and the equality-checking group.
     """
     eq_cell = comp.eq(cell, 32)
@@ -17,11 +17,11 @@ def add_eq(comp: cb.ComponentBuilder, port, const, cell, group):
 
 
 def add_lt(comp: cb.ComponentBuilder, port, const, cell, group):
-    """Adds wiring into component `comp` to check `port < const`.
-    1. Within component `comp`, creates a group called `group`.
-    2. Within `group`, creates a cell called `cell` that checks for lt.
-    3. Puts the values of `port` and `const` into `cell`.
-    4. Returns the lt-checking cell and the lt-checking group.
+    """Adds wiring into component {comp} to check if {port} < {const}.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, creates a cell called {cell} that checks for less-than.
+    3. Puts the values of {port} and {const} into {cell}.
+    4. Returns the less-than-checking cell and the less-than-checking group.
     """
     lt_cell = comp.lt(cell, 32)
     with comp.comb_group(group) as lt_group:
@@ -31,13 +31,18 @@ def add_lt(comp: cb.ComponentBuilder, port, const, cell, group):
 
 
 def add_sub(comp: cb.ComponentBuilder, port, const, sub_cell, ans_reg, group):
-    """Adds wiring into component `comp` to compute `port - const`.
-    1. Within component `comp`, creates a group called `group`.
-    2. Within `group`, assumes there is a cell `cell` that computes the difference.
-    3. Puts the values of `port` and `const` into `cell`.
-    4. Then puts the answer of the computation into `ans_reg`
+    """Adds wiring into component {comp} to compute {port} - {const}.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, assumes there is a cell {cell} that computes differences.
+    3. Puts the values of {port} and {const} into {cell}.
+    4. Then puts the answer of the computation into {ans_reg}.
     4. Returns the sub-checking group.
     """
+    # Note, this one is a little different than the others.
+    # 1. We assume the subtraction cell already exists.
+    # 2. We're not returning the cell, because we don't need to.
+    # 3. We write the answer into `ans_reg`.
+
     with comp.group(group) as sub_group:
         sub_cell.left = port
         sub_cell.right = const
@@ -47,15 +52,15 @@ def add_sub(comp: cb.ComponentBuilder, port, const, sub_cell, ans_reg, group):
     return sub_group
 
 
-def add_mem_load(comp: cb.ComponentBuilder, mem, j, ans, group):
+def add_mem_load(comp: cb.ComponentBuilder, mem, i, ans, group):
     """Loads a value from one memory into another.
-    1. Within component `comp`, creates a group called `group`.
-    2. Within `group`, reads from memory `mem` at address `j`.
-    3. Writes the value into memory `ans` at address 0.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, reads from memory {mem} at address {i}.
+    3. Writes the value into memory {ans} at address 0.
     4. Returns the group that does this.
     """
     with comp.group(group) as load_grp:
-        mem.addr0 = j
+        mem.addr0 = i
         ans.write_en = 1
         ans.write_data = mem.read_data
         load_grp.done = ans.done
@@ -63,9 +68,10 @@ def add_mem_load(comp: cb.ComponentBuilder, mem, j, ans, group):
 
 
 def add_reg_load(comp: cb.ComponentBuilder, port, ans_reg, group):
-    """Creates a group called `group`.
-    In that, loads the value of `port` into `ans_reg`.
-    Returns the group."""
+    """Creates a group called {group}.
+    In that group, loads the value of {port} into {ans_reg}.
+    Returns the group.
+    """
     with comp.group(group) as grp:
         ans_reg.write_en = 1
         ans_reg.in_ = port
@@ -84,9 +90,8 @@ def add_wrap2(prog):
     The invoker wants to pretend that there are actually
     _two_ memories of size _12_ each.
     The invoker wants to index the memories while living in this fiction.
-
     This component will return mem[i][j], but indexed according to the fiction.
-    Accordingly, 0 <= i < 2 and 0 <= j < 12.
+    Accordingly, we assume 0 <= i < 2 and 0 <= j < 12.
     """
 
     wrap: cb.ComponentBuilder = prog.component("wrap2")
@@ -144,9 +149,8 @@ def add_wrap3(prog):
     The invoker wants to pretend that there are actually
     _two_ memories of size _12_ each.
     The invoker wants to index the memories while living in this fiction.
-
     This component will return mem[i][j], but indexed according to the fiction.
-    Accordingly, 0 <= i < 2 and 0 <= j < 12.
+    Accordingly, we assume 0 <= i < 2 and 0 <= j < 12.
     """
 
     wrap: cb.ComponentBuilder = prog.component("wrap3")
@@ -200,12 +204,12 @@ def add_main(prog, wrap2, wrap3):
     along with the inputs i = 1, j = 3.
     """
     main: cb.ComponentBuilder = prog.component("main")
-    A = main.mem_d1("A", 32, 4, 32, is_external=True)
-    B = main.mem_d1("B", 32, 4, 32, is_external=True)
-    C = main.mem_d1("C", 32, 4, 32, is_external=True)
-    D = main.mem_d1("D", 32, 4, 32, is_external=True)
-    E = main.mem_d1("E", 32, 4, 32, is_external=True)
-    F = main.mem_d1("F", 32, 4, 32, is_external=True)
+    mem_a = main.mem_d1("A", 32, 4, 32, is_external=True)
+    mem_b = main.mem_d1("B", 32, 4, 32, is_external=True)
+    mem_c = main.mem_d1("C", 32, 4, 32, is_external=True)
+    mem_d = main.mem_d1("D", 32, 4, 32, is_external=True)
+    mem_e = main.mem_d1("E", 32, 4, 32, is_external=True)
+    mem_f = main.mem_d1("F", 32, 4, 32, is_external=True)
 
     out2 = main.mem_d1("out2", 32, 1, 32, is_external=True)
     out3 = main.mem_d1("out3", 32, 1, 32, is_external=True)
