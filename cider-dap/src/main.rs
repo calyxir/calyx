f33f366a00dd928c0d51a42caa1f6eaeeea72eaa
Basant-khalil <bak94@cornell.edu>
2023-06-15 14:22:27 -0400

    connecting to server and client in dap.rs; not fully successfull yet

--- cider-dap/src/main.rs
+++ cider-dap/src/main.rs
@@ -1,54 +1,128 @@
-use std::io::{Read, Write};
+use dap::prelude::*;
+use std::io::{BufReader, BufWriter, Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::time::Duration;
-
+use thiserror::Error;
 mod tcp_client;
+
+#[derive(Error, Debug)]
+enum MyAdapterError {
+    #[error("Unhandled command")]
+    UnhandledCommandError,
+}
+
+struct MyAdapter;
+
+impl Adapter for MyAdapter {
+    type Error = MyAdapterError;
+
+    fn accept(
+        &mut self,
+        request: Request,
+        _ctx: &mut dyn Context,
+    ) -> Result<Response, Self::Error> {
+        eprintln!("Accept {:#?}\n", request.command);
+
+        match &request.command {
+            Command::Initialize(args) => {
+                if let Some(client_name) = args.client_name.as_ref() {
+                    eprintln!(
+                        "> Client '{client_name}' requested initialization."
+                    );
+                    Ok(Response::make_success(
+                        &request,
+                        ResponseBody::Initialize(Some(types::Capabilities {
+                            supports_configuration_done_request: Some(true),
+                            supports_evaluate_for_hovers: Some(true),
+                            ..Default::default()
+                        })),
+                    ))
+                } else {
+                    Ok(Response::make_error(&request, "Missing client name"))
+                }
+            }
+            Command::Next(_) => Ok(Response::make_ack(&request).unwrap()),
+            _ => Err(MyAdapterError::UnhandledCommandError),
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+        handle_client(stream)?;
+    }
+
+    Ok(())
+}
+
+struct CloneStreams {
+    client_stream: BufReader<TcpStream>,
+    dap_stream: TcpStream,
+}
+
+impl CloneStreams {
+    fn new(client_stream: TcpStream, dap_stream: TcpStream) -> Self {
+        Self {
+            client_stream: BufReader::new(client_stream),
+            dap_stream,
+        }
+    }
+}
+
+impl Read for CloneStreams {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        self.client_stream.read(buf)
+    }
+}
+
+impl Write for CloneStreams {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.dap_stream.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.dap_stream.flush()
+    }
+}
 
 fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     println!("Handling client connection...");
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer)?; // Read the request message from the client
 
-    // Process the received message and send appropriate responses
-    let response = match String::from_utf8_lossy(&buffer).as_ref() {
-        "{\"seq\": 152, \"type\": \"request\", \"command\": \"initialize\", \"arguments\": {\"adapterId\": \"0001e357-72c7-4f03-ae8f-c5b54bd8dabf\", \"clientName\": \"Some Cool Editor\"}}" => {
-            "{\"seq\": 152, \"type\": \"response\", \"command\": \"initialize\", \"success\": true}"
-        }
-        "{\"seq\": 153, \"type\": \"request\", \"command\": \"next\", \"arguments\": {\"threadId\": 3}}" => {
-            "{\"seq\": 153, \"type\": \"response\", \"command\": \"next\", \"success\": true}"
-        }
-        _ => "{\"seq\": 0, \"type\": \"response\", \"command\": \"unknown\", \"success\": false}",
-    };
+    stream.read(&mut buffer)?; // Read the request message from the client.
 
-    stream.write_all(response.as_bytes())?; // Send the response back to the client
-    stream.flush()?; // Flush the stream to ensure data is sent
+    // Create CloneStreams and pass it to handle_dap_rs
+    let dap_stream = TcpStream::connect("127.0.0.1:8080")?;
+    let clone_streams = CloneStreams::new(stream.try_clone()?, dap_stream);
+    handle_dap_rs(clone_streams)?;
+
+    // Create a BasicClient instance with the TCP stream
+    let client = BasicClient::new(stream.try_clone()?);
+
+    // Create a Server instance with an appropriate adapter and the client
+    let mut server = Server::new(MyAdapter, client);
+
+    // Create a BufReader from the TcpStream
+    let mut reader = BufReader::new(&mut stream);
+
+    // Run the server to handle the request and generate a response
+    match server.run(&mut reader) {
+        Ok(()) => println!("Request handled successfully"),
+        Err(err) => eprintln!("Error handling request: {:?}", err),
+    }
+
     Ok(())
 }
-fn main() {
-    let listener =
-        TcpListener::bind("127.0.0.1:8080").expect("Failed to bind address");
-    listener
-        .set_nonblocking(false)
-        .expect("Failed to set non-blocking mode");
 
-    println!("DAP server listening on port 8080...");
+fn handle_dap_rs(mut streams: CloneStreams) -> std::io::Result<()> {
+    // Read from `streams` and send data to `dap-rs` (server.rs).
+    // needs change?
+    // Example usage:
+    let mut buffer = [0; 1024];
+    streams.read(&mut buffer)?;
+    streams.write_all(&buffer)?;
 
-    loop {
-        match listener.accept() {
-            Ok((stream, _)) => {
-                println!("Handling client connection...");
-                handle_client(stream);
-            }
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                // No incoming connections yet, handle other tasks or sleep
-                // to avoid busy-waiting
-                std::thread::sleep(Duration::from_millis(100));
-                println!("Err");
-            }
-            Err(e) => {
-                eprintln!("Error: {}", e);
-            }
-        }
-    }
+    Ok(())
 }
--- cider-dap/src/tcp_client.rs
+++ cider-dap/src/tcp_client.rs
@@ -1,49 +1,53 @@
-use std::io::prelude::*;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-pub fn main() -> std::io::Result<()> {
+pub(crate) fn send_request(
+    stream: &mut TcpStream,
+    request: &str,
+) -> std::io::Result<String> {
+    let content_length = request.len();
+
+    let formatted_request =
+        format!("Content-Length: {}\r\n\r\n{}", content_length, request);
+
+    stream.write_all(formatted_request.as_bytes())?;
+
+    let mut response = String::new();
+    stream.read_to_string(&mut response)?;
+
+    Ok(response)
+}
+
+fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:8080")
         .expect("Failed to connect to server");
 
     // Request 1
-    let request1 = r#"Content-Length: 210
+    let request1 = r#"{
+        "seq": 1,
+        "type": "request",
+        "command": "initialize",
+        "arguments": {
+            "adapterID": "0001e357-72c7-4f03-ae8f-c5b54bd8dabf",
+            "clientName": "Some Cool Editor"
+        }
+    }"#;
 
-{
-    "seq": 152,
-    "type": "request",
-    "command": "initialize",
-    "arguments": {
-        "adapterId": "0001e357-72c7-4f03-ae8f-c5b54bd8dabf",
-        "clientName": "Some Cool Editor"
-    }
-}
-"#;
-
-    stream.write(request1.as_bytes())?;
-
-    let mut response1 = String::new();
-    stream.read_to_string(&mut response1)?;
-    println!("Received response: {}", response1);
+    let response1 = send_request(&mut stream, request1)?;
+    println!("Received response 1: {}", response1);
 
     // Request 2
-    let request2 = r#"Content-Length: 119
+    let request2 = r#"{
+        "seq": 2,
+        "type": "request",
+        "command": "next",
+        "arguments": {
+            "threadId": 3
+        }
+    }"#;
 
-{
-    "seq": 153,
-    "type": "request",
-    "command": "next",
-    "arguments": {
-        "threadId": 3
-    }
-}
-"#;
-
-    stream.write(request2.as_bytes())?;
-
-    let mut response2 = String::new();
-    stream.read_to_string(&mut response2)?;
-    println!("Received response: {}", response2);
+    let response2 = send_request(&mut stream, request2)?;
+    println!("Received response 2: {}", response2);
 
     Ok(())
 }
