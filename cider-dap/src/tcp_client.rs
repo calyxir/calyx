49e66e1747b55c6826eba90e2073c84663e4b9eb
Basant-khalil <bak94@cornell.edu>
2023-06-20 11:44:40 -0400

    progress can parse first_response

--- cider-dap/src/tcp_client.rs
+++ cider-dap/src/tcp_client.rs
@@ -10,12 +10,19 @@ pub(crate) fn send_request(
     let formatted_request =
         format!("Content-Length: {}\r\n\r\n{}", content_length, request);
 
+    println!("{} ", formatted_request);
     stream.write_all(formatted_request.as_bytes())?;
 
     let mut response = String::new();
-    stream.read_to_string(&mut response)?;
+    let mut buf = vec![0u8; 1024];
+    /*
+    read the first 2 lines into a buffer
+    extract the content length header
+    use read.take with the specified length number from the content header
+     */
+    stream.read(&mut buf)?;
 
-    Ok(response)
+    Ok(String::from_utf8(buf).unwrap())
 }
 
 fn main() -> std::io::Result<()> {
@@ -28,10 +35,10 @@ fn main() -> std::io::Result<()> {
         "type": "request",
         "command": "initialize",
         "arguments": {
-            "adapterID": "0001e357-72c7-4f03-ae8f-c5b54bd8dabf",
+            "adapterId": "0001e357-72c7-4f03-ae8f-c5b54bd8dabf",
             "clientName": "Some Cool Editor"
-        }
-    }"#;
+        }}
+"#;
 
     let response1 = send_request(&mut stream, request1)?;
     println!("Received response 1: {}", response1);
@@ -43,8 +50,8 @@ fn main() -> std::io::Result<()> {
         "command": "next",
         "arguments": {
             "threadId": 3
-        }
-    }"#;
+        }}
+"#;
 
     let response2 = send_request(&mut stream, request2)?;
     println!("Received response 2: {}", response2);
