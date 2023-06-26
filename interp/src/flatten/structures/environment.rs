4119ff22d3abb37b04ec44c2ca1fa5e4d2d28c74
Griffin Berlstein <griffin@berlste.in>
2023-06-26 11:28:07 -0400

    let the env hold a reference to the context

--- interp/src/flatten/structures/environment.rs
+++ interp/src/flatten/structures/environment.rs
@@ -73,7 +73,7 @@ pub(crate) struct ProgramCounter {
 }
 
 #[derive(Debug)]
-pub struct Environment {
+pub struct Environment<'a> {
     /// A map from global port IDs to their current values.
     ports: PortMap,
     /// A map from global cell IDs to their current state and execution info.
@@ -85,10 +85,13 @@ pub struct Environment {
 
     /// The program counter for the whole program execution.
     pcs: ProgramCounter,
+
+    /// The immutable context. This is retained for ease of use.
+    ctx: &'a Context,
 }
 
-impl Environment {
-    pub fn new(ctx: &Context) -> Self {
+impl<'a> Environment<'a> {
+    pub fn new(ctx: &'a Context) -> Self {
         let root = ctx.entry_point;
         let aux = &ctx.secondary[root];
 
@@ -102,29 +105,30 @@ impl Environment {
                 aux.ref_port_offset_map.count(),
             ),
             pcs: ProgramCounter {},
+            ctx,
         };
 
         let root_node = CellLedger::comp(root, &env);
         let root = env.cells.push(root_node);
-        env.layout_component(root, ctx);
+        env.layout_component(root);
 
         env
     }
 
-    fn layout_component(&mut self, comp: GlobalCellId, ctx: &Context) {
+    fn layout_component(&mut self, comp: GlobalCellId) {
         let ComponentLedger {
             index_bases,
             comp_id,
         } = self.cells[comp]
             .as_comp()
             .expect("Called layout component with a non-component cell.");
-        let comp_aux = &ctx.secondary[*comp_id];
+        let comp_aux = &self.ctx.secondary[*comp_id];
 
         let comp_id = *comp_id;
 
         // first layout the signature
         for sig_port in comp_aux.signature.iter() {
-            let width = ctx.lookup_port_def(&comp_id, sig_port).width;
+            let width = self.ctx.lookup_port_def(&comp_id, sig_port).width;
             let idx = self.ports.push(Value::zeroes(width));
             debug_assert_eq!(index_bases + sig_port, idx);
         }
@@ -136,18 +140,21 @@ impl Environment {
 
             //go
             let go = self.ports.push(Value::bit_low());
-            debug_assert_eq!(go, index_bases + ctx.primary[group_idx].go);
+            debug_assert_eq!(go, index_bases + self.ctx.primary[group_idx].go);
 
             //done
             let done = self.ports.push(Value::bit_low());
-            debug_assert_eq!(done, index_bases + ctx.primary[group_idx].done);
+            debug_assert_eq!(
+                done,
+                index_bases + self.ctx.primary[group_idx].done
+            );
         }
 
         for (cell_off, def_idx) in comp_aux.cell_offset_map.iter() {
-            let info = &ctx.secondary[*def_idx];
+            let info = &self.ctx.secondary[*def_idx];
             if !info.prototype.is_component() {
                 for port in info.ports.iter() {
-                    let width = ctx.lookup_port_def(&comp_id, port).width;
+                    let width = self.ctx.lookup_port_def(&comp_id, port).width;
                     let idx = self.ports.push(Value::zeroes(width));
                     debug_assert_eq!(
                         &self.cells[comp].as_comp().unwrap().index_bases + port,
@@ -171,13 +178,13 @@ impl Environment {
                     cell
                 );
 
-                self.layout_component(cell, ctx);
+                self.layout_component(cell);
             }
         }
 
         // ref cells and ports are initialized to None
         for (ref_cell, def_idx) in comp_aux.ref_cell_offset_map.iter() {
-            let info = &ctx.secondary[*def_idx];
+            let info = &self.ctx.secondary[*def_idx];
             for port_idx in info.ports.iter() {
                 let port_actual = self.ref_ports.push(None);
                 debug_assert_eq!(
