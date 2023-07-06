67e42438a6b2da0ef312acb75fce058774fdf56c
Basant Khalil <bak94@cornell.edu>
2023-07-06 11:27:25 -0400

    adding optional path argument

--- cider-dap/src/main.rs
+++ cider-dap/src/main.rs
@@ -1,27 +1,61 @@
+mod adapter;
+mod client;
+mod error;
+
+use adapter::MyAdapter;
+use client::TcpClient;
+use error::MyAdapterError;
+
 use dap::prelude::*;
+use error::AdapterResult;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
+use std::path::PathBuf;
 
-mod client;
-use client::TcpClient;
-mod adapter;
-use adapter::MyAdapter;
+#[derive(argh::FromArgs)]
+/// Positional arguments for file path
+struct Opts {
+    /// input file
+    #[argh(positional, from_str_fn(read_path))]
+    file: Option<PathBuf>,
+    #[argh(switch, long = "tcp")]
+    /// runs in tcp mode
+    is_multi_session: bool,
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    #[argh(option, short = 'p', long = "port", default = "8080")]
+    /// port for the TCP server
+    port: u16,
+}
+fn read_path(path: &str) -> Result<PathBuf, String> {
+    Ok(PathBuf::from(path))
+}
 
-    match listener.accept() {
-        Ok((stream, addr)) => {
-            println!("Accepted client on: {} ", addr);
-            handle_client(stream)?;
-        }
-        Err(_) => todo!(),
-    }
+fn main() -> AdapterResult<()> {
+    let opts: Opts = argh::from_env();
+    println!("{:?}", opts.file);
+    let path = match opts.file {
+        Some(p) => Ok(p),
+        None => Err(MyAdapterError::MissingFile),
+    }?;
+    /* let file = opts.file.unwrap_or_else(|| {
+        eprintln!("Missing input file argument");
+        std::process::exit(1);
+    });*/
 
+    /*  let listener = TcpListener::bind("127.0.0.1:8080")?;
+
+       match listener.accept() {
+           Ok((stream, addr)) => {
+               println!("Accepted client on: {}", addr);
+               handle_client(stream, file)?;
+           }
+           Err(_) => todo!(),
+       }
+    */
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+fn handle_client(mut stream: TcpStream, file: PathBuf) -> std::io::Result<()> {
     // Create a TcpClient instance with the TCP stream
     let client = TcpClient::new(stream.try_clone()?);
 
