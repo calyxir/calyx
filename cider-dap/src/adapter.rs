fd3a354584618f72b854e339a253ec842c6392be
Basant Khalil <bak94@cornell.edu>
2023-07-05 23:12:54 -0400

    Make adapter agnostic to read/write operations

--- cider-dap/src/adapter.rs
+++ cider-dap/src/adapter.rs
@@ -1,5 +1,3 @@
-use std::io::{self, Write};
-
 use dap::prelude::*;
 
 pub struct MyAdapter;
@@ -11,8 +9,6 @@ pub mod error {
     pub enum MyAdapterError {
         #[error("Unhandled command")]
         UnhandledCommandError,
-        #[error(transparent)]
-        Io(#[from] std::io::Error),
         // Add more error variants as needed
     }
 }
@@ -28,41 +24,12 @@ impl Adapter for MyAdapter {
         _ctx: &mut dyn Context,
     ) -> Result<Response, Self::Error> {
         eprintln!("Accept {:#?}\n", request.command);
-        let mut stdout = io::stdout();
 
         match &request.command {
-            Command::Initialize(args) => {
-                eprintln!("entered initialize handler");
-                if let Some(client_name) = args.client_name.as_ref() {
-                    writeln!(
-                        stdout,
-                        "> Client '{}' requested initialization.",
-                        client_name
-                    )
-                    .map_err(|err| MyAdapterError::from(err))?;
-                    stdout.flush().map_err(MyAdapterError::from)?;
-                    Ok(Response::make_success(
-                        &request,
-                        ResponseBody::Initialize(Some(types::Capabilities {
-                            supports_configuration_done_request: Some(true),
-                            supports_evaluate_for_hovers: Some(true),
-                            ..Default::default()
-                        })),
-                    ))
-                } else {
-                    writeln!(stdout, "Missing client name")
-                        .map_err(MyAdapterError::from)?;
-                    stdout.flush().map_err(MyAdapterError::from)?;
-                    Ok(Response::make_error(&request, "Missing client name"))
-                }
-            }
-            Command::Next(_) => {
-                writeln!(stdout, "Next command received")
-                    .map_err(MyAdapterError::from)?;
-                stdout.flush().map_err(MyAdapterError::from)?;
+            _ => {
+                // Handle the command generically
                 Ok(Response::make_ack(&request).unwrap())
             }
-            _ => Err(MyAdapterError::UnhandledCommandError),
         }
     }
 }
