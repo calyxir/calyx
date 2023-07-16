3fa217e62755d11246602ed28a5ebd8db478e14f
Andrew Butt <andrewb1999@gmail.com>
2023-07-16 15:22:05 -0400

    True single port memories

--- primitives/memories.sv
+++ primitives/memories.sv
@@ -14,19 +14,18 @@ module seq_mem_d1 #(
    input wire logic clk,
    input wire logic reset,
    input wire logic [IDX_SIZE-1:0] addr0,
+   input wire logic content_en,
+   output logic done,
 
    // Read signal
-   input wire logic read_en,
    output logic [ WIDTH-1:0] read_data,
-   output logic read_done,
 
    // Write signals
    input wire logic [ WIDTH-1:0] write_data,
-   input wire logic write_en,
-   output logic write_done
+   input wire logic write_en
 );
   // Internal memory
-  (* ram_style = "ultra" *)  logic [WIDTH-1:0] mem[SIZE-1:0];
+  logic [WIDTH-1:0] mem[SIZE-1:0];
 
   // Register for the read output
   logic [WIDTH-1:0] read_out;
@@ -36,10 +35,10 @@ module seq_mem_d1 #(
   always_ff @(posedge clk) begin
     if (reset) begin
       read_out <= '0;
-    end else if (read_en) begin
+    end else if (content_en && !write_en) begin
       /* verilator lint_off WIDTH */
       read_out <= mem[addr0];
-    end else if (write_en) begin
+    end else if (content_en && write_en) begin
       // Explicitly clobber the read output when a write is performed
       read_out <= 'x;
     end else begin
@@ -47,48 +46,33 @@ module seq_mem_d1 #(
     end
   end
 
-  // Propagate the read_done signal
+  // Propagate the done signal
   always_ff @(posedge clk) begin
     if (reset) begin
-      read_done <= '0;
-    end else if (read_en) begin
-      read_done <= '1;
+      done <= '0;
+    end else if (content_en) begin
+      done <= '1;
     end else begin
-      read_done <= '0;
+      done <= '0;
     end
   end
 
   // Write value to the memory
   always_ff @(posedge clk) begin
-    if (!reset && write_en)
+    if (!reset && content_en && write_en)
       mem[addr0] <= write_data;
-  end
-
-  // Propagate the write_done signal
-  always_ff @(posedge clk) begin
-    if (reset) begin
-      write_done <= '0;
-    end else if (write_en) begin
-      write_done <= 1'd1;
-    end else begin
-      write_done <= '0;
-    end
   end
 
   // Check for out of bounds access
   `ifdef VERILATOR
     always_comb begin
-      if (read_en)
+      if (content_en && !write_en)
         if (addr0 >= SIZE)
           $error(
             "std_mem_d1: Out of bounds access\n",
             "addr0: %0d\n", addr0,
             "SIZE: %0d", SIZE
           );
-    end
-    always_comb begin
-      if (read_en && write_en)
-        $error("Simultaneous read and write attempted\n");
     end
   `endif
 endmodule
@@ -105,24 +89,23 @@ module seq_mem_d2 #(
    input wire logic reset,
    input wire logic [D0_IDX_SIZE-1:0] addr0,
    input wire logic [D1_IDX_SIZE-1:0] addr1,
+   input wire logic content_en,
+   output logic done,
 
    // Read signal
-   input wire logic read_en,
    output logic [WIDTH-1:0] read_data,
-   output logic read_done,
 
    // Write signals
    input wire logic write_en,
-   input wire logic [ WIDTH-1:0] write_data,
-   output logic write_done
+   input wire logic [ WIDTH-1:0] write_data
 );
   wire [D0_IDX_SIZE+D1_IDX_SIZE-1:0] addr;
   assign addr = addr0 * D1_SIZE + addr1;
 
   seq_mem_d1 #(.WIDTH(WIDTH), .SIZE(D0_SIZE * D1_SIZE), .IDX_SIZE(D0_IDX_SIZE+D1_IDX_SIZE)) mem
      (.clk(clk), .reset(reset), .addr0(addr),
-    .read_en(read_en), .read_data(read_data), .read_done(read_done), .write_data(write_data), .write_en(write_en),
-    .write_done(write_done));
+    .content_en(content_en), .read_data(read_data), .write_data(write_data), .write_en(write_en),
+    .done(done));
 endmodule
 
 module seq_mem_d3 #(
@@ -140,24 +123,23 @@ module seq_mem_d3 #(
    input wire logic [D0_IDX_SIZE-1:0] addr0,
    input wire logic [D1_IDX_SIZE-1:0] addr1,
    input wire logic [D2_IDX_SIZE-1:0] addr2,
+   input wire logic content_en,
+   output logic done,
 
    // Read signal
-   input wire logic read_en,
    output logic [WIDTH-1:0] read_data,
-   output logic read_done,
 
    // Write signals
    input wire logic write_en,
-   input wire logic [ WIDTH-1:0] write_data,
-   output logic write_done
+   input wire logic [ WIDTH-1:0] write_data
 );
   wire [D0_IDX_SIZE+D1_IDX_SIZE+D2_IDX_SIZE-1:0] addr;
   assign addr = addr0 * (D1_SIZE * D2_SIZE) + addr1 * (D2_SIZE) + addr2;
 
   seq_mem_d1 #(.WIDTH(WIDTH), .SIZE(D0_SIZE * D1_SIZE * D2_SIZE), .IDX_SIZE(D0_IDX_SIZE+D1_IDX_SIZE+D2_IDX_SIZE)) mem
      (.clk(clk), .reset(reset), .addr0(addr),
-    .read_en(read_en), .read_data(read_data), .read_done(read_done), .write_data(write_data), .write_en(write_en),
-    .write_done(write_done));
+    .content_en(content_en), .read_data(read_data), .write_data(write_data), .write_en(write_en),
+    .done(done));
 endmodule
 
 module seq_mem_d4 #(
@@ -178,22 +160,21 @@ module seq_mem_d4 #(
    input wire logic [D1_IDX_SIZE-1:0] addr1,
    input wire logic [D2_IDX_SIZE-1:0] addr2,
    input wire logic [D3_IDX_SIZE-1:0] addr3,
+   input wire logic content_en,
+   output logic done,
 
    // Read signal
-   input wire logic read_en,
    output logic [WIDTH-1:0] read_data,
-   output logic read_done,
 
    // Write signals
    input wire logic write_en,
-   input wire logic [ WIDTH-1:0] write_data,
-   output logic write_done
+   input wire logic [ WIDTH-1:0] write_data
 );
   wire [D0_IDX_SIZE+D1_IDX_SIZE+D2_IDX_SIZE+D3_IDX_SIZE-1:0] addr;
   assign addr = addr0 * (D1_SIZE * D2_SIZE * D3_SIZE) + addr1 * (D2_SIZE * D3_SIZE) + addr2 * (D3_SIZE) + addr3;
 
   seq_mem_d1 #(.WIDTH(WIDTH), .SIZE(D0_SIZE * D1_SIZE * D2_SIZE * D3_SIZE), .IDX_SIZE(D0_IDX_SIZE+D1_IDX_SIZE+D2_IDX_SIZE+D3_IDX_SIZE)) mem
      (.clk(clk), .reset(reset), .addr0(addr),
-    .read_en(read_en), .read_data(read_data), .read_done(read_done), .write_data(write_data), .write_en(write_en),
-    .write_done(write_done));
+    .content_en(content_en), .read_data(read_data), .write_data(write_data), .write_en(write_en),
+    .done(done));
 endmodule
