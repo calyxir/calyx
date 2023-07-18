e941d900a229d8e62d93051cb3db898b810d2ddd
Anshuman Mohan <anshumanmohan@live.com>
2023-07-17 20:39:28 -0400

    fifo component being generated correctly

--- calyx-py/test/correctness/fifo.py
+++ calyx-py/test/correctness/fifo.py
@@ -2,57 +2,54 @@
 import calyx.builder as cb
 
 
-def add_eq(comp: cb.ComponentBuilder, port, const, cell, group):
+def add_eq(comp: cb.ComponentBuilder, port, const, cell, width, group):
     """Adds wiring into component {comp} to check if {port} == {const}.
     1. Within {comp}, creates a group called {group}.
-    2. Within {group}, creates a cell called {cell} that checks equality.
+    2. Within {group}, creates a cell called {cell} that checks equality of width {width}.
     3. Puts the values of {port} and {const} into {cell}.
     4. Returns the equality-checking cell and the equality-checking group.
     """
-    eq_cell = comp.eq(cell, 32)
+    eq_cell = comp.eq(cell, width)
     with comp.comb_group(group) as eq_group:
         eq_cell.left = port
         eq_cell.right = const
     return eq_cell, eq_group
 
 
-def add_lt(comp: cb.ComponentBuilder, port, const, cell, group):
-    """Adds wiring into component {comp} to check if {port} < {const}.
+def add_incr(comp: cb.ComponentBuilder, reg, cell, group):
+    """Adds wiring into component {comp} to increment {reg} by 1.
     1. Within component {comp}, creates a group called {group}.
-    2. Within {group}, creates a cell called {cell} that checks for less-than.
-    3. Puts the values of {port} and {const} into {cell}.
-    4. Returns the less-than-checking cell and the less-than-checking group.
+    2. Within {group}, adds a cell {cell} that computes sums.
+    3. Puts the values of {port} and 1 into {cell}.
+    4. Then puts the answer of the computation back into {port}.
+    4. Returns the add-computing group.
     """
-    lt_cell = comp.lt(cell, 32)
-    with comp.comb_group(group) as lt_group:
-        lt_cell.left = port
-        lt_cell.right = const
-    return lt_cell, lt_group
+    incr_cell = comp.add(cell, 32)
+    with comp.group(group) as incr_group:
+        incr_cell.left = reg.out
+        incr_cell.right = 1
+        reg.write_en = 1
+        reg.in_ = incr_cell.out
+        incr_group.done = reg.done
+    return incr_group
 
 
-def add_sub(comp: cb.ComponentBuilder, port, const, sub_cell, ans_reg, group):
-    """Adds wiring into component {comp} to compute {port} - {const}.
+def zero_out_reg(comp: cb.ComponentBuilder, reg, group):
+    """Adds wiring into component {comp} to zero out {reg}.
     1. Within component {comp}, creates a group called {group}.
-    2. Within {group}, assumes there is a cell {cell} that computes differences.
-    3. Puts the values of {port} and {const} into {cell}.
-    4. Then puts the answer of the computation into {ans_reg}.
-    4. Returns the sub-checking group.
+    2. Within {group}, adds a cell {cell} that computes sums.
+    3. Puts the values of {port} and 1 into {cell}.
+    4. Then puts the answer of the computation back into {port}.
+    4. Returns the add-computing group.
     """
-    # Note, this one is a little different than the others.
-    # 1. We assume the subtraction cell already exists.
-    # 2. We're not returning the cell, because we don't need to.
-    # 3. We write the answer into `ans_reg`.
-
-    with comp.group(group) as sub_group:
-        sub_cell.left = port
-        sub_cell.right = const
-        ans_reg.write_en = 1
-        ans_reg.in_ = sub_cell.out
-        sub_group.done = ans_reg.done
-    return sub_group
+    with comp.group(group) as zero_group:
+        reg.in_ = 0
+        reg.write_en = 1
+        zero_group.done = reg.done
+    return zero_group
 
 
-def add_mem_load(comp: cb.ComponentBuilder, mem, i, ans, group):
+def mem_load(comp: cb.ComponentBuilder, mem, i, ans, group):
     """Loads a value from one memory into another.
     1. Within component {comp}, creates a group called {group}.
     2. Within {group}, reads from memory {mem} at address {i}.
@@ -67,232 +64,169 @@ def add_mem_load(comp: cb.ComponentBuilder, mem, i, ans, group):
     return load_grp
 
 
-def add_reg_load(comp: cb.ComponentBuilder, port, ans_reg, group):
-    """Creates a group called {group}.
-    In that group, loads the value of {port} into {ans_reg}.
-    Returns the group.
+def mem_store(comp: cb.ComponentBuilder, mem, i, val, group):
+    """Stores a value from one memory into another.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, reads from memory {mem} at address {i}.
+    3. Writes the value into memory {ans} at address 0.
+    4. Returns the group that does this.
     """
-    with comp.group(group) as grp:
-        ans_reg.write_en = 1
-        ans_reg.in_ = port
-        grp.done = ans_reg.done
-    return grp
+    with comp.group(group) as store_grp:
+        mem.addr0 = i
+        mem.write_en = 1
+        mem.write_data = val
+        store_grp.done = mem.done
+    return store_grp
 
 
-def add_wrap2(prog):
-    """Inserts the component `wrap2` into the program.
+def set_flag(comp: cb.ComponentBuilder, flagname, flagval, group):
+    """Sets a flag to a value.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, sets the flag {flagname} to {flagval}.
+    3. Returns the group that does this.
+    """
+    with comp.group(group) as flag_grp:
+        flagname.in_ = flagval
+        flagname.write_en = 1
+        flag_grp.done = flagname.done
+    return flag_grp
+
+
+def set_flag_mem(comp: cb.ComponentBuilder, flagname, flagval, group):
+    """Sets a flag to a value.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, sets the flag {flagname} to {flagval}.
+    3. Returns the group that does this.
+    """
+    with comp.group(group) as flag_grp:
+        flagname.addr0 = 0
+        flagname.write_en = cb.const(1, 1)
+        flagname.write_data = flagval
+        flag_grp.done = flagname.done
+    return flag_grp
+
+
+def add_fifo(prog):
+    """Inserts the component `fifo` into the program.
 
     It has:
-    - two inputs, `i` and `j`.
-    - six ref memories, `mem1` through `mem6`, of size 4 each.
-    - one ref memory, `ans`.
-
-    The invoker wants to pretend that there are actually
-    _two_ memories of size _12_ each.
-    The invoker wants to index the memories while living in this fiction.
-    This component will return mem[i][j], but indexed according to the fiction.
-    Accordingly, we assume 0 <= i < 2 and 0 <= j < 12.
+    - three inputs, `pop`, `push`, and `payload`.
+    - one memory, `mem`, of size 10.
+    - four registers, `next_write`, `next_read`, `full`, and `empty`.
+    - two ref memories, `ans` and `err`.
     """
 
-    wrap: cb.ComponentBuilder = prog.component("wrap2")
-    i = wrap.input("i", 32)
-    j = wrap.input("j", 32)
+    fifo: cb.ComponentBuilder = prog.component("fifo")
+    pop = fifo.input("pop", 1)
+    push = fifo.input("push", 1)
+    payload = fifo.input("payload", 32)
 
-    # Six memory cells, plus an answer cell.
-    mems = [wrap.mem_d1(f"mem{i}", 32, 4, 32, is_ref=True) for i in range(6)]
-    ans = wrap.mem_d1("ans", 32, 1, 32, is_ref=True)
+    # A memory cell.
+    mem = fifo.mem_d1("mem", 32, 10, 32)
 
-    # We will need j % 4, so we'll store it in a cell.
-    j_mod_4 = wrap.reg("j_mod_4", 32)
+    # Four registers.
+    next_write = fifo.reg("next_write", 32)
+    next_read = fifo.reg("next_read", 32)
+    full = fifo.reg("full", 1)
+    empty = fifo.reg("empty", 1)
 
-    # Additional cells and groups to compute equality and lt
-    eq0cell, eq0grp = add_eq(wrap, i, 0, "eq0", "i_eq_0")
-    eq1cell, eq1grp = add_eq(wrap, i, 1, "eq1", "i_eq_1")
-    lt1cell, lt1grp = add_lt(wrap, j, 4, "lt1", "j_lt_4")
-    lt2cell, lt2grp = add_lt(wrap, j, 8, "lt2", "j_lt_8")
+    # Two ref memories.
+    ans = fifo.mem_d1("ans", 32, 1, 32, is_ref=True)
+    err = fifo.mem_d1("err", 1, 1, 1, is_ref=True)
 
-    # Load `j` unchanged into `j_mod_4`.
-    unchanged = add_reg_load(wrap, j, j_mod_4, "j_unchanged")
+    # Additional cells and groups to compute equality and sums
+    eq0cell, eq0grp = add_eq(fifo, pop, push, "eq0", 1, "pop_eq_push")
+    eq1cell, eq1grp = add_eq(fifo, pop, 1, "eq1", 1, "pop_eq_1")
+    eq2cell, eq2grp = add_eq(fifo, push, 1, "eq2", 1, "push_eq_1")
+    eq3cell, eq3grp = add_eq(
+        fifo, next_read.out, next_write.out, "eq3", 32, "next_read_eq_next_write"
+    )
 
-    # A subtraction cell and wiring to perform j-4 and j-8.
-    sub_cell = wrap.sub("sub", 32)
-    sub1cell = add_sub(wrap, j, cb.const(32, 4), sub_cell, j_mod_4, "j_minus_4")
-    sub2cell = add_sub(wrap, j, cb.const(32, 8), sub_cell, j_mod_4, "j_minus_8")
+    write_incr = add_incr(fifo, next_write, "add1", "next_write_incr")
+    read_incr = add_incr(fifo, next_read, "add2", "next_read_incr")
 
-    load_from_mems = [
-        # Add wiring to load the value `j_mod_4` from all of the memory cells.
-        # We'll have to invoke the correct one of these groups later on.
-        add_mem_load(wrap, mems[i], j_mod_4.out, ans, f"load_from_mem{i}")
-        for i in range(6)
-    ]
+    eq5acell, eq5agrp = add_eq(fifo, next_write.out, 10, "eq5b", 32, "next_write_eq_10")
+    eq5bcell, eq5bgrp = add_eq(fifo, next_read.out, 10, "eq5a", 32, "next_read_eq_10")
 
-    wrap.control += [
+    write_wrap = zero_out_reg(fifo, next_write, "next_write_wraparound")
+    read_wrap = zero_out_reg(fifo, next_read, "next_read_wraparound")
+
+    # Load and store
+    write_to_mem = mem_store(fifo, mem, next_write.out, payload, "write_payload_to_mem")
+    read_from_mem = mem_load(fifo, mem, next_read.out, ans, "read_payload_from_mem")
+
+    # Set flags
+    raise_full = set_flag(fifo, full, 1, "raise_full_flag")
+    lower_full = set_flag(fifo, full, 0, "lower_full_flag")
+    raise_empty = set_flag(fifo, empty, 1, "raise_empty_flag")
+    lower_empty = set_flag(fifo, empty, 0, "lower_empty_flag")
+
+    eq4acell, eq4agrp = add_eq(fifo, full.out, 1, "eq4a", 1, "is_full")
+    eq4bcell, eq4bgrp = add_eq(fifo, empty.out, 1, "eq4b", 1, "is_empty")
+
+    raise_err = set_flag_mem(fifo, err, cb.const(1, 1), "raise_err_flag")
+    lower_err = set_flag_mem(fifo, err, cb.const(1, 0), "lower_err_flag")
+
+    fifo.control += [
         cb.if_(
-            lt1cell.out,
-            lt1grp,
-            unchanged,
-            cb.if_(lt2cell.out, lt2grp, sub1cell, sub2cell),
-        ),
-        cb.par(
-            cb.if_(
-                eq0cell.out,
-                eq0grp,
+            eq0cell.out,
+            eq0grp,
+            raise_err,
+            cb.par(
                 cb.if_(
-                    lt1cell.out,
-                    lt1grp,
-                    load_from_mems[0],
+                    eq1cell.out,
+                    eq1grp,
                     cb.if_(
-                        lt2cell.out,
-                        lt2grp,
-                        load_from_mems[1],
-                        load_from_mems[2],
+                        eq4bcell.out,
+                        eq4bgrp,
+                        raise_err,
+                        [
+                            lower_err,
+                            read_from_mem,
+                            read_incr,
+                            cb.if_(eq5bcell.out, eq5bgrp, read_wrap),
+                            cb.if_(eq3cell.out, eq3grp, raise_empty),
+                            cb.if_(eq4acell.out, eq4agrp, lower_full),
+                        ],
+                    ),
+                ),
+                cb.if_(
+                    eq2cell.out,
+                    eq2grp,
+                    cb.if_(
+                        eq4acell.out,
+                        eq4agrp,
+                        raise_err,
+                        [
+                            lower_err,
+                            write_to_mem,
+                            write_incr,
+                            cb.if_(eq5acell.out, eq5agrp, write_wrap),
+                            cb.if_(eq3cell.out, eq3grp, raise_full),
+                            cb.if_(eq4bcell.out, eq4bgrp, lower_empty),
+                        ],
                     ),
                 ),
             ),
-            cb.if_(
-                eq1cell.out,
-                eq1grp,
-                cb.if_(
-                    lt1cell.out,
-                    lt1grp,
-                    load_from_mems[3],
-                    cb.if_(
-                        lt2cell.out,
-                        lt2grp,
-                        load_from_mems[4],
-                        load_from_mems[5],
-                    ),
-                ),
-            ),
-        ),
+        )
     ]
 
-    return wrap
+    return fifo
 
 
-def add_wrap3(prog):
-    """Inserts the component `wrap2` into the program.
-
-    It has:
-    - two inputs, `i` and `j`.
-    - six ref memories, `mem1` through `mem6`, of size 4 each.
-    - one ref memory, `ans`.
-
-    The invoker wants to pretend that there are actually
-    _two_ memories of size _12_ each.
-    The invoker wants to index the memories while living in this fiction.
-    This component will return mem[i][j], but indexed according to the fiction.
-    Accordingly, we assume 0 <= i < 2 and 0 <= j < 12.
-    """
-
-    wrap: cb.ComponentBuilder = prog.component("wrap3")
-    i = wrap.input("i", 32)
-    j = wrap.input("j", 32)
-
-    # Six memory cells, plus an answer cell.
-    mems = [wrap.mem_d1(f"mem{i}", 32, 4, 32, is_ref=True) for i in range(6)]
-    ans = wrap.mem_d1("ans", 32, 1, 32, is_ref=True)
-
-    # We will need j % 4, so we'll store it in a cell.
-    j_mod_4 = wrap.reg("j_mod_4", 32)
-
-    # Additional cells to compute equality, and lt
-    eq0cell, eq0grp = add_eq(wrap, i, 0, "eq0", "i_eq_0")
-    eq1cell, eq1grp = add_eq(wrap, i, 1, "eq1", "i_eq_1")
-    eq2cell, eq2grp = add_eq(wrap, i, 2, "eq2", "i_eq_2")
-    ltcell, ltgrp = add_lt(wrap, j, 4, "lt", "j_lt_4")
-
-    # Load `j` unchanged into `j_mod_4`.
-    unchanged = add_reg_load(wrap, j, j_mod_4, "j_unchanged")
-
-    # A subtraction cell and wiring to perform j-4.
-    sub_cell = wrap.sub("sub", 32)
-    subcell = add_sub(wrap, j, cb.const(32, 4), sub_cell, j_mod_4, "j_minus_4")
-
-    emit_from_mems = [
-        add_mem_load(wrap, mems[i], j_mod_4.out, ans, f"load_from_mem{i}")
-        for i in range(6)
-    ]
-
-    wrap.control += [
-        cb.if_(ltcell.out, ltgrp, unchanged, subcell),
-        cb.par(
-            cb.if_(
-                eq0cell.out,
-                eq0grp,
-                cb.if_(ltcell.out, ltgrp, emit_from_mems[0], emit_from_mems[1]),
-            ),
-            cb.if_(
-                eq1cell.out,
-                eq1grp,
-                cb.if_(ltcell.out, ltgrp, emit_from_mems[2], emit_from_mems[3]),
-            ),
-            cb.if_(
-                eq2cell.out,
-                eq2grp,
-                cb.if_(ltcell.out, ltgrp, emit_from_mems[4], emit_from_mems[5]),
-            ),
-        ),
-    ]
-
-    return wrap
-
-
-def add_main(prog, wrap2, wrap3):
+def add_main(prog, fifo):
     """Inserts the component `main` into the program.
-    This will be used to `invoke` the component `wrap`.
-
-    For now, I'd like to pass it memory cells `A` and `B` by reference,
-    along with the inputs i = 1, j = 3.
+    This will be used to `invoke` the component `fifo`.
     """
     main: cb.ComponentBuilder = prog.component("main")
-
-    # Six memory cells, plus an two answer cells.
-
-    [mem_a, mem_b, mem_c, mem_d, mem_e, mem_f] = [
-        main.mem_d1(name, 32, 4, 32, is_external=True)
-        for name in ["A", "B", "C", "D", "E", "F"]
-    ]
-    out2 = main.mem_d1("out2", 32, 1, 32, is_external=True)
-    out3 = main.mem_d1("out3", 32, 1, 32, is_external=True)
-
-    together2 = main.cell("together2", wrap2)
-    together3 = main.cell("together3", wrap3)
-
-    main.control += [
-        cb.invoke(
-            together2,
-            in_i=cb.const(32, 1),
-            in_j=cb.const(32, 11),
-            ref_mem0=mem_a,
-            ref_mem1=mem_b,
-            ref_mem2=mem_c,
-            ref_mem3=mem_d,
-            ref_mem4=mem_e,
-            ref_mem5=mem_f,
-            ref_ans=out2,
-        ),
-        cb.invoke(
-            together3,
-            in_i=cb.const(32, 2),
-            in_j=cb.const(32, 7),
-            ref_mem0=mem_a,
-            ref_mem1=mem_b,
-            ref_mem2=mem_c,
-            ref_mem3=mem_d,
-            ref_mem4=mem_e,
-            ref_mem5=mem_f,
-            ref_ans=out3,
-        ),
-    ]
+    fifo = main.cell("fifo", fifo)
 
 
 def build():
     """Top-level function to build the program."""
     prog = cb.Builder()
-    wrap2 = add_wrap2(prog)
-    wrap3 = add_wrap3(prog)
-    add_main(prog, wrap2, wrap3)
+    fifo = add_fifo(prog)
+    # add_main(prog, fifo)
     return prog.program
 
 
