dcbd1299d1621960054c53e6d0c07956d2b5a809
Anshuman Mohan <anshumanmohan@live.com>
2023-07-18 16:57:00 -0400

    New IO

--- calyx-py/test/correctness/fifo.py
+++ calyx-py/test/correctness/fifo.py
@@ -16,6 +16,21 @@ def add_eq(comp: cb.ComponentBuilder, a, b, cell, width):
     return eq_cell, eq_group
 
 
+def add_eq_mem(comp: cb.ComponentBuilder, mem, i, b, cell, width):
+    """Adds wiring into component {comp} to check if {mem[i]} == {b}.
+    1. Within {comp}, creates a group called {cell}_group.
+    2. Within the group, creates a cell {cell} that checks equalities of width {width}.
+    3. Puts the values {mem[i]} and {b} into {cell}.
+    4. Returns the equality-checking cell and the equality-checking group.
+    """
+    eq_cell = comp.eq(cell, width)
+    with comp.comb_group(f"{cell}_group") as eq_group:
+        eq_cell.left = mem.read_data
+        eq_cell.right = b
+        mem.addr0 = i
+    return eq_cell, eq_group
+
+
 def add_incr(comp: cb.ComponentBuilder, reg, cell, group):
     """Adds wiring into component {comp} to increment {reg} by 1.
     1. Within component {comp}, creates a group called {group}.
@@ -44,9 +59,24 @@ def mem_load(comp: cb.ComponentBuilder, mem, i, ans, group):
     with comp.group(group) as load_grp:
         mem.addr0 = i
         ans.write_en = 1
-        ans.addr0 = 0
+        ans.addr0 = cb.const(32, 0)
         ans.write_data = mem.read_data
         load_grp.done = ans.done
+    return load_grp
+
+
+def mem_load_to_reg(comp: cb.ComponentBuilder, mem, i, reg, group):
+    """Loads a value from one memory into a register.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, reads from memory {mem} at address {i}.
+    3. Writes the value into register {reg}.
+    4. Returns the group that does this.
+    """
+    with comp.group(group) as load_grp:
+        mem.addr0 = i
+        reg.write_en = 1
+        reg.in_ = mem.read_data
+        load_grp.done = reg.done
     return load_grp
 
 
@@ -61,6 +91,22 @@ def mem_store(comp: cb.ComponentBuilder, mem, i, val, group):
         mem.addr0 = i
         mem.write_en = 1
         mem.write_data = val
+        store_grp.done = mem.done
+    return store_grp
+
+
+def mem_store_from_mem(comp: cb.ComponentBuilder, mem, i, val_mem, val_i, group):
+    """Stores a value from one memory into another.
+    1. Within component {comp}, creates a group called {group}.
+    2. Within {group}, reads from {val_mem} at address {val_i}.
+    3. Writes the value into memory {mem} at address i.
+    4. Returns the group that does this.
+    """
+    with comp.group(group) as store_grp:
+        mem.addr0 = i
+        mem.write_en = 1
+        mem.write_data = val_mem.read_data
+        val_mem.addr0 = val_i
         store_grp.done = mem.done
     return store_grp
 
@@ -152,8 +198,7 @@ def add_fifo(prog):
     lower_full = set_flag_reg(fifo, full, 0, "lower_full")  # set `full` to 0
     raise_empty = set_flag_reg(fifo, empty, 1, "raise_empty")  # set `empty` to 1
     lower_empty = set_flag_reg(fifo, empty, 0, "lower_empty")  # set `empty` to 0
-    raise_err = set_flag_mem(fifo, err, cb.const(1, 1), "raise_err")  # set `err` to 1
-    lower_err = set_flag_mem(fifo, err, cb.const(1, 0), "lower_err")  # set `err` to 0
+    raise_err = mem_store(fifo, err, 0, cb.const(1, 1), "raise_err")  # set `err` to 1
 
     # Load and store into arbitary slot in memory
     write_to_mem = mem_store(fifo, mem, write.out, payload, "write_payload_to_mem")
@@ -176,7 +221,6 @@ def add_fifo(prog):
                         empty_eq_1[1],
                         raise_err,  # The queue is empty: underflow.
                         [  # The queue is not empty. Proceed.
-                            lower_err,  # Clear the error flag.
                             read_from_mem,  # Read from the queue.
                             read_incr,  # Increment the read pointer.
                             cb.if_(
@@ -210,7 +254,6 @@ def add_fifo(prog):
                         full_eq_1[1],
                         raise_err,  # The queue is full: overflow.
                         [  # The queue is not full. Proceed.
-                            lower_err,  # Clear the error flag.
                             write_to_mem,  # Write to the queue.
                             write_incr,  # Increment the write pointer.
                             cb.if_(
@@ -247,56 +290,92 @@ def add_main(prog, fifo):
     """
     main: cb.ComponentBuilder = prog.component("main")
 
-    ans = main.mem_d1("ans_in", 32, 1, 32, is_external=True)
-    err = main.mem_d1("err_in", 1, 1, 1, is_external=True)
+    # The user-facing interface is:
+    # - a list of commands (the input)
+    #    where each command is a 32-bit unsigned integer, with the following format:
+    #    `0`: pop
+    #    any other value: push that value
+    # - a list of answers (the output).
+    commands = main.mem_d1("commands", 32, 15, 32, is_external=True)
+    ans_mem = main.mem_d1("ans_mem", 32, 10, 32, is_external=True)
+
+    # We will use the `invoke` method to call the `fifo` component.
     fifo = main.cell("myfifo", fifo)
+    # The component takes as `ref` input:
+    err = main.mem_d1("err", 1, 1, 1)  # A flag to indicate an error
+    ans = main.mem_d1("ans", 32, 1, 32)  # A memory to hold the answer of a pop
+    # We will set up a while loop that runs over the command list, relaying
+    # the commands to the `fifo` component.
+    # It will run until the `err` flag is raised by the `fifo` component or
+    # until it reaches the end of the command list.
 
-    ten_pushes = [
-        cb.invoke(
-            fifo,
-            in_pop=cb.const(1, 0),
-            in_push=cb.const(1, 1),
-            in_payload=cb.const(32, 100 + i),
-            ref_ans=ans,
-            ref_err=err,
-        )
-        for i in range(10)
+    i = main.reg("i", 32)  # The index of the command we're currently processing
+    j = main.reg("j", 32)  # The index on the answer-list we'll write to
+    command = main.reg("command", 32)  # The command we're currently processing
+
+    zero_i = set_flag_reg(main, i, 0, "zero_i")  # zero out `i`
+    zero_j = set_flag_reg(main, j, 0, "zero_j")  # zero out `j`
+    incr_i = add_incr(main, i, "add3", "incr_i")  # i = i + 1
+    incr_j = add_incr(main, j, "add4", "incr_j")  # j = j + 1
+    err_eq_zero = add_eq_mem(main, err, 0, 0, "err_eq_0", 1)  # is the `err` flag down?
+    # i_eq_15 = add_eq(main, i.out, 15, "i_eq_15", 32)  # is `i` == 15?
+    # raise_err = mem_store(main, err, 0, cb.const(1, 1), "raise_err")  # set `err` to 1
+    read_command = mem_load_to_reg(main, commands, i.out, command, "read_command")
+    command_eq_zero = add_eq(main, command.out, 0, "command_eq_zero", 32)
+    write_ans = mem_store_from_mem(main, ans_mem, j.out, ans, 0, "write_ans")
+
+    main.control += [
+        zero_i,
+        zero_j,
+        cb.while_(
+            err_eq_zero[0].out,
+            err_eq_zero[1],  # Run while the `err` flag is down
+            [
+                read_command,  # Read the command at `i`
+                cb.if_(
+                    # Is this a pop or a push?
+                    command_eq_zero[0].out,
+                    command_eq_zero[1],
+                    [  # a pop
+                        cb.invoke(  # first we call pop
+                            fifo,
+                            in_pop=cb.const(1, 1),
+                            in_push=cb.const(1, 0),
+                            ref_ans=ans,
+                            ref_err=err,
+                        ),
+                        cb.if_(  # but was it successful?
+                            err_eq_zero[0].out,
+                            err_eq_zero[1],
+                            [  # yes, it was successful: write the answer to ans_mem
+                                write_ans,
+                                incr_j,
+                            ],
+                        ),
+                        # AM:
+                        # The if-check above is giving me trouble:
+                        # I need to check on the `err` flag, but I am getting complaints
+                        # from Calyx because the same `err` flag is checked in the while-loop.
+                    ],
+                    cb.invoke(  # a push
+                        fifo,
+                        in_pop=cb.const(1, 0),
+                        in_push=cb.const(1, 1),
+                        in_payload=command.out,
+                        ref_ans=ans,
+                        ref_err=err,
+                    ),
+                ),
+                incr_i,  # Increment the command index
+                # cb.if_(
+                #     # Did we reach the end of the command list?
+                #     i_eq_15[0].out,
+                #     i_eq_15[1],
+                #     raise_err,  # Yes, raise the error flag.
+                # ),
+            ],
+        ),
     ]
-
-    pop = cb.invoke(
-        fifo,
-        in_pop=cb.const(1, 1),
-        in_push=cb.const(1, 0),
-        ref_ans=ans,
-        ref_err=err,
-    )
-
-    ten_pops = [pop for _ in range(10)]
-
-    main.control += (
-        ten_pushes  # These will succeed
-        + [
-            cb.invoke(  # This will fail
-                fifo,
-                in_pop=cb.const(1, 0),
-                in_push=cb.const(1, 1),
-                in_payload=cb.const(32, 110),
-                ref_ans=ans,
-                ref_err=err,
-            ),
-            pop,  # This will succeed
-            cb.invoke(  # As will this
-                fifo,
-                in_pop=cb.const(1, 0),
-                in_push=cb.const(1, 1),
-                in_payload=cb.const(32, 110),
-                ref_ans=ans,
-                ref_err=err,
-            ),
-        ]
-        + ten_pops  # These will succeed
-        + [pop]  # This will fail
-    )
 
 
 def build():
