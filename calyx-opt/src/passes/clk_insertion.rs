f9b2881142c05ce2675a9c3dab0e581b11b4b949
Rachit Nigam <rachit.nigam12@gmail.com>
2023-07-26 14:18:13 -0400

    Dont require `@clk` and `@reset` ports in comb components (#1641)
    * comb components in clk and reset insertion
    
    * update changelog

--- calyx-opt/src/passes/clk_insertion.rs
+++ calyx-opt/src/passes/clk_insertion.rs
@@ -1,5 +1,6 @@
 use crate::traversal::{Action, Named, VisResult, Visitor};
 use calyx_ir::{self as ir, LibrarySignatures};
+use calyx_utils::Error;
 use std::rc::Rc;
 
 #[derive(Default)]
@@ -25,22 +26,38 @@ impl Visitor for ClkInsertion {
         _comps: &[ir::Component],
     ) -> VisResult {
         let builder = ir::Builder::new(comp, sigs);
+        // Find @clk port in the component. If it doesn't exist,
+        // then we don't need to do anything.
         let clk = builder
             .component
             .signature
             .borrow()
-            .get_with_attr(ir::BoolAttr::Clk);
+            .find_with_attr(ir::BoolAttr::Clk);
 
-        for cell_ref in builder.component.cells.iter() {
-            let cell = cell_ref.borrow();
-            if let Some(port) = cell.find_with_attr(ir::BoolAttr::Clk) {
-                builder.component.continuous_assignments.push(
-                    builder.build_assignment(
-                        port,
-                        Rc::clone(&clk),
-                        ir::Guard::True,
-                    ),
-                )
+        if let Some(clk) = clk {
+            for cell_ref in builder.component.cells.iter() {
+                let cell = cell_ref.borrow();
+                if let Some(port) = cell.find_with_attr(ir::BoolAttr::Clk) {
+                    builder.component.continuous_assignments.push(
+                        builder.build_assignment(
+                            port,
+                            Rc::clone(&clk),
+                            ir::Guard::True,
+                        ),
+                    )
+                }
+            }
+        } else {
+            for cell_ref in builder.component.cells.iter() {
+                let cell = cell_ref.borrow();
+                if cell.find_with_attr(ir::BoolAttr::Clk).is_some() {
+                    return Err(Error::malformed_structure(format!(
+                        "Cell `{}' in component `{}' has a clk port, \
+                        but the component does not have a clk port.",
+                        cell.name(),
+                        builder.component.name
+                    )));
+                }
             }
         }
 
