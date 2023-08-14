6dc5892b33bc449564aded2990ea7e2d8064d682
Anshuman Mohan <anshumanmohan@live.com>
2023-08-14 16:17:36 -0400

    Nit

--- calyx-py/test/correctness/pifo_tree.py
+++ calyx-py/test/correctness/pifo_tree.py
@@ -12,7 +12,7 @@ def build():
     fifo_tangerine = fifo.insert_fifo(prog, "fifo_tangerine")
     pifo_red = pifo.insert_pifo(prog, "pifo_red", fifo_purple, fifo_tangerine, 100)
     fifo_blue = fifo.insert_fifo(prog, "fifo_blue")
-    pifo_root = pifo.insert_pifo(prog, "pifo", pifo_red, fifo_blue, 200)
+    pifo_root = pifo.insert_pifo(prog, "pifo_root", pifo_red, fifo_blue, 200)
     qc.insert_main(prog, pifo_root)
     return prog.program
 
