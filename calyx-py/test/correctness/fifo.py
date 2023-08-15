d8534de40f35c93e15c61e76d22b5a4971b9a482
Anshuman Mohan <anshumanmohan@live.com>
2023-08-15 12:39:08 -0400

    Catch up queue_call, fifo to new interface

--- calyx-py/test/correctness/fifo.py
+++ calyx-py/test/correctness/fifo.py
@@ -8,15 +8,17 @@ def insert_fifo(prog, name):
     """Inserts the component `fifo` into the program.
 
     It has:
-    - one input, `cmd`.
+    - two inputs, `cmd` and `value`.
     - one memory, `mem`, of size 10.
     - two registers, `next_write` and `next_read`.
     - two ref registers, `ans` and `err`.
     """
 
     fifo: cb.ComponentBuilder = prog.component(name)
-    cmd = fifo.input("cmd", 32)
-    # If this is 0, we pop. If it is 1, we peek. Otherwise, we push the value.
+    cmd = fifo.input("cmd", 2)
+    # If this is 0, we pop. If it is 1, we peek.
+    # If it is 2, we push `value` to the queue.
+    value = fifo.input("value", 32)  # The value to push to the queue
 
     mem = fifo.seq_mem_d1("mem", 32, 10, 32)
     write = fifo.reg("next_write", 32)  # The next address to write to
@@ -25,44 +27,37 @@ def insert_fifo(prog, name):
     # simulate a circular queue of size 10.
 
     ans = fifo.reg("ans", 32, is_ref=True)
-    # If the user wants to pop, we will write the popped value to `ans`
+    # If the user wants to pop or peek, we will write the value to `ans`.
 
     err = fifo.reg("err", 1, is_ref=True)
-    # We'll raise this as a general error flag for overflow and underflow
+    # We'll raise this as a general error flag for overflow and underflow.
 
-    len = fifo.reg("len", 32)  # The length of the FIFO
+    len = fifo.reg("len", 32)  # The length of the FIFO.
 
-    # Cells and groups to compute equality
-    cmd_eq_0 = util.insert_eq(fifo, cmd, 0, "cmd_eq_0", 32)  # `cmd` == 0
-    cmd_eq_1 = util.insert_eq(fifo, cmd, 1, "cmd_eq_1", 32)  # `cmd` == 1
-    cmd_gt_1 = util.insert_gt(fifo, cmd, 1, "cmd_gt_1", 32)  # `cmd` > 1
+    # Cells and groups to compute equality.
+    cmd_eq_0 = util.insert_eq(fifo, cmd, 0, "cmd_eq_0", 2)
+    cmd_eq_1 = util.insert_eq(fifo, cmd, 1, "cmd_eq_1", 2)
+    cmd_eq_2 = util.insert_eq(fifo, cmd, 2, "cmd_eq_2", 2)
+    write_eq_10 = util.insert_eq(fifo, write.out, 10, "write_eq_10", 32)
+    read_eq_10 = util.insert_eq(fifo, read.out, 10, "read_eq_10", 32)
+    len_eq_0 = util.insert_eq(fifo, len.out, 0, "len_eq_0", 32)
+    len_eq_10 = util.insert_eq(fifo, len.out, 10, "len_eq_10", 32)
 
-    write_eq_10 = util.insert_eq(
-        fifo, write.out, 10, "write_eq_10", 32
-    )  # `write` == 10
-    read_eq_10 = util.insert_eq(fifo, read.out, 10, "read_eq_10", 32)  # `read` == 10
-    len_eq_0 = util.insert_eq(fifo, len.out, 0, "len_eq_0", 32)  # `len` == 0
-    len_eq_10 = util.insert_eq(fifo, len.out, 10, "len_eq_10", 32)  # `len` == 10
-
-    # Cells and groups to increment read and write registers
+    # Cells and groups to increment read and write registers.
     write_incr = util.insert_incr(fifo, write, "write_incr")  # write++
     read_incr = util.insert_incr(fifo, read, "read_incr")  # read++
     len_incr = util.insert_incr(fifo, len, "len_incr")  # len++
     len_decr = util.insert_decr(fifo, len, "len_decr")  # len--
 
-    # Cells and groups to modify flags, which are registers
-    write_wrap = util.insert_reg_store(
-        fifo, write, 0, "write_wraparound"
-    )  # zero out `write`
-    read_wrap = util.insert_reg_store(
-        fifo, read, 0, "read_wraparound"
-    )  # zero out `read`
-    raise_err = util.insert_reg_store(fifo, err, 1, "raise_err")  # set `err` to 1
-    zero_out_ans = util.insert_reg_store(fifo, ans, 0, "zero_out_ans")  # zero out `ans`
+    # Cells and groups to modify flags, which are registers.
+    flash_write = util.insert_reg_store(fifo, write, 0, "flash_write")  # write := 0
+    flash_read = util.insert_reg_store(fifo, read, 0, "flash_read")  # read := 0
+    flash_ans = util.insert_reg_store(fifo, ans, 0, "flash_ans")  # ans := 0
+    raise_err = util.insert_reg_store(fifo, err, 1, "raise_err")  # err := 1
 
-    # Load and store into an arbitary slot in memory
+    # Load/store to/from an arbitary slot in `mem`.
     write_to_mem = util.mem_store_seq_d1(
-        fifo, mem, write.out, cmd, "write_payload_to_mem"
+        fifo, mem, write.out, value, "write_payload_to_mem"
     )
     read_from_mem = util.mem_read_seq_d1(
         fifo, mem, read.out, "read_payload_from_mem_phase1"
@@ -82,7 +77,7 @@ def insert_fifo(prog, name):
                     # Yes, the user called pop. But is the queue empty?
                     len_eq_0[0].out,
                     len_eq_0[1],
-                    [raise_err, zero_out_ans],  # The queue is empty: underflow.
+                    [raise_err, flash_ans],  # The queue is empty: underflow.
                     [  # The queue is not empty. Proceed.
                         read_from_mem,  # Read from the queue.
                         write_to_ans,  # Write the answer to the answer register.
@@ -91,7 +86,7 @@ def insert_fifo(prog, name):
                             # Wrap around if necessary.
                             read_eq_10[0].out,
                             read_eq_10[1],
-                            read_wrap,
+                            flash_read,
                         ),
                         len_decr,  # Decrement the length.
                     ],
@@ -104,7 +99,7 @@ def insert_fifo(prog, name):
                 cb.if_(  # Yes, the user called peek. But is the queue empty?
                     len_eq_0[0].out,
                     len_eq_0[1],
-                    [raise_err, zero_out_ans],  # The queue is empty: underflow.
+                    [raise_err, flash_ans],  # The queue is empty: underflow.
                     [  # The queue is not empty. Proceed.
                         read_from_mem,  # Read from the queue.
                         write_to_ans,  # Write the answer to the answer register.
@@ -114,21 +109,21 @@ def insert_fifo(prog, name):
             ),
             cb.if_(
                 # Did the user call push?
-                cmd_gt_1[0].out,
-                cmd_gt_1[1],
+                cmd_eq_2[0].out,
+                cmd_eq_2[1],
                 cb.if_(
                     # Yes, the user called push. But is the queue full?
                     len_eq_10[0].out,
                     len_eq_10[1],
-                    [raise_err, zero_out_ans],  # The queue is full: overflow.
+                    [raise_err, flash_ans],  # The queue is full: overflow.
                     [  # The queue is not full. Proceed.
-                        write_to_mem,  # Write to the queue.
+                        write_to_mem,  # Write `value` to the queue.
                         write_incr,  # Increment the write pointer.
                         cb.if_(
                             # Wrap around if necessary.
                             write_eq_10[0].out,
                             write_eq_10[1],
-                            write_wrap,
+                            flash_write,
                         ),
                         len_incr,  # Increment the length.
                     ],
