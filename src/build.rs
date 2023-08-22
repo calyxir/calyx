0dd9bdab602fdd7fa10ca931bd83e03abdd75150
Rachit Nigam <rachit.nigam12@gmail.com>
2023-08-22 19:15:31 +0530

    Install calyx primitives in global directory

--- src/build.rs
+++ src/build.rs
@@ -1,10 +1,19 @@
-use std::fs;
-use std::io::Result;
-use std::path;
+use std::{env, fs, io::Result, path};
+
+/// Location to install the primitives library
+const PRIM_DIR: &str = "CALYX_PRIMITIVES_DIR";
 
 fn write_primitive() -> Result<path::PathBuf> {
     // Get the OUT_DIR environment variable from Cargo.
-    let base: path::PathBuf = std::env::var("OUT_DIR").unwrap().into();
+    let base: path::PathBuf = match env::var_os(PRIM_DIR) {
+        Some(v) => path::PathBuf::from(v),
+        None => {
+            println!("cargo:warning={PRIM_DIR} is not set. Using $HOME/.calyx as the default location for the primitives library.");
+            let mut path: path::PathBuf = env::var_os("HOME").unwrap().into();
+            path.push(".calyx");
+            path
+        }
+    };
     let mut prims = base.clone();
     prims.push("primitives");
     fs::create_dir_all(&prims)?;
@@ -29,10 +38,7 @@ fn main() {
         Ok(p) => {
             // The build succeeded. We're going to define the CALYX_PRIMITVE_DIR environment variable
             // so that it can be used by the compiler.
-            println!(
-                "cargo:rustc-env=CALYX_PRIMITIVES_DIR={}",
-                p.to_string_lossy()
-            );
+            println!("cargo:rustc-env={PRIM_DIR}={}", p.to_string_lossy());
         }
         Err(e) => {
             println!(
