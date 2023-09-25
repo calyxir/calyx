6c1287e481731f833fb25130f542a15c8c9f3350
Ryan Doenges <me@ryandoeng.es>
2023-09-25 15:43:48 -0400

    semantics for std_mem_d1

--- vcalyx/coq/Semantics.v
+++ vcalyx/coq/Semantics.v
@@ -19,6 +19,18 @@ Scheme Equality for value.
 Global Instance value_EqDec : EqDec value eq :=
   value_eq_dec.
 
+Definition expect_V (v: value) : option N :=
+  match v with
+  | V val => Some val
+  | _ => None
+  end.
+
+Definition is_high (v: value) :=
+  v ==b (V 1%N).
+
+Definition bool_to_value (b: bool) : value :=
+  if b then V 1%N else V 0%N.
+
 (* Testing out the eqdec instance *)
 Eval compute in (Z == Z).
 Eval compute in (Z ==b V 12).
@@ -36,8 +48,8 @@ Definition mem_data := list N.
 Inductive state : Type :=
 (* std_reg *)
 | StateReg (write_done: bool) (val: value)
-(* std_mem *)
-| StateMem (fmt: mem_fmt) (contents: mem_data).
+(* std_mem_d1 *)
+| StateMemD1 (write_done: bool) (fmt: mem_fmt) (contents: mem_data).
 
 Definition get_reg_state (st: state) :=
   match st with
@@ -45,15 +57,11 @@ Definition get_reg_state (st: state) :=
   | _ => None
   end.
 
-Definition get_mem_state (st: state) :=
+Definition get_mem_d1_state (st: state) :=
   match st with
-  | StateMem fmt contents => Some (fmt, contents)
+  | StateMemD1 write_done fmt contents => Some (write_done, fmt, contents)
   | _ => None
   end.
-
-Inductive hole :=
-| Go
-| Done.
 
 Section Semantics.
   (* ENVIRONMENTS AND STORES *)
@@ -70,8 +78,13 @@ Section Semantics.
 
   (* environment collecting all defined cells *)
   Definition cell_env : Type := ident_map cell.
+
+  Record prim_sem :=
+    { prim_sem_poke: state -> val_map -> option val_map;
+      prim_sem_tick: state -> val_map -> option state }.
+  
   (* An environment collecting all defined primitives *)
-  Definition prim_map : Type := ident_map (state -> val_map -> option (val_map)).
+  Definition prim_map : Type := ident_map prim_sem.
 
   (* An environment collecting all defined groups *)
   Definition group_env : Type := ident_map group.
@@ -82,9 +95,38 @@ Section Semantics.
   Definition calyx_prims : prim_map :=
     list_to_map 
       [("std_reg",
-         fun st inputs =>
-           '(_, v) ← get_reg_state st;
-           Some (<["out" := v]>inputs))].
+         {| prim_sem_poke st inputs :=
+              '(write_done, v) ← get_reg_state st;
+              Some (<["done" := bool_to_value write_done]>(<["out" := v]>inputs));
+            prim_sem_tick st inputs :=
+              '(_, val_old) ← get_reg_state st;
+              write_en ← inputs !! "write_en";
+              if is_high write_en
+              then val_in ← inputs !! "in";
+                   Some (StateReg true val_in)
+              else Some (StateReg false val_old)
+         |});
+       ("std_mem_d1",
+         {| prim_sem_poke st inputs :=
+              '(write_done, fmt, contents) ← get_mem_d1_state st;
+              addr ← inputs !! "addr0";
+              mem_val ← match addr with
+                        | Z => Some Z
+                        | V idx => V <$> contents !! (N.to_nat idx)
+                        | X => Some X
+                        end;
+              Some (<["done" := bool_to_value write_done]>(<["read_data" := mem_val]>inputs));
+           prim_sem_tick st inputs :=
+              '(_, fmt, contents) ← get_mem_d1_state st;
+              write_en ← inputs !! "write_en";
+              if is_high write_en
+              then val_in ← inputs !! "write_data";
+                   val ← expect_V val_in;
+                   addr ← inputs !! "addr0";
+                   idx ← expect_V addr;
+                   Some (StateMemD1 true fmt (<[N.to_nat idx := val]>contents))
+              else Some (StateMemD1 false fmt contents)
+         |})].
   
   Definition is_entrypoint (entrypoint: ident) (c: comp) : bool :=
     bool_decide (entrypoint = c.(comp_name)).
@@ -141,8 +183,8 @@ Section Semantics.
 
   (* COMBINATIONAL UPDATES *)
   Definition poke_prim (prim: ident) (param_binding: list (ident * N)) (st: state) (inputs: val_map) : option val_map := 
-    fn ← calyx_prims !! prim;
-    fn st inputs.
+    fns ← calyx_prims !! prim;
+    fns.(prim_sem_poke) st inputs.
   
   Definition poke_cell (c: cell) (ρ: state_map) (σ: cell_map) : option cell_map :=
     match c.(cell_proto) with
@@ -154,8 +196,19 @@ Section Semantics.
     | _ => None (* unimplemented *)
     end.
 
-  Definition tick_cell (c: cell) (ρ: state_map) (σ: cell_map) : option (state_map * cell_map) :=
-    None.
+  Definition tick_prim (prim: ident) (param_binding: list (ident * N)) (st: state) (inputs: val_map) : option state := 
+    fns ← calyx_prims !! prim;
+    fns.(prim_sem_tick) st inputs.
+
+  Definition tick_cell (c: cell) (ρ: state_map) (σ: cell_map) : option state_map :=
+    match c.(cell_proto) with
+    | ProtoPrim prim param_binding _ =>
+        st ← ρ !! c.(cell_name);
+        vs ← σ !! c.(cell_name);
+        st' ← tick_prim prim param_binding st vs;
+        Some (<[c.(cell_name) := st']>ρ)
+    | _ => None (* unimplemented *)
+    end.
 
   Definition poke_all_cells (ce: cell_env) (ρ: state_map) (σ: cell_map) : option cell_map :=
     map_fold (fun _ cell σ_opt =>
@@ -165,11 +218,11 @@ Section Semantics.
              ce.
 
   (* Update the state, invalidate outgoing wires *)
-  Definition tick_all_cells (ce: cell_env) (ρ: state_map) (σ: cell_map) : option (state_map * cell_map) :=
-    map_fold (fun _ cell ρσ_opt =>
-                '(ρ, σ) ← ρσ_opt;
+  Definition tick_all_cells (ce: cell_env) (ρ: state_map) (σ: cell_map) : option state_map :=
+    map_fold (fun _ cell ρ_opt =>
+                ρ ← ρ_opt;
                 tick_cell cell ρ σ)
-             (Some (ρ, σ))
+             (Some ρ)
              ce.
 
   Definition catch {X} (c1 c2: option X) : option X :=
@@ -223,12 +276,12 @@ Section Semantics.
   Definition is_done (γ: group_map) (g: group) : bool :=
     match holes ← γ !! g.(group_name);
           holes !! "done" with
-    | Some v => v ==b (V 1%N)
+    | Some v => is_high v
     | None => false
     end.
 
   Fixpoint interp_group (ce: cell_env) (ρ: state_map) (σ: cell_map) (γ: group_map) (g: group) (gas: nat) : option (state_map * cell_map * group_map) :=
-    '(ρ, σ) ← tick_all_cells ce ρ σ;
+    ρ ← tick_all_cells ce ρ σ;
     '(σ, γ) ← poke_group ce ρ σ γ g;
     if is_done γ g
     then Some (ρ, σ, γ)
