2bcd8db9c327531c504ff4bf327303b037264ed7
Nathaniel Navarro <nathanielnrn99@gmail.com>
2023-09-24 22:09:41 -0400

    Let IDL generator handle multi-dim mems
    Previously could only handle 1 dimension mems.
    NOTE: This flattens dimensions into a single size variable that is spit
    out. However it may be useful to maintain info about shape of memory?
    Need to think about this more

--- calyx-backend/src/xilinx/idl.rs
+++ calyx-backend/src/xilinx/idl.rs
@@ -14,7 +14,7 @@ pub struct IdlBackend;
 #[derive(Serialize)]
 struct ProgramInterface<'a> {
     toplevel_name: &'a str,
-    memories : Vec<Memory<'a>>,
+    memories: Vec<Memory<'a>>,
 }
 
 #[derive(Serialize)]
@@ -51,18 +51,17 @@ impl Backend for IdlBackend {
             .unwrap();
 
         let memory_names = external_memories(toplevel);
-        let mem_infos  = get_mem_info(toplevel);
-        
+        let mem_infos = get_mem_info(toplevel);
+
         let memories: Vec<Memory> = memory_names
-        .iter()
-        .zip(mem_infos.iter())
-        .map(|(memory_name, mem_info)| {
-            Memory{
+            .iter()
+            .zip(mem_infos.iter())
+            .map(|(memory_name, mem_info)| Memory {
                 name: memory_name,
                 width: mem_info.width,
                 size: mem_info.size,
-            }
-        }).collect();
+            })
+            .collect();
 
         let program_interface = ProgramInterface {
             toplevel_name: &toplevel.name.to_string(),
@@ -75,53 +74,77 @@ impl Backend for IdlBackend {
         write!(
             file.get_write(),
             "{}",
-            serde_json::to_string_pretty(&program_interface).expect("IDL Serialization failed")
+            serde_json::to_string_pretty(&program_interface)
+                .expect("IDL Serialization failed")
         )?;
 
         Ok(())
     }
-    }
+}
 
+/// Parameters for single dimensional memory
+struct MemInfo {
+    width: u64,
+    size: u64,
+}
 
+// Gets all external memory cells in top level
+fn external_memories_cells(comp: &ir::Component) -> Vec<ir::RRC<ir::Cell>> {
+    comp.cells
+        .iter()
+        // find external memories
+        .filter(|cell_ref| {
+            let cell = cell_ref.borrow();
+            if cell.attributes.has(ir::BoolAttr::External) {
+                true
+            } else {
+                false
+            }
+        })
+        .cloned()
+        .collect()
+}
 
-    /// Parameters for single dimensional memory
-    struct MemInfo {
-        width: u64,
-        size: u64,
-    }
-
-    // Gets all external memory cells in top level
-    fn external_memories_cells(comp: &ir::Component) -> Vec<ir::RRC<ir::Cell>> {
-        comp.cells
-            .iter()
-            // find external memories
-            .filter(|cell_ref| {
-                let cell = cell_ref.borrow();
-                if cell.attributes.has(ir::BoolAttr::External) {
-                    true
-                } else {
-                    false
+fn get_mem_info(comp: &ir::Component) -> Vec<MemInfo> {
+    external_memories_cells(comp)
+        .iter()
+        .map(|cr| {
+            let cell = cr.borrow();
+            let cell_size = match cell.prototype.get_name().unwrap().as_ref() {
+                "std_mem_d1" | "seq_mem_d1" => {
+                    cell.get_parameter("SIZE").unwrap()
                 }
-            })
-            .cloned()
-            .collect()
-    }
-
-    fn get_mem_info(comp: &ir::Component) -> Vec<MemInfo> {
-        external_memories_cells(comp)
-            .iter()
-            .map(|cr| {
-                let cell = cr.borrow();
-                MemInfo {
-                    width: cell.get_parameter("WIDTH").unwrap(),
-                    size: cell.get_parameter("SIZE").unwrap(),
+                "std_mem_d2" | "seq_mem_d2" => {
+                    cell.get_parameter("D0_SIZE").unwrap()
+                        * cell.get_parameter("D1_SIZE").unwrap()
                 }
-            })
-            .collect()
-    }
+                "std_mem_d3" | "seq_mem_d3" => {
+                    cell.get_parameter("D0_SIZE").unwrap()
+                        * cell.get_parameter("D1_SIZE").unwrap()
+                        * cell.get_parameter("D2_SIZE").unwrap()
+                }
 
-    // Returns Vec<String> of memory names
-    fn external_memories(comp: &ir::Component) -> Vec<String> {
+                "std_mem_d4" | "seq_mem_d4" => {
+                    cell.get_parameter("D0_SIZE").unwrap()
+                        * cell.get_parameter("D1_SIZE").unwrap()
+                        * cell.get_parameter("D2_SIZE").unwrap()
+                        * cell.get_parameter("D3_SIZE").unwrap()
+                }
+                _ => {
+                    panic!("cell `{}' marked with `@external' but is not a memory primitive.", cell.name())
+                }
+            };
+
+            MemInfo {
+                width: cell.get_parameter("WIDTH").unwrap(),
+                size: cell_size,
+            }
+        })
+        .collect()
+}
+
+// Returns Vec<String> of memory names
+fn external_memories(comp: &ir::Component) -> Vec<String> {
     external_memories_cells(comp)
         .iter()
         .map(|cell_ref| cell_ref.borrow().name().to_string())
