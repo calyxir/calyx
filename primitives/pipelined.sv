77adcc21da0eaca149c128158b43946e7a68c5c6
Matt Hofmann <47065711+matth2k@users.noreply.github.com>
2023-10-20 15:27:15 -0400

    Add width parameter to `pipelined_mult` primitive (#1748)
    * add newest iteration of mul primitives
    
    * revert old primitive changes
    
    * remove std_mult_pipe in favor of std_mult_seq
    
    * Revert mult_pipe name change for now
    
    * Update binary_operators.sv
    
    * add pipelined_mult parameter to regression tests
    
    ---------
    
    Co-authored-by: Andrew Butt <andrewb1999@gmail.com>

--- primitives/pipelined.sv
+++ primitives/pipelined.sv
@@ -1,34 +1,34 @@
 
 /// This is mostly used for testing the static guarantees currently.
 /// A realistic implementation would probably take four cycles.
-module pipelined_mult (
+module pipelined_mult #(
+    parameter WIDTH = 32
+) (
     input wire clk,
     input wire reset,
     // inputs
-    input wire [31:0] left,
-    input wire [31:0] right,
+    input wire [WIDTH-1:0] left,
+    input wire [WIDTH-1:0] right,
     // The input has been committed
-    output wire [31:0] out
+    output wire [WIDTH-1:0] out
 );
 
-logic [31:0] lt, rt, buff0, buff1, buff2, tmp_prod;
+logic [WIDTH-1:0] buff0, buff1, buff2, buff3, tmp_prod;
 
-assign out = buff2;
-assign tmp_prod = lt * rt;
+assign out = buff3;
+assign tmp_prod = left * right;
 
 always_ff @(posedge clk) begin
     if (reset) begin
-        lt <= 0;
-        rt <= 0;
         buff0 <= 0;
         buff1 <= 0;
         buff2 <= 0;
+        buff3 <= 0;
     end else begin
-        lt <= left;
-        rt <= right;
         buff0 <= tmp_prod;
         buff1 <= buff0;
         buff2 <= buff1;
+        buff3 <= buff2;
     end
 end
 
