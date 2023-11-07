3c3079e6acf7d96b67de7e1635e26d9c1ae477ff
Anshuman Mohan <anshumanmohan@live.com>
2023-11-07 13:31:19 -0500

    Oracle for FIFOs

---
+++ calyx-py/test/correctness/fifo_oracle.py
@@ -0,0 +1,54 @@
+import json
+
+
+def parse_json():
+    """Effectively the opposite of `data_gen`:
+    Given a JSON file formatted for Calyx purposes, parse it into its two lists:
+    - The `commands` array, which has MAX_CMDS items.
+    - The `values` array, which has MAX_CMDS items.
+
+    Returns the three lists.
+    """
+    with open("fifo.data", "r") as f:
+        data = json.load(f)
+    commands = data["commands"]["data"]
+    values = data["values"]["data"]
+    return commands, values
+
+
+def operate_fifo(commands, values):
+    """Given the three lists, operate a FIFO routine.
+    - Read the comammands list in order.
+    - When the value is 0, we "pop" the FIFO and write the value to the answer memory.
+    - When it is 1, we "peek" into the FIFO and write the value to the answer memory.
+    - When it is 2, we push the coressponding item in the `values` list to the FIFO.
+
+    In the end, we return the answer memory.
+    """
+    fifo = []
+    ans = []
+    for cmd, val in zip(commands, values):
+        if cmd == 0:
+            ans.append(fifo.pop(0))
+        elif cmd == 1:
+            ans.append(fifo[0])
+        elif cmd == 2:
+            fifo.append(val)
+    # Pad the answer memory with zeroes until it is of length ANS_MEM_LEN.
+    ans += [0] * (10 - len(ans))
+    return ans
+
+
+def dump_json(commands, values, ans_mem):
+    payload = {
+        "ans_mem": ans_mem,
+        "commands": commands,
+        "values": values,
+    }
+    print(json.dumps(payload, indent=2))
+
+
+if __name__ == "__main__":
+    commands, values = parse_json()
+    ans = operate_fifo(commands, values)
+    dump_json(commands, values, ans)
