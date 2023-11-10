36e38fd459dc3d3b2ab896fa3dcdc2b8b860227d
bcarlet <8906114+bcarlet@users.noreply.github.com>
2023-11-10 17:29:28 -0500

    Stages for HLS place and route

--- fud/synth/hls.tcl
+++ fud/synth/hls.tcl
@@ -1,12 +1,33 @@
+# Usage: vivado_hls -f hls.tcl -tclargs [-impl] [-top <name>]
+
+set impl 0
+set top kernel
 set hls_prj benchmark.prj
+
+while {[llength $argv]} {
+  switch -exact -- [lshift argv] {
+    -impl {
+      set impl 1
+    }
+    -top {
+      set top [lshift argv]
+    }
+  }
+}
+
 open_project ${hls_prj} -reset
-set_top kernel; # The name of the hardware function.
-add_files [glob ./*.cpp] -cflags "-std=c++11 -DVHLS" ; # HLS source files.
+set_top $top; # The name of the hardware function.
+add_files [glob ./*.cpp] -cflags "-std=c++11 -DVHLS"; # HLS source files.
+
 open_solution "solution1"
 set_part xczu3eg-sbva484-1-e
 create_clock -period 7
 
 # Actions we can take include csim_design, csynth_design, or cosim_design.
 csynth_design
+
+if {$impl} {
+  export_design -format ip_catalog -version 1.1.0 -flow impl
+}
 
 exit
