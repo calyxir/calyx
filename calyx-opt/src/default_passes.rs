db02f5e098b0b56d573a91ed01e5514708a76b3b
Pai Li <pl582@cornell.edu>
2023-11-11 23:42:01 -0500

    change pass ordering

--- calyx-opt/src/default_passes.rs
+++ calyx-opt/src/default_passes.rs
@@ -95,9 +95,9 @@ impl PassManager {
                 DeadCellRemoval, // Clean up dead wires left by CombProp
                 CellShare,       // LiveRangeAnalaysis should handle comb groups
                 SimplifyWithControl, // Must run before compile-invoke
-                CompileInvoke,   // creates dead comb groups
                 AttributePromotion,
                 StaticPromotion,
+                CompileInvoke, // creates dead comb groups
                 ScheduleCompaction,
                 CompileRepeat,
                 DeadGroupRemoval, // Since previous passes potentially create dead groups
