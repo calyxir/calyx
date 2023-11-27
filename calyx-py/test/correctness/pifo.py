fcf5f805f860e12c318b7294f279d9de580f557e
Anshuman Mohan <anshumanmohan@live.com>
2023-11-26 21:46:26 -0500

    Another par bites the dust

--- calyx-py/test/correctness/pifo.py
+++ calyx-py/test/correctness/pifo.py
@@ -148,50 +148,40 @@ def insert_pifo(prog, name, queue_l, queue_r, boundary, stats=None, static=False
                     [  # The queue is not empty. Proceed.
                         # We must check if `hot` is 0 or 1.
                         lower_err,
-                        cb.par(  # We'll check both cases in parallel.
-                            cb.if_with(
-                                # Check if `hot` is 0.
-                                hot_eq_0,
-                                [  # `hot` is 0. We'll invoke `pop` on `queue_l`.
-                                    invoke_subqueue(queue_l, cmd, value, ans, err),
-                                    # Our next step depends on whether `queue_l`
-                                    # raised the error flag.
-                                    # We can check these cases in parallel.
-                                    cb.if_with(
-                                        err_neq_0,
-                                        [  # `queue_l` raised an error.
-                                            # We'll try to pop from `queue_r`.
-                                            # We'll pass it a lowered err
-                                            lower_err,
-                                            invoke_subqueue(
-                                                queue_r, cmd, value, ans, err
-                                            ),
-                                        ],
-                                        # `queue_l` succeeded.
-                                        # Its answer is our answer.
-                                        flip_hot
-                                        # We'll just make `hot` point
-                                        # to the other sub-queue.
-                                    ),
-                                ],
-                            ),
-                            # If `hot` is 1, we proceed symmetrically.
-                            cb.if_with(
-                                hot_eq_1,
-                                [
-                                    invoke_subqueue(queue_r, cmd, value, ans, err),
-                                    cb.if_with(
-                                        err_neq_0,
-                                        [
-                                            lower_err,
-                                            invoke_subqueue(
-                                                queue_l, cmd, value, ans, err
-                                            ),
-                                        ],
-                                        [flip_hot],
-                                    ),
-                                ],
-                            ),
+                        cb.if_with(
+                            # Check if `hot` is 0.
+                            hot_eq_0,
+                            [  # `hot` is 0. We'll invoke `pop` on `queue_l`.
+                                invoke_subqueue(queue_l, cmd, value, ans, err),
+                                # Our next step depends on whether `queue_l`
+                                # raised the error flag.
+                                # We can check these cases in parallel.
+                                cb.if_with(
+                                    err_neq_0,
+                                    [  # `queue_l` raised an error.
+                                        # We'll try to pop from `queue_r`.
+                                        # We'll pass it a lowered err
+                                        lower_err,
+                                        invoke_subqueue(queue_r, cmd, value, ans, err),
+                                    ],
+                                    # `queue_l` succeeded.
+                                    # Its answer is our answer.
+                                    flip_hot
+                                    # We'll just make `hot` point
+                                    # to the other sub-queue.
+                                ),
+                            ],
+                            [  # Else: `hot` is 1. Proceed symmetrically.
+                                invoke_subqueue(queue_r, cmd, value, ans, err),
+                                cb.if_with(
+                                    err_neq_0,
+                                    [
+                                        lower_err,
+                                        invoke_subqueue(queue_l, cmd, value, ans, err),
+                                    ],
+                                    [flip_hot],
+                                ),
+                            ],
                         ),
                         len_decr,  # Decrement the length.
                         # It is possible that an irrecoverable error was raised above,
