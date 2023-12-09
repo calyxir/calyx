7a5ce0a43fa9082777db8438b8ef03a61a8955fa
Ayaka Yorihiro <ay436@cornell.edu>
2023-12-09 17:17:33 -0500

    Fix formatting errors and try to leverage VerilogBackend

--- calyx-backend/src/firrtl.rs
+++ calyx-backend/src/firrtl.rs
@@ -3,7 +3,7 @@
 //! Transforms an [`ir::Context`](crate::ir::Context) into a formatted string that represents a
 //! valid FIRRTL program.
 
-use crate::traits::Backend;
+use crate::{traits::Backend, verilog, VerilogBackend};
 use calyx_ir::{self as ir, Control, FlatGuard, Group, Guard, GuardRef, RRC};
 use calyx_utils::{CalyxResult, Error, OutputFile};
 // use ir::Nothing;
@@ -24,26 +24,17 @@ impl Backend for FirrtlBackend {
     }
 
     fn link_externs(
-        prog: &calyx_ir::Context,
-        write: &mut calyx_utils::OutputFile,
+        _prog: &calyx_ir::Context,
+        _write: &mut calyx_utils::OutputFile,
     ) -> calyx_utils::CalyxResult<()> {
-        todo!("Ayaka: Not yet implemented");
-    }
-
-    fn run(
-        &self,
-        prog: calyx_ir::Context,
-        mut file: calyx_utils::OutputFile,
-    ) -> calyx_utils::CalyxResult<()> {
-        todo!("Ayaka: Not yet implemented");
+        todo!("Not yet implemented");
     }
 
     fn validate(prog: &calyx_ir::Context) -> calyx_utils::CalyxResult<()> {
-        todo!("Ayaka: Not yet implemented");
+        VerilogBackend::validate(prog) // FIXME: would this work if we wanted to check for the same things?
     }
 
-    fn emit(ctx: &ir::Context, file: &mut OutputFile) -> CalyxResult<()> {
-        log::info!("Writing!");
-        Ok(())
+    fn emit(_ctx: &ir::Context, _file: &mut OutputFile) -> CalyxResult<()> {
+        todo!("Not yet implemented");
     }
 }
