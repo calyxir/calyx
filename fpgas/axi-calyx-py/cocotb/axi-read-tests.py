1f98f41b4dd1beff46628c29120a98a4b7c22fc6
Rachit Nigam <rachit.nigam12@gmail.com>
2023-12-13 13:41:40 -0500

    sync debug

--- fpgas/axi-calyx-py/cocotb/axi-read-tests.py
+++ fpgas/axi-calyx-py/cocotb/axi-read-tests.py
@@ -1,23 +1,33 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotbext.axi import AxiReadBus, AxiRamRead
-from cocotb.triggers import Timer
+from cocotb.triggers import Timer, ClockCycles
 import mmap
 
 # TODO(nathanielnrn): If optional signals like WSTRB are not recognized,
 # install cocotb-bus directly from github, as 0.2.1 has a bug
 
 
-
-
 @cocotb.test()
 async def read_channels_tests(main):
+    cocotb.start_soon(Clock(main.clk, 2, units="ns").start())
+
+    # Assert reset for 5 cycles
+    main.reset.value = 1
+    await ClockCycles(main.clk, 5)  # wait a bit
+    main.reset.value = 0
+
+    # Start the execution
+    main.go.value = 1
 
     memmap = mmap.mmap(-1, 128)
-
     axi_ram_read = AxiRamRead(
-            #NOTE: prefix should not contain the final "_"
-            AxiReadBus.from_prefix(main, "m"), main.clk, main.reset, size=128, mem=memmap
+        # NOTE: prefix should not contain the final "_"
+        AxiReadBus.from_prefix(main, "m"),
+        main.clk,
+        main.reset,
+        size=128,
+        mem=memmap,
     )
 
     memmap.seek(0)
@@ -26,10 +36,8 @@ async def read_channels_tests(main):
     print(f"memmap read: {memmap.read(3).decode('utf-8')}")
 
     await Timer(20, "ns")
-    #main.m_ap_start.value = 1
+    # main.m_ap_start.value = 1
     axi_ram_read.hexdump(0x0000, 4, prefix="RAM")
-
-    cocotb.start_soon(Clock(main.clk, 2, units="ns").start())
 
     await Timer(1000, "ns")
     print(f"vec1_data: {main.vec1_data}")
