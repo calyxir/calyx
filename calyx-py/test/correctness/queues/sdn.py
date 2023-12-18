1a6373f8fe483a60e7516e0d3542b2fd8f1b14e8
Anshuman Mohan <anshumanmohan@live.com>
2023-12-18 00:52:45 -0500

    SDN.py now takes a --static argument

--- calyx-py/test/correctness/queues/sdn.py
+++ calyx-py/test/correctness/queues/sdn.py
@@ -1,4 +1,5 @@
 # pylint: disable=import-error
+import sys
 import fifo
 import pifo
 import calyx.builder as cb
@@ -172,5 +173,7 @@ def build(static=False):
     return prog.program
 
 
+# We will have a command line argument to determine whether the program is static.
 if __name__ == "__main__":
-    build().emit()
+    static = sys.argv[1] == "--static" if len(sys.argv) > 1 else False
+    build(static=static).emit()
