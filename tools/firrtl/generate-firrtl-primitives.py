aef7bd29c97a907d95f0f469a588162f3aaf0d3b
Ayaka Yorihiro <ay436@cornell.edu>
2024-01-20 22:29:24 -0500

    Hack to generate module name

--- tools/firrtl/generate-firrtl-primitives.py
+++ tools/firrtl/generate-firrtl-primitives.py
@@ -6,11 +6,17 @@ import sys
 # Generates the arguments for m4 based on the JSON data
 def generate_m4_arguments(inst):
     args = []
-    # get the parameters to the file
+    # hack to replace the module name with the corresponding parameterized version
+    # FIXME: figure out a way to do substring replacement in m4
+    module_name_value = inst["name"]
+    # get the parameters for the primitive
     for param in inst["params"]:
         key = param["param_name"]
         value = param["param_value"]
         args.append(f"-D{key}={value}")
+        module_name_value += "_" + str(value)
+
+    args.append(f"-DMODULE_NAME={module_name_value}")
 
     # get the filename
     firrtl_file_path = os.path.join(sys.path[0], "templates", inst['name'] + ".fir")
@@ -27,6 +33,7 @@ def main():
         m4_args += generate_m4_arguments(inst)
         # execute the subprocess containing m4
         subprocess.run(m4_args)
+        print()
 
 
 if __name__ == '__main__':
