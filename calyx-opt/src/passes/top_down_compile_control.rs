d94ce9356f5e01d621d7dcada284cdf1b9ff6cad
calebmkim <55243755+calebmkim@users.noreply.github.com>
2024-02-09 10:54:53 -0500

    Deprecate `@static` (#1897)
    * replace static w// interval
    
    * change primitives to @interval
    
    * modified tests
    
    * promte_static -> promotable
    
    * get rid of static
    
    * remove attribute_promotion
    
    * py frontend uses promotable
    
    * rewrite exp tests
    
    * corrected some tests
    
    * overwrite tests
    
    * overwrite failing tests
    
    * change interface
    
    * add tests
    
    * add well formed check
    
    * change compile_invoke
    
    * add tests
    
    * compile invoke test
    
    * change iterface
    
    * adjustments
    
    * fix borrow error
    
    * clippy
    
    * move files
    
    * fix test
    
    * rewrite mlir test
    
    * always promote @interval
    
    * interp tests
    
    * documentation
    
    * dummy change
    
    * static->interval
    
    * update tests
    
    * add documentation to compile-static-interface
    
    * well formed check'
    
    * small change
    
    * done
    
    * tests
    
    * rewrite test
    
    * dummy change
    
    * github workflow
    
    * docker file checks out main
    
    * checkout master
    
    * checkout specific version
    
    * go back to og dockerfile
    
    * checkout specific commit
    
    * does this work
    
    * still not working
    
    * dummy change so non-merge commit
    
    * chatgpt no cache
    
    * does this change things
    
    * modify relay tests

--- calyx-opt/src/passes/top_down_compile_control.rs
+++ calyx-opt/src/passes/top_down_compile_control.rs
@@ -454,7 +454,7 @@ impl Schedule<'_, '_> {
         ir::Control::Par(_) => unreachable!(),
         ir::Control::Repeat(_) => unreachable!("`repeat` statements should have been compiled away. Run `{}` before this pass.", passes::CompileRepeat::name()),
         ir::Control::Invoke(_) => unreachable!("`invoke` statements should have been compiled away. Run `{}` before this pass.", passes::CompileInvoke::name()),
-        ir::Control::Empty(_) => unreachable!("`empty` statements should have been compiled away. Run `{}` before this pass.", passes::CompileEmpty::name()),
+        ir::Control::Empty(_) => unreachable!("`calculate_states_recur` should not see an `empty` control."),
         ir::Control::Static(_) => unreachable!("static control should have been compiled away. Run the static compilation passes before this pass")
     }
     }
