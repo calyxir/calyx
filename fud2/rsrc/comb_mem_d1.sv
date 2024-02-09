bee9275f4e163ccf1a7670fece47903a2e7398d8
Ayaka Yorihiro <ay436@cornell.edu>
2024-02-09 18:01:10 -0500

    Some cleanup from previous experimentation

--- fud2/rsrc/comb_mem_d1.sv
+++ fud2/rsrc/comb_mem_d1.sv
@@ -1,4 +1,4 @@
-module std_mem_d1 #(
+module comb_mem_d1 #(
     parameter WIDTH = 32,
     parameter SIZE = 16,
     parameter IDX_SIZE = 4
@@ -13,13 +13,6 @@ module std_mem_d1 #(
 );
 
    logic [WIDTH-1:0]         mem[SIZE-1:0];
-
-   // initial begin
-   //    $readmemh({"sim_data/mem.dat"}, mem);
-   // end
-   // final begin
-   //    $writememh({"sim_data/mem.out"}, mem);
-   // end
 
   /* verilator lint_off WIDTH */
   assign read_data = mem[addr0];
@@ -43,7 +36,7 @@ module std_mem_d1 #(
     always_comb begin
       if (addr0 >= SIZE)
         $error(
-          "std_mem_d1: Out of bounds access\n",
+          "comb_mem_d1: Out of bounds access\n",
           "addr0: %0d\n", addr0,
           "SIZE: %0d", SIZE
         );
