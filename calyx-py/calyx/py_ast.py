5f062d2461c8979a8eacb92fbd41111e2fad8d50
Nathaniel Navarro <nathanielnrn99@gmail.com>
2024-02-17 18:30:45 -0500

    Attempt to add backwrd compatability

--- calyx-py/calyx/py_ast.py
+++ calyx-py/calyx/py_ast.py
@@ -56,11 +56,11 @@ class Component:
     def __init__(
         self,
         name: str,
-        attributes: list[CompAttribute],
         inputs: list[PortDef],
         outputs: list[PortDef],
         structs: list[Structure],
         controls: Control,
+        attributes: Optional[list[CompAttribute]] = None,
         latency: Optional[int] = None,
     ):
         self.name = name
@@ -89,7 +89,7 @@ class Component:
         ins = ", ".join([s.doc() for s in self.inputs])
         outs = ", ".join([s.doc() for s in self.outputs])
         latency_annotation = (
-            f"static<{self.latency}>" if self.latency is not None else ""
+                f"static<{self.latency}> " if self.latency is not None else ""
         )
         attribute_annotation = f"<{', '.join([f'{a.doc()}' for a in self.attributes])}>" if self.attributes else ""
         signature = f"{latency_annotation}component {self.name}{attribute_annotation}({ins}) -> ({outs})"
