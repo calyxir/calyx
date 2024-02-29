8a838046aa0db84abfe665dcb28b0539a21cfa97
Ayaka Yorihiro <ay436@cornell.edu>
2024-02-29 13:30:27 -0500

    Script to generate a custom testbench given the memories in a program

--- tools/firrtl/generate-testbench.py
+++ tools/firrtl/generate-testbench.py
@@ -1,29 +1,115 @@
+import os
 import sys
-import json
 
+template_file = os.path.join(sys.path[0], "custom_tb_template.sv")
+
+def generate_fields(memory_cell_dicts):
+    out_str = ""
+    for memory_cell_dict in memory_cell_dicts:
+         name = memory_cell_dict['cell-name']
+         width = memory_cell_dict['WIDTH'] - 1
+         out_str += f"wire {name}_addr0;\n"
+         out_str += f"wire [{width}:0] {name}_write_data;\n"
+         out_str += f"wire {name}_write_en;\n"
+         out_str += f"wire [{width}:0] {name}_read_data;\n"
+         out_str += f"wire {name}_done;\n\n"
+
+    return out_str
+
+def generate_memory_dec(memory_cell_dicts):
+     out_str = ""
+     for memory_cell_dict in memory_cell_dicts:
+          name = memory_cell_dict['cell-name']
+          out_str += f'''comb_mem_d1 # (
+    .IDX_SIZE({memory_cell_dict["IDX_SIZE"]}),
+    .SIZE({memory_cell_dict["SIZE"]}),
+    .WIDTH({memory_cell_dict["WIDTH"]})
+) {name} (
+    .addr0({name}_addr0),
+    .clk(clk),
+    .done({name}_done),
+    .read_data({name}_read_data),
+    .reset(reset),
+    .write_data({name}_write_data),
+    .write_en({name}_write_en)
+);
+'''
+     return out_str
+
+def generate_main_decl(memory_cell_dicts):
+     out_str = '''main #() main (
+  .go(go),
+  .clk(clk),
+  .reset(reset),
+  .done(done)'''
+     for memory_cell_dict in memory_cell_dicts:
+          name = memory_cell_dict["cell-name"]
+          out_str += ",\n"
+          out_str += f'''  .{name}_addr0({name}.addr0),
+  .{name}_write_data({name}.write_data),
+  .{name}_write_en({name}.write_en),
+  .{name}_read_data({name}.read_data),
+  .{name}_done({name}.done)
+'''
+     out_str += "\n);"
+     return out_str
+
+def generate_readmemh(memory_cell_dicts):
+     out_str = ""
+     for memory_cell_dict in memory_cell_dicts:
+          name = memory_cell_dict["cell-name"]
+          out_str += f"  $readmemh({{DATA, \"/{name}.dat\"}}, {name}.mem);\n"
+     return out_str
+
+def generate_writememh(memory_cell_dicts):
+     out_str = ""
+     if len(memory_cell_dicts) > 0:
+          out_str += "final begin\n"
+          for memory_cell_dict in memory_cell_dicts:
+               name = memory_cell_dict["cell-name"]
+               out_str += f"    $writememh({{DATA, \"/{name}.out\"}}, {name}.mem);\n"
+          out_str += "end"
+     return out_str
+         
 def get_memory_cells(calyx_program):
-     memory_cell_dict = {}
+     memory_cell_dicts = []
      with open(calyx_program) as f:
           for line in f:
                if "ref" in line and "comb_mem_d1" in line:
+                    memory_cell_dict = {}
                     # parse line that contains a ref cell
                     memory_cell_dict["cell-name"] = line.lstrip().split()[1]
                     # we're assuming that comb_mem_d1 for now
                     args = line.split("(")[1].split(")")[0].split(",")
-                    memory_cell_dict["WIDTH"] = args[0]
-                    memory_cell_dict["SIZE"] = args[1]
-                    memory_cell_dict["IDX_SIZE"] = args[2]
+                    memory_cell_dict["WIDTH"] = int(args[0])
+                    memory_cell_dict["SIZE"] = int(args[1])
+                    memory_cell_dict["IDX_SIZE"] = int(args[2])
+                    memory_cell_dicts.append(memory_cell_dict)
 
                elif "comb_mem_d" in line:
                     print(f"Multidimensional memory found: {line}")
                     print("Aborting.")
                     sys.exit(1)
 
-     return memory_cell_dict
+     return memory_cell_dicts
 
 
 def generate(calyx_program):
-     print(get_memory_cells(calyx_program))
+     memory_cell_dicts = get_memory_cells(calyx_program)
+     with open(template_file) as t:
+          for line in t:
+               if line.strip() == "MEMORY_FIELDS":
+                    print(generate_fields(memory_cell_dicts))
+               elif line.strip() == "MEMORY_DECLS":
+                    print(generate_memory_dec(memory_cell_dicts))
+               elif line.strip() == "MAIN_DECL":
+                    print(generate_main_decl(memory_cell_dicts))
+               elif line.strip() == "READMEMH_STATEMENTS":
+                    print(generate_readmemh(memory_cell_dicts))
+               elif line.strip() == "WRITEMEMH_STATEMENTS":
+                    print(generate_writememh(memory_cell_dicts))
+               else:
+                    print(line.rstrip())
 
 def main():
     if len(sys.argv) != 2:
