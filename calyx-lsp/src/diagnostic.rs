ad97e2412abcf440d08596b955f5a787d84e0a48
Sammy Thomas <sgpthomas@gmail.com>
2024-03-01 11:39:14 -0600

    run some error reporting passes on source files

--- calyx-lsp/src/diagnostic.rs
+++ calyx-lsp/src/diagnostic.rs
@@ -1,5 +1,10 @@
 use std::path::Path;
 
+use calyx_opt::{
+    pass_manager::PassManager,
+    passes::{Papercut, SynthesisPapercut, WellFormed},
+    traversal::Named,
+};
 use resolve_path::PathResolveExt;
 
 pub struct Diagnostic;
@@ -17,11 +22,31 @@ pub struct CalyxError {
 impl Diagnostic {
     /// Run the `calyx` compiler on `path` with libraries at `lib_path`
     pub fn did_save(path: &Path, lib_path: &Path) -> Vec<CalyxError> {
+        let mut pm = PassManager::default();
+        pm.register_pass::<WellFormed>()
+            .expect("pass registration failed");
+        pm.register_pass::<Papercut>()
+            .expect("pass registration failed");
+        pm.register_pass::<SynthesisPapercut>()
+            .expect("pass registration failed");
+
         calyx_frontend::Workspace::construct(
             &Some(path.to_path_buf()),
             lib_path.resolve().as_ref(),
         )
         .and_then(calyx_ir::from_ast::ast_to_ir)
+        .and_then(|mut ctx| {
+            pm.execute_plan(
+                &mut ctx,
+                &[
+                    WellFormed::name().to_string(),
+                    Papercut::name().to_string(),
+                    SynthesisPapercut::name().to_string(),
+                ],
+                &[],
+                false,
+            )
+        })
         // TODO: call well-formed pass
         .map(|_| vec![])
         .unwrap_or_else(|e| {
