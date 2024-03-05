013c472b5aa3159c1ccc1fbea121a77e8f6ce01d
jiahanxie353 <jx353@cornell.edu>
2024-03-05 15:02:55 -0500

    using a script to get HardFloat Verilog source code

---
+++ primitives/float/get_hardfloat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+HARDFLOAT_DIR="."
+
+HARDFLOAT_URL="http://www.jhauser.us/arithmetic/HardFloat-1.zip"
+
+ZIP_FILE="HardFloat-1.zip"
+
+mkdir -p "${HARDFLOAT_DIR}"
+
+cd "${HARDFLOAT_DIR}"
+
+curl -LO "${HARDFLOAT_URL}"
+
+if [ -f "$ZIP_FILE" ]; then
+    unzip "$ZIP_FILE" && rm "$ZIP_FILE"
+    echo "HardFloat library fetched and extracted to ${HARDFLOAT_DIR}"
+else
+    echo "Failed to download HardFloat library from ${HARDFLOAT_URL}"
+fi
