df905748fcf326a3681f5ec7e95917316a4a5262
eys29 <eys29@cornell.edu>
2024-03-10 01:09:34 -0500

    to verilog should theoretically work

--- yxi/axi-calyx/axi.sh
+++ yxi/axi-calyx/axi.sh
@@ -6,20 +6,26 @@
 
 # get yxi
 target/debug/calyx $1 -b yxi > yxi/axi-calyx/outputs/fudaxi/input.yxi 
-# external to ref pass
+# external to ref pass - doesn't have same behavior as sim.sh workflow
 target/debug/calyx $1 -p external-to-ref > yxi/axi-calyx/outputs/fudaxi/after-pass.futil
 cd yxi/axi-calyx
 # 
 python3 axi-generator.py outputs/fudaxi/input.yxi > outputs/fudaxi/generated-axi.futil
-cd outputs
-cat fudaxi/generated-axi.futil fudaxi/after-pass.futil > fudaxi/cat.futil
+cd outputs/fudaxi
+cat generated-axi.futil after-pass.futil > cat.futil
+
+cd ../../cocotb 
+fud e ../outputs/fudaxi/generated-axi.futil --from calyx --to synth-verilog -o ../outputs/$2.v 
+../vcdump.py ../outputs/$2.v 
+make WAVES=1 vfile=../outputs/$2.v 
+mv out.vcd ../outputs/$2.fst 
 
 # fud e fudaxi-cat.futil --from calyx --to synth-verilog -o ../outputs/$2.v \
 #     && ../vcdump.py ../outputs/$2.v \
 #     && make WAVES=1 vfile=../outputs/$2.v\
 #     && mv out.vcd ../outputs/$2.fst 
 
-# rm fudaxi-input.yxi
-# rm fudaxi-generated-axi.futil
-# rm fudaxi-after-pass.futil
-# rm fudaxi-cat.futil
+# rm fudaxi/input.yxi
+# rm fudaxi/generated-axi.futil
+# rm fudaxi/after-pass.futil
+# rm fudaxi/cat.futil
