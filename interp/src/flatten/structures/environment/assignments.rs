fff1e6547f8ee587934053f1b5c2df5b3645d7bb
Griffin Berlstein <griffin@berlste.in>
2024-03-14 16:19:10 -0400

    checkpoint

--- interp/src/flatten/structures/environment/assignments.rs
+++ interp/src/flatten/structures/environment/assignments.rs
@@ -1,15 +1,23 @@
 use crate::flatten::{
-    flat_ir::prelude::{Assignment, AssignmentIdx, GlobalCellIdx},
+    flat_ir::prelude::{
+        Assignment, AssignmentIdx, GlobalCellIdx, LocalPortOffset,
+    },
     structures::context::Context,
 };
 
 use super::env::AssignmentRange;
 
+#[derive(Debug)]
+pub struct GroupInterfacePorts {
+    pub go: LocalPortOffset,
+    pub done: LocalPortOffset,
+}
+
 /// A collection of assignments represented using a series of half-open ranges
 /// via [AssignmentRange]
 #[derive(Debug)]
 pub(crate) struct AssignmentBundle {
-    assigns: Vec<(GlobalCellIdx, AssignmentRange)>,
+    assigns: Vec<(GlobalCellIdx, AssignmentRange, Option<GroupInterfacePorts>)>,
 }
 
 // TODO griffin: remove the dead stuff later
@@ -28,42 +36,41 @@ impl AssignmentBundle {
     }
 
     #[inline]
-    pub fn push(&mut self, value: (GlobalCellIdx, AssignmentRange)) {
+    pub fn push(
+        &mut self,
+        value: (GlobalCellIdx, AssignmentRange, Option<GroupInterfacePorts>),
+    ) {
         self.assigns.push(value)
     }
 
     pub fn iter(
         &self,
-    ) -> impl Iterator<Item = &(GlobalCellIdx, AssignmentRange)> {
+    ) -> impl Iterator<
+        Item = &(GlobalCellIdx, AssignmentRange, Option<GroupInterfacePorts>),
+    > {
         self.assigns.iter()
-    }
-
-    pub fn iter_over_indices(
-        &self,
-    ) -> impl Iterator<Item = (GlobalCellIdx, AssignmentIdx)> + '_ {
-        self.assigns
-            .iter()
-            .flat_map(|(c, x)| x.iter().map(|y| (*c, y)))
-    }
-
-    pub fn iter_over_assignments<'a>(
-        &'a self,
-        ctx: &'a Context,
-    ) -> impl Iterator<Item = (GlobalCellIdx, &'a Assignment)> {
-        self.iter_over_indices()
-            .map(|(c, idx)| (c, &ctx.primary[idx]))
     }
 
     /// The total number of assignments. Not the total number of index ranges!
     pub fn len(&self) -> usize {
         self.assigns
             .iter()
-            .fold(0, |acc, (_, range)| acc + range.size())
+            .fold(0, |acc, (_, range, _)| acc + range.size())
     }
 }
 
-impl FromIterator<(GlobalCellIdx, AssignmentRange)> for AssignmentBundle {
-    fn from_iter<T: IntoIterator<Item = (GlobalCellIdx, AssignmentRange)>>(
+impl FromIterator<(GlobalCellIdx, AssignmentRange, Option<GroupInterfacePorts>)>
+    for AssignmentBundle
+{
+    fn from_iter<
+        T: IntoIterator<
+            Item = (
+                GlobalCellIdx,
+                AssignmentRange,
+                Option<GroupInterfacePorts>,
+            ),
+        >,
+    >(
         iter: T,
     ) -> Self {
         Self {
