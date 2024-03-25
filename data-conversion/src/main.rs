e548afb4e005ce5e93abbf86462c9c2928a27429
Angelica Schell <ams893@cornell.edu>
2024-03-25 01:02:58 -0400

    Added binary to hex and binary to float and I fixed some random errors.

--- data-conversion/src/main.rs
+++ data-conversion/src/main.rs
@@ -5,32 +5,52 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    convert(&args[1], &args[2], &args[3]);
+    convert(&args[1], &args[2], &args[3], &args[4]);
 }
 
 
-//Converts [filepath_get] to binary in [filepath_send]
-fn convert(filepath_get: &String, filepath_send: &String, to_convert: &String) {
+/// Converts [filepath_get] from type [convert_from] to [convert_to] in [filepath_send]
+fn convert(filepath_get: &String, filepath_send: &String, 
+                                convert_from: &String, convert_to: &String) {
     // Create a file called converted.txt
     let mut converted = File::create(filepath_send).expect("creation failed");
-
-    // Read file line by line
-    for line in read_to_string(filepath_get).unwrap().lines() {
-        //If convert from hex
-        if to_convert == "hex"{
-            hex_to_binary(line, &mut converted)
-                .expect("Failed to write binary to file");
-        } else if to_convert == "binary"{
-            float_to_binary(line, &mut converted)
-                .expect("Failed to write binary to file");
-         } 
-
-    }
-    println!("Successfully converted to {} in {}", to_convert, filepath_send);
+    
+        if convert_to == "binary"{
+            //Convert from hex to binary
+            if convert_from == "hex"{
+                for line in read_to_string(filepath_get).unwrap().lines() {
+                    hex_to_binary(line, &mut converted)
+                        .expect("Failed to write binary to file");
+                }
+            //Convert from float to binary
+            } else if convert_from == "float"{
+                for line in read_to_string(filepath_get).unwrap().lines() {
+                    float_to_binary(line, &mut converted)
+                        .expect("Failed to write binary to file");
+                }
+            } 
+        } else if convert_to == "hex"{
+            //Convert from binary to hex
+            if convert_from == "binary"{
+                for line in read_to_string(filepath_get).unwrap().lines() {
+                    binary_to_hex(line, &mut converted)
+                        .expect("Failed to write hex to file");
+                }
+            }
+        } else if convert_to == "float"{
+            //Convert from binary to float
+            if convert_from == "binary"{
+                for line in read_to_string(filepath_get).unwrap().lines() {
+                    binary_to_float(line, &mut converted)
+                        .expect("Failed to write float to file");
+                }
+            }
+        }
+    println!("Successfully converted to {} in {}", convert_to, filepath_send);
 }
 
+/// Formats [binary_of_float] properly 
 fn format_binary(binary_of_float: u32) -> String{
-    // Format output nicely
     let binary_str = format!("{:032b}", binary_of_float);
     let formatted_binary_str = format!(
         "{} {} {}",
@@ -38,14 +58,14 @@ fn format_binary(binary_of_float: u32) -> String{
         &binary_str[1..9], // Exponent
         &binary_str[9..]   // Significand
     );
-    return formatted_binary_str;
+    formatted_binary_str
 }
  
-fn float_to_binary(line: &str, filepath_send: &mut File) -> std::io::Result<()> {
-
+/// Converts [binary_string] to binary and appends to [filepath_send]
+fn float_to_binary(binary_string: &str, filepath_send: &mut File) -> std::io::Result<()> {
     let float_of_string: f32;
             // Convert string to float
-            match line.parse::<f32>() {
+            match binary_string.parse::<f32>() {
                 Ok(parsed_num) => float_of_string = parsed_num,
                 Err(_) => {
                     panic!("Failed to parse float from string")
@@ -63,6 +83,7 @@ fn float_to_binary(line: &str, filepath_send: &mut File) -> std::io::Result<()> 
     Ok(())
 }
 
+/// Converts [hex_string] to binary and appends to [filepath_send]
 fn hex_to_binary(hex_string: &str, filepath_send: &mut File) -> io::Result<()> {
     // Convert hex to binary
     let binary_of_hex = match u32::from_str_radix(hex_string, 16) {
@@ -70,11 +91,43 @@ fn hex_to_binary(hex_string: &str, filepath_send: &mut File) -> io::Result<()> {
         Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err)),
     };
 
-    // Convert to binary string
+    // Format nicely
     let formatted_binary_str = format!("{:b}", binary_of_hex);
 
     // Write binary string to the file
     filepath_send.write_all(formatted_binary_str.as_bytes())?;
+    filepath_send.write_all(b"\n")?;
+
+    Ok(())
+}
+
+fn binary_to_hex(binary_string: &str, filepath_send: &mut File) -> io::Result<()> {
+    let hex_of_binary = match u32::from_str_radix(binary_string, 2){
+        Ok(value) => value,
+        Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err)),
+    };
+
+    // Format the integer as a hexadecimal string
+    let formatted_hex_str = format!("{:X}", hex_of_binary);
+
+    filepath_send.write(formatted_hex_str.as_bytes())?;
+    filepath_send.write_all(b"\n")?;
+
+    Ok(())
+}
+
+fn binary_to_float(binary_string: &str, filepath_send: &mut File) -> io::Result<()>{
+    let binary_value = match u32::from_str_radix(binary_string, 2) {
+        Ok(value) => value,
+        Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err)),
+    };
+
+    // Interpret the integer as the binary representation of a floating-point number
+    let float_value = unsafe { std::mem::transmute::<u32, f32>(binary_value) };
+
+    let formated_float_str = format!("{:?}", float_value);
+
+    filepath_send.write_all(formated_float_str.as_bytes())?;
     filepath_send.write_all(b"\n")?;
 
     Ok(())
