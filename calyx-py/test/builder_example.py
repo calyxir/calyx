1d542295f27d98bed2f9123afdb3bd18391d2685
Anshuman Mohan <anshumanmohan@live.com>
2024-04-13 16:51:37 -0400

    Mux component

--- calyx-py/test/builder_example.py
+++ calyx-py/test/builder_example.py
@@ -2,6 +2,10 @@ import calyx.builder as cb
 
 
 def add_adder_component(prog):
+    """Insert an adder component into the program.
+    This is a contrived example:
+    the component basically just wraps the adder primitive.
+    """
     # ANCHOR: component
     comp = prog.component("adder")
     # ANCHOR_END: component
@@ -46,10 +50,41 @@ def add_adder_component(prog):
     # ANCHOR_END: return
 
 
+def add_mux_component(prog):
+    """Insert a multiplexer component into the program.
+    The user provides two values and a select signal.
+    If the select signal is high, the component outputs the sum of the two values.
+    If the select signal is low, the component outputs the difference of the two values.
+    """
+    comp = prog.component("mux")
+
+    val1 = comp.input("val1", 32)
+    val2 = comp.input("val2", 32)
+    sel = comp.input("sel", 1)
+    comp.output("out", 32)
+
+    mux = comp.reg("mux", 32)
+
+    sel_eq_0 = comp.eq_use(sel, 0)
+
+    # ANCHOR: adder_group_and_reg
+    sum_group, _ = comp.add_store_in_reg(val1, val2, ans_reg=mux)
+    # ANCHOR_END: adder_group_and_reg
+    diff_group, _ = comp.sub_store_in_reg(val1, val2, ans_reg=mux)
+
+    with comp.continuous:
+        comp.this().out = mux.out
+
+    comp.control += cb.if_with(sel_eq_0, sum_group, diff_group)
+
+    return comp
+
+
 # ANCHOR: build
 def build():
     prog = cb.Builder()
     add_adder_component(prog)
+    add_mux_component(prog)
     return prog.program
     # ANCHOR_END: build
 
