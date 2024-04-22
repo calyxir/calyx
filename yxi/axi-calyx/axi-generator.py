6758972118943e574af6e1e7ed85c460b78716e7
Anshuman Mohan <anshumanmohan@live.com>
2024-04-22 13:31:13 -0400

    More hunting

--- yxi/axi-calyx/axi-generator.py
+++ yxi/axi-calyx/axi-generator.py
@@ -45,7 +45,7 @@ def add_arread_channel(prog, mem):
 
 def add_awwrite_channel(prog, mem):
     aw_channel = _add_m_to_s_address_channel(prog, mem, "AW")
-    max_transfers = aw_channel.reg("max_transfers", 8, is_ref=True)
+    max_transfers = aw_channel.reg(8, "max_transfers", is_ref=True)
 
     # TODO(nathanielnrn): We eventually want to move beyond
     # the implicit 1 transaction that is the size of the memory
@@ -87,20 +87,19 @@ def _add_m_to_s_address_channel(prog, mem, prefix: Literal["AW", "AR"]):
     add_comp_params(m_to_s_address_channel, channel_inputs, channel_outputs)
 
     # Cells
-    xvalid = m_to_s_address_channel.reg(f"{lc_x}valid", 1)
-    xhandshake_occurred = m_to_s_address_channel.reg(f"{lc_x}_handshake_occurred", 1)
-    curr_addr_axi = m_to_s_address_channel.reg("curr_addr_axi", 64, is_ref=True)
-    xlen = m_to_s_address_channel.reg(f"{lc_x}len", 8)
+    xvalid = m_to_s_address_channel.reg(1, f"{lc_x}valid")
+    xhandshake_occurred = m_to_s_address_channel.reg(1, f"{lc_x}_handshake_occurred")
+    curr_addr_axi = m_to_s_address_channel.reg(64, "curr_addr_axi", is_ref=True)
+    xlen = m_to_s_address_channel.reg(8, f"{lc_x}len")
 
     # Number of txns we want to occur before m_arread_channel is done
     # TODO: parameterize
     txn_n = m_to_s_address_channel.const("txn_n", 32, 1)
-    txn_count = m_to_s_address_channel.reg("txn_count", 32)
-    txn_adder = m_to_s_address_channel.add(32, "txn_adder")
+    txn_count = m_to_s_address_channel.reg(32, "txn_count")
+    txn_adder = m_to_s_address_channel.add("txn_adder", 32)
 
     # Need to put block_transfer register here to avoid combinational loops
-    bt_reg = m_to_s_address_channel.reg("bt_reg", 1)
-
+    bt_reg = m_to_s_address_channel.reg(1, "bt_reg")
     # Wires
     with m_to_s_address_channel.continuous:
         m_to_s_address_channel.this()[f"{x}VALID"] = xvalid.out
@@ -202,19 +201,19 @@ def add_read_channel(prog, mem):
     )
 
     # according to zipcpu, rready should be registered
-    rready = read_channel.reg("rready", 1)
+    rready = read_channel.reg(1, "rready")
     curr_addr_internal_mem = read_channel.reg(
-        "curr_addr_internal_mem", clog2_or_1(mem["size"]), is_ref=True
+        clog2_or_1(mem["size"]), "curr_addr_internal_mem", is_ref=True
     )
-    curr_addr_axi = read_channel.reg("curr_addr_axi", 64, is_ref=True)
+    curr_addr_axi = read_channel.reg(64, "curr_addr_axi", is_ref=True)
     # Registed because RLAST is high with laster transfer, not after
     # before this we were terminating immediately with
     # last transfer and not servicing it
-    n_RLAST = read_channel.reg("n_RLAST", 1)
+    n_RLAST = read_channel.reg(1, "n_RLAST")
     # Stores data we want to write to our memory at end of block_transfer group
-    read_data_reg = read_channel.reg("read_data_reg", mem["width"])
+    read_data_reg = read_channel.reg(mem["width"], "read_data_reg")
 
-    bt_reg = read_channel.reg("bt_reg", 1)
+    bt_reg = read_channel.reg(1, "bt_reg")
 
     # Groups
     with read_channel.continuous:
@@ -318,29 +317,29 @@ def add_write_channel(prog, mem):
     )
 
     # according to zipcpu, rready should be registered
-    wvalid = write_channel.reg("wvalid", 1)
-    w_handshake_occurred = write_channel.reg("w_handshake_occurred", 1)
+    wvalid = write_channel.reg(1, "wvalid")
+    w_handshake_occurred = write_channel.reg(1, "w_handshake_occurred")
     # internal calyx memory indexing
     curr_addr_internal_mem = write_channel.reg(
-        "curr_addr_internal_mem", clog2_or_1(mem["size"]), is_ref=True
+        clog2_or_1(mem["size"]), "curr_addr_internal_mem", is_ref=True
     )
     # host indexing, must be 64 bits
-    curr_addr_axi = write_channel.reg("curr_addr_axi", 64, is_ref=True)
+    curr_addr_axi = write_channel.reg(64, "curr_addr_axi", is_ref=True)
 
-    curr_transfer_count = write_channel.reg("curr_transfer_count", 8)
+    curr_transfer_count = write_channel.reg(8, "curr_transfer_count")
     # Number of transfers we want to do in current txn
-    max_transfers = write_channel.reg("max_transfers", 8, is_ref=True)
+    max_transfers = write_channel.reg(8, "max_transfers", is_ref=True)
 
     # Register because w last is high with last transfer. Before this
     # We were terminating immediately with last transfer and not servicing it.
-    n_finished_last_transfer = write_channel.reg("n_finished_last_transfer", 1)
+    n_finished_last_transfer = write_channel.reg(1, "n_finished_last_transfer")
 
-    bt_reg = write_channel.reg("bt_reg", 1)
+    bt_reg = write_channel.reg(1, "bt_reg")
 
     # Groups
     with write_channel.continuous:
         write_channel.this()["WVALID"] = wvalid.out
-        #Needed due to default assignment bug #1930
+        # Needed due to default assignment bug #1930
         mem_ref.write_en = 0
 
     with write_channel.group("service_write_transfer") as service_write_transfer:
@@ -363,8 +362,12 @@ def add_write_channel(prog, mem):
         mem_ref.content_en = 1
         write_channel.this()["WDATA"] = mem_ref.read_data
 
-        write_channel.this()["WLAST"] = (max_transfers.out == curr_transfer_count.out) @ 1
-        write_channel.this()["WLAST"] = (max_transfers.out != curr_transfer_count.out) @ 0
+        write_channel.this()["WLAST"] = (
+            max_transfers.out == curr_transfer_count.out
+        ) @ 1
+        write_channel.this()["WLAST"] = (
+            max_transfers.out != curr_transfer_count.out
+        ) @ 0
 
         # set high when WLAST is high and a handshake occurs
         n_finished_last_transfer.in_ = (
@@ -424,8 +427,8 @@ def add_bresp_channel(prog, mem):
     add_comp_params(bresp_channel, channel_inputs, channel_outputs)
 
     # Cells
-    bready = bresp_channel.reg("bready", 1)
-    bt_reg = bresp_channel.reg("bt_reg", 1)
+    bready = bresp_channel.reg(1, "bready")
+    bt_reg = bresp_channel.reg(1, "bt_reg")
 
     # Groups
     with bresp_channel.continuous:
@@ -452,7 +455,7 @@ def add_bresp_channel(prog, mem):
 # NOTE: Unlike the channel functions, this can expect multiple mems
 def add_main_comp(prog, mems):
     wrapper_comp = prog.component("wrapper")
-    wrapper_comp.attribute("toplevel",1)
+    wrapper_comp.attribute("toplevel", 1)
     # Get handles to be used later
     read_channel = prog.get_component("m_read_channel")
     write_channel = prog.get_component("m_write_channel")
@@ -466,63 +469,63 @@ def add_main_comp(prog, mems):
     writes_par = []
     ref_mem_kwargs = {}
 
-    #Create single main cell
-    main_compute = wrapper_comp.comp_instance("main_compute", "main", check_undeclared=False)
-    
+    # Create single main cell
+    main_compute = wrapper_comp.comp_instance(
+        "main_compute", "main", check_undeclared=False
+    )
 
     for mem in mems:
         mem_name = mem["name"]
         # Inputs/Outputs
         wrapper_inputs = [
-                (f"{mem_name}_ARESETn", 1),
-                (f"{mem_name}_ARREADY", 1),
-                (f"{mem_name}_RVALID", 1),
-                (f"{mem_name}_RLAST", 1),
-                (f"{mem_name}_RDATA", mem["width"]),
-                (f"{mem_name}_RRESP", 2),
-                (f"{mem_name}_AWREADY", 1),
-                (f"{mem_name}_WRESP", 2),
-                (f"{mem_name}_WREADY", 1),
-                (f"{mem_name}_BVALID", 1),
-                # Only used for waveform tracing, not sent anywhere
-                (f"{mem_name}_BRESP", 2),
-                # Only needed for coctb compatability, tied low
-                (f"{mem_name}_RID", 1),
-            ]
-        
+            (f"{mem_name}_ARESETn", 1),
+            (f"{mem_name}_ARREADY", 1),
+            (f"{mem_name}_RVALID", 1),
+            (f"{mem_name}_RLAST", 1),
+            (f"{mem_name}_RDATA", mem["width"]),
+            (f"{mem_name}_RRESP", 2),
+            (f"{mem_name}_AWREADY", 1),
+            (f"{mem_name}_WRESP", 2),
+            (f"{mem_name}_WREADY", 1),
+            (f"{mem_name}_BVALID", 1),
+            # Only used for waveform tracing, not sent anywhere
+            (f"{mem_name}_BRESP", 2),
+            # Only needed for coctb compatability, tied low
+            (f"{mem_name}_RID", 1),
+        ]
+
         wrapper_outputs = [
-                (f"{mem_name}_ARVALID", 1),
-                (f"{mem_name}_ARADDR", 64),
-                (f"{mem_name}_ARSIZE", 3),
-                (f"{mem_name}_ARLEN", 8),
-                (f"{mem_name}_ARBURST", 2),
-                (f"{mem_name}_RREADY", 1),
-                (f"{mem_name}_AWVALID", 1),
-                (f"{mem_name}_AWADDR", 64),
-                (f"{mem_name}_AWSIZE", 3),
-                (f"{mem_name}_AWLEN", 8),
-                (f"{mem_name}_AWBURST", 2),
-                (f"{mem_name}_AWPROT", 3),
-                (f"{mem_name}_WVALID", 1),
-                (f"{mem_name}_WLAST", 1),
-                (f"{mem_name}_WDATA", mem["width"]),
-                (f"{mem_name}_BREADY", 1),
-                # ID signals are needed for coco compatability, tied low
-                (f"{mem_name}_ARID", 1),
-                (f"{mem_name}_AWID", 1),
-                (f"{mem_name}_WID", 1),
-                (f"{mem_name}_BID", 1),
-            ]
-        
+            (f"{mem_name}_ARVALID", 1),
+            (f"{mem_name}_ARADDR", 64),
+            (f"{mem_name}_ARSIZE", 3),
+            (f"{mem_name}_ARLEN", 8),
+            (f"{mem_name}_ARBURST", 2),
+            (f"{mem_name}_RREADY", 1),
+            (f"{mem_name}_AWVALID", 1),
+            (f"{mem_name}_AWADDR", 64),
+            (f"{mem_name}_AWSIZE", 3),
+            (f"{mem_name}_AWLEN", 8),
+            (f"{mem_name}_AWBURST", 2),
+            (f"{mem_name}_AWPROT", 3),
+            (f"{mem_name}_WVALID", 1),
+            (f"{mem_name}_WLAST", 1),
+            (f"{mem_name}_WDATA", mem["width"]),
+            (f"{mem_name}_BREADY", 1),
+            # ID signals are needed for coco compatability, tied low
+            (f"{mem_name}_ARID", 1),
+            (f"{mem_name}_AWID", 1),
+            (f"{mem_name}_WID", 1),
+            (f"{mem_name}_BID", 1),
+        ]
 
         add_comp_params(wrapper_comp, wrapper_inputs, wrapper_outputs)
 
         # Cells
         # Read stuff
         curr_addr_internal_mem = wrapper_comp.reg(
-            f"curr_addr_internal_mem_{mem_name}", clog2_or_1(mem["size"])
+            clog2_or_1(mem["size"]), f"curr_addr_internal_mem_{mem_name}"
         )
-        curr_addr_axi = wrapper_comp.reg(f"curr_addr_axi_{mem_name}", 64)
+        curr_addr_axi = wrapper_comp.reg(64, f"curr_addr_axi_{mem_name}")
 
         wrapper_comp.cell(f"ar_channel_{mem_name}", ar_channel)
         wrapper_comp.cell(f"read_channel_{mem_name}", read_channel)
@@ -536,9 +539,8 @@ def add_main_comp(prog, mems):
             idx_size=clog2_or_1(mem["size"]),
         )
 
-
         # Write stuff
-        max_transfers = wrapper_comp.reg(f"max_transfers_{mem_name}", 8)
+        max_transfers = wrapper_comp.reg(8, f"max_transfers_{mem_name}")
         wrapper_comp.cell(f"aw_channel_{mem_name}", aw_channel)
         wrapper_comp.cell(f"write_channel_{mem_name}", write_channel)
         wrapper_comp.cell(f"bresp_channel_{mem_name}", bresp_channel)
@@ -555,7 +557,7 @@ def add_main_comp(prog, mems):
         # No groups needed!
 
         # set up internal control blocks
-        #TODO: turn these into parts of a par block
+        # TODO: turn these into parts of a par block
         this_component = wrapper_comp.this()
 
         ar_channel_invoke = invoke(
@@ -568,80 +570,75 @@ def add_main_comp(prog, mems):
             out_ARADDR=this_component[f"{mem_name}_ARADDR"],
             out_ARSIZE=this_component[f"{mem_name}_ARSIZE"],
             out_ARLEN=this_component[f"{mem_name}_ARLEN"],
-            out_ARBURST=this_component[f"{mem_name}_ARBURST"]
+            out_ARBURST=this_component[f"{mem_name}_ARBURST"],
         )
 
         read_channel_invoke = invoke(
             wrapper_comp.get_cell(f"read_channel_{mem_name}"),
-            ref_mem_ref = internal_mem,
-            ref_curr_addr_internal_mem = curr_addr_internal_mem,
-            ref_curr_addr_axi = curr_addr_axi,
-            in_ARESETn = this_component[f"{mem_name}_ARESETn"],
-            in_RVALID = this_component[f"{mem_name}_RVALID"],
-            in_RLAST = this_component[f"{mem_name}_RLAST"],
-            in_RDATA = this_component[f"{mem_name}_RDATA"],
-            #TODO: Do we need this? Don't think this goes anywhere
-            in_RRESP = this_component[f"{mem_name}_RRESP"],
-            out_RREADY = this_component[f"{mem_name}_RREADY"]
+            ref_mem_ref=internal_mem,
+            ref_curr_addr_internal_mem=curr_addr_internal_mem,
+            ref_curr_addr_axi=curr_addr_axi,
+            in_ARESETn=this_component[f"{mem_name}_ARESETn"],
+            in_RVALID=this_component[f"{mem_name}_RVALID"],
+            in_RLAST=this_component[f"{mem_name}_RLAST"],
+            in_RDATA=this_component[f"{mem_name}_RDATA"],
+            # TODO: Do we need this? Don't think this goes anywhere
+            in_RRESP=this_component[f"{mem_name}_RRESP"],
+            out_RREADY=this_component[f"{mem_name}_RREADY"],
         )
 
         aw_channel_invoke = invoke(
             wrapper_comp.get_cell(f"aw_channel_{mem_name}"),
-            ref_curr_addr_axi = curr_addr_axi,
-            ref_max_transfers = max_transfers,
-            in_ARESETn = this_component[f"{mem_name}_ARESETn"],
-            in_AWREADY = this_component[f"{mem_name}_AWREADY"],
-            out_AWVALID = this_component[f"{mem_name}_AWVALID"],
-            out_AWADDR = this_component[f"{mem_name}_AWADDR"],
-            out_AWSIZE = this_component[f"{mem_name}_AWSIZE"],
-            out_AWLEN = this_component[f"{mem_name}_AWLEN"],
-            out_AWBURST = this_component[f"{mem_name}_AWBURST"],
-            out_AWPROT = this_component[f"{mem_name}_AWPROT"]
+            ref_curr_addr_axi=curr_addr_axi,
+            ref_max_transfers=max_transfers,
+            in_ARESETn=this_component[f"{mem_name}_ARESETn"],
+            in_AWREADY=this_component[f"{mem_name}_AWREADY"],
+            out_AWVALID=this_component[f"{mem_name}_AWVALID"],
+            out_AWADDR=this_component[f"{mem_name}_AWADDR"],
+            out_AWSIZE=this_component[f"{mem_name}_AWSIZE"],
+            out_AWLEN=this_component[f"{mem_name}_AWLEN"],
+            out_AWBURST=this_component[f"{mem_name}_AWBURST"],
+            out_AWPROT=this_component[f"{mem_name}_AWPROT"],
         )
 
         write_channel_invoke = invoke(
             wrapper_comp.get_cell(f"write_channel_{mem_name}"),
-            ref_mem_ref = internal_mem,
-            ref_curr_addr_internal_mem = curr_addr_internal_mem,
-            ref_curr_addr_axi = curr_addr_axi,
-            ref_max_transfers = max_transfers,
-            in_ARESETn = this_component[f"{mem_name}_ARESETn"],
-            in_WREADY = this_component[f"{mem_name}_WREADY"],
-            out_WVALID = this_component[f"{mem_name}_WVALID"],
-            out_WLAST = this_component[f"{mem_name}_WLAST"],
-            out_WDATA = this_component[f"{mem_name}_WDATA"],
+            ref_mem_ref=internal_mem,
+            ref_curr_addr_internal_mem=curr_addr_internal_mem,
+            ref_curr_addr_axi=curr_addr_axi,
+            ref_max_transfers=max_transfers,
+            in_ARESETn=this_component[f"{mem_name}_ARESETn"],
+            in_WREADY=this_component[f"{mem_name}_WREADY"],
+            out_WVALID=this_component[f"{mem_name}_WVALID"],
+            out_WLAST=this_component[f"{mem_name}_WLAST"],
+            out_WDATA=this_component[f"{mem_name}_WDATA"],
         )
 
         bresp_channel_invoke = invoke(
             wrapper_comp.get_cell(f"bresp_channel_{mem_name}"),
-            in_BVALID = this_component[f"{mem_name}_BVALID"],
-            out_BREADY = this_component[f"{mem_name}_BREADY"]
+            in_BVALID=this_component[f"{mem_name}_BVALID"],
+            out_BREADY=this_component[f"{mem_name}_BREADY"],
         )
 
-        curr_addr_axi_invoke = invoke(
-            curr_addr_axi, in_in=0x1000
-        )
-        curr_addr_internal_invoke = invoke(
-            curr_addr_internal_mem, in_in=0x0000
-        )
+        curr_addr_axi_invoke = invoke(curr_addr_axi, in_in=0x1000)
+        curr_addr_internal_invoke = invoke(curr_addr_internal_mem, in_in=0x0000)
 
         curr_addr_axi_par.append(curr_addr_axi_invoke)
         curr_addr_internal_par.append(curr_addr_internal_invoke)
         reads_par.append([ar_channel_invoke, read_channel_invoke])
-        writes_par.append([aw_channel_invoke, write_channel_invoke, bresp_channel_invoke])
-        #Creates `<mem_name> = internal_mem_<mem_name>` as refs in invocation of `main_compute`
+        writes_par.append(
+            [aw_channel_invoke, write_channel_invoke, bresp_channel_invoke]
+        )
+        # Creates `<mem_name> = internal_mem_<mem_name>` as refs in invocation of `main_compute`
         ref_mem_kwargs[f"ref_{mem_name}"] = internal_mem
 
-
-    #Compute invoke
-    #Assumes refs should be of form `<mem_name> = internal_mem_<mem_name>`
+    # Compute invoke
+    # Assumes refs should be of form `<mem_name> = internal_mem_<mem_name>`
     main_compute_invoke = invoke(
-        wrapper_comp.get_cell("main_compute"),
-        **ref_mem_kwargs
+        wrapper_comp.get_cell("main_compute"), **ref_mem_kwargs
     )
 
-
-    #Compiler should reschedule these 2 seqs to be in parallel right?
+    # Compiler should reschedule these 2 seqs to be in parallel right?
     wrapper_comp.control += par(*curr_addr_axi_par)
     wrapper_comp.control += par(*curr_addr_internal_par)
 
@@ -650,10 +647,6 @@ def add_main_comp(prog, mems):
     # Reset axi adress to 0
     wrapper_comp.control += par(*curr_addr_axi_par)
     wrapper_comp.control += par(*writes_par)
-
-    
-
-
 
 
 # Helper functions
@@ -673,6 +666,7 @@ def clog2(x):
     if x <= 0:
         raise ValueError("x must be positive")
     return (x - 1).bit_length()
+
 
 def clog2_or_1(x):
     """Ceiling log2 or 1 if clog2(x) == 0"""
