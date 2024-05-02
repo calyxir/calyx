84d39655e52e48f81d1b5302d55f39519591d292
Caleb <calebkim707@gmail.com>
2024-05-02 09:10:42 -0400

    even more abstraction/refactorign

--- calyx-opt/src/analysis/static_schedule.rs
+++ calyx-opt/src/analysis/static_schedule.rs
@@ -5,6 +5,165 @@ use calyx_ir::{guard, structure};
 use std::collections::{HashSet, VecDeque};
 use std::rc::Rc;
 
+#[derive(Debug)]
+// Define an enum called Fruit
+pub enum FSMEncoding {
+    Binary,
+    OneHot,
+}
+
+#[derive(Debug)]
+struct StaticFSM {
+    pub _num_states: u64,
+    pub _encoding: FSMEncoding,
+    pub static_component_interface: bool,
+    bitwidth: u64,
+    cell: ir::RRC<ir::Cell>,
+}
+impl StaticFSM {
+    pub fn from_basic_info(
+        num_states: u64,
+        encoding: FSMEncoding,
+        static_component_interface: bool,
+        builder: &mut ir::Builder,
+    ) -> Self {
+        // First build the fsm we will use to realize the schedule.
+        let fsm_size =
+            get_bit_width_from(num_states + 1 /* represent 0..latency */);
+        let fsm = builder.add_primitive("fsm", "std_reg", &[fsm_size]);
+        StaticFSM {
+            _num_states: num_states,
+            _encoding: encoding,
+            static_component_interface,
+            bitwidth: fsm_size,
+            cell: fsm,
+        }
+    }
+
+    pub fn count_to_n(
+        &self,
+        builder: &mut ir::Builder,
+        n: u64,
+    ) -> Vec<ir::Assignment<Nothing>> {
+        // Only support Binary encoding currently.
+        assert!(matches!(self._encoding, FSMEncoding::Binary));
+        // Add assignments to increment the fsm by one unconditionally.
+        structure!( builder;
+            // done hole will be undefined bc of early reset
+            let signal_on = constant(1,1);
+            let adder = prim std_add(self.bitwidth);
+            let const_one = constant(1, self.bitwidth);
+            let first_state = constant(0, self.bitwidth);
+            let final_state = constant(n, self.bitwidth);
+        );
+        let fsm_cell = Rc::clone(&self.cell);
+        let final_state_guard: ir::Guard<Nothing> =
+            guard!(fsm_cell["out"] == final_state["out"]);
+        if self.static_component_interface {
+            // The requirements for components that need support the static
+            // interface are slightly different.
+            // We need to guard the FSM 0->1 transition with (fsm == 0 & comp.go)
+            // The reason why we can't just gaurd with `comp.go` is because
+            // we want clients to be able to assert `go` while the component
+            // is executing without messing things up,
+            // (even if asserting `go` is unnecessary.)
+            let this: Rc<std::cell::RefCell<ir::Cell>> =
+                Rc::clone(&builder.component.signature.clone());
+            let this_go: ir::Guard<Nothing> = guard!(this["go"]);
+            let first_state_guard: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] == first_state["out"]);
+            let go_and_first_state = ir::Guard::and(this_go, first_state_guard);
+            let not_first_state: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] != first_state["out"]);
+            let not_last_state: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] != final_state["out"]);
+            let in_between_guard =
+                ir::Guard::and(not_first_state, not_last_state);
+            build_assignments!(
+              builder;
+              // Incrementsthe fsm
+              adder["left"] = ? fsm_cell["out"];
+              adder["right"] = ? const_one["out"];
+              // Always write into fsm.
+              fsm_cell["write_en"] = ? signal_on["out"];
+              // If fsm == 0 and comp.go is high, then we start an execution.
+              fsm_cell["in"] = go_and_first_state ? const_one["out"];
+              // If 1 < fsm < n - 1, then we unconditionally increment the fsm.
+              fsm_cell["in"] = in_between_guard ? adder["out"];
+              // If fsm == n -1 , then we reset the FSM.
+              fsm_cell["in"] = final_state_guard ? first_state["out"];
+              // Otherwise the FSM is not assigned to, so it defaults to 0.
+              // If we want, we could add an explicit assignment here that sets it
+              // to zero.
+            )
+            .to_vec()
+        } else {
+            // "Normal" logic to increment FSM by one.
+            let not_final_state_guard: ir::Guard<ir::Nothing> =
+                guard!(fsm_cell["out"] != final_state["out"]);
+            build_assignments!(
+              builder;
+              // increments the fsm
+              adder["left"] = ? fsm_cell["out"];
+              adder["right"] = ? const_one["out"];
+              fsm_cell["write_en"] = ? signal_on["out"];
+              fsm_cell["in"] =  not_final_state_guard ? adder["out"];
+               // resets the fsm early
+               fsm_cell["in"] = final_state_guard ? first_state["out"];
+            )
+            .to_vec()
+        }
+    }
+
+    pub fn query_between(
+        &self,
+        builder: &mut ir::Builder,
+        query: (u64, u64),
+        comp_sig: Option<ir::RRC<ir::Cell>>,
+    ) -> Box<ir::Guard<Nothing>> {
+        let (beg, end) = query;
+        let fsm_cell = Rc::clone(&self.cell);
+        if self.static_component_interface && beg == 0 && end == 1 {
+            // Replace `%0`` with `fsm == 0 & comp.go`.
+            // The reason why we can't just gaurd with `comp.go` is because
+            // we want clients to be able to assert `go` while the component
+            // is executing without messing things up,
+            // (even if asserting `go` is unnecessary.)
+            let interval_const = builder.add_constant(0, self.bitwidth);
+            let sig = comp_sig.unwrap();
+            let g1 = guard!(sig["go"]);
+            let g2 = guard!(fsm_cell["out"] == interval_const["out"]);
+            let g = ir::Guard::And(Box::new(g1), Box::new(g2));
+            return Box::new(g);
+        }
+        if beg + 1 == end {
+            // if beg + 1 == end then we only need to check if fsm == beg
+            let interval_const = builder.add_constant(beg, self.bitwidth);
+            let g = guard!(fsm_cell["out"] == interval_const["out"]);
+            Box::new(g)
+        } else if beg == 0 {
+            // if beg == 0, then we only need to check if fsm < end
+            let end_const = builder.add_constant(end, self.bitwidth);
+            let lt: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] < end_const["out"]);
+            Box::new(lt)
+        } else {
+            // otherwise, check if fsm >= beg & fsm < end
+            let beg_const = builder.add_constant(beg, self.bitwidth);
+            let end_const = builder.add_constant(end, self.bitwidth);
+            let beg_guard: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] >= beg_const["out"]);
+            let end_guard: ir::Guard<Nothing> =
+                guard!(fsm_cell["out"] < end_const["out"]);
+            Box::new(ir::Guard::And(Box::new(beg_guard), Box::new(end_guard)))
+        }
+    }
+
+    pub fn get_cell(&self) -> ir::RRC<ir::Cell> {
+        Rc::clone(&self.cell)
+    }
+}
+
 /// Represents a static schedule.
 /// A static schedule does not need transitions--it will unconditionally increment
 /// by one each time.
@@ -89,10 +248,12 @@ impl StaticSchedule {
         static_component_interface: bool,
     ) -> (VecDeque<Vec<ir::Assignment<Nothing>>>, ir::RRC<ir::Cell>) {
         // First build the fsm we will use to realize the schedule.
-        let fsm_size = get_bit_width_from(
-            self.num_states + 1, /* represent 0..latency */
+        let fsm_object = StaticFSM::from_basic_info(
+            self.num_states,
+            FSMEncoding::Binary,
+            static_component_interface,
+            builder,
         );
-        let fsm = builder.add_primitive("fsm", "std_reg", &[fsm_size]);
 
         // Instantiate the vecdeque.
         let mut res = VecDeque::new();
@@ -113,86 +274,21 @@ impl StaticSchedule {
                 .map(|static_assign| {
                     Self::make_assign_dyn(
                         static_assign,
-                        &fsm,
-                        fsm_size,
+                        &fsm_object,
                         builder,
                         static_component_interface,
                         Some(Rc::clone(&builder.component.signature)),
                     )
                 })
                 .collect();
-
-            // Add assignments to increment the fsm by one unconditionally.
-            structure!( builder;
-                // done hole will be undefined bc of early reset
-                let signal_on = constant(1,1);
-                let adder = prim std_add(fsm_size);
-                let const_one = constant(1, fsm_size);
-                let first_state = constant(0, fsm_size);
-                let final_state = constant(static_group_ref.get_latency() - 1, fsm_size);
+            assigns.extend(
+                fsm_object
+                    .count_to_n(builder, static_group_ref.get_latency() - 1),
             );
-            let final_state_guard: ir::Guard<Nothing> =
-                guard!(fsm["out"] == final_state["out"]);
-            let fsm_incr_assigns = if static_component_interface {
-                // The requirements for components that need support the static
-                // interface are slightly different.
-                // We need to guard the FSM 0->1 transition with (fsm == 0 & comp.go)
-                // The reason why we can't just gaurd with `comp.go` is because
-                // we want clients to be able to assert `go` while the component
-                // is executing without messing things up,
-                // (even if asserting `go` is unnecessary.)
-                let this: Rc<std::cell::RefCell<ir::Cell>> =
-                    Rc::clone(&builder.component.signature.clone());
-                let this_go: ir::Guard<Nothing> = guard!(this["go"]);
-                let first_state_guard: ir::Guard<Nothing> =
-                    guard!(fsm["out"] == first_state["out"]);
-                let go_and_first_state =
-                    ir::Guard::and(this_go, first_state_guard);
-                let not_first_state: ir::Guard<Nothing> =
-                    guard!(fsm["out"] != first_state["out"]);
-                let not_last_state: ir::Guard<Nothing> =
-                    guard!(fsm["out"] != final_state["out"]);
-                let in_between_guard =
-                    ir::Guard::and(not_first_state, not_last_state);
-                build_assignments!(
-                  builder;
-                  // Incrementsthe fsm
-                  adder["left"] = ? fsm["out"];
-                  adder["right"] = ? const_one["out"];
-                  // Always write into fsm.
-                  fsm["write_en"] = ? signal_on["out"];
-                  // If fsm == 0 and comp.go is high, then we start an execution.
-                  fsm["in"] = go_and_first_state ? const_one["out"];
-                  // If 1 < fsm < n - 1, then we unconditionally increment the fsm.
-                  fsm["in"] = in_between_guard ? adder["out"];
-                  // If fsm == n -1 , then we reset the FSM.
-                  fsm["in"] = final_state_guard ? first_state["out"];
-                  // Otherwise the FSM is not assigned to, so it defaults to 0.
-                  // If we want, we could add an explicit assignment here that sets it
-                  // to zero.
-                )
-                .to_vec()
-            } else {
-                // "Normal" logic to increment FSM by one.
-                let not_final_state_guard: ir::Guard<ir::Nothing> =
-                    guard!(fsm["out"] != final_state["out"]);
-                build_assignments!(
-                  builder;
-                  // increments the fsm
-                  adder["left"] = ? fsm["out"];
-                  adder["right"] = ? const_one["out"];
-                  fsm["write_en"] = ? signal_on["out"];
-                  fsm["in"] =  not_final_state_guard ? adder["out"];
-                   // resets the fsm early
-                  fsm["in"] = final_state_guard ? first_state["out"];
-                )
-                .to_vec()
-            };
-            assigns.extend(fsm_incr_assigns);
 
             res.push_back(assigns);
         }
-        (res, fsm)
+        (res, fsm_object.get_cell())
     }
 
     // Takes in a static guard `guard`, and returns equivalent dynamic guard
@@ -202,8 +298,7 @@ impl StaticSchedule {
     // is_static_comp is necessary becasue it ...
     fn make_guard_dyn(
         guard: ir::Guard<ir::StaticTiming>,
-        fsm: &ir::RRC<ir::Cell>,
-        fsm_size: u64,
+        fsm_object: &StaticFSM,
         builder: &mut ir::Builder,
         static_component_interface: bool,
         comp_sig: Option<ir::RRC<ir::Cell>>,
@@ -212,16 +307,14 @@ impl StaticSchedule {
             ir::Guard::Or(l, r) => Box::new(ir::Guard::Or(
                 Self::make_guard_dyn(
                     *l,
-                    fsm,
-                    fsm_size,
+                    fsm_object,
                     builder,
                     static_component_interface,
                     comp_sig.clone(),
                 ),
                 Self::make_guard_dyn(
                     *r,
-                    fsm,
-                    fsm_size,
+                    fsm_object,
                     builder,
                     static_component_interface,
                     comp_sig,
@@ -230,16 +323,14 @@ impl StaticSchedule {
             ir::Guard::And(l, r) => Box::new(ir::Guard::And(
                 Self::make_guard_dyn(
                     *l,
-                    fsm,
-                    fsm_size,
+                    fsm_object,
                     builder,
                     static_component_interface,
                     comp_sig.clone(),
                 ),
                 Self::make_guard_dyn(
                     *r,
-                    fsm,
-                    fsm_size,
+                    fsm_object,
                     builder,
                     static_component_interface,
                     comp_sig,
@@ -248,8 +339,7 @@ impl StaticSchedule {
             ir::Guard::Not(g) => {
                 Box::new(ir::Guard::Not(Self::make_guard_dyn(
                     *g,
-                    fsm,
-                    fsm_size,
+                    fsm_object,
                     builder,
                     static_component_interface,
                     comp_sig,
@@ -260,46 +350,11 @@ impl StaticSchedule {
             }
             ir::Guard::Port(p) => Box::new(ir::Guard::Port(p)),
             ir::Guard::True => Box::new(ir::Guard::True),
-            ir::Guard::Info(static_timing) => {
-                let (beg, end) = static_timing.get_interval();
-                if static_component_interface && beg == 0 && end == 1 {
-                    // Replace `%0`` with `fsm == 0 & comp.go`.
-                    // The reason why we can't just gaurd with `comp.go` is because
-                    // we want clients to be able to assert `go` while the component
-                    // is executing without messing things up,
-                    // (even if asserting `go` is unnecessary.)
-                    let interval_const = builder.add_constant(0, fsm_size);
-                    let sig = comp_sig.unwrap();
-                    let g1 = guard!(sig["go"]);
-                    let g2 = guard!(fsm["out"] == interval_const["out"]);
-                    let g = ir::Guard::And(Box::new(g1), Box::new(g2));
-                    return Box::new(g);
-                }
-                if beg + 1 == end {
-                    // if beg + 1 == end then we only need to check if fsm == beg
-                    let interval_const = builder.add_constant(beg, fsm_size);
-                    let g = guard!(fsm["out"] == interval_const["out"]);
-                    Box::new(g)
-                } else if beg == 0 {
-                    // if beg == 0, then we only need to check if fsm < end
-                    let end_const = builder.add_constant(end, fsm_size);
-                    let lt: ir::Guard<Nothing> =
-                        guard!(fsm["out"] < end_const["out"]);
-                    Box::new(lt)
-                } else {
-                    // otherwise, check if fsm >= beg & fsm < end
-                    let beg_const = builder.add_constant(beg, fsm_size);
-                    let end_const = builder.add_constant(end, fsm_size);
-                    let beg_guard: ir::Guard<Nothing> =
-                        guard!(fsm["out"] >= beg_const["out"]);
-                    let end_guard: ir::Guard<Nothing> =
-                        guard!(fsm["out"] < end_const["out"]);
-                    Box::new(ir::Guard::And(
-                        Box::new(beg_guard),
-                        Box::new(end_guard),
-                    ))
-                }
-            }
+            ir::Guard::Info(static_timing) => fsm_object.query_between(
+                builder,
+                static_timing.get_interval(),
+                comp_sig,
+            ),
         }
     }
 
@@ -307,8 +362,7 @@ impl StaticSchedule {
     // Mainly transforms the guards from %[2:3] -> fsm.out >= 2 & fsm.out <= 3
     fn make_assign_dyn(
         assign: ir::Assignment<ir::StaticTiming>,
-        fsm: &ir::RRC<ir::Cell>,
-        fsm_size: u64,
+        fsm_object: &StaticFSM,
         builder: &mut ir::Builder,
         static_component_interface: bool,
         comp_sig: Option<ir::RRC<ir::Cell>>,
@@ -319,8 +373,7 @@ impl StaticSchedule {
             attributes: assign.attributes,
             guard: Self::make_guard_dyn(
                 *assign.guard,
-                fsm,
-                fsm_size,
+                &fsm_object,
                 builder,
                 static_component_interface,
                 comp_sig,
