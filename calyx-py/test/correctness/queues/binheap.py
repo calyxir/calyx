981c350fb7ce7fc552936caa1f294a507973082e
Anshuman Mohan <anshumanmohan@live.com>
2024-05-29 16:42:20 -0400

    Simplify parent-finding routine

--- calyx-py/test/correctness/queues/binheap.py
+++ calyx-py/test/correctness/queues/binheap.py
@@ -57,7 +57,6 @@ def insert_binheap(prog, name):
 
     sub = comp.sub(4)
     rsh = comp.rsh(4)
-    eq = comp.eq(4)
 
     parent_idx = comp.reg(4)
     parent_val = comp.reg(64)
@@ -68,25 +67,14 @@ def insert_binheap(prog, name):
     read_child = comp.mem_load_d1(mem, child_idx.out, child_val, "read_child")
 
     with comp.group("find_parent_idx") as find_parent_idx:
-        # Find the parent of the `child`th element and store it in `parent`.
-        # If child is 0, parent is 0.
-        # Otherwise, parent := floor((child − 1) / 2)
-
-        # Case when child = 0: parent := 0
-        eq.left = child_idx.out
-        eq.right = 0
-        parent_idx.in_ = eq.out @ cb.const(4, 0)
-        parent_idx.write_en = eq.out @ cb.HI
-
-        # Else case: parent := floor((child − 1) / 2)
-        sub.left = ~eq.out @ child_idx.out
-        sub.right = ~eq.out @ 1
-        rsh.left = ~eq.out @ sub.out
-        rsh.right = ~eq.out @ cb.const(4, 1)
-        parent_idx.in_ = ~eq.out @ rsh.out
-        parent_idx.write_en = ~eq.out @ cb.HI
-
-        # In either case, we are done when parent_idx is done.
+        # Find the parent of the `child_idx`th element and store it in `parent_idx`.
+        # parent_idx := floor((child_idx − 1) / 2)
+        sub.left = child_idx.out
+        sub.right = 1
+        rsh.left = sub.out
+        rsh.right = cb.const(4, 1)
+        parent_idx.in_ = rsh.out
+        parent_idx.write_en = cb.HI
         find_parent_idx.done = parent_idx.done
 
     set_child_idx = comp.reg_store(child_idx, size.out)  # child_idx := size
