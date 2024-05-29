2c2e8dbc780c634dc42422de4f05083c58a08016
Nathaniel Navarro <nathanielnrn99@gmail.com>
2024-05-29 12:29:04 -0400

    add runt test for port attributes

---
+++ calyx-py/test/port_attributes.py
@@ -0,0 +1,22 @@
+import calyx.builder as cb
+
+
+def insert_foo_component(prog):
+    comp = prog.component("foo")
+
+    foo_inputs = [
+        ("in_1", 1),
+        ("in_2", 2, ["data"]),
+        ("in_3", 2, ["data", ("write_together", 1)])
+        ]
+
+    cb.add_comp_ports(comp, foo_inputs, [])
+
+    comp.output("out_1", 1)
+    comp.output("out_2", 1, ["data"])
+    comp.output("out_3", 1, ["data", ("done", 1)])
+
+if __name__ == "__main__":
+    prog = cb.Builder()
+    insert_foo_component(prog)
+    prog.program.emit()
