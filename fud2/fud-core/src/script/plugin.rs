450260d180fd5ae412f8325072379179aa8ac09c
Adrian Sampson <adrian@radbox.org>
2024-06-02 22:07:35 -0400

    Refactor the rest of the functions

--- fud2/fud-core/src/script/plugin.rs
+++ fud2/fud-core/src/script/plugin.rs
@@ -65,6 +65,88 @@ impl ScriptBuilder {
             },
         );
     }
+
+    fn reg_get_state(&self, engine: &mut rhai::Engine) {
+        let t = self.0.clone();
+        engine.register_fn("get_state", move |state_name: &str| {
+            t.borrow().find_state(state_name).map_err(to_rhai_err)
+        });
+    }
+
+    fn reg_get_setup(&self, engine: &mut rhai::Engine) {
+        let t = self.0.clone();
+        engine.register_fn("get_setup", move |setup_name: &str| {
+            t.borrow().find_setup(setup_name).map_err(to_rhai_err)
+        });
+    }
+
+    // TODO: Revisit whether these parameters can be in the struct.
+    fn reg_rule(
+        &self,
+        engine: &mut rhai::Engine,
+        path: &PathBuf,
+        ast: &rhai::AST,
+    ) {
+        let t = self.0.clone();
+        let rule_ast = ast.clone_functions_only();
+        let path_copy = path.clone();
+        engine.register_fn::<_, 4, true, OpRef, true, _>(
+            "rule",
+            move |ctx: rhai::NativeCallContext,
+                  setups: rhai::Array,
+                  input: StateRef,
+                  output: StateRef,
+                  rule_name: &str| {
+                let setups = to_setup_refs(
+                    &ctx,
+                    setups,
+                    path_copy.clone(),
+                    rule_ast.clone(),
+                    Rc::clone(&t),
+                )?;
+                Ok(t.borrow_mut().rule(&setups, input, output, rule_name))
+            },
+        );
+    }
+
+    fn reg_op(
+        &self,
+        engine: &mut rhai::Engine,
+        path: &PathBuf,
+        ast: &rhai::AST,
+    ) {
+        let t = self.0.clone();
+        let rule_ast = ast.clone_functions_only();
+        let path_copy = path.clone();
+        engine.register_fn::<_, 5, true, OpRef, true, _>(
+            "op",
+            move |ctx: rhai::NativeCallContext,
+                  name: &str,
+                  setups: rhai::Array,
+                  input: StateRef,
+                  output: StateRef,
+                  build: rhai::FnPtr| {
+                let setups = to_setup_refs(
+                    &ctx,
+                    setups,
+                    path_copy.clone(),
+                    rule_ast.clone(),
+                    Rc::clone(&t),
+                )?;
+                Ok(t.borrow_mut().add_op(
+                    name,
+                    &setups,
+                    input,
+                    output,
+                    RhaiSetupCtx {
+                        path: path_copy.clone(),
+                        ast: rule_ast.clone(),
+                        name: build.fn_name().to_string(),
+                    },
+                ))
+            },
+        );
+    }
 }
 
 pub trait LoadPlugins {
@@ -88,16 +170,8 @@ impl LoadPlugins for DriverBuilder {
 
             // register AST independent functions
             bld.reg_state(&mut engine);
-
-            let t = Rc::clone(&bld.0);
-            engine.register_fn("get_state", move |state_name: &str| {
-                t.borrow().find_state(state_name).map_err(to_rhai_err)
-            });
-
-            let t = Rc::clone(&bld.0);
-            engine.register_fn("get_setup", move |setup_name: &str| {
-                t.borrow().find_setup(setup_name).map_err(to_rhai_err)
-            });
+            bld.reg_get_state(&mut engine);
+            bld.reg_get_setup(&mut engine);
 
             // go through each plugin file, and execute the script which adds a plugin
             // we need to define the following two functions in the loop because they
@@ -106,59 +180,8 @@ impl LoadPlugins for DriverBuilder {
                 // compile the file into an Ast
                 let ast = engine.compile_file(path.clone()).unwrap();
 
-                let t = Rc::clone(&bld.0);
-                let rule_ast = ast.clone_functions_only();
-                let path_copy = path.clone();
-                engine.register_fn::<_, 4, true, OpRef, true, _>(
-                    "rule",
-                    move |ctx: rhai::NativeCallContext,
-                          setups: rhai::Array,
-                          input: StateRef,
-                          output: StateRef,
-                          rule_name: &str| {
-                        let setups = to_setup_refs(
-                            &ctx,
-                            setups,
-                            path_copy.clone(),
-                            rule_ast.clone(),
-                            Rc::clone(&t),
-                        )?;
-                        Ok(t.borrow_mut()
-                            .rule(&setups, input, output, rule_name))
-                    },
-                );
-
-                let t = Rc::clone(&bld.0);
-                let rule_ast = ast.clone_functions_only();
-                let path_copy = path.clone();
-                engine.register_fn::<_, 5, true, OpRef, true, _>(
-                    "op",
-                    move |ctx: rhai::NativeCallContext,
-                          name: &str,
-                          setups: rhai::Array,
-                          input: StateRef,
-                          output: StateRef,
-                          build: rhai::FnPtr| {
-                        let setups = to_setup_refs(
-                            &ctx,
-                            setups,
-                            path_copy.clone(),
-                            rule_ast.clone(),
-                            Rc::clone(&t),
-                        )?;
-                        Ok(t.borrow_mut().add_op(
-                            name,
-                            &setups,
-                            input,
-                            output,
-                            RhaiSetupCtx {
-                                path: path_copy.clone(),
-                                ast: rule_ast.clone(),
-                                name: build.fn_name().to_string(),
-                            },
-                        ))
-                    },
-                );
+                bld.reg_rule(&mut engine, &path, &ast);
+                bld.reg_op(&mut engine, &path, &ast);
 
                 engine.run_ast(&ast).report(&path);
             }
