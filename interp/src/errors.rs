be274e6f0645ca61f90a4a9124acebebf0f100fb
Griffin Berlstein <griffin@berlste.in>
2024-06-11 13:56:35 -0400

    [Cider 2] Invoke fixes, mult imp, and error messages (#2128)
    * add multiplier and fix an invoke bug
    
    * Update the errors to be a bit more helpful
    
    * all hail clippy

--- interp/src/errors.rs
+++ interp/src/errors.rs
@@ -12,6 +12,12 @@ pub type InterpreterResult<T> = Result<T, BoxedInterpreterError>;
 
 pub struct BoxedInterpreterError(Box<InterpreterError>);
 
+impl BoxedInterpreterError {
+    pub fn into_inner(&mut self) -> &mut InterpreterError {
+        &mut self.0
+    }
+}
+
 impl std::fmt::Display for BoxedInterpreterError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         std::fmt::Display::fmt(&*self.0, f)
@@ -179,16 +185,22 @@ pub enum InterpreterError {
     IOError(#[from] std::io::Error),
 
     //TODO Griffin: Make this more descriptive
-    #[error("Attempted to write an undefined value to register or memory")]
-    UndefinedWrite,
+    #[error(
+        "Attempted to write an undefined value to register or memory named \"{0}\""
+    )]
+    UndefinedWrite(String),
 
     //TODO Griffin: Make this more descriptive
-    #[error("Attempted to write an undefined memory address")]
-    UndefinedWriteAddr,
+    #[error(
+        "Attempted to write an undefined memory address in memory named \"{0}\""
+    )]
+    UndefinedWriteAddr(String),
 
     // TODO Griffin: Make this more descriptive
-    #[error("Attempted to read an undefined memory address")]
-    UndefinedReadAddr,
+    #[error(
+        "Attempted to read an undefined memory address from memory named \"{0}\""
+    )]
+    UndefinedReadAddr(String),
 
     #[error(transparent)]
     SerializationError(
