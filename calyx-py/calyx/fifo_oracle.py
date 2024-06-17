e6abad1399296a3aee61caacacc8d6d0ca76e17c
Anshuman Mohan <anshumanmohan@live.com>
2024-06-17 15:06:54 -0400

    Stray file

--- calyx-py/calyx/fifo_oracle.py
+++ calyx-py/calyx/fifo_oracle.py
@@ -3,6 +3,6 @@ from calyx import queue_util
 
 if __name__ == "__main__":
     commands, values = queue_util.parse_json()
-    fifo = queues.Fifo(20000)
+    fifo = queues.Fifo(16)
     ans = queues.operate_queue(commands, values, fifo)
     queue_util.dump_json(commands, values, ans)
