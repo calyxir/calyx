ea6599ea0c8195e654139bba69ae078265ebf5f6
Nathaniel Navarro <nathanielnrn99@gmail.com>
2024-06-17 17:09:50 -0400

    get axi-wrapped version outputting vcd's (fsts?)

--- fud2/fud-core/src/utils.rs
+++ fud2/fud-core/src/utils.rs
@@ -1,5 +1,6 @@
 use camino::{Utf8Path, Utf8PathBuf};
 use pathdiff::diff_utf8_paths;
+use std::path::Path;
 
 /// Get a version of `path` that works when the working directory is `base`. This is
 /// opportunistically a relative path, but we can always fall back to an absolute path to make sure
@@ -11,4 +12,16 @@ pub fn relative_path(path: &Utf8Path, base: &Utf8Path) -> Utf8PathBuf {
             .canonicalize_utf8()
             .expect("could not get absolute path"),
     }
+}
+
+/// Get the basename of a file as designated in a &str.
+pub fn basename(path: &str) -> &str {
+    let file_stem = Path::new(path).file_stem().unwrap_or_else(|| {
+        panic!("Could not extract a file name from path: {}", path)
+    });
+    return file_stem.to_str().unwrap_or_else(|| {
+        unreachable!(
+            "Error when converting file_name to &str. Shouldn't be possible."
+        )
+    });
 }
