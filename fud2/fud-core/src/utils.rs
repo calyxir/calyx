38c67901357762852734cf96376d9c751617e39c
Nathaniel Navarro <nathanielnrn99@gmail.com>
2024-06-19 14:38:26 -0400

    use utf8path instead of path in basname

--- fud2/fud-core/src/utils.rs
+++ fud2/fud-core/src/utils.rs
@@ -1,6 +1,5 @@
 use camino::{Utf8Path, Utf8PathBuf};
 use pathdiff::diff_utf8_paths;
-use std::path::Path;
 
 /// Get a version of `path` that works when the working directory is `base`. This is
 /// opportunistically a relative path, but we can always fall back to an absolute path to make sure
@@ -16,12 +15,5 @@ pub fn relative_path(path: &Utf8Path, base: &Utf8Path) -> Utf8PathBuf {
 
 /// Get the basename of a file as designated in a &str.
 pub fn basename(path: &str) -> &str {
-    let file_stem = Path::new(path).file_stem().unwrap_or_else(|| {
-        panic!("Could not extract a file name from path: {}", path)
-    });
-    return file_stem.to_str().unwrap_or_else(|| {
-        unreachable!(
-            "Error when converting file_name to &str. Shouldn't be possible."
-        )
-    });
+    return Utf8Path::new(path).file_stem().expect(&format!("Could not extract a file name from path: {}", path));
 }
