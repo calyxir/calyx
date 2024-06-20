d8c740a1b8542e1ac09684c823492870b91edc00
Anshuman Mohan <anshumanmohan@live.com>
2024-06-20 12:48:01 -0400

    Builder support for latching d1 mems

--- calyx-py/calyx/builder.py
+++ calyx-py/calyx/builder.py
@@ -832,6 +832,18 @@ class ComponentBuilder:
             load_grp.done = reg.done
         return load_grp
 
+    def mem_latch_d1(self, mem, i, reg, groupname):
+        """Inserts wiring into `self` to latch `mem[i]`,
+        where `mem` is a comb_mem_d1 memory.
+        A user can later read `mem.out` and get the latched value.
+        """
+        assert mem.is_comb_mem_d1()
+        with self.group(groupname) as latch_grp:
+            mem.addr0 = i
+            mem.content_en = 1
+            latch_grp.done = mem.done
+        return latch_grp
+
     def mem_load_d2(self, mem, i, j, reg, groupname):
         """Inserts wiring into `self` to perform `reg := mem[i][j]`,
         where `mem` is a seq_d2 memory or a comb_mem_d2 memory
