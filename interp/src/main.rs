b95143e7e81b7a0c688f0eb94d09236fa14c737f
Griffin Berlstein <griffin@berlste.in>
2024-06-24 15:12:43 -0400

    Here we go, this is gonna be a process

--- interp/src/main.rs
+++ interp/src/main.rs
@@ -41,8 +41,8 @@ pub struct Opts {
     #[argh(option, short = 'l', default = "Path::new(\"..\").into()")]
     pub lib_path: PathBuf,
 
-    /// path to optional datafile used to initialze memories. If it is not
-    /// provided memories will be initialzed with zeros
+    /// path to optional datafile used to initialize memories. If it is not
+    /// provided memories will be initialized with zeros
     #[argh(option, long = "data", short = 'd')]
     pub data_file: Option<PathBuf>,
 
@@ -66,10 +66,6 @@ pub struct Opts {
     #[argh(switch, short = 'q', long = "--quiet")]
     quiet: bool,
 
-    /// serializes values using base64 encoding
-    #[argh(switch, long = "raw")]
-    raw: bool,
-
     #[argh(subcommand)]
     comm: Option<Command>,
 }
@@ -79,23 +75,12 @@ pub struct Opts {
 enum Command {
     Interpret(CommandInterpret),
     Debug(CommandDebug),
-    Flat(FlatInterp),
 }
 
 #[derive(FromArgs)]
 #[argh(subcommand, name = "interpret")]
-/// Interpret the given program directly
-struct CommandInterpret {}
-
-#[derive(FromArgs)]
-#[argh(subcommand, name = "debug")]
-/// Interpret the given program with the interactive debugger
-struct CommandDebug {}
-
-#[derive(FromArgs)]
-#[argh(subcommand, name = "flat")]
 /// tests the flattened interpreter
-struct FlatInterp {
+struct CommandInterpret {
     /// dump registers as memories
     #[argh(switch, long = "dump-registers")]
     dump_registers: bool,
@@ -103,6 +88,11 @@ struct FlatInterp {
     #[argh(switch, long = "all-memories")]
     dump_all_memories: bool,
 }
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "debug")]
+/// Interpret the given program with the interactive debugger
+struct CommandDebug {}
 
 #[inline]
 fn print_res(
@@ -157,53 +147,13 @@ fn main() -> InterpreterResult<()> {
         pm.execute_plan(&mut ctx, &["validate".to_string()], &[], &[], false)?;
     }
 
-    let command = opts.comm.unwrap_or(Command::Interpret(CommandInterpret {}));
+    let command = opts.comm.unwrap_or(Command::Interpret(CommandInterpret {
+        dump_registers: false,
+        dump_all_memories: false,
+    }));
 
     match &command {
-        comm @ (Command::Interpret(_) | Command::Debug(_)) => {
-            let entry_point = ctx.entrypoint;
-            let metadata = ctx.metadata;
-
-            let components: iir::ComponentCtx = Rc::new(
-                ctx.components
-                    .into_iter()
-                    .map(|x| Rc::new(x.into()))
-                    .collect(),
-            );
-
-            let main_component = components
-                .iter()
-                .find(|&cm| cm.name == entry_point)
-                .ok_or(InterpreterError::MissingMainComponent)?;
-
-            let mut mems = interp::MemoryMap::inflate_map(&opts.data_file)?;
-
-            let env = InterpreterState::init_top_level(
-                &components,
-                main_component,
-                &mut mems,
-                &config,
-            )?;
-
-            let res = if matches!(comm, Command::Interpret(_)) {
-                ComponentInterpreter::interpret_program(env, main_component)
-            } else {
-                let map = if let Some(map_res) =
-                    metadata.map(SourceMap::from_string)
-                {
-                    Some(map_res?)
-                } else {
-                    None
-                };
-
-                let cidb =
-                    Debugger::new(&components, main_component, map, env)?;
-                cidb.main_loop()
-            };
-
-            print_res(res, opts.raw)
-        }
-        Command::Flat(configs) => {
+        Command::Interpret(configs) => {
             let i_ctx = interp::flatten::flat_ir::translate(&ctx);
             let data_dump = opts
                 .data_file
@@ -225,6 +175,9 @@ fn main() -> InterpreterResult<()> {
 
             output.serialize(&mut stdout())?;
             Ok(())
+        }
+        Command::Debug(_configs) => {
+            todo!()
         }
     }
 }
