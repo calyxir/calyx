5f81dc210a4557495329d9be733f96e0dc9c72e5
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-06-30 02:09:33 -0400

    Fix

--- tools/tb/src/testbench.rs
+++ tools/tb/src/testbench.rs
@@ -7,14 +7,22 @@ pub mod verilator;
 pub type TestbenchResult = std::io::Result<()>;
 
 fn testbench_error_unknown_tb<S: AsRef<str>>(tb: S) -> TestbenchResult {
-    TestbenchResult::Err(std::io::Error::other(format!("Unknown testbench '{}'", tb.as_ref())))
+    TestbenchResult::Err(std::io::Error::other(format!(
+        "Unknown testbench '{}'",
+        tb.as_ref()
+    )))
 }
 
 pub trait Testbench {
     // TODO: add a config system here
     /// - `input` is a relative path to the input file in `work_dir`.
     /// - `tests` are a relative paths to the testing harnesses in `work_dir`.
-    fn run(&self, input: String, tests: &[String], work_dir: TempDir) -> TestbenchResult;
+    fn run(
+        &self,
+        input: String,
+        tests: &[String],
+        work_dir: TempDir,
+    ) -> TestbenchResult;
 }
 
 pub type TestbenchRef = Box<dyn Testbench>;
@@ -34,12 +42,17 @@ impl TestbenchManager {
 
     pub fn register<S: AsRef<str>>(&mut self, name: S, tb: TestbenchRef) {
         assert!(
-            self.tbs.insert(name.as_ref().to_string(), tb).is_none(), 
+            self.tbs.insert(name.as_ref().to_string(), tb).is_none(),
             "cannot re-register the same testbench name for a different testbench"
         );
     }
 
-    pub fn run<S: AsRef<str>>(&self, name: S, input: String, tests: &[String]) -> TestbenchResult {
+    pub fn run<S: AsRef<str>>(
+        &self,
+        name: S,
+        input: String,
+        tests: &[String],
+    ) -> TestbenchResult {
         if let Some(tb) = self.tbs.get(name.as_ref()) {
             let work_dir = TempDir::new(".tb")?;
             let input = copy_into(&input, &work_dir)?;
@@ -54,9 +67,17 @@ impl TestbenchManager {
     }
 }
 
-fn copy_into<S: AsRef<str>>(file: S, work_dir: &TempDir) -> std::io::Result<String> {
+fn copy_into<S: AsRef<str>>(
+    file: S,
+    work_dir: &TempDir,
+) -> std::io::Result<String> {
     let from_path = PathBuf::from(file.as_ref());
-    let basename = from_path.file_name().expect("path ended with ..").to_str().expect("invalid unicode").to_string();
+    let basename = from_path
+        .file_name()
+        .expect("path ended with ..")
+        .to_str()
+        .expect("invalid unicode")
+        .to_string();
     let mut to_path = work_dir.path().to_path_buf();
     to_path.push(&basename);
     fs::copy(from_path, to_path)?;
