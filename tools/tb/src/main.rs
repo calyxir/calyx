d7e28a495628de522726375e65b8c937ee3663bd
Ethan Uppal <113849268+ethanuppal@users.noreply.github.com>
2024-07-02 13:30:57 -0400

    fud2 works

--- tools/tb/src/main.rs
+++ tools/tb/src/main.rs
@@ -8,17 +8,10 @@ use tb::{
 const CONFIG_FILE_NAME: &str = "calyx-tb.toml";
 
 fn main() -> LocalResult<()> {
-    let args: CLI = argh::from_env();
+    let args = CLI::from_env();
 
     if args.version {
-        println!(
-            "{} v{}",
-            std::env::current_exe()
-                .expect("how did you call this without argv[0]??")
-                .to_str()
-                .expect("argv[0] not valid unicode"),
-            env!("CARGO_PKG_VERSION")
-        );
+        println!("{}", env!("CARGO_PKG_VERSION"));
         return Ok(());
     }
 
