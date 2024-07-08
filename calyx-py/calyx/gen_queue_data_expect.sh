81373d529b5ab4633b18eddd3bf81491cd868e7d
Kabir Samsi <samsikabir@gmail.com>
2024-07-08 15:20:36 -0400

    Reverted to older queue implementation

--- calyx-py/calyx/gen_queue_data_expect.sh
+++ calyx-py/calyx/gen_queue_data_expect.sh
@@ -5,8 +5,8 @@ queue_size=16
 
 # For SDN, we use piezo mode when making the data file and
 # use pifotree_oracle to generate the expected output
-python3 queue_data_gen.py $num_cmds --no-err $queue_size > ../test/correctness/queues/sdn.data
-cat ../test/correctness/queues/sdn.data | python3 pifo_tree_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/sdn.expect
+# python3 queue_data_gen.py $num_cmds --no-err $queue_size > ../test/correctness/queues/sdn.data
+# cat ../test/correctness/queues/sdn.data | python3 pifo_tree_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/sdn.expect
 
 # For the others, we drop piezo mode for data gen, and we use the appropriate
 # oracle, which is one of the following:
@@ -14,12 +14,12 @@ cat ../test/correctness/queues/sdn.data | python3 pifo_tree_oracle.py $num_cmds 
 # - pifo_oracle.py
 # - pifo_tree_oracle.py
 
-for queue_kind in fifo pifo pifo_tree; do
-    python3 queue_data_gen.py $num_cmds > ../test/correctness/queues/$queue_kind.data
-    [[ "$queue_kind" != "pifo_tree" ]] && cp ../test/correctness/queues/$queue_kind.data ../test/correctness/queues/binheap/$queue_kind.data
-    cat ../test/correctness/queues/$queue_kind.data | python3 ${queue_kind}_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/$queue_kind.expect
-    [[ "$queue_kind" != "pifo_tree" ]] && cp ../test/correctness/queues/$queue_kind.expect ../test/correctness/queues/binheap/$queue_kind.expect
-done
+# for queue_kind in fifo pifo pifo_tree; do
+#     python3 queue_data_gen.py $num_cmds > ../test/correctness/queues/$queue_kind.data
+#     [[ "$queue_kind" != "pifo_tree" ]] && cp ../test/correctness/queues/$queue_kind.data ../test/correctness/queues/binheap/$queue_kind.data
+#     cat ../test/correctness/queues/$queue_kind.data | python3 ${queue_kind}_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/$queue_kind.expect
+#     [[ "$queue_kind" != "pifo_tree" ]] && cp ../test/correctness/queues/$queue_kind.expect ../test/correctness/queues/binheap/$queue_kind.expect
+# done
 
 # Here, we test the queues for non-work-conserving algorithms,
 # which are the following:
@@ -33,13 +33,13 @@ done
 
 # For the Binary Heap, we drop piezo mode and enable ranks for data gen and
 # use binheap_oracle to generate the expected output
-python3 queue_data_gen.py $num_cmds 0 --use-rank > ../test/correctness/queues/binheap/stable_binheap.data
-cat ../test/correctness/queues/binheap/stable_binheap.data | python3 binheap_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/binheap/stable_binheap.expect
+# python3 queue_data_gen.py $num_cmds 0 --use-rank > ../test/correctness/queues/binheap/stable_binheap.data
+# cat ../test/correctness/queues/binheap/stable_binheap.data | python3 binheap_oracle.py $num_cmds $queue_size --keepgoing > ../test/correctness/queues/binheap/stable_binheap.expect
 
 # For the Round Robin queues, we drop piezo mode as well and use rrqueue_oracle to
 # generate the expected output for queues with 2..7 flows. This generates 6 data expect file pairs.
 
-for n in {2..7}; do
-    python3 queue_data_gen.py $num_cmds > ../test/correctness/queues/rr_queues/rr_queue_${n}flows.data
-    cat ../test/correctness/queues/rr_queues/rr_queue_${n}flows.data | python3 rrqueue_oracle.py $num_cmds $queue_size $n --keepgoing > ../test/correctness/queues/rr_queues/rr_queue_${n}flows.expect
-done
+# for n in {2..7}; do
+#     python3 queue_data_gen.py $num_cmds > ../test/correctness/queues/rr_queues/rr_queue_${n}flows.data
+#     cat ../test/correctness/queues/rr_queues/rr_queue_${n}flows.data | python3 rrqueue_oracle.py $num_cmds $queue_size $n --keepgoing > ../test/correctness/queues/rr_queues/rr_queue_${n}flows.expect
+# done
