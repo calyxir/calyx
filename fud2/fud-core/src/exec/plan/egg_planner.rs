74198d341035895f3eee82356cdce54548b6ad23
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2024-07-24 08:07:30 -0400

    remove outdated comment

--- fud2/fud-core/src/exec/plan/egg_planner.rs
+++ fud2/fud-core/src/exec/plan/egg_planner.rs
@@ -176,7 +176,6 @@ impl FindPlan for EggPlanner {
                 .unwrap();
 
         // If the end expression exists, retrieve it using steps.
-        // TODO: this currently ignores `through`.
         if runner.egraph.lookup_expr(&end_expr).is_some() {
             let mut explanation = runner.explain_existance(&end_expr);
             Some(
