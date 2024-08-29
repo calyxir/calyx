f44c38f114ebd4e8e0153c4700633eb36f852786
Akash Dhiraj <akashdhiraj2019@gmail.com>
2024-08-29 03:46:25 -0400

    Remove large .data and .expect files
    - alter CI to run gen script before runt tests
    - keep tests/binheap/binheap_test.{data, expect} (they're small)

---
+++ frontends/queues/test_data_gen/clean_test_data.sh
@@ -0,0 +1,14 @@
+cd "$(dirname "$0")/../tests"
+
+rm -rf *.data *.expect
+
+cd binheap/
+for test in fifo_test pifo_test stable_binheap_test; do
+    rm -rf $test.data $test.expect
+done
+
+cd ../round_robin
+rm -rf *.data *.expect
+
+cd ../strict
+rm -rf *.data *.expect
