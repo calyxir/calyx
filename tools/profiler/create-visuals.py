fa45dc1c910187b14ef927da4c9311db0d4b40ad
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2024-09-06 16:09:20 -0400

    slowly chipping at finding when events are simulatenous

--- tools/profiler/create-visuals.py
+++ tools/profiler/create-visuals.py
@@ -15,27 +15,73 @@ class FlameInfo:
         else:
             return f'{self.name} {self.cycles}'
 
+# def create_backptrs(name, main_component, cells_map):
+#     after_main = prefix.split(f"{main_component}.")[1]
+
+#     return
+
+def create_timeline_map(profiled_info):
+    summary = list(filter(lambda x : x["name"] == "TOTAL", profiled_info))[0]
+    total_cycles = summary["total_cycles"]
+    timeline_map = {i : set() for i in range(total_cycles)}
+    timeline_map_fsm = {i : set() for i in range(total_cycles)}
+    for group_info in profiled_info:
+        if group_info["name"] == "TOTAL" or group_info["component"] is None: # only care about actual groups
+            continue
+        if group_info["fsm_name"] is not None: # FIXME: get FSM version of events later as well
+            continue
+        for segment in group_info["closed_segments"]:
+            for i in range(segment["start"], segment["end"]): # really janky, I wonder if there's a better way to do this?
+                timeline_map[i].add((group_info["name"], group_info["component"]))
+
+    print(timeline_map)
+    return timeline_map
+
+# attempt to rehash the create_flame_graph to take care of stacks
+def create_flame_graph_2(profiled_info, flame_out):
+    timeline = create_timeline_map(profiled_info)
+    summary = list(filter(lambda x : x["name"] == "TOTAL", profiled_info))[0]
+    main_component = summary["main_full_path"]
+    main_shortname = main_component.split("TOP.toplevel.")[1]
+    total_cycles = summary["total_cycles"]
+    stacks = {} # each stack to the # of cycles it was active for?
+    for i in timeline: # keys in the timeline are clock time stamps
+        for group_full_name, group_component in timeline[i]:
+            stack = ""
+            group_name = group_full_name.split(".")[-1]
+            if group_component == main_shortname: # group within main so it has to be at the bottom
+                stack = main_component + ";" + group_name
+            else:
+                stack = ";".join((main_component, group_component, group_name)) # FIXME: temp
+            
+            if stack not in stacks:
+                stacks[stack] = 0
+            stacks[stack] += 1
+
+    print("STACKS")
+    print(stacks)
+    return stacks
+
 # Creates folded log
-def create_flame_graph(profiled_info, flame_out, fsm_flame_out):
+def create_flame_graph(profiled_info, cells_map, flame_out, fsm_flame_out):
     summary = list(filter(lambda x : x["name"] == "TOTAL", profiled_info))[0]
     main_component = summary["main_full_path"]
     total_cycles = summary["total_cycles"]
     stacks = {}
-    # FIXME: NOT going to deal with multicomponent programs for now
-    # in the future I think we need to create and chase back pointers
     for group_info in profiled_info:
-        if group_info["name"] == "TOTAL": # already processed the summary
+        if group_info["name"] == "TOTAL" or group_info["name"] == main_component: # already processed the summary
             continue
         name_split = group_info["name"].split(".")
         name = name_split[-1] # FIXME: still not correct for multicomponent programs
         prefix = ".".join(name_split[:-1])
         if prefix == main_component:
             backptr = prefix # base case?
-        else:
-            # FIXME: this would be wrong if the cell had a FSM? maybe?
+        else: # multicomponent
             after_main = prefix.split(f"{main_component}.")[1]
-            after_main.replace(".", ";")
-            backptr = main_component + ";" + after_main
+            backptr = main_component
+            # for cell in after_main.split("."):
+                
+            # after_main.replace(".", ";")
         cycles = group_info["total_cycles"]
         if name not in stacks:
             stacks[name] = {}
@@ -89,21 +135,28 @@ def create_timeline_view(profiled_info, out_file):
     with open(out_file, "w") as out:
         json.dump(events, out, indent=4)
 
-def main(profiler_dump_file, timeline_out, flame_out, fsm_flame_out):
+def build_cells_map(json_file):
+    cell_json = json.load(open(json_file))
+    return {x["name"] : x for x in cell_json}        
+
+def main(profiler_dump_file, cells_json, timeline_out, flame_out, fsm_flame_out):
     profiled_info = json.load(open(profiler_dump_file, "r"))
-    create_timeline_view(profiled_info, timeline_out)
-    create_flame_graph(profiled_info, flame_out, fsm_flame_out)
+    create_flame_graph_2(profiled_info, flame_out)
+    # create_timeline_view(profiled_info, timeline_out)
+    # create_flame_graph(profiled_info, cells_json, flame_out, fsm_flame_out)
 
 if __name__ == "__main__":
-    if len(sys.argv) > 4:
+    if len(sys.argv) > 5:
         profiler_dump_json = sys.argv[1]
-        timeline_out = sys.argv[2]
-        flame_out = sys.argv[3]
-        fsm_flame_out = sys.argv[4]
-        main(profiler_dump_json, timeline_out, flame_out, fsm_flame_out)
+        cells_json = sys.argv[2]
+        timeline_out = sys.argv[3]
+        flame_out = sys.argv[4]
+        fsm_flame_out = sys.argv[5]
+        main(profiler_dump_json, cells_json, timeline_out, flame_out, fsm_flame_out)
     else:
         args_desc = [
             "PROFILER_JSON",
+            "CELLS_JSON",
             "TIMELINE_VIEW_JSON",
             "FLAME_GRAPH_FOLDED",
             "FSM_FLAME_GRAPH_FOLDED",
