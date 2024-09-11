999895ac631ff240735b60af3f76d9dff6183f85
Akash Dhiraj <akashdhiraj2019@gmail.com>
2024-09-11 10:48:00 -0400

    Add cycle counts
    - setup script for generating cycles counts: cycles.sh
    - report cycles counts in cycles.txt
    - tweak clean_test_data.sh to remove binheap/round_robin and
      binheap/strict tests

---
+++ frontends/queues/cycles.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+shopt -s globstar
+
+cd "$(dirname "$0")/../.." # move to root
+
+for file in frontends/queues/tests/**/*.py; do
+    name="$(basename $file .py)"
+    dir="$(dirname $file)"
+    
+    echo -n ${file#*tests/}
+    echo -n ": "
+
+    python3 $file 20000 --keepgoing |\
+        fud e --from calyx --to jq \
+            --through verilog \
+            --through dat \
+            -s verilog.data "$dir/$name.data" \
+            -s jq.expr ".cycles" \
+            -q
+done
