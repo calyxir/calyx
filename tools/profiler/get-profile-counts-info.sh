651c47642892066383ecc1ec6635744013e0119d
Ayaka Yorihiro <36107281+ayakayorihiro@users.noreply.github.com>
2024-09-18 12:29:56 -0700

    [Profiling] Visualizations: Cycle flame graph, frequency flame graph, timeline view (#2284)
    The profiling script (`get-profile-counts-info.sh`) now produces
    visualizations!
    
    - Cycle flame graph: `flame.svg` can be viewed using your favorite web
    browser. For any group, it shows the "call stack" and the number of
    cycles it was active for.
    - Frequency flame graph: `frequency-flame.svg` can also be viewed using
    your favorite web browser. For any group, it shows the "call stack" and
    the number of times the group was active.
    - Timeline view: `timeline.json` is a JSON file in the [Google Trace
    File
    Format](https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview).
    You can visualize the timeline using [Perfetto
    UI](https://ui.perfetto.dev/).
    
    The biggest caveat to the profiler right now is that the visualizations
    only work for **non-optimized, sequential (i.e. no `par`) Calyx
    programs**. I will work on expanding the profiler's capabilities in the
    near future (after finding some example use cases where knowing
    performance information was helpful).

--- tools/profiler/get-profile-counts-info.sh
+++ tools/profiler/get-profile-counts-info.sh
@@ -20,11 +20,24 @@ else
     OUT_CSV=${TMP_DIR}/summary.csv
 fi
 
+FLAMEGRAPH_DIR=${SCRIPT_DIR}/fg-tmp
+
+if [ ! -d ${FLAMEGRAPH_DIR} ]; then
+    (
+	cd ${SCRIPT_DIR}
+	git clone git@github.com:brendangregg/FlameGraph.git fg-tmp
+    )
+fi
+
 TMP_VERILOG=${TMP_DIR}/no-opt-verilog.sv
 FSM_JSON=${TMP_DIR}/fsm.json
 CELLS_JSON=${TMP_DIR}/cells.json
 OUT_JSON=${TMP_DIR}/dump.json
-VISUALS_JSON=${TMP_DIR}/visual.json
+TIMELINE_VIEW_JSON=${TMP_DIR}/timeline.json
+FSM_TIMELINE_VIEW_JSON=${TMP_DIR}/fsm-timeline.json
+FLAME_GRAPH_FOLDED=${TMP_DIR}/flame.folded
+FSM_FLAME_GRAPH_FOLDED=${TMP_DIR}/fsm-flame.folded
+FREQUENCY_FLAME_GRAPH_FOLDED=${TMP_DIR}/frequency-flame.folded
 VCD_FILE=${TMP_DIR}/trace.vcd
 LOGS_DIR=${DATA_DIR}/logs
 if [ -d ${DATA_DIR} ]; then
@@ -39,9 +52,14 @@ echo "[${SCRIPT_NAME}] Obtaining FSM info from TDCC"
 (
     cd ${CALYX_DIR}
     set -o xtrace
-    cargo run -- ${INPUT_FILE} -p no-opt -x tdcc:dump-fsm-json="${FSM_JSON}"
+    cargo run -- ${INPUT_FILE} -p compile-repeat -p no-opt -x tdcc:dump-fsm-json="${FSM_JSON}" #  -p par-to-seq
     set +o xtrace
 ) &> ${LOGS_DIR}/gol-tdcc
+
+if [ ! -f ${FSM_JSON} ]; then
+    echo "[${SCRIPT_NAME}] Failed to generate ${FSM_JSON}! Exiting"
+    exit 1
+fi
 
 # Run component-cells backend to get cell information
 echo "[${SCRIPT_NAME}] Obtaining cell information from component-cells backend"
@@ -51,27 +69,59 @@ echo "[${SCRIPT_NAME}] Obtaining cell information from component-cells backend"
     cargo run --manifest-path tools/component_cells/Cargo.toml ${INPUT_FILE} -o ${CELLS_JSON}
 ) &> ${LOGS_DIR}/gol-cells
 
+if [ ! -f ${CELLS_JSON} ]; then
+    echo "[${SCRIPT_NAME}] Failed to generate ${CELLS_JSON}! Exiting"
+    exit 1
+fi
+
 # Run simuation to get VCD
 echo "[${SCRIPT_NAME}] Obtaining VCD file via simulation"
 (
     set -o xtrace
-    fud2 ${INPUT_FILE} -o ${VCD_FILE} --through verilator -s calyx.args='-p no-opt' -s sim.data=${SIM_DATA_JSON}
+    fud2 ${INPUT_FILE} -o ${VCD_FILE} --through verilator -s calyx.args='-p compile-repeat -p no-opt' -s sim.data=${SIM_DATA_JSON} # -p par-to-seq 
     set +o xtrace
 ) &> ${LOGS_DIR}/gol-vcd
+
+if [ ! -f ${VCD_FILE} ]; then
+    echo "[${SCRIPT_NAME}] Failed to generate ${VCD_FILE}! Exiting"
+    exit 1
+fi
 
 # Run script to get cycle level counts
 echo "[${SCRIPT_NAME}] Using FSM info and VCD file to obtain cycle level counts"
 (
+    set -o xtrace
     python3 ${SCRIPT_DIR}/parse-vcd.py ${VCD_FILE} ${FSM_JSON} ${CELLS_JSON} ${OUT_CSV} ${OUT_JSON}
+    set +o xtrace
 ) &> ${LOGS_DIR}/gol-process
 
 if [ "$4" == "-d" ]; then
-    cat ${LOGS_DIR}/gol-process | grep -v Writing
+    cat ${LOGS_DIR}/gol-process | grep -v Writing # exclude lines that show paths
 else
-    tail -2 ${LOGS_DIR}/gol-process
+    tail -3 ${LOGS_DIR}/gol-process | head -2 # last line is the set +o xtrace, which we don't need to show
 fi
 
 echo "[${SCRIPT_NAME}] Writing visualization"
 (
-    python3 ${SCRIPT_DIR}/convert-dump.py ${OUT_JSON} ${VISUALS_JSON}
+    set -o xtrace
+    python3 ${SCRIPT_DIR}/create-visuals.py ${OUT_JSON} ${CELLS_JSON} ${TIMELINE_VIEW_JSON} ${FSM_TIMELINE_VIEW_JSON} ${FLAME_GRAPH_FOLDED} ${FSM_FLAME_GRAPH_FOLDED} ${FREQUENCY_FLAME_GRAPH_FOLDED}
+    set +o xtrace
 ) &> ${LOGS_DIR}/gol-visuals
+
+echo "[${SCRIPT_NAME}] Creating flame graph svg"
+(
+    set -o xtrace
+    for opt in "" "--inverted" "--reverse"; do
+	if [ "${opt}" == "" ]; then
+	    filename=flame
+	else
+	    filename=flame"${opt:1}"
+	fi
+	${FLAMEGRAPH_DIR}/flamegraph.pl ${opt} --countname="cycles" ${FLAME_GRAPH_FOLDED} > ${TMP_DIR}/${filename}.svg
+	echo
+	${FLAMEGRAPH_DIR}/flamegraph.pl ${opt} --countname="cycles" ${FSM_FLAME_GRAPH_FOLDED} > ${TMP_DIR}/fsm-${filename}.svg
+	echo
+	${FLAMEGRAPH_DIR}/flamegraph.pl ${opt} --countname="times active" ${FREQUENCY_FLAME_GRAPH_FOLDED} > ${TMP_DIR}/frequency-${filename}.svg	
+    done
+    set +o xtrace
+) &> ${LOGS_DIR}/gol-flamegraph
