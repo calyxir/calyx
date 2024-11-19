5b7ad8bcf05fbec612c8571046f0627a6181e7f6
Akash Dhiraj <akashdhiraj2019@gmail.com>
2024-11-19 15:26:41 -0500

    Factor out flow_inference for strict_or_rr.py

--- frontends/queues/tests/round_robin/rr_3flow_test.py
+++ frontends/queues/tests/round_robin/rr_3flow_test.py
@@ -1,15 +1,34 @@
 import sys
 import calyx.builder as cb
 import queues.queue_call as qc
-from queues.strict_or_rr import generate
+import queues.sim_pcap as sp
+import queues.strict_or_rr as st_or_rr
+import queues.fifo as fifo
+import queues.flow_inference as fi
+
+NUMFLOWS = 3
 
 
 if __name__ == "__main__":
     """Invoke the top-level function to build the program, with 3 flows."""
     num_cmds = int(sys.argv[1])
     keepgoing = "--keepgoing" in sys.argv
+    sim_pcap = "--sim-pcap" in sys.argv
 
     prog = cb.Builder()
-    pifo = generate(prog, 3, True)
-    qc.insert_main(prog, pifo, num_cmds, keepgoing=keepgoing)
+
+    fifo_queue = fifo.insert_fifo(prog, "fifo")
+    subqueues = [fifo_queue] * NUMFLOWS
+    if sim_pcap:
+        flow_infer = fi.insert_tuple_flow_inference(prog, "flow_inference", NUMFLOWS)
+        pifo = st_or_rr.insert_queue(prog, "pifo", True, subqueues, flow_infer)
+        sp.insert_main(prog, pifo, num_cmds, NUMFLOWS)
+    else:
+        boundaries = [133, 266, 400]
+        flow_infer = fi.insert_boundary_flow_inference(
+            prog, "flow_inference", boundaries
+        )
+        pifo = st_or_rr.insert_queue(prog, "pifo", True, subqueues, flow_infer)
+        qc.insert_main(prog, pifo, num_cmds, keepgoing=keepgoing)
+
     prog.program.emit()
