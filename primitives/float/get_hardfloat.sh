93c67e2a43d452188efb9046c64ca8e0f42c1e91
Jiahan Xie <jx353@cornell.edu>
2025-01-05 09:31:41 -0500

    change runt and get_hardfloat to try to pass the CI

--- primitives/float/get_hardfloat.sh
+++ primitives/float/get_hardfloat.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-HARDFLOAT_DIR="$PWD"
+HARDFLOAT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 HARDFLOAT_URL="http://www.jhauser.us/arithmetic/HardFloat-1.zip"
 
 ZIP_FILE="HardFloat-1.zip"
-
-rm -rf "${HARDFLOAT_DIR}/HardFloat-1"
-
-mkdir -p "${HARDFLOAT_DIR}"
 
 cd "${HARDFLOAT_DIR}"
 
 curl -LO "${HARDFLOAT_URL}"
 
 if [ -f "$ZIP_FILE" ]; then
-    unzip "$ZIP_FILE" && rm "$ZIP_FILE"
+    unzip -o "$ZIP_FILE"
     echo "HardFloat library fetched and extracted to ${HARDFLOAT_DIR}"
 else
     echo "Failed to download HardFloat library from ${HARDFLOAT_URL}"
+    exit 1
 fi
