160f8496df42cfa6d277c756e3a423dd3c3da7c9
Griffin Berlstein <griffin@berlste.in>
2025-01-29 14:28:36 -0500

    remove the unused command buffer

--- cider/src/debugger/io_utils.rs
+++ cider/src/debugger/io_utils.rs
@@ -2,27 +2,20 @@ use super::commands::parse_command;
 use super::commands::Command;
 use crate::errors::{BoxedCiderError, CiderResult};
 use rustyline::{DefaultEditor, Editor};
-use std::collections::VecDeque;
 
 const SHELL_PROMPT: &str = " > ";
 
 pub struct Input {
     buffer: DefaultEditor,
-    command_buffer: VecDeque<Command>,
 }
 
 impl Input {
     pub fn new() -> CiderResult<Self> {
         Ok(Self {
             buffer: Editor::new()?,
-            command_buffer: VecDeque::default(),
         })
     }
     pub fn next_command(&mut self) -> CiderResult<Command> {
-        if !self.command_buffer.is_empty() {
-            return Ok(self.command_buffer.pop_front().unwrap());
-        }
-
         match self.buffer.readline(SHELL_PROMPT) {
             Ok(command_str) => {
                 self.buffer.add_history_entry(command_str.clone())?;
