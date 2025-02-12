79a5219d42074e3df109513da44b63d960a4ac03
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2025-02-12 17:59:19 -0500

    create attrbutes for components when locations are being tracked

--- calyx-py/calyx/py_ast.py
+++ calyx-py/calyx/py_ast.py
@@ -180,8 +180,12 @@ class Component:
         latency_annotation = (
             f"static<{self.latency}> " if self.latency is not None else ""
         )
-        if self.loc is not None and self.attributes:
-            self.attributes.add(CompAttribute("pos", self.loc))
+        if self.loc is not None:
+            loc_attribute = CompAttribute("pos", self.loc)
+            if self.attributes:
+                self.attributes.add(loc_attribute)
+            else:
+                self.attributes = {loc_attribute}
         attribute_annotation = (
             f"<{', '.join([f'{a.doc()}' for a in sorted(self.attributes, key=lambda a: a.name)])}>"
             if self.attributes
