eafb25b8f95045e4aa54f6cd0c17baa4f3216e03
Cassandra Sziklai <cnsziklai@gmail.com>
2025-04-22 21:13:36 -0400

    get initial implementation to compile

--- frontends/queues/tests/compile_queue.py
+++ frontends/queues/tests/compile_queue.py
@@ -1,11 +1,12 @@
 # pylint: disable=import-error
 import sys
-import fifo
 import calyx.builder as cb
-import calyx.queue_call as qc
-import strict_and_rr_queues.gen_strict_or_rr as strict_or_rr
+import queues.queue_call as qc
+import queues.strict_or_rr as strict_or_rr
+import queues.fifo as fifo
+import queues.flow_inference as fi
 import json
-
+import os
 
 # def unroll(data):
 #   """Need to recurse until we hit leaves, and then build up"""
@@ -17,22 +18,18 @@ import json
 #       # need to continue recursing to find leaf
 #       unroll(value)
 
-def create(data, lower, upper):
+def create(data, lower, upper, prog):
   """
   Recursively creates the pifo
   """
   if isinstance(data, dict):
     for key, val in data.items():
       if key == "FIFO":
-        classes = ""
-        for clss in val:
-          classes += clss
-        name = "fifo_" + classes
-        return fifo.insert_fifo(prog, name)
+        return fifo.insert_fifo(prog, "fifo")
       elif key == "RR":
         num_children = len(val)
         interval = (upper - lower) // num_children
-        boundaries = [lower]
+        boundaries = []
         for i in range(1, num_children):
           boundaries.append(lower + (interval * i))
         boundaries.append(upper)
@@ -44,22 +41,20 @@ def create(data, lower, upper):
           u = l + interval
           if child == num_children - 1:
             u = upper
-          children.append(create(child, l, u))
+          children.append(create(child, l, u, prog))
           l = u
 
-        pifo_rr = strict_or_rr.insert_queue(
+        return strict_or_rr.insert_queue(
         prog,
         "pifo_rr",
-        children,
-        boundaries, # boundaries comes from the range it is given divided equally amongst num of flows
-        num_children,
-        [],
         True,
+        children,
+        fi.insert_boundary_flow_inference(prog, "fi_rr", boundaries), # could be problem with using the same name for different flow inferences?
         )
       elif key == "Strict":
-                num_children = len(val)
+        num_children = len(val)
         interval = (upper - lower) // num_children
-        boundaries = [lower]
+        boundaries = []
         for i in range(1, num_children):
           boundaries.append(lower + (interval * i))
         boundaries.append(upper)
@@ -71,36 +66,39 @@ def create(data, lower, upper):
           u = l + interval
           if child == num_children - 1:
             u = upper
-          children.append(create(child, l, u))
+          children.append(create(child, l, u, prog))
           l = u
 
-        pifo_strict = strict_or_rr.insert_queue(
+        return strict_or_rr.insert_queue(
         prog,
         "pifo_strict",
-        children,
-        boundaries,
-        num_children,
-        [n for n in range(num_children)],
         False,
+        children,
+        fi.insert_boundary_flow_inference(prog, "fi_strict", boundaries),
+        order = [n for n in range(num_children)],
         )
   elif isinstance(data, list):
-    return create(child) for child in data
+    for child in data:
+      create(child)
 
 
 
-def build():
+def build(json_file):
     """Top-level function to build the program."""
     num_cmds = int(sys.argv[1])
     keepgoing = "--keepgoing" in sys.argv
     prog = cb.Builder()
 
-    # for now assume sys.argv[2] is the name of the json file
+    #data = json.loads(json_file)
+    base_dir = os.path.dirname(__file__)
+    file_path = os.path.join(base_dir, json_file)
+    with open(file_path) as f:
+      data = json.load(f)
 
-    data = json.loads(sys.argv[2])
-    root = create(data, 0, 400)
+    root = create(data, 0, 400, prog)
 
     qc.insert_main(prog, root, num_cmds, keepgoing=keepgoing)
-    return prog,program
+    return prog.program
 
 
 if __name__ == "__main__":
