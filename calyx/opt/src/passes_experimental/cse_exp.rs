7362893fed0bad8db2cb28fbea75eacc04c6cefb
Ayana Alemayehu <ayana@mit.edu>
2025-05-12 17:29:43 -0400

    stopped tracking depth, will refactor soon. made own visitor to control traversals and tested it with if branches. Seems to work. Refactor coming next

--- calyx/opt/src/passes_experimental/cse_exp.rs
+++ calyx/opt/src/passes_experimental/cse_exp.rs
@@ -1,7 +1,7 @@
-use std::{cell::RefCell, collections::HashMap};
-
 use crate::traversal::{Action, Named, VisResult, Visitor};
 use calyx_ir::{self as ir};
+use ir::Control::{self as Control};
+use std::{collections::HashMap, rc::Rc, string};
 
 const SUPPORTED_STD: &[&str] = &["std_add"];
 
@@ -43,6 +43,15 @@ struct ExpressionMetadata {
     group: String,               // group name that created the expression
 }
 
+impl Clone for ExpressionMetadata {
+    fn clone(&self) -> Self {
+        ExpressionMetadata {
+            reg_port: self.reg_port.clone(),
+            group: self.group.clone(),
+        }
+    }
+}
+
 struct AvailableExpressions {
     current_depth: i32,
     safe_depth: i32,
@@ -68,6 +77,94 @@ impl AvailableExpressions {
         }
     }
 
+    pub fn intersection(
+        one: &AvailableExpressions,
+        two: &AvailableExpressions,
+    ) -> (
+        HashMap<String, Vec<ExpressionMetadata>>,
+        HashMap<String, HashMap<String, Vec<ExpressionMetadata>>>,
+    ) {
+        // <- i was working on an intersection function to take two available expressions structs/objects and create a new hashmap representing their intersections (to then finish the correct if implementation)
+        // we follow one's order of stuff for the intersection, since it has to exist in both branches its an alright order to follow
+        let mut running_intersection =
+            HashMap::<String, Vec<ExpressionMetadata>>::new();
+        let mut group_intersection =
+            HashMap::<String, HashMap<String, Vec<ExpressionMetadata>>>::new();
+        for (_, expressions) in one.running_expressions.iter() {
+            for (string_expression, metadata_vec) in expressions {
+                let mut new_metadata_vec = Vec::<ExpressionMetadata>::new();
+                for metadata in metadata_vec {
+                    if two.contains_metadata(&string_expression, &metadata) {
+                        new_metadata_vec.push(metadata.clone());
+                    }
+                }
+                if new_metadata_vec.len() > 0 {
+                    running_intersection
+                        .insert(string_expression.clone(), new_metadata_vec);
+                }
+            }
+        }
+        for (group, expressions) in one.per_group_expressions.iter() {
+            // do intersection if both available expressions have the group
+            if two.per_group_expressions.contains_key(group) {
+                for (string_expression, metadata_vec) in expressions {
+                    for metadata in metadata_vec {
+                        if two.group_contains_metadata(
+                            &group,
+                            &string_expression,
+                            &metadata,
+                        ) {
+                            if !group_intersection.contains_key(group) {
+                                let group_expressions = HashMap::<
+                                    String,
+                                    Vec<ExpressionMetadata>,
+                                >::new(
+                                );
+                                group_intersection
+                                    .insert(group.clone(), group_expressions);
+                            }
+                            let group_expressions =
+                                group_intersection.get_mut(group).expect(
+                                    &format!("expected expressions {group}"),
+                                );
+                            if group_expressions.contains_key(string_expression)
+                            {
+                                let mut group_metadata_vec = group_expressions.get(string_expression).expect(&format!("expected metadata vec {string_expression}")).clone();
+                                group_metadata_vec.push(metadata.clone());
+                                group_expressions
+                                    .insert(group.clone(), group_metadata_vec);
+                            } else {
+                                let mut group_metadata_vec =
+                                    Vec::<ExpressionMetadata>::new();
+                                group_metadata_vec.push(metadata.clone());
+                                group_expressions
+                                    .insert(group.clone(), group_metadata_vec);
+                            }
+                        }
+                    }
+                }
+            } else {
+                // else do union since the group was unique to the one branch
+                group_intersection.insert(group.clone(), expressions.clone());
+            }
+        }
+        // catch groups that two saw that one didn't see
+        for (group, expressions) in two.per_group_expressions.iter() {
+            if !one.per_group_expressions.contains_key(group) {
+                group_intersection.insert(group.clone(), expressions.clone());
+            }
+        }
+        return (running_intersection, group_intersection);
+    }
+    fn clone(&self) -> AvailableExpressions {
+        AvailableExpressions {
+            current_depth: self.current_depth,
+            safe_depth: self.safe_depth,
+            running_expressions: self.running_expressions.clone(),
+            per_group_expressions: self.per_group_expressions.clone(),
+        }
+    }
+    // for checking running_expressions
     fn contains_metadata(
         &self,
         string_expression: &String,
@@ -91,6 +188,36 @@ impl AvailableExpressions {
                 None => {
                     panic!("no HashMap allocated for depth {depth}?");
                 }
+            }
+        }
+        return contains_flag;
+    }
+
+    // for checking group_expressions
+    fn group_contains_metadata(
+        &self,
+        group: &String,
+        string_expression: &String,
+        metadata: &ExpressionMetadata,
+    ) -> bool {
+        let mut contains_flag = false;
+        match self.per_group_expressions.get(group) {
+            Some(expressions) => match expressions.get(string_expression) {
+                Some(metadata_vec) => {
+                    for met in metadata_vec {
+                        if met.group == metadata.group
+                            && met.reg_port == metadata.reg_port
+                        {
+                            contains_flag = true;
+                        }
+                    }
+                }
+                None => {}
+            },
+            None => {
+                log::debug!(
+                    "function called without containing hashmap for group {group}"
+                );
             }
         }
         return contains_flag;
@@ -682,6 +809,177 @@ impl AvailableExpressions {
     }
 }
 
+/*
+    modified for available expression detection purposes.
+*/
+trait ExpressionVisitor {
+    /// Executed before visiting the children of a [ir::Seq] node.
+    fn start_seq(&mut self, _s: &mut ir::Seq) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed after visiting the children of a [ir::Seq] node.
+    fn finish_seq(&mut self, _s: &mut ir::Seq) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed before visiting the children of a [ir::Par] node.
+    fn start_par(&mut self, _s: &mut ir::Par) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed after visiting the children of a [ir::Par] node.
+    fn finish_par(&mut self, _s: &mut ir::Par) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed before visiting the children of a [ir::If] node.
+    fn start_if(&mut self, _s: &mut ir::If) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed after visiting the children of a [ir::If] node.
+    fn finish_if(&mut self, _s: &mut ir::If) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed before visiting the children of a [ir::While] node.
+    fn start_while(&mut self, _s: &mut ir::While) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed after visiting the children of a [ir::While] node.
+    fn finish_while(&mut self, _s: &mut ir::While) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed before visiting the children of a [ir::Repeat] node.
+    fn start_repeat(&mut self, _s: &mut ir::Repeat) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed after visiting the children of a [ir::Repeat] node.
+    fn finish_repeat(&mut self, _s: &mut ir::Repeat) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed at an [ir::Enable] node.
+    fn enable(&mut self, _s: &mut ir::Enable) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed at an [ir::Empty] node.
+    fn empty(&mut self, _s: &mut ir::Empty) -> VisResult {
+        Ok(Action::Continue)
+    }
+
+    /// Executed at an [ir::Invoke] node.
+    fn invoke(&mut self, _s: &mut ir::Invoke) -> VisResult {
+        Ok(Action::Continue)
+    }
+}
+
+// grabbing all of the private
+impl Action {
+    /// Run the traversal specified by `next` if this traversal succeeds.
+    /// If the result of this traversal is not `Action::Continue`, do not
+    /// run `next()`.
+    fn and_then_local<F>(self, mut next: F) -> VisResult
+    where
+        F: FnMut() -> VisResult,
+    {
+        match self {
+            Action::Continue => next(),
+            Action::Change(_)
+            | Action::Stop
+            | Action::SkipChildren
+            | Action::StaticChange(_) => Ok(self),
+        }
+    }
+    /// Applies the Change action if `self` is a Change action.
+    /// Otherwise passes the action through unchanged
+    fn apply_change_local(self, con: &mut Control) -> Action {
+        match self {
+            Action::Change(c) => {
+                *con = *c;
+                Action::Continue
+            }
+            action => action,
+        }
+    }
+    /// Changes a Action::SkipChildren to Action::Continue.
+    /// Should be called to indicate the boundary of traversing the children
+    /// of a node.
+    fn pop_local(self) -> Self {
+        match self {
+            Action::SkipChildren => Action::Continue,
+            x => x,
+        }
+    }
+}
+
+trait ExpressionVisitable {
+    /// Perform the traversal.
+    fn visit(&mut self, visitor: &mut dyn ExpressionVisitor) -> VisResult;
+}
+
+impl ExpressionVisitable for Control {
+    fn visit(&mut self, visitor: &mut dyn ExpressionVisitor) -> VisResult {
+        let res = match self {
+            Control::Seq(ctrl) => visitor
+                .start_seq(ctrl)?
+                .and_then_local(|| ctrl.stmts.visit(visitor))?
+                .pop_local()
+                .and_then_local(|| visitor.finish_seq(ctrl))?,
+            Control::Par(ctrl) => visitor
+                .start_par(ctrl)?
+                .and_then_local(|| ctrl.stmts.visit(visitor))?
+                .pop_local()
+                .and_then_local(|| visitor.finish_par(ctrl))?,
+            Control::If(ctrl) => visitor
+                .start_if(ctrl)?
+                .and_then_local(|| ctrl.tbranch.visit(visitor))?
+                .and_then_local(|| ctrl.fbranch.visit(visitor))?
+                .pop_local()
+                .and_then_local(|| visitor.finish_if(ctrl))?,
+            Control::While(ctrl) => visitor
+                .start_while(ctrl)?
+                .and_then_local(|| ctrl.body.visit(visitor))?
+                .pop_local()
+                .and_then_local(|| visitor.finish_while(ctrl))?,
+            Control::Repeat(ctrl) => visitor
+                .start_repeat(ctrl)?
+                .and_then_local(|| ctrl.body.visit(visitor))?
+                .pop_local()
+                .and_then_local(|| visitor.finish_repeat(ctrl))?,
+            Control::Enable(ctrl) => visitor.enable(ctrl)?,
+            Control::Static(_) => panic!("not supported yet"),
+            Control::Empty(ctrl) => visitor.empty(ctrl)?,
+            Control::Invoke(data) => visitor.invoke(data)?,
+        };
+        Ok(res.apply_change_local(self))
+    }
+}
+
+/// Blanket implementation for Vectors of Visitables
+impl<V: ExpressionVisitable> ExpressionVisitable for Vec<V> {
+    fn visit(&mut self, visitor: &mut dyn ExpressionVisitor) -> VisResult {
+        for t in self {
+            let res = t.visit(visitor)?;
+            match res {
+                Action::Continue
+                | Action::SkipChildren
+                | Action::Change(_)
+                | Action::StaticChange(_) => {
+                    continue;
+                }
+                Action::Stop => return Ok(Action::Stop),
+            };
+        }
+        Ok(Action::Continue)
+    }
+}
+
 impl Visitor for CseExp {
     /*
         Start is treated like a seq block, so simple safe increment
@@ -693,59 +991,19 @@ impl Visitor for CseExp {
         _sigs: &ir::LibrarySignatures,
         _comps: &[ir::Component],
     ) -> VisResult {
-        log::debug!("toplevel start");
+        log::debug!("[START] Start AvailableExpression Analysis");
         // create depth 0 dictionary, this is basically a seq block
         self.available_expressions.inc_depth(true);
-        Ok(Action::Continue)
-    }
-    fn start_seq(
-        &mut self,
-        _s: &mut calyx_ir::Seq,
-        _comp: &mut calyx_ir::Component,
-        _sigs: &calyx_ir::LibrarySignatures,
-        _comps: &[calyx_ir::Component],
-    ) -> VisResult {
-        log::debug!("start_seq");
-        self.available_expressions.inc_depth(true);
-        Ok(Action::Continue)
-    }
-    fn finish_seq(
-        &mut self,
-        _s: &mut calyx_ir::Seq,
-        _comp: &mut calyx_ir::Component,
-        _sigs: &calyx_ir::LibrarySignatures,
-        _comps: &[calyx_ir::Component],
-    ) -> VisResult {
-        log::debug!("finish_seq");
-        self.available_expressions.dec_depth();
-        Ok(Action::Continue)
-    }
-    /*
-        Do:
-            1) add expressions this group creates
-            2) remove expressions this group killed
-            3) update the expressions availalbe to this group specifically
-               which is either...
-                3.0) adding the current running expressions entirely if
-                     there arent expressions logged for the group already
-                3.1) adding the intersection of current running expressions
-                     /w this groups logged expressions
-    */
-    fn enable(
-        &mut self,
-        _s: &mut calyx_ir::Enable,
-        _comp: &mut calyx_ir::Component,
-        _sigs: &calyx_ir::LibrarySignatures,
-        _comps: &[calyx_ir::Component],
-    ) -> VisResult {
-        let group = &_s.group;
-        let group_name = group.borrow().name().to_string();
-        log::debug!("group {group_name} enable");
-        self.available_expressions
-            .add_exp(&group.borrow().assignments, group_name.clone());
-        self.available_expressions
-            .kill_exp(&group.borrow().assignments, group_name.clone());
-        self.available_expressions.group_exp(group_name.clone());
+        // Create a clone of the reference to the Control
+        // program.
+        let control_ref = Rc::clone(&_comp.control);
+        if let Control::Empty(_) = &*control_ref.borrow() {
+            // Don't traverse if the control program is empty.
+            return Ok(Action::Continue);
+        }
+        // Mutably borrow the control program and traverse.
+        control_ref.borrow_mut().visit(self)?;
+        // dont need the real visitor to be called here
         Ok(Action::Continue)
     }
 
@@ -758,13 +1016,66 @@ impl Visitor for CseExp {
         _sigs: &calyx_ir::LibrarySignatures,
         _comps: &[calyx_ir::Component],
     ) -> VisResult {
-        log::debug!("optimize");
+        log::debug!("[FINISH] Optimize");
         for group in _comp.get_groups_mut().iter_mut() {
             let group_name = group.borrow().name().to_string();
-            log::debug!("group {group_name}");
+            log::debug!("Group: {group_name}");
             self.available_expressions
                 .optimize(&mut group.borrow_mut(), group_name);
         }
+        Ok(Action::Continue)
+    }
+}
+
+impl ExpressionVisitor for CseExp {
+    fn start_if(&mut self, _s: &mut calyx_ir::If) -> VisResult {
+        log::debug!("start_if");
+        // need to run both branches separately and combine common outputs.
+        let mut true_cse_exp = CseExp {
+            available_expressions: self.available_expressions.clone(),
+        };
+        let mut false_cse_exp = CseExp {
+            available_expressions: self.available_expressions.clone(),
+        };
+        log::debug!("running true branch");
+        let _ = _s.tbranch.visit(&mut true_cse_exp);
+        log::debug!("running false branch");
+        let _ = _s.fbranch.visit(&mut false_cse_exp);
+        log::debug!("intersecting branches");
+        let (intersection_running, intersection_group) =
+            AvailableExpressions::intersection(
+                &true_cse_exp.available_expressions,
+                &false_cse_exp.available_expressions,
+            );
+        // finally overwrite the current available expressions
+        log::debug!("overwriting local expressions with branch merge");
+        self.available_expressions.running_expressions.insert(
+            self.available_expressions.current_depth.clone(),
+            intersection_running,
+        );
+        self.available_expressions.per_group_expressions = intersection_group;
+        Ok(Action::SkipChildren)
+    }
+    /*
+        Do:
+            1) add expressions this group creates
+            2) remove expressions this group killed
+            3) update the expressions availalbe to this group specifically
+               which is either...
+                3.0) adding the current running expressions entirely if
+                     there arent expressions logged for the group already
+                3.1) adding the intersection of current running expressions
+                     /w this groups logged expressions
+    */
+    fn enable(&mut self, _s: &mut calyx_ir::Enable) -> VisResult {
+        let group = &_s.group;
+        let group_name = group.borrow().name().to_string();
+        log::debug!("group {group_name} enable");
+        self.available_expressions
+            .add_exp(&group.borrow().assignments, group_name.clone());
+        self.available_expressions
+            .kill_exp(&group.borrow().assignments, group_name.clone());
+        self.available_expressions.group_exp(group_name.clone());
         Ok(Action::Continue)
     }
 }
