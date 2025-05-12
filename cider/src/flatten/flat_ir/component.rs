calyxir/calyx

commit 7311484fd801b4583bc1c946a5c8fab75628346b

[Cider] First pass at structural group enables + some performance tuning (#2456)
Introduces rudimentary support for structural group enables. There are
still issues with the current approach but this does work for some
programs at least.

This PR also includes some tweaking to improve overall execution
performance

cider/src/flatten/structures/environment/env.rs
@@ -46,17 +46,20 @@ use crate::{
     errors::{RuntimeError, RuntimeResult},
     flatten::structures::environment::wave::WaveWriter,
 };
-use ahash::HashSet;
-use ahash::HashSetExt;
-use ahash::{HashMap, HashMapExt};
+use ahash::{HashMap, HashMapExt, HashSet, HashSetExt};
 use baa::{BitVecOps, BitVecValue};
 use calyx_frontend::source_info::PositionId;
-use cider_idx::{IndexRef, iter::IndexRange, maps::IndexedMap};
+use cider_idx::{
+    IndexRef,
+    iter::IndexRange,
+    maps::{IndexedMap, SecondaryMap},
+};
+use fxhash::{FxHashMap, FxHashSet};
 use itertools::Itertools;
 use owo_colors::OwoColorize;
 
 use slog::{Logger, info, warn};
-use std::fmt::Write;
+use std::{collections::VecDeque, fmt::Write};
 use std::{convert::Into, fmt::Debug};
 
 #[derive(Debug, Clone)]
@@ -115,6 +118,15 @@ impl PortMap {
         }
     }
 
+    #[inline]
+    pub(crate) fn insert_val_unchecked(
+        &mut self,
+        idx: GlobalPortIdx,
+        val: AssignedValue,
+    ) {
+        self[idx] = PortValue::new(val)
+    }
+
     /// Sets the given index to undefined without checking whether or not it was
     /// already defined
     #[inline]
@@ -122,33 +134,32 @@ impl PortMap {
         self[target] = PortValue::new_undef();
     }
 
+    #[inline(always)]
     pub fn insert_val(
         &mut self,
         target: GlobalPortIdx,
         val: AssignedValue,
     ) -> Result<UpdateStatus, Box<ConflictingAssignments>> {
         match self[target].as_option() {
-            // unchanged
-            Some(t) if t.eq_no_transitive_clocks(&val) => {
-                Ok(UpdateStatus::Unchanged)
-            }
-            // conflict
-            // TODO: Fix to make the error more helpful
-            Some(t)
-                if t.has_conflict_with(&val)
-                // Assignment & cell values are allowed to override implicit but not the
-                // other way around
-                    && !(*t.winner() == AssignmentWinner::Implicit) =>
-            {
-                Err(ConflictingAssignments {
-                    target,
-                    a1: t.clone(),
-                    a2: val,
+            Some(t) => {
+                if *t.winner() != AssignmentWinner::Implicit
+                    && t.has_conflict_with(&val)
+                {
+                    Err(ConflictingAssignments {
+                        target,
+                        a1: t.clone(),
+                        a2: val,
+                    }
+                    .into())
+                } else if t.eq_no_transitive_clocks(&val) {
+                    Ok(UpdateStatus::Unchanged)
+                } else {
+                    self[target] = PortValue::new(val);
+                    Ok(UpdateStatus::Changed)
                 }
-                .into())
             }
             // changed
-            Some(_) | None => {
+            None => {
                 self[target] = PortValue::new(val);
                 Ok(UpdateStatus::Changed)
             }
@@ -158,6 +169,7 @@ impl PortMap {
     /// Identical to `insert_val` but returns a `RuntimeError` instead of a
     /// `ConflictingAssignments` error. This should be used inside of primitives
     /// while the latter is used in the general simulation flow.
+    #[inline]
     pub fn insert_val_general(
         &mut self,
         target: GlobalPortIdx,
@@ -381,7 +393,7 @@ pub struct Environment<C: AsRef<Context> + Clone> {
 
     clocks: ClockMap,
     thread_map: ThreadMap,
-    control_ports: HashMap<GlobalPortIdx, u32>,
+    control_ports: FxHashMap<GlobalPortIdx, u32>,
 
     /// The immutable context. This is retained for ease of use.
     /// This value should have a cheap clone implementation, such as &Context
@@ -390,6 +402,9 @@ pub struct Environment<C: AsRef<Context> + Clone> {
 
     memory_header: Option<Vec<MemoryDeclaration>>,
     logger: Logger,
+
+    group_instances: Vec<(GlobalCellIdx, GroupIdx)>,
+    ports_to_cells_map: SecondaryMap<GlobalPortIdx, GlobalCellIdx>,
 }
 
 impl<C: AsRef<Context> + Clone> Environment<C> {
@@ -509,8 +524,10 @@ impl<C: AsRef<Context> + Clone> Environment<C> {
             ctx,
             memory_header: None,
             pinned_ports: PinnedPorts::new(),
-            control_ports: HashMap::new(),
+            control_ports: FxHashMap::new(),
             logger: logging::initialize_logger(logging_conf),
+            group_instances: vec![],
+            ports_to_cells_map: SecondaryMap::new_with_default(0.into()),
         };
 
         let root_node = CellLedger::new_comp(root, &env);
@@ -552,6 +569,29 @@ impl<C: AsRef<Context> + Clone> Environment<C> {
 
         if let Some(header) = data_map {
             env.memory_header = Some(header.header.memories);
+        }
+
+        env.ports_to_cells_map =
+            SecondaryMap::capacity_with_default(0.into(), env.ports.len());
+
+        for (cell, ledger) in env.cells.iter() {
+            if let Some(comp_ledger) = ledger.as_comp() {
+                let ctx = env.ctx.as_ref();
+                for group in
+                    ctx.secondary[comp_ledger.comp_id].definitions.groups()
+                {
+                    env.group_instances.push((cell, group))
+                }
+
+                for port in comp_ledger.signature_ports(ctx) {
+                    env.ports_to_cells_map.insert(port, cell);
+                }
+            } else {
+                let cell_dyn = ledger.as_primitive().unwrap();
+                for port in cell_dyn.get_ports().iter_all() {
+                    env.ports_to_cells_map.insert(port, cell);
+                }
+            }
         }
 
         env
@@ -1905,12 +1945,24 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
         &self,
         control_points: &[ControlTuple],
     ) -> Vec<ScheduledAssignments> {
-        control_points
+        let mut skiplist = HashSet::new();
+        let mut additional_groups = VecDeque::new();
+
+        let mut out: Vec<ScheduledAssignments> = control_points
             .iter()
             .filter_map(|(thread, node)| {
                 match &self.ctx().primary[node.control_node_idx].control {
                     Control::Enable(e) => {
                         let group = &self.ctx().primary[e.group()];
+
+                        skiplist.insert((node.comp, e.group()));
+                        additional_groups.extend(
+                            group
+                                .structural_enables
+                                .iter()
+                                .copied()
+                                .map(|grp| (node.comp, grp)),
+                        );
 
                         Some(ScheduledAssignments::new_control(
                             node.comp,
@@ -1952,7 +2004,33 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                     )
                 },
             ))
-            .collect()
+            .collect();
+
+        while let Some((cell, grp)) = additional_groups.pop_front() {
+            if skiplist.insert((cell, grp)) {
+                let group = &self.ctx().primary[grp];
+
+                additional_groups.extend(
+                    group
+                        .structural_enables
+                        .iter()
+                        .copied()
+                        .map(|grp| (cell, grp)),
+                );
+
+                out.push(ScheduledAssignments::new_control(
+                    cell,
+                    group.assignments,
+                    Some(GroupInterfacePorts {
+                        go: group.go,
+                        done: group.done,
+                    }),
+                    None,
+                ))
+            }
+        }
+
+        out
     }
 
     /// A helper function which inserts indices for the ref cells and ports
@@ -2233,27 +2311,32 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
             self.env.clocks = clock_map;
         }
 
-        let out: Result<(), BoxedRuntimeError> = self
-            .env
-            .cells
-            .values_mut()
-            .filter_map(|cell| match cell {
+        let mut prim_step_res = Ok(());
+        for cell in self.env.cells.values_mut() {
+            match cell {
                 CellLedger::Primitive { cell_dyn } => {
-                    Some(cell_dyn.exec_cycle(&mut self.env.ports))
+                    let res = cell_dyn.exec_cycle(&mut self.env.ports);
+                    if res.is_err() {
+                        prim_step_res = res;
+                        break;
+                    }
                 }
 
                 CellLedger::RaceDetectionPrimitive { cell_dyn } => {
-                    Some(cell_dyn.exec_cycle_checked(
+                    let res = cell_dyn.exec_cycle_checked(
                         &mut self.env.ports,
                         &mut self.env.clocks,
                         &self.env.thread_map,
-                    ))
+                    );
+                    if res.is_err() {
+                        prim_step_res = res;
+                        break;
+                    }
                 }
-                CellLedger::Component(_) => None,
-            })
-            .collect();
-
-        out.map_err(|e| e.prettify_message(&self.env))?;
+                CellLedger::Component(_) => {}
+            }
+        }
+        prim_step_res.map_err(|e| e.prettify_message(&self.env))?;
 
         self.env.pc.clear_finished_comps();
 
@@ -2763,6 +2846,10 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
             info!(self.env.logger, "Started combinational convergence");
         }
 
+        let mut rerun_all_primitives = true;
+
+        let mut changed_cells: FxHashSet<GlobalCellIdx> = FxHashSet::new();
+
         while has_changed {
             has_changed = false;
 
@@ -2802,15 +2889,6 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                                 .get_global_port_idx(&assign.src, *active_cell);
                             let val = &self.env.ports[port];
 
-                            if self.conf.debug_logging {
-                                self.log_assignment(
-                                    active_cell,
-                                    ledger,
-                                    assign_idx,
-                                    val,
-                                );
-                            }
-
                             let dest = self
                                 .get_global_port_idx(&assign.dst, *active_cell);
 
@@ -2826,6 +2904,15 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                                 }
                             }
 
+                            if self.conf.debug_logging {
+                                self.log_assignment(
+                                    active_cell,
+                                    ledger,
+                                    assign_idx,
+                                    val,
+                                );
+                            }
+
                             if let Some(v) = val.as_option() {
                                 let mut assigned_value = AssignedValue::new(
                                     v.val().clone(),
@@ -2837,8 +2924,9 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                                 // are present. Since clocks aren't present
                                 // when not running with `check_data_race`, this
                                 // won't happen when the flag is not set
-                                if (val.clocks().is_some()
-                                    || val.transitive_clocks().is_some())
+                                if self.conf.check_data_race
+                                    && (val.clocks().is_some()
+                                        || val.transitive_clocks().is_some())
                                     && (assign_type.is_combinational()
                                         || assign_type.is_continuous())
                                 {
@@ -2885,6 +2973,12 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                                     }
                                 };
 
+                                if changed.as_bool() {
+                                    changed_cells.insert(
+                                        self.env.ports_to_cells_map[dest],
+                                    );
+                                }
+
                                 has_changed |= changed.as_bool();
                             }
                             // attempts to undefine a control port that is zero
@@ -2917,7 +3011,14 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                 self.propagate_comb_reads(assigns_bundle)?;
             }
 
-            let changed = self.run_primitive_comb_path()?;
+            let changed = if rerun_all_primitives {
+                rerun_all_primitives = false;
+                self.run_primitive_comb_path(
+                    self.env.cells.range().into_iter(),
+                )?
+            } else {
+                self.run_primitive_comb_path(changed_cells.drain())?
+            };
 
             has_changed |= changed;
 
@@ -2932,6 +3033,7 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                         self.env.ports[*port] =
                             PortValue::new_implicit(BitVecValue::zero(*width));
                         has_changed = true;
+                        rerun_all_primitives = true;
 
                         if self.conf.debug_logging {
                             info!(
@@ -3237,18 +3339,28 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
         }
     }
 
-    fn run_primitive_comb_path(&mut self) -> Result<bool, BoxedRuntimeError> {
-        let changed: bool = self
-            .env
-            .cells
-            .values_mut()
-            .filter_map(|x| match x {
-                CellLedger::Primitive { cell_dyn } => {
-                    let result = cell_dyn.exec_comb(&mut self.env.ports);
+    fn run_primitive_comb_path<I>(
+        &mut self,
+        cells_to_run: I,
+    ) -> Result<bool, BoxedRuntimeError>
+    where
+        I: Iterator<Item = GlobalCellIdx>,
+    {
+        if self.conf.debug_logging {
+            info!(self.env().logger, "Starting primitive combinational update");
+        }
 
-                    if result.is_ok()
-                        && self.conf.check_data_race
-                        && cell_dyn.is_combinational()
+        let mut changed = UpdateStatus::Unchanged;
+
+        for cell in cells_to_run {
+            let cell = &mut self.env.cells[cell];
+            match cell {
+                CellLedger::Primitive { cell_dyn } => {
+                    let result = cell_dyn.exec_comb(&mut self.env.ports)?;
+
+                    changed |= result;
+
+                    if self.conf.check_data_race && cell_dyn.is_combinational()
                     {
                         let mut working_set = vec![];
                         let signature = cell_dyn.get_ports();
@@ -3279,23 +3391,25 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
                             todo!("comb primitive with multiple outputs")
                         }
                     }
-                    Some(result)
                 }
                 CellLedger::RaceDetectionPrimitive { cell_dyn } => {
-                    Some(cell_dyn.exec_comb_checked(
+                    let result = cell_dyn.exec_comb_checked(
                         &mut self.env.ports,
                         &mut self.env.clocks,
                         &self.env.thread_map,
-                    ))
+                    )?;
+                    changed |= result;
                 }
 
-                CellLedger::Component(_) => None,
-            })
-            .fold_ok(UpdateStatus::Unchanged, |has_changed, update| {
-                has_changed | update
-            })?
-            .as_bool();
-        Ok(changed)
+                CellLedger::Component(_) => {}
+            }
+        }
+
+        if self.conf.debug_logging {
+            info!(self.env().logger, "Finished primitive combinational update");
+        }
+
+        Ok(changed.as_bool())
     }
 
     /// A wrapper function for [check_read] which takes in a [PortRef] and the

cider/Cargo.toml
@@ -40,6 +40,7 @@ num-traits = "0.2.19"
 ciborium = "0.2.2"
 fst-writer = "0.2.3"
 bon = "2.3"
+fxhash = "0.2.1"
 
 [dev-dependencies]
 proptest = "1.0.0"

cider/idx/src/indexed_map.rs
@@ -222,7 +222,7 @@ where
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct SecondaryMap<K, D>
 where

cider/src/flatten/flat_ir/base.rs
@@ -437,12 +437,13 @@ pub struct AssignedValue {
 }
 
 impl AssignedValue {
+    #[inline]
     pub fn eq_no_transitive_clocks(&self, other: &Self) -> bool {
-        self.val == other.val
+        self.propagate_clocks == other.propagate_clocks
             && self.winner == other.winner
             && self.thread == other.thread
             && self.clocks == other.clocks
-            && self.propagate_clocks == other.propagate_clocks
+            && self.val == other.val
     }
 }
 

cider/src/flatten/flat_ir/control/translator.rs
@@ -9,7 +9,7 @@ use crate::{
     as_raw::AsRaw,
     flatten::{
         flat_ir::{
-            base::SignatureRange,
+            base::{LocalPortOffset, SignatureRange},
             cell_prototype::{CellPrototype, ConstantType},
             component::{
                 AuxiliaryComponentInfo, CombComponentCore, ComponentCore,
@@ -279,15 +279,36 @@ fn translate_component(
 
     let group_base = ctx.primary.groups.peek_next_idx();
 
+    let mut go_port_group_map: HashMap<LocalPortOffset, GroupIdx> =
+        HashMap::new();
+
     for group in comp.groups.iter() {
         let group_brw = group.borrow();
-        let group_idx =
+        let translated_group =
             translate_group(&group_brw, ctx, &layout.port_map, hash_cons_map);
-        let k = ctx.primary.groups.push(group_idx);
+        let group_go = translated_group.go;
+        let k = ctx.primary.groups.push(translated_group);
+        go_port_group_map.insert(group_go, k);
         group_map.insert(group.as_raw(), k);
     }
     auxiliary_component_info
         .set_group_range(group_base, ctx.primary.groups.peek_next_idx());
+
+    for group in auxiliary_component_info.definitions.groups() {
+        for assignment in ctx.primary[group].assignments {
+            let dst = ctx.primary[assignment].dst;
+            if let Some(local) = dst.as_local() {
+                if let Some(other_group) = go_port_group_map.get(local) {
+                    ctx.primary
+                        .groups
+                        .get_mut(group)
+                        .unwrap()
+                        .structural_enables
+                        .push(*other_group);
+                }
+            }
+        }
+    }
 
     let comb_group_base = ctx.primary.comb_groups.peek_next_idx();
     // Translate comb groups

cider/src/flatten/flat_ir/wires/core.rs
@@ -1,4 +1,5 @@
 use cider_idx::{iter::IndexRange, maps::IndexedMap};
+use smallvec::SmallVec;
 
 use crate::flatten::flat_ir::prelude::*;
 
@@ -32,6 +33,8 @@ pub struct Group {
     pub go: LocalPortOffset,
     /// the done signal for this group
     pub done: LocalPortOffset,
+    /// the list of groups which this group could potentially activate
+    pub structural_enables: SmallVec<[GroupIdx; 4]>,
 }
 
 impl Group {
@@ -47,6 +50,7 @@ impl Group {
             assignments,
             go,
             done,
+            structural_enables: SmallVec::new(),
         }
     }
 

cider/src/flatten/primitives/stateful/memories.rs
@@ -112,21 +112,26 @@ impl Primitive for StdReg {
             done: Self::DONE,
             out_idx: Self::OUT];
 
-        let out_signal = port_map.insert_val_general(
-            out_idx,
-            AssignedValue::cell_value(self.internal_state.value.clone())
-                .with_clocks(self.internal_state.clocks),
-        )?;
-        let done_signal = port_map.insert_val_general(
-            done,
-            AssignedValue::cell_value(if self.done_is_high {
-                BitVecValue::new_true()
-            } else {
-                BitVecValue::new_false()
-            }),
-        )?;
+        let out_signal =
+            port_map[done].is_undef() || port_map[out_idx].is_undef();
 
-        Ok(out_signal | done_signal)
+        if out_signal {
+            port_map.insert_val_unchecked(
+                out_idx,
+                AssignedValue::cell_value(self.internal_state.value.clone())
+                    .with_clocks(self.internal_state.clocks),
+            );
+            port_map.insert_val_unchecked(
+                done,
+                AssignedValue::cell_value(if self.done_is_high {
+                    BitVecValue::new_true()
+                } else {
+                    BitVecValue::new_false()
+                }),
+            );
+        }
+
+        Ok(out_signal.into())
     }
 
     fn serialize(&self, code: Option<PrintCode>) -> Serializable {

cider/tests/control/simple.expect
@@ -1,0 +1,11 @@
+{
+  "r1": [
+    50
+  ],
+  "r2": [
+    100
+  ],
+  "r3": [
+    150
+  ]
+}

commit fabf065c18cbf9a0df395ac23491c9e677278d33

[Cider] Structural enable fixes (#2459)
Minor set of changes to fix some code that was broken by #2456, namely
the ability to search for assignments in groups that do not appear in
the control tree.

cider/src/flatten/flat_ir/component.rs
@@ -129,13 +129,14 @@ impl PrimaryComponentInfo {
         &self,
         ctx: &Context,
         assign: AssignmentIdx,
+        comp: ComponentIdx,
     ) -> Option<AssignmentDefinitionLocation> {
         match self {
             PrimaryComponentInfo::Comb(info) => {
                 info.contains_assignment(assign)
             }
             PrimaryComponentInfo::Standard(info) => {
-                info.contains_assignment(ctx, assign)
+                info.contains_assignment(ctx, assign, comp)
             }
         }
     }
@@ -220,6 +221,7 @@ impl ComponentCore {
         &self,
         ctx: &Context,
         assign: AssignmentIdx,
+        comp: ComponentIdx,
     ) -> Option<AssignmentDefinitionLocation> {
         if self.continuous_assignments.contains(assign) {
             return Some(AssignmentDefinitionLocation::ContinuousAssignment);
@@ -291,6 +293,15 @@ impl ComponentCore {
                             ));
                         }
                     }
+                }
+            }
+
+            // if we get to this point then it isn't in the control tree, but
+            // may be structurally invoked
+            for group in ctx.secondary.comp_aux_info[comp].definitions.groups()
+            {
+                if ctx.primary[group].assignments.contains(assign) {
+                    return Some(AssignmentDefinitionLocation::Group(group));
                 }
             }
         }

cider/src/flatten/structures/context.rs
@@ -469,12 +469,15 @@ impl Context {
         target: AssignmentIdx,
     ) -> (ComponentIdx, AssignmentDefinitionLocation) {
         for (idx, comp) in self.primary.components.iter() {
-            let found = comp.contains_assignment(self, target);
+            let found = comp.contains_assignment(self, target, idx);
             if let Some(found) = found {
                 return (idx, found);
             }
         }
-        unreachable!("Assignment does not belong to any component");
+        unreachable!(
+            "Assignment '{:?}' does not belong to any component",
+            target
+        );
     }
 
     /// Returns the assignment definition information, if it exists. This
@@ -486,7 +489,7 @@ impl Context {
         target: AssignmentIdx,
         comp: ComponentIdx,
     ) -> Option<AssignmentDefinitionLocation> {
-        self.primary.components[comp].contains_assignment(self, target)
+        self.primary.components[comp].contains_assignment(self, target, comp)
     }
 }
 

cider/src/flatten/structures/environment/env.rs
@@ -3022,6 +3022,31 @@ impl<C: AsRef<Context> + Clone> BaseSimulator<C> {
 
             has_changed |= changed;
 
+            if !has_changed {
+                for ScheduledAssignments {
+                    active_cell,
+                    interface_ports,
+                    ..
+                } in assigns_bundle.iter()
+                {
+                    if let Some(i) = interface_ports {
+                        let ledger =
+                            self.env.cells[*active_cell].as_comp().unwrap();
+                        let go = &ledger.index_bases + i.go;
+                        let done = &ledger.index_bases + i.done;
+
+                        if self.env.ports[go].as_bool().unwrap_or_default()
+                            && self.env.ports[done].is_undef()
+                        {
+                            self.env.ports[done] = PortValue::new_implicit(
+                                BitVecValue::new_false(),
+                            );
+                            has_changed = true;
+                        }
+                    }
+                }
+            }
+
             // check for undefined done ports. If any remain after we've
             // converged then they should be set to zero and we should continue
             // convergence. Since these ports cannot become undefined again we