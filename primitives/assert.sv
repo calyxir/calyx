067c2682a26464be02104e0dbbdc80c09e2ebfae
Elam Day-Friedland <elamdf@berkeley.edu>
2025-05-25 18:04:31 -0700

    add assert primitive and basic test
    TODO: get it working with runt

---
+++ primitives/assert.sv
@@ -0,0 +1,14 @@
+module assert_prim #() (
+   input wire logic clk,
+   input wire logic reset,                        
+   input logic in,
+   input logic en,
+                        output logic out
+);
+    always_ff @(posedge clk) begin
+       out <= in;
+       if (in == '0 & en) begin
+            $fatal(1, "Assertion failed in assert primitive: input was 0");
+        end
+    end
+endmodule
