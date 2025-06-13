884116825bcf99843ebd64ed829c84d7abfb4aec
Griffin Berlstein <griffin@berlste.in>
2025-06-13 19:56:52 +0000

    [Cider] Remove `SearchPath` and minor tweaks (#2478)
    Finishing what I started in #2477. This just removes the code for
    `SearchPath` and adds a utility function to cut down on repetition.

--- cider/src/flatten/structures/environment/mod.rs
+++ cider/src/flatten/structures/environment/mod.rs
@@ -9,5 +9,4 @@ mod wave;
 pub use env::{BaseSimulator, Environment, Simulator};
 pub(crate) use maps::CellLedger;
 pub use maps::PortMap;
-pub use program_counter::SearchPath;
 pub use traverser::{Path, PathError, PathResolution};
