a1490cf6a0f7cd13b01d580f4aed35c5f49f35e2
Parth Sarkar <ps679@cornell.edu>
2025-07-06 15:04:24 -0400

    progress on non-concrete representation of fsms in programs

--- calyx/opt/src/analysis/fsm_call_graph.rs
+++ calyx/opt/src/analysis/fsm_call_graph.rs
@@ -4,68 +4,135 @@ struct FSMCallGraph {
     graph: Vec<AbstractFSM>,
 }
 
-struct AbstractFSM {
-    states: Vec<StatePossibility>,
+impl FSMCallGraph {
+    /// Empty FSMCallGraph analysis struct
+    fn new() -> Self {
+        Self { graph: vec![] }
+    }
+
+    #[inline]
+    fn new_id(&self) -> u64 {
+        self.graph.len().try_into().unwrap()
+    }
+
+    #[inline]
+    fn register_fsm(&mut self, fsm: AbstractFSM) {
+        self.graph.push(fsm);
+    }
+
+    /// Recursively builds empty FSMCallGraph struct given a control node. Returns
+    /// None if `ctrl` is ir::Empty, Some(HardwareEnable) if `ctrl` is a group ir::Enable,
+    /// and Some(Call(...)) if the `ctrl` is a non-leaf node in control tree.
+    fn from_control(&mut self, ctrl: &ir::Control) -> Option<StatePossibility> {
+        match ctrl {
+            ir::Control::Empty(_) => None,
+            ir::Control::Enable(_) => Some(StatePossibility::HardwareEnable),
+            ir::Control::FSMEnable(_) => {
+                self.register_fsm(AbstractFSM::UserDefined);
+                Some(StatePossibility::Call(FSMCallGraphNode::UserDefined))
+            }
+            ir::Control::Seq(seq) => {
+                let seq_node = AbstractFSM::Generated {
+                    states: seq
+                        .stmts
+                        .iter()
+                        .filter_map(|stmt| self.from_control(stmt))
+                        .collect(),
+                };
+                self.register_fsm(seq_node);
+                Some(StatePossibility::Call(FSMCallGraphNode::DynamicSeq {
+                    pointer: self.new_id(),
+                }))
+            }
+            ir::Control::Repeat(rep) => match self.from_control(&rep.body) {
+                None => None,
+                Some(state_possibility) => {
+                    let repeat_body_wrapper = FSMCallGraphNode::DynamicRepeat {
+                        pointer: Box::new(state_possibility),
+                        num_repeats: rep.num_repeats,
+                    };
+                    Some(StatePossibility::Call(repeat_body_wrapper))
+                }
+            },
+            ir::Control::Par(par) => {
+                let par_threads_wrapper = FSMCallGraphNode::DynamicPar {
+                    pointers: par
+                        .stmts
+                        .iter()
+                        .filter_map(|stmt| self.from_control(stmt))
+                        .collect(),
+                };
+                Some(StatePossibility::Call(par_threads_wrapper))
+            }
+            ir::Control::If(ifc) => {
+                // avoid code duplication between branches
+                let mut f = |branch| {
+                    self.from_control(branch)
+                        .map(|state_possibility| Box::new(state_possibility))
+                };
+
+                let if_branches_wrapper = FSMCallGraphNode::DynamicIf {
+                    t_branch_pointer: f(&ifc.tbranch),
+                    f_branch_pointer: f(&ifc.fbranch),
+                };
+                Some(StatePossibility::Call(if_branches_wrapper))
+            }
+
+            ir::Control::Invoke(_) => {
+                unreachable!("Invoke nodes should have been compiled away")
+            }
+            _ => None,
+            // ir::Control::While(whle) => (),
+            // ir::Control::Static(stc) => (),
+        }
+    }
+}
+
+enum AbstractFSM {
+    UserDefined, // need to add node_id field (i.e. pointer to actual control node)
+    Generated { states: Vec<StatePossibility> },
 }
 
 enum StatePossibility {
-    Hardware {
-        assignments: Vec<ir::Assignment<ir::Nothing>>,
-        transitions: ir::Transition,
-    },
+    HardwareEnable,
     Call(FSMCallGraphNode),
 }
 
 enum FSMCallGraphNode {
-    UserDefined {
-        local_id: u64,
-    },
+    UserDefined,
     StaticRepeat {
         body_latency: u64,
-        local_id: u64,
+        pointer: u64,
         num_repeats: u64,
     },
     StaticPar {
         max_thread_latency: u64,
-        local_ids: Vec<u64>,
+        pointer: u64,
     },
     StaticSeq {
         latency: u64,
-        local_id: u64,
+        pointer: u64,
     },
     StaticIf {
         max_branch_latency: u64,
-        t_branch_id: u64,
-        f_branch_id: u64,
+        t_branch_pointer: u64,
+        f_branch_pointer: u64,
     },
     DynamicSeq {
-        local_id: u64,
+        pointer: u64,
     },
     DynamicPar {
-        local_ids: Vec<u64>,
+        pointers: Vec<StatePossibility>,
     },
     DynamicIf {
-        t_branch_id: u64,
-        f_branch_id: u64,
+        t_branch_pointer: Option<Box<StatePossibility>>,
+        f_branch_pointer: Option<Box<StatePossibility>>,
     },
     DynamicWhile {
-        t_branch_id: u64,
-        f_branch_id: u64,
+        pointer: u64,
     },
     DynamicRepeat {
-        child_id: u64,
+        pointer: Box<StatePossibility>,
         num_repeats: u64,
     },
-}
-
-impl FSMCallGraphNode {
-    fn is_static(&self) -> bool {
-        match self {
-            Self::StaticRepeat { .. }
-            | Self::StaticPar { .. }
-            | Self::StaticSeq { .. }
-            | Self::StaticIf { .. } => true,
-            _ => false,
-        }
-    }
 }
