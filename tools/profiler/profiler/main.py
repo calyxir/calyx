bd8f4b9441a304cb0ee04079f9362d3738c35388
Pedro Pontes García <62205816+pedropontesgarcia@users.noreply.github.com>
2025-07-07 17:15:13 -0400

    [Profiler] Primitive cell-sharing (#2511)
    This PR adds support for primitives cell-sharing and provides a log of
    unaccessed utilization values.
    
    ---------
    
    Co-authored-by: Pedro Pontes García <pp457@havarti.cs.cornell.edu>

--- tools/profiler/profiler/main.py
+++ tools/profiler/profiler/main.py
@@ -15,6 +15,7 @@ from profiler.classes import (
     ControlMetadata,
     TraceData,
     ControlRegUpdateType,
+    Utilization,
 )
 
 
@@ -42,7 +43,7 @@ def process_vcd(
     control_metadata: ControlMetadata,
     tracedata: TraceData,
     vcd_filename: str,
-    utilization: dict[str, dict] | None = None,
+    utilization: Utilization | None = None,
 ):
     """
     Wrapper function to process the VCD file to produce a trace.
@@ -160,20 +161,20 @@ def main():
 
     cell_metadata, shared_cells_map, control_metadata, tracedata = setup_metadata(args)
 
-    utilization: dict[str, dict] | None = None
+    utilization: Utilization | None = None
     utilization_variable: str | None = None
 
     if args.utilization_report_json is not None:
         print("Utilization report mode enabled.")
         with open(args.utilization_report_json) as f:
-            utilization = json.load(f)
-            map = {
+            utilization = Utilization(json.load(f))
+            varmap = {
                 "ff": "FFs",
                 "lut": "Total LUTs",
                 "llut": "Logic LUTs",
                 "lutram": "LUTRAMs",
             }
-            utilization_variable = map[args.utilization_variable]
+            utilization_variable = varmap[args.utilization_variable]
 
     control_reg_updates_per_cycle: dict[int, ControlRegUpdateType] = process_vcd(
         cell_metadata,
@@ -185,6 +186,10 @@ def main():
     )
 
     tracedata.print_trace(threshold=args.print_trace_threshold, ctrl_trace=True)
+    if utilization:
+        print(
+            f"Unaccessed utilization values: {', '.join(utilization.get_unaccessed())}"
+        )
 
     create_visuals(
         cell_metadata,
