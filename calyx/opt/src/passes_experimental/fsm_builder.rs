5e9a2873698092a7aa1f3d61a1e51076128a7c32
Cynthia Shao <cynyu.shao@gmail.com>
2025-07-25 11:45:26 -0400

    Clean up comments.

--- calyx/opt/src/passes_experimental/fsm_builder.rs
+++ calyx/opt/src/passes_experimental/fsm_builder.rs
@@ -76,61 +76,14 @@ impl StaticSchedule<'_, '_> {
                         )],
                         looped_once_guard,
                     )
-
-                    // // use parth's onestate code here
-                    // // for all parts of the FSM that want to transition to this enable,
-                    // // register their transitions in self.state2trans
-                    // self.register_transitions(
-                    //     self.state,
-                    //     &mut transitions_to_curr,
-                    //     guard.clone(),
-                    // );
-
-                    // // one state for every
-                    // // cycle otherwise
-
-                    // sen.group.borrow().assignments.iter().for_each(|sassign| {
-                    //     sassign
-                    //         .guard
-                    //         .compute_live_states(sen.group.borrow().latency)
-                    //         .into_iter()
-                    //         .for_each(|offset| {
-                    //             // convert the static assignment to a normal one
-                    //             let mut assign: ir::Assignment<ir::Nothing> =
-                    //                 ir::Assignment::from(sassign.clone());
-                    //             // "and" the assignment's guard with argument guard
-                    //             assign.and_guard(guard.clone());
-                    //             // add this assignment to the list of assignments
-                    //             // that are supposed to be valid at this state
-                    //             self.state2assigns
-                    //                 .entry(self.state + offset)
-                    //                 .and_modify(|other_assigns| {
-                    //                     other_assigns.push(assign.clone())
-                    //                 })
-                    //                 .or_insert(vec![assign]);
-                    //         })
-                    // });
-                    // self.state += sen.group.borrow().latency;
-                    // // Don't know where to transition next; let the parent that called
-                    // // `build_abstract_fsm` deal with registering the transition from the state(s)
-                    // // we just built.
-                    // vec![IncompleteTransition::new(
-                    //     self.state - 1,
-                    //     ir::Guard::True,
-                    // )]
                 } else {
                     // must be cyclic otherwise
-                    // use parth's onestate code here
-                    // for all parts of the FSM that want to transition to this enable,
-                    // register their transitions in self.state2trans
+                    // placeholder code for now.
                     self.register_transitions(
                         self.state,
                         &mut transitions_to_curr,
                         guard.clone(),
                     );
-
-                    // allocate one state if requested, and have one state for every
-                    // cycle otherwise
 
                     let final_state_guard =
                         self.leave_one_state_condition(guard, sen);
@@ -145,16 +98,16 @@ impl StaticSchedule<'_, '_> {
                     )
                 }
             }
-            ir::StaticControl::Seq(sseq) => {
+            ir::StaticControl::Seq(_sseq) => {
                 todo!()
             }
-            ir::StaticControl::Repeat(srep) => {
+            ir::StaticControl::Repeat(_srep) => {
                 todo!()
             }
-            ir::StaticControl::If(sif) => {
+            ir::StaticControl::If(_sif) => {
                 todo!()
             }
-            ir::StaticControl::Par(spar) => {
+            ir::StaticControl::Par(_spar) => {
                 todo!()
             }
             ir::StaticControl::Invoke(_) => {
@@ -247,41 +200,6 @@ impl StaticSchedule<'_, '_> {
         // Instantiate the FSM with the assignments and transitions we built
         fsm.borrow_mut().extend_fsm(assignments, transitions);
         fsm
-        // let fsm = self.builder.add_fsm("fsm");
-
-        // let mut remaining_assignments =
-        //     self.build_abstract(control, ir::Guard::True, vec![]);
-
-        // // add loopback transitions to first state
-        // self.register_transitions(
-        //     0,
-        //     &mut remaining_assignments,
-        //     ir::Guard::True,
-        // );
-
-        // let (assignments, transitions, state2wires) =
-        //     self.build_fsm_pieces(ir::RRC::clone(&fsm));
-
-        // self.builder.add_continuous_assignments(
-        //     self.state2assigns
-        //         .drain()
-        //         .flat_map(|(state, mut assigns)| {
-        //             assigns.iter_mut().for_each(|assign| {
-        //                 assign.and_guard(ir::Guard::port(
-        //                     state2wires
-        //                         .get(state as usize)
-        //                         .unwrap()
-        //                         .borrow()
-        //                         .get("out"),
-        //                 ));
-        //             });
-        //             assigns
-        //         })
-        //         .collect(),
-        // );
-
-        // fsm.borrow_mut().extend_fsm(assignments, transitions);
-        // fsm
     }
 }
 
@@ -293,8 +211,7 @@ impl Visitor for FSMBuilder {
         sigs: &calyx_ir::LibrarySignatures,
         _comps: &[calyx_ir::Component],
     ) -> crate::traversal::VisResult {
-        // non-promoted static components are static islands
-        // pretend that only non-promoted so far.
+        // implementation for single static enable components for now.
         let mut builder = ir::Builder::new(comp, sigs);
 
         let mut ssch = StaticSchedule::from(&mut builder);
@@ -302,7 +219,5 @@ impl Visitor for FSMBuilder {
         Ok(Action::change(ir::Control::fsm_enable(
             ssch.fsm_build(scon),
         )))
-        // otherwise need to do a dynamic handshake
-        // Ok(Action::Continue)
     }
 }
