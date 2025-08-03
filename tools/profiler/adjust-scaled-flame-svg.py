1b166a01566df7ca3e90dbf0e75aa342aa1dd4e7
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2025-08-03 17:59:39 -0400

    Changing colors of scaled flame based on element type

--- tools/profiler/adjust-scaled-flame-svg.py
+++ tools/profiler/adjust-scaled-flame-svg.py
@@ -2,16 +2,33 @@ import sys
 
 
 # Takes in a flame graph svg that is scaled by 1000 and prints a version with fixed cycles.
+# Also adjust the colors of elements based on cells/groups/primitives/control groups
+cell_fill = 'fill="rgb(255,0,0)"'
+control_group_fill = 'fill="rgb(255,128,0)"'
+group_fill = 'fill="rgb(255,255,102)"'
+primitive_fill = 'fill="rgb(204,255,153)"'
+
 def main(svg_in):
     oin = open(svg_in, "r")
 
     for line in oin:
         if line.startswith("<title>"):
+            if "(primitive)" in line:
+                fill = primitive_fill
+            elif "[" in line or "<title>main" in line:
+                fill = cell_fill
+            elif "(ctrl)" in line:
+                fill = control_group_fill
+            else:
+                fill = group_fill
             line_split = line.strip().split(" ")
             target_idx = 0
+            fill_target_idx = 0
             for i in range(len(line_split)):
                 if line_split[i] == "cycles,":
                     target_idx = i - 1
+                if line_split[i].startswith("fill="):
+                    fill_target_idx = i
             new_number = (
                 int(line_split[target_idx].split("(")[1].replace(",", "")) / 1000
             )
@@ -20,7 +37,9 @@ def main(svg_in):
                 + " ("
                 + "{:,}".format(new_number)
                 + " "
-                + " ".join(line_split[target_idx + 1 :])
+                + " ".join(line_split[target_idx + 1 : fill_target_idx])
+                + " " + fill + " "
+                + " ".join(line_split[fill_target_idx + 1 :])
             )
         else:
             print(line.strip())
