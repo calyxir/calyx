78e5fcf6f5f2a2b0b9f4396d9b4c59f67a432fa4
jeremy ku-benjet <jeremykubenjetschool@gmail.com>
2025-10-20 17:50:15 -0400

    Document sat planner.

--- fud2/fud-core/src/exec/plan/sat_planner.rs
+++ fud2/fud-core/src/exec/plan/sat_planner.rs
@@ -5,6 +5,36 @@
 //! one of those ops a dependency for the state. Looking at every ops, every state will have many
 //! dependencies, and if a state is chosen, at least one of those dependencies must be fulfilled.
 //! This can be modeled by the state implying all of its dependencies.
+//!
+//! To do so, the planner makes the observation that an op which returns multiple states can be
+//! broken into a bunch of ops which return one state. The nomenclature I use is to call these
+//! dependencies. Precisely, if state `s1` can be made using `op1`, I say all of the inputs to
+//! `op1` form a dependency of `s1`. Therefore over all the ops, each state gets a bunch of
+//! possible dependencies, different ways to make it, using different ops.
+//!
+//! Dependencies are straightforward to encode in boolean logic. Let each op and state be
+//! variables. If that variable is true, that state is taken or that op is used. Let `s1` be the
+//! variable representing a state with possible dependencies `s2, s3` using `op1` and `s1, s4`
+//! using `op2`. This can be encoded as follows: `(s1 ^ op1 => s2 ^ s3) => (s1 ^ op2 => s1 ^ s4)`.
+//!
+//! Putting an op as a condition of the implication might feel counter-intuitive, but it is
+//! necessary. Using the encoding `(s1 => s2 ^ s3 ^ op1) v (s1 => s1 ^ s4 ^ op2)` would allow a
+//! solution were ops are taken but never used, for example the following variables being true:
+//! `s1, op1, op2, s4`.
+//!
+//! To respond to a request with given input states, output states, and required ops. First, each
+//! output state is conjoined together and added to the expression. Then for each possible state a
+//! file could be in, if that state is not an input (or an output*) and cannot be constructed with
+//! any ops, the state's negation is conjoined to the boolean expression, representing that the
+//! state can never be constructed. If the state can be constructed, taking the state implies one
+//! of the ops is used. This is then conjoined to the boolean expression. To encode the required
+//! ops, they are simply conjoined to the final expression.
+//!
+//! As an example, say the same ops as above are used. The desired output is `s1` and the input is
+//! `s2, s3` and the required op is `op1`. The final encoding would be:
+//! ```text
+//! (s1 ^ op1 => s2 ^ s3) ^ (s1 ^ op2 => s1 ^ s4) ^ op1 ^ s1 ^ ~s4 ^ (s1 => op1 ^ op2)
+//! ```
 
 use std::collections::HashMap;
 
@@ -21,15 +51,25 @@ use super::{
     planner::Step,
 };
 
+/// Used to build up the big boolean expression checked by the planner.
 struct DepClauses<'a> {
+    /// A map from boolean variables to the state they represent.
     state_of_lit: HashMap<Lit, StateRef>,
+
+    /// A map from a state to the boolean variable representing it.
     lit_of_state: HashMap<StateRef, Lit>,
+
+    /// A map from boolean variables to the op they represent.
     op_of_lit: HashMap<Lit, OpRef>,
+
+    /// A map from an op to the boolean variable representing it.
     lit_of_op: HashMap<OpRef, Lit>,
 
-    ops: &'a PrimaryMap<OpRef, Operation>,
-
+    /// A map from a variable representing a state to that variables representing that variable's
+    /// dependencies.
     made_from: HashMap<Lit, Vec<Lit>>,
+
+    ops: &'a PrimaryMap<OpRef, Operation>,
     instance: SatInstance,
 }
 
@@ -46,6 +86,7 @@ impl<'a> DepClauses<'a> {
         }
     }
 
+    /// Returns the variable representing a state, creating one if it doesn't already exist.
     pub fn state_lit(&mut self, s: StateRef) -> Lit {
         if !self.lit_of_state.contains_key(&s) {
             let lit = self.instance.new_lit();
@@ -57,6 +98,7 @@ impl<'a> DepClauses<'a> {
         }
     }
 
+    /// Returns the variable representing an op, creating one if it doesn't already exist.
     pub fn op_lit(&mut self, o: OpRef) -> Lit {
         if !self.lit_of_op.contains_key(&o) {
             let lit = self.instance.new_lit();
@@ -68,6 +110,8 @@ impl<'a> DepClauses<'a> {
         }
     }
 
+    /// Adds a dependency to the boolean expression for which `state_id` depends on the inputs of
+    /// `dep_ref`.
     pub fn add_dep(&mut self, state_id: StateRef, dep_ref: OpRef) {
         let ls = self.state_lit(state_id);
         let dep = &self.ops[dep_ref];
@@ -78,6 +122,8 @@ impl<'a> DepClauses<'a> {
         self.made_from.entry(ls).or_default().push(op_lit);
     }
 
+    /// Returns a `SatInstance` representing a request using deps previously added to `self` and
+    /// `inputs`, `outputs`, and `through`.
     pub fn instance(
         &mut self,
         inputs: &[StateRef],
@@ -85,13 +131,18 @@ impl<'a> DepClauses<'a> {
         through: &[OpRef],
     ) -> SatInstance {
         let mut out_instance = self.instance.clone();
-        // We must take outputs.
+
+        // Require outputs to be created.
         for &output in outputs {
             let lo = self.state_lit(output);
             out_instance.add_unit(lo);
         }
-        // We need to mark primitive inputs, ones with no deps as things which can never be taken.
+
+        // Mark inputs ones with no deps as things which can never be taken. If there are deps,
+        // make sure whenever if that state is constructed, some ops is used to actually create it.
         for (&lit, state_id) in &self.state_of_lit {
+            // The case `outputs.contains(state_id)` must exist because an output must be
+            // constructed even if it is already present as an input.
             if !inputs.contains(state_id) || outputs.contains(state_id) {
                 let ops_making_lit = self.made_from.entry(lit).or_default();
                 if !ops_making_lit.is_empty() {
@@ -102,7 +153,8 @@ impl<'a> DepClauses<'a> {
                 }
             }
         }
-        // We need to make sure all the ops in through are taken.
+
+        // Make sure all the ops in through are taken.
         for &op in through {
             out_instance.add_unit(self.op_lit(op));
         }
