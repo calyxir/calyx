2798c6eb9f4395e0ce6e4999db85d7c682d2a93a
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2025-11-19 22:49:50 -0500

    Fix formatting and add tests

--- calyx/opt/src/passes_experimental/simplify_if_comb.rs
+++ calyx/opt/src/passes_experimental/simplify_if_comb.rs
@@ -1,37 +1,15 @@
-use core::borrow;
-use std::collections::{HashMap, HashSet};
-
 use crate::traversal::{Action, ConstructVisitor, Named, VisResult, Visitor};
-use calyx_ir::{
-    self as ir, GetAttributes, Id, LibrarySignatures, Primitive, RRC, Rewriter,
-    rewriter::{self, RewriteMap},
-};
+use calyx_ir::{self as ir, LibrarySignatures, Rewriter, rewriter::RewriteMap};
 use calyx_utils::CalyxResult;
 use itertools::Itertools;
 
-/// Transforms all `par` into `seq`. When the `correctness-checking` option is on,
-/// uses [analysis::ControlOrder] to get a sequentialization of `par` such that
-/// the program still computes the same value, and errors out when
-/// there is no such sequentialization.
-///
-/// # Example
-/// ```
-/// par {
-///     par { A; B }
-///     C;
-/// }
-/// ```
-/// into
-/// ```
-/// seq { seq { A; B } C; }
-/// ```
-///
-/// To remove uneccessarily nested `par` blocks, run collapse-control.
-
 /// Transforms `if`s with `comb` groups where the `then` block of the `if` consists
 /// of a single enable into `if`s with the condition being computed via continuous
-/// assignments. (Probably should check if any cells being used in the cond group
-/// are used anywhere else)
+/// assignments.
+///
+/// The cell used for the condition (and any other cells on the LHS of an assignment
+/// in the comb group) will be cloned. Therefore, it is important that dead-cell-removal
+/// and dead-group-removal is ran after this pass.
 ///
 /// # Example
 /// ```
@@ -48,10 +26,10 @@ use itertools::Itertools;
 ///
 /// ```
 /// // continuous assignments
-/// lt.left = x.out;
-/// lt.right = 32'd10;
+/// lt0.left = x.out;
+/// lt0.right = 32'd10;
 /// ...
-/// if lt.out {
+/// if lt0.out {
 ///     then_group;
 /// }
 /// ```
@@ -67,105 +45,22 @@ impl Named for SimplifyIfComb {
     }
 
     fn opts() -> Vec<crate::traversal::PassOpt> {
-        // vec![PassOpt::new(
-        //     "correctness-checking",
-        //     "Errors out when dataflow dependencies cannot be preserved.",
-        //     ParseVal::Bool(false),
-        //     PassOpt::parse_bool,
-        // )]
         vec![]
     }
 }
 
 impl ConstructVisitor for SimplifyIfComb {
-    fn from(ctx: &ir::Context) -> CalyxResult<Self>
+    fn from(_ctx: &ir::Context) -> CalyxResult<Self>
     where
         Self: Sized + Named,
     {
-        // let opts = Self::get_opts(ctx);
-
-        Ok(SimplifyIfComb {
-            // correctness_checking: opts[&"correctness-checking"].bool(),
-        })
+        Ok(SimplifyIfComb {})
     }
 
-    fn clear_data(&mut self) {
-        /* All data can be transferred between components */
-    }
+    fn clear_data(&mut self) {}
 }
 
-// fn create_new_cell(builder: &mut ir::Builder, cell_ref: &RRC<ir::Cell>) {
-//     let cell = cell_ref.borrow();
-//     match &cell.prototype {
-//         calyx_ir::CellType::Primitive {
-//             name,
-//             param_binding,
-//             is_comb,
-//             latency,
-//         } => todo!(),
-//         calyx_ir::CellType::Component { name } => todo!(),
-//         calyx_ir::CellType::ThisComponent => todo!(),
-//         calyx_ir::CellType::Constant { val, width } => todo!(),
-//     }
-// }
-
 impl Visitor for SimplifyIfComb {
-    // fn start(
-    //     &mut self,
-    //     comp: &mut calyx_ir::Component,
-    //     _sigs: &LibrarySignatures,
-    //     _comps: &[calyx_ir::Component],
-    // ) -> VisResult {
-    //     // for each cell used in a comb_group, track if it's used anywhere else
-    //     let mut comb_group_cell_map: HashMap<ir::Id, Vec<ir::Id>> =
-    //         HashMap::new();
-    //     for comb_group_ref in comp.comb_groups.iter() {
-    //         let comb_group = comb_group_ref.borrow();
-    //         let comb_group_name = comb_group.name();
-    //         let mut cells_this_comb_group = HashSet::new();
-    //         // collect cells that are used in this comb group
-    //         for comb_group_asgn in &comb_group.assignments {
-    //             if let calyx_ir::PortParent::Cell(c) =
-    //                 &comb_group_asgn.dst.borrow().parent
-    //             {
-    //                 let cell_ref = c.upgrade();
-    //                 let cell_name = cell_ref.borrow().name();
-    //                 cells_this_comb_group.insert(cell_name);
-    //             }
-    //         }
-    //         for cell in cells_this_comb_group {
-    //             match comb_group_cell_map.get_mut(&cell) {
-    //                 Some(v) => {
-    //                     v.push(comb_group_name);
-    //                 }
-    //                 None => {
-    //                     let mut v = Vec::new();
-    //                     v.push(comb_group_name);
-    //                     comb_group_cell_map.insert(cell, v);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // filter out any cells that were used in two comb groups
-    //     comb_group_cell_map.retain(|_, v| v.len() == 1);
-    //     // go through normal groups; remove any cells that were used in a normal group
-    //     for group_ref in comp.groups.iter() {
-    //         let group = group_ref.borrow();
-    //         for group_asgn in &group.assignments {
-    //             if let calyx_ir::PortParent::Cell(c) =
-    //                 &group_asgn.dst.borrow().parent
-    //             {
-    //                 let cell_ref = c.upgrade();
-    //                 let cell_name = cell_ref.borrow().name();
-    //                 if comb_group_cell_map.contains_key(&cell_name) {
-    //                     comb_group_cell_map.remove(&cell_name);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     Ok(Action::Continue)
-    // }
-
     fn finish_if(
         &mut self,
         s: &mut calyx_ir::If,
@@ -188,7 +83,6 @@ impl Visitor for SimplifyIfComb {
                         {
                             let c_ref = c.upgrade();
                             let c_name = c_ref.borrow().name();
-                            // let new_c_name = comp.generate_name(c_name);
 
                             if !rewrite_map.contains_key(&c_name)
                                 && let ir::CellType::Primitive {
@@ -199,7 +93,7 @@ impl Visitor for SimplifyIfComb {
                             {
                                 let new_cell = builder.add_primitive(
                                     c_name,
-                                    name.clone(),
+                                    *name,
                                     &param_binding
                                         .iter()
                                         .map(|(_, v)| *v)
@@ -207,7 +101,6 @@ impl Visitor for SimplifyIfComb {
                                 );
                                 rewrite_map.insert(c_name, new_cell);
                             }
-                            // create copy of cell?
                         }
                     }
                     let rewrite = Rewriter {
@@ -221,7 +114,7 @@ impl Visitor for SimplifyIfComb {
                         rewrite.rewrite_assign(&mut new_asgn);
                         comp.continuous_assignments.push(new_asgn);
                     }
-                    // create new enable
+                    // create new enable for the true branch
                     let new_tbranch =
                         calyx_ir::Control::enable(enable.group.clone());
                     let mut new_if = calyx_ir::Control::if_(
@@ -238,23 +131,5 @@ impl Visitor for SimplifyIfComb {
             }
             _ => Ok(Action::Continue),
         }
-
-        // only transform if the true branch consists of a single enable, the false branch is empty, and there is a cond group
-        // if let calyx_ir::Control::Enable(_) = *s.tbranch
-        //     && let Some(cond_group_ref) = &s.cond
-        //     && s.fbranch.is_empty()
-        // // there technically needs to be another check here for whether the cells used in the cond group are used in any other group
-        // {
-        //     // create a new version of the if
-        //     let new_if_group = calyx_ir::Control::if_(
-        //         s.port.clone(),
-        //         None,
-        //         Box::new(*(s.tbranch.as_ref())),
-        //         s.fbranch,
-        //     );
-        //     Ok(Action::Continue)
-        // } else {
-        //     Ok(Action::Continue)
-        // }
     }
 }
