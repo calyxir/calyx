d9c44abca01d32047eec6bf070e53df8f0f04862
Pedro Pontes García <pp457@havarti.cs.cornell.edu>
2025-12-04 01:08:16 -0500

    fixed python formatting, deleted extraneous graph pdf

--- tools/profiler/profiler/visuals/stats.py
+++ tools/profiler/profiler/visuals/stats.py
@@ -272,8 +272,7 @@ def write_par_stats(tracedata: TraceData, out_dir):
         writer.writeheader()
         writer.writerows(stats)
 
+
 def write_total_cycles(tracedata: TraceData, out_dir):
-    with open(
-        os.path.join(out_dir, "total_cycles.txt"), "w", encoding="utf-8"
-    ) as file:
+    with open(os.path.join(out_dir, "total_cycles.txt"), "w", encoding="utf-8") as file:
         file.write(str(len(tracedata.trace.trace)))
