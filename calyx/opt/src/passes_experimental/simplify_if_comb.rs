d861e823deb7989ffc3c7cabfeaeac3bd22b3f04
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2025-12-09 17:21:02 -0500

    update after PR comments

--- calyx/opt/src/passes_experimental/simplify_if_comb.rs
+++ calyx/opt/src/passes_experimental/simplify_if_comb.rs
@@ -69,54 +69,57 @@ impl Visitor for SimplifyIfComb {
     ) -> VisResult {
         let mut builder = ir::Builder::new(comp, sigs);
         let mut rewrite_map = RewriteMap::new();
-        if let Some(cond_group_ref) = &s.cond {
-            // create new cell for all cells in LHS of cond group assignments
-            for cond_group_asgn in &cond_group_ref.borrow().assignments {
-                if let calyx_ir::PortParent::Cell(c) =
-                    &cond_group_asgn.dst.borrow().parent
-                {
-                    let c_ref = c.upgrade();
-                    let c_name = c_ref.borrow().name();
+        let mut new_continuous_assignments = Vec::new();
 
-                    if !rewrite_map.contains_key(&c_name)
-                        && let ir::CellType::Primitive {
-                            name,
-                            param_binding,
-                            ..
-                        } = &c_ref.borrow().prototype
-                    {
-                        let new_cell = builder.add_primitive(
-                            c_name,
-                            *name,
-                            &param_binding
-                                .iter()
-                                .map(|(_, v)| *v)
-                                .collect_vec(),
-                        );
-                        rewrite_map.insert(c_name, new_cell);
-                    }
+        let Some(cond_group_ref) = &s.cond else {
+            return Ok(Action::Continue);
+        };
+        // create new cell for all cells in LHS of cond group assignments
+        for cond_group_asgn in &cond_group_ref.borrow().assignments {
+            if let calyx_ir::PortParent::Cell(c) =
+                &cond_group_asgn.dst.borrow().parent
+            {
+                let c_ref = c.upgrade();
+                let c_name = c_ref.borrow().name();
+                if !rewrite_map.contains_key(&c_name)
+                    && let ir::CellType::Primitive {
+                        name,
+                        param_binding,
+                        ..
+                    } = &c_ref.borrow().prototype
+                {
+                    let new_cell = builder.add_primitive(
+                        c_name,
+                        *name,
+                        &param_binding.iter().map(|(_, v)| *v).collect_vec(),
+                    );
+                    rewrite_map.insert(c_name, new_cell);
                 }
             }
-            let rewrite = Rewriter {
-                cell_map: rewrite_map,
-                ..Default::default()
-            };
-            // move all assignments in cond group to continuous and rewrite cells
-            for cond_group_asgn in &cond_group_ref.borrow_mut().assignments {
-                let mut new_asgn = cond_group_asgn.clone();
-                rewrite.rewrite_assign(&mut new_asgn);
-                comp.continuous_assignments.push(new_asgn);
-            }
-            let mut new_if = calyx_ir::Control::if_(
-                s.port.clone(),
-                None,
-                Box::new(s.tbranch.take_control()),
-                Box::new(s.fbranch.take_control()),
-            );
-            rewrite.rewrite_control(&mut new_if);
-            Ok(Action::change(new_if))
-        } else {
-            Ok(Action::Continue)
         }
+
+        // move all assignments in cond group to continuous and rewrite cells
+        for cond_group_asgn in &cond_group_ref.borrow().assignments {
+            let new_asgn = cond_group_asgn.clone();
+            new_continuous_assignments.push(new_asgn);
+        }
+
+        let rewrite = Rewriter {
+            cell_map: rewrite_map,
+            ..Default::default()
+        };
+        for asgn in new_continuous_assignments.iter_mut() {
+            rewrite.rewrite_assign(asgn);
+            comp.continuous_assignments.push(asgn.clone());
+        }
+        s.cond = None;
+        let mut new_if = calyx_ir::Control::if_(
+            s.port.clone(),
+            None,
+            Box::new(s.tbranch.take_control()),
+            Box::new(s.fbranch.take_control()),
+        );
+        rewrite.rewrite_control(&mut new_if);
+        Ok(Action::Change(Box::new(new_if)))
     }
 }
