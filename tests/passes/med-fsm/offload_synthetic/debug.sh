94e6d8ee22ac7acf40e0dbc365d4dce9ce074382
CynyuS <cynyu.shao@gmail.com>
2026-02-09 17:12:55 -0500

    current test case to reduce and debug

---
+++ tests/passes/med-fsm/offload_synthetic/debug.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+set -x
+
+FLAGS='-p med-fsm -x tdcc:infer-fsms -p lower'
+INPUT_RANDOM_DATA_PATH=/home/cynyu_s/calyx/tests/passes/med-fsm/offload_synthetic/random.json
+DESIGN_PATH=/home/cynyu_s/calyx/tests/passes/med-fsm/offload_synthetic/offload_less_synthetic.futil
+
+fud exec --from calyx --to jq --through icarus-verilog --through dat \
+  -s calyx.flags "$FLAGS" \
+  -s verilog.data "$INPUT_RANDOM_DATA_PATH" \
+  -s jq.expr '.memories' \
+  "$DESIGN_PATH" -q
+
+fud exec --from calyx --to jq --through icarus-verilog --through dat \
+  -s verilog.data "$INPUT_RANDOM_DATA_PATH" \
+  -s jq.expr '.memories' \
+  "$DESIGN_PATH" -q
