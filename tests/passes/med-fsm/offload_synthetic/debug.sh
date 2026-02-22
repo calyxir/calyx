d6a4af10a0aad2e405de01633c66e2b7b6c15b1a
CynyuS <cynyu.shao@gmail.com>
2026-02-22 18:03:40 -0500

    uhhh fixed merge?

--- tests/passes/med-fsm/offload_synthetic/debug.sh
+++ tests/passes/med-fsm/offload_synthetic/debug.sh
@@ -18,6 +18,12 @@ fud exec --from calyx --to jq --through icarus-verilog --through dat \
   "$DESIGN_PATH" -q
 
 fud exec --from calyx --to jq --through icarus-verilog --through dat \
+  -s calyx.flags "$FLAGS" \
+  -s verilog.data "$INPUT_RANDOM_DATA_PATH" \
+  -s jq.expr '.memories' \
+  "$REDUCE_PATH" -q
+
+fud exec --from calyx --to jq --through icarus-verilog --through dat \
   -s verilog.data "$INPUT_RANDOM_DATA_PATH" \
   -s jq.expr '.memories' \
   "$DESIGN_PATH" -q
