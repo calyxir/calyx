7c02e0019adf7ce2b3c42b78ee2178770f95ab57
jku20 <59588824+jku20@users.noreply.github.com>
2026-06-11 23:33:09 -0400

    [fud2] Planner Based Around Inductively Defining All Constructable States (#2616)
    I came up with this writing up fud2 for latte and trying to prove this
    planner problem NP hard w.r.t. the number of ops and states. Uhhh, I
    think the problem isn't actually that hard and this just kind of works
    and is actually okay-ishly efficient.
    
    The big idea here is to keep a set of creatable states and iteratively
    look through every op to see if it's inputs are satisfied. If its inputs
    are satisfied, add the op's outputs to the set of creatable states and
    write the op down as used. This will find all creatable states from an
    input set.
    
    However, this likely will create too large a set of ops, so this set is
    pruned. Ops whose outputs are redundant to other ops or whose outputs
    are never used are removed. This is iterated until ops stop getting
    removed.
    
    It is possible for valid plans to exist this algorithm does not fine,
    but I think it does a pretty good job. It passes tests at least...
    
    The complexity analysis on this one is a bit hard because it requires
    thinking up annoying worst case counter examples, but I can bound it by
    O(Ops^2 * States). The construction and pruning stage have nice
    symmetries where both require iterating over every op and each of those
    ops's inputs/output (the ops * states parameter) with an amount of
    iterations bound by the number of ops.
    
    ---------
    
    Co-authored-by: bcarlet <8906114+bcarlet@users.noreply.github.com>
    Co-authored-by: rhf841 <rhfu012@gmail.com>
    Co-authored-by: Ayaka Yorihiro <ayakayorihiro@gmail.com>
    Co-authored-by: Ayaka Yorihiro <36107281+ayakayorihiro@users.noreply.github.com>
    Co-authored-by: Abhilekh Gautam <67783712+Abhilekhgautam@users.noreply.github.com>

--- fud2/fud-core/src/exec/plan/mod.rs
+++ fud2/fud-core/src/exec/plan/mod.rs
@@ -2,6 +2,7 @@
 mod sat_planner;
 
 mod enumerative_planner;
+mod inductive_planner;
 mod json_planner;
 mod legacy_planner;
 mod op_list_converter;
@@ -11,6 +12,7 @@ mod planner;
 pub use sat_planner::SatPlanner;
 
 pub use enumerative_planner::EnumeratePlanner;
+pub use inductive_planner::InductivePlanner;
 pub use json_planner::JsonPlanner;
 pub use legacy_planner::LegacyPlanner;
 pub use planner::{FindPlan, Request, Step};
