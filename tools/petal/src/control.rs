049bfb9713da3f9f0461a478a9188b367b3b38ce
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2026-06-15 20:03:12 -0400

    WIP

--- tools/petal/src/control.rs
+++ tools/petal/src/control.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Ok, Result, anyhow};
+use cranelift_entity::{PrimaryMap, entity_impl};
 use std::{
     collections::{BTreeMap, BTreeSet, HashMap, HashSet},
     fs::File,
@@ -27,7 +28,7 @@ struct PathDescriptorInfo {
     /// enable id --> descriptor
     pub enables: BTreeMap<String, String>,
     /// descriptor --> position set
-    /// (Stringeally I'd do a position set --> descriptor mapping but
+    /// (Ideally I'd do a position set --> descriptor mapping but
     /// a set shouldn't be a key.)
     pub control_pos: BTreeMap<String, BTreeSet<u32>>,
 }
@@ -91,25 +92,134 @@ struct FSMStateInfo {
     group: String,
 }
 
-pub fn parse(
-    tdcc_filename: String,
-    pd_filename: String,
-    pos_filename: String,
-) -> Result<()> {
-    println!("Parsing {}", tdcc_filename);
-    let tdcc_file = File::open(tdcc_filename)?;
-    let tdcc_profiling_info: HashSet<ProfilingInfo> =
-        serde_json::from_reader(BufReader::new(tdcc_file))?;
-
-    println!("Parsing {}", pd_filename);
-    let pd_file = File::open(pd_filename)?;
-    let pd: BTreeMap<String, PathDescriptorInfo> =
-        serde_json::from_reader(BufReader::new(pd_file))?;
-
-    println!("Parsing {}", pos_filename);
-    let pos_file = File::open(pos_filename)?;
-    let pos: HashMap<String, Vec<ControlCalyxPosInfo>> =
-        serde_json::from_reader(BufReader::new(pos_file))?;
-
-    Ok(())
+/// Represents the registers that do the bookkeeping for a
+/// control group
+enum ControlRegister {
+    FSM(String),
+    PD(Vec<String>),
 }
+
+struct ControlMeta {
+    name: String, // name of the TDCC group (later changed into signal?)
+    groups: Vec<String>, // groups that are invoked under this control node
+    component: String,
+    control_type: String, // seq, par, etc. optimize into enum?
+    registers: Option<ControlRegister>,
+    pos: u32,
+    line_num: u32,
+}
+
+struct AllControl {
+    component_to_controls: HashMap<String, HashMap<u32, ControlMeta>>,
+}
+
+impl AllControl {
+    pub fn new(
+        tdcc_filename: String,
+        pd_filename: String,
+        pos_filename: String,
+    ) -> Result<Self> {
+        let mut component_to_controls = HashMap::new();
+
+        // need to process pos_file first because it gives us the Calyx positions of all control nodes.
+        println!("Parsing {}", pos_filename);
+        let pos_file = File::open(pos_filename)?;
+        let pos: HashMap<String, Vec<ControlCalyxPosInfo>> =
+            serde_json::from_reader(BufReader::new(pos_file))?;
+        for (component, pos_list) in pos {
+            let mut controls: HashMap<u32, ControlMeta> = HashMap::new();
+            for pos in pos_list {
+                controls.insert(
+                    pos.pos_num,
+                    ControlMeta {
+                        name: "".to_string(), // will be filled in by tdcc_file
+                        groups: vec![],       // will be filled in by pd_file
+                        component: component.clone(),
+                        control_type: pos.ctrl_node,
+                        registers: None, // will be filled in by tdcc_file
+                        pos: pos.pos_num,
+                        line_num: pos.linenum,
+                    },
+                );
+            }
+            component_to_controls.insert(component, controls);
+        }
+
+        println!("Parsing {}", tdcc_filename);
+        let tdcc_file = File::open(tdcc_filename)?;
+        let tdcc_profiling_info: HashSet<ProfilingInfo> =
+            serde_json::from_reader(BufReader::new(tdcc_file))?;
+        for control_group in tdcc_profiling_info {
+            match control_group {
+                ProfilingInfo::Fsm(fsminfo) => {
+                    // search component_to_controls for the entry that matches this FSM
+                    let mut found = false;
+                    let comp_ctrl_map = component_to_controls
+                        .get_mut(&fsminfo.component)
+                        .unwrap();
+                    for pos in fsminfo.pos {
+                        // there can be multiple positions because of
+                        if let Some(c) = comp_ctrl_map.get_mut(&pos) {
+                            c.name = fsminfo.group.clone();
+                            c.registers =
+                                Some(ControlRegister::FSM(fsminfo.fsm.clone()));
+                            found = true;
+                        }
+                    }
+                    assert!(found);
+                }
+                ProfilingInfo::Par(par_info) => {
+                    // search component_to_controls for the entry that matches this par group
+                }
+                ProfilingInfo::SingleEnable(_single_enable_info) => { // do nothing since there is no control group
+                }
+            }
+        }
+
+        println!("Parsing {}", pd_filename);
+        let pd_file = File::open(pd_filename)?;
+        let pd: BTreeMap<String, PathDescriptorInfo> =
+            serde_json::from_reader(BufReader::new(pd_file))?;
+
+        let mut out = Self {
+            component_to_controls,
+        };
+        Ok(out)
+    }
+    // pub fn parse(
+    //     tdcc_filename: String,
+    //     pd_filename: String,
+    //     pos_filename: String,
+    // ) -> Result<()> {
+    //     println!("Parsing {}", tdcc_filename);
+    //     let tdcc_file = File::open(tdcc_filename)?;
+    //     let tdcc_profiling_info: HashSet<ProfilingInfo> =
+    //         serde_json::from_reader(BufReader::new(tdcc_file))?;
+
+    //     println!("Parsing {}", pd_filename);
+    //     let pd_file = File::open(pd_filename)?;
+    //     let pd: BTreeMap<String, PathDescriptorInfo> =
+    //         serde_json::from_reader(BufReader::new(pd_file))?;
+
+    //     println!("Parsing {}", pos_filename);
+    //     let pos_file = File::open(pos_filename)?;
+    //     let pos: HashMap<String, Vec<ControlCalyxPosInfo>> =
+    //         serde_json::from_reader(BufReader::new(pos_file))?;
+
+    //     Ok(())
+    // }
+}
+
+// #[derive(Clone, Copy, Hash, PartialEq, Eq, Default)]
+// pub struct ControlId(u32);
+// entity_impl!(ControlId, "control");
+
+// #[derive(Debug, Clone)]
+
+// struct Control {
+//     name: String,
+
+//     // probe: SignalRef,
+//     // invokes: SmallVec<[InvokeId; 6]>,
+//     // probe_idx: u32,
+// }
