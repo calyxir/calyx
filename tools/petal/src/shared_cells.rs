f9fd35797a7737a1fe40b3b244385c5fefb34a94
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2026-06-24 10:44:27 -0400

    Avoid error when the shared-cells file does not exist

--- tools/petal/src/shared_cells.rs
+++ tools/petal/src/shared_cells.rs
@@ -3,6 +3,7 @@ use rustc_hash::FxHashMap;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 use std::fs::File;
+use std::path::Path;
 
 #[derive(PartialEq, Eq, Hash, Clone, Deserialize)]
 /// An instance of a cell being shared; obtained from cell-share compiler pass.
@@ -22,18 +23,21 @@ impl SharedCellsInfo {
     pub fn new(fname: String) -> Result<Self> {
         let mut shared_map: FxHashMap<String, FxHashMap<String, String>> =
             FxHashMap::default();
-        let f = File::open(fname)?;
-        let shared_cells: BTreeMap<String, Vec<ShareEntry>> =
-            serde_json::from_reader(f).unwrap();
+        let p = Path::new(&fname);
+        if p.exists() {
+            let f = File::open(fname)?;
+            let shared_cells: BTreeMap<String, Vec<ShareEntry>> =
+                serde_json::from_reader(f).unwrap();
 
-        for (component, shared_vec) in shared_cells {
-            // old -> new
-            let mut internal_map: FxHashMap<String, String> =
-                FxHashMap::default();
-            for shared in shared_vec {
-                internal_map.insert(shared.original, shared.new);
+            for (component, shared_vec) in shared_cells {
+                // old -> new
+                let mut internal_map: FxHashMap<String, String> =
+                    FxHashMap::default();
+                for shared in shared_vec {
+                    internal_map.insert(shared.original, shared.new);
+                }
+                shared_map.insert(component, internal_map);
             }
-            shared_map.insert(component, internal_map);
         }
         Ok(Self { shared_map })
     }
