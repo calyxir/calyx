4908a84b43bc610227ab4a24792c73fee5d8764e
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2026-07-03 16:57:30 -0400

    [fileinfo-emitter] Handle case where ADL file does not exist

--- tools/fileinfo_emitter/src/main.rs
+++ tools/fileinfo_emitter/src/main.rs
@@ -153,13 +153,13 @@ fn gen_component_info(
             .attributes
             .get_set(SetAttribute::Set(SetAttr::Pos))
             .unwrap();
-        let comb_group_pos = comb_group_set_attr
-            .iter()
-            .find(|x| adl_posids.contains(x))
-            .unwrap();
-        component_pos_id
-            .groups
-            .insert(comb_group.borrow().name(), *comb_group_pos);
+        if let Some(comb_group_pos) =
+            comb_group_set_attr.iter().find(|x| adl_posids.contains(x))
+        {
+            component_pos_id
+                .groups
+                .insert(comb_group.borrow().name(), *comb_group_pos);
+        }
     }
 
     // get pos for cell
@@ -410,13 +410,16 @@ fn create_file_map(
 ) -> HashMap<String, Vec<String>> {
     let mut toplevel_file_map: HashMap<String, Vec<String>> = HashMap::new();
     for (_, path) in source_info_table.iter_file_map() {
-        let file_lines: Vec<String> = read_to_string(path)
-            .unwrap()
-            .lines()
-            .map(String::from)
-            .collect();
-        let filename = path.as_path().to_str().unwrap().to_string();
-        toplevel_file_map.insert(filename, file_lines);
+        if path.exists() {
+            let file_lines: Vec<String> = read_to_string(path)
+                .unwrap()
+                .lines()
+                .map(String::from)
+                .collect();
+            let filename = path.as_path().to_str().unwrap().to_string();
+            toplevel_file_map.insert(filename, file_lines);
+        }
+        // message for else case given in create_lang_to_posid_map
     }
     toplevel_file_map
 }
@@ -437,6 +440,13 @@ fn create_lang_to_posid_map(
         if let Some(path_ext) = path.extension()
             && let Some(path_ext_str) = path_ext.to_str()
         {
+            if !path.exists() {
+                println!(
+                    "[fileinfo_emitter] File does not exist: {}. Petal will ignore this file during ADL analysis.",
+                    path.to_str().unwrap()
+                );
+                continue;
+            }
             match path_ext_str {
                 "futil" => {
                     fileid_to_lang.insert(*fileid, Adl::Calyx);
