ebe00cd4d88a1741d4453b7e19b5013dfd611757
mercure67 <57854733+hhe07@users.noreply.github.com>
2026-07-15 10:18:20 -0400

    move yxi tests

---
+++ yxi/tests/run_axi_test.py
@@ -0,0 +1,26 @@
+import cocotb
+import os
+
+# Idea is to have
+
+# MAKE file -> calls cocotb, runs a single function from here
+# this function looks for datapath
+
+# Makefile sets datapath and verilog interested in testing
+
+
+@cocotb.test()
+async def run(toplevel):
+    from axi_test import run_kernel_test
+
+    data_path = os.environ.get("DATA_PATH")
+    if data_path and not os.path.isabs(data_path):
+        data_path = os.getcwd() + "/" + data_path
+    assert data_path is not None and os.path.isfile(data_path), (
+        f"DATA_PATH must be set and must be a valid file. Got: {data_path}"
+    )
+
+    hex_mode = os.environ.get("HEX_MODE")
+    hex_on = True if hex_mode else False
+
+    await run_kernel_test(toplevel, data_path, hex_on)
