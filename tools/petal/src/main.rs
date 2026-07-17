adddf7a094bb1d161724b5ff0c0758346291d9ac
Ayaka Yorihiro <ayakayorihiro@gmail.com>
2026-07-17 11:16:12 -0400

    Starter; resolve conflicts with main

--- tools/petal/src/main.rs
+++ tools/petal/src/main.rs
@@ -4,11 +4,13 @@ mod shared_cells;
 mod timeline;
 mod visuals;
 
-#[path = "perfetto.protos.rs"]
+#[path = "visuals/perfetto.protos.rs"]
 #[allow(clippy::all)]
 #[rustfmt::skip]
 mod perfetto_protos;
+mod dahlia_design;
 mod statistics;
+
 
 use crate::design::{Design, RegisterId, Stack};
 use crate::statistics::Statistics;
